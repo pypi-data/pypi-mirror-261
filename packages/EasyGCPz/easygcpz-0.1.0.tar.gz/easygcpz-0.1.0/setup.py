9603bf0185fa435d43031056d691e94d5272eb58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 17:03:45 +0000

    Add package EasyGCPz/0.1.0/easygcpz-0.1.0.tar.gz

---
+++ packages/EasyGCPz/easygcpz-0.1.0.tar.gz/easygcpz-0.1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find-packages
+setup(
+    name='EasyGCPz',
+    version='0.1.0',
+    packages=find_packages()
+    author='Mitchell Williams',
+    url='https://github.com/mw-os/EasyGCPz'
+    install_requires=['google.cloud']
+)
