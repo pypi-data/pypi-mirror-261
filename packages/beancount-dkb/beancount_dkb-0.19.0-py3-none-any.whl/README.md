02450210efb0232c7d60442678d7965b809466aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 17:03:26 +0000

    Add package beancount-dkb/0.19.0/beancount_dkb-0.19.0-py3-none-any.whl

---
+++ packages/beancount-dkb/beancount_dkb-0.19.0-py3-none-any.whl/README.md
@@ -0,0 +1,160 @@
+# Beancount DKB Importer
+
+[![image](https://github.com/siddhantgoel/beancount-dkb/workflows/beancount-dkb/badge.svg)](https://github.com/siddhantgoel/beancount-dkb/workflows/beancount-dkb/badge.svg)
+
+[![image](https://img.shields.io/pypi/v/beancount-dkb.svg)](https://pypi.python.org/pypi/beancount-dkb)
+
+[![image](https://img.shields.io/pypi/pyversions/beancount-dkb.svg)](https://pypi.python.org/pypi/beancount-dkb)
+
+[![image](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+
+`beancount-dkb` provides an Importer for converting CSV exports of [DKB]
+(Deutsche Kreditbank) account summaries to the [Beancount] format.
+
+## Installation
+
+```sh
+$ pip install beancount-dkb
+```
+
+In case you prefer installing from the Github repository, please note that
+`main` is the development branch so `stable` is what you should be installing
+from.
+
+## Usage
+
+If you're not familiar with how to import external data into Beancount, please
+read [this guide] first.
+
+Adjust your [config file] to include `ECImporter` and `CreditImporter`
+(depending on what account you're trying to import).
+
+A sample configuration might look like the following:
+
+```python
+from beancount_dkb import ECImporter, CreditImporter
+
+IBAN_NUMBER = 'DE99 9999 9999 9999 9999 99' # your real IBAN number
+
+CARD_NUMBER = '9999 9999 9999 9999'         # your real Credit Card number
+
+CONFIG = [
+    ECImporter(
+        IBAN_NUMBER,
+        'Assets:DKB:EC',
+        currency='EUR',
+    ),
+
+    CreditImporter(
+        CARD_NUMBER,
+        'Assets:DKB:Credit',
+        currency='EUR',
+    )
+]
+```
+
+Once this is in place, you should be able to run `bean-extract` on the command
+line to extract the transactions and pipe all of them into your Beancount file.
+
+```sh
+$ bean-extract /path/to/config.py transaction.csv >> you.beancount
+```
+
+### Transaction Codes as Meta Tags
+
+By default, the ECImporter prepends the transaction code ("Buchungstext") to the
+transaction description. To achieve shorter descriptions and use meta tags to
+query for certain transaction codes, the importer may be configured to store the
+transaction code in a user provided meta tag.
+
+The following configuration instructs the importer to use a meta tag `code` to
+store transaction codes:
+
+```python
+...
+CONFIG = [
+    ECImporter(
+        IBAN_NUMBER,
+        'Assets:DKB:EC',
+        currency='EUR',
+        meta_code='code',
+    ),
+...
+
+```
+
+This is how an example transaction looks without the option:
+
+```beancount
+2021-03-01 * "Kartenzahlung" "XY Supermarket"
+    Assets:DKB:EC                        -133.72 EUR
+```
+
+And this is the resulting transaction using `meta_code='code'`
+
+```beancount
+2021-03-01 * "XY Supermarket"
+    code: Kartenzahlung
+    Assets:DKB:EC                        -133.72 EUR
+```
+
+### Pattern-matching Transactions
+
+It's possible to give the importer classes hints if you'd like them to include a
+second posting based on specific characteristics of the original transaction.
+
+For instance, if the payee or the description in a transaction always matches a
+certain value, it's possible to tell the `ECImporter` or `CreditImporter` to
+automatically place a second posting in the returned lits of transactions.
+
+#### `ECImporter`
+
+`ECImporter` accepts a `payee_patterns` argument, which should be a list of
+`(pattern, account)` tuples.
+
+```python
+CONFIG = [
+    ECImporter(
+        IBAN_NUMBER,
+        'Assets:DKB:EC',
+        currency='EUR',
+        payee_patterns=[
+            ('REWE Filialen', 'Expenses:Supermarket:REWE'),
+            ('NETFLIX', 'Expenses:Online:Netflix'),
+        ],
+    ),
+```
+
+#### `CreditImporter`
+
+`CreditImporter` accepts a `description_patterns` argument, which should be a
+list of `(pattern, account)` tuples.
+
+```python
+CONFIG = [
+    CreditImporter(
+        CARD_NUMBER,
+        'Assets:DKB:Credit',
+        currency='EUR',
+        description_patterns=[
+            ('REWE sagt Danke', 'Expenses:Supermarket:REWE'),
+            ('NETFLIX', 'Expenses:Online:Netflix'),
+        ],
+    )
+```
+
+## Contributing
+
+Contributions are most welcome!
+
+Please make sure you have Python 3.8+ and [Poetry] installed.
+
+1. Clone the repository: `git clone https://github.com/siddhantgoel/beancount-dkb`
+2. Install the packages required for development: `poetry install`
+3. That's basically it. You should now be able to run the test suite: `poetry run py.test`.
+
+[Beancount]: http://furius.ca/beancount/
+[config file]: https://beancount.github.io/docs/importing_external_data.html#configuration
+[DKB]: https://www.dkb.de
+[Poetry]: https://python-poetry.org/
+[this guide]: https://beancount.github.io/docs/importing_external_data.html
---
+++ packages/beancount-dkb/beancount_dkb-0.19.0-py3-none-any.whl/beancount_dkb/credit.py
@@ -0,0 +1,206 @@
+import warnings
+from collections import namedtuple
+from datetime import datetime, timedelta
+from textwrap import dedent
+from typing import Dict, Optional
+
+from beancount.core import data
+from beancount.core.amount import Amount
+from beancount.core.number import Decimal
+from beancount.ingest import importer
+
+from .exceptions import InvalidFormatError
+from .extractors.credit import V1Extractor, V2Extractor
+from .helpers import AccountMatcher, csv_dict_reader, csv_reader, fmt_number_de
+
+Meta = namedtuple("Meta", ["value", "line_index"])
+
+
+class CreditImporter(importer.ImporterProtocol):
+    def __init__(
+        self,
+        card_number,
+        account,
+        currency="EUR",
+        file_encoding: Optional[str] = None,
+        description_patterns=None,
+    ):
+        self.card_number = card_number
+        self.account = account
+        self.currency = currency
+        self.description_matcher = AccountMatcher(description_patterns)
+
+        self._v1_extractor = V1Extractor(card_number)
+        self._v2_extractor = V2Extractor(card_number)
+
+        self._date_from = None
+        self._date_to = None
+
+        self._file_date = None
+
+        # The balance amount is picked from the "Saldo" meta entry, and
+        # corresponds to the amount at the end of the date contained in the
+        # "Datum" meta. From the data seen so far, this date is a few days
+        # behind the end of the last date, and marks the border between
+        # "Gebucht" and "Vorgemerkt" transactions.
+        #
+        # Also, since there is no documentation on the file format, this
+        # behavior is implemented purely based on intuition, but has worked out
+        # OK so far.
+        #
+        # Beancount expects the balance amount to be from the beginning of the
+        # day, while the Tagessaldo entries in the DKB exports seem to be from
+        # the end of the day. So when setting the balance date, we add a
+        # timedelta of 1 day to the original value to make the balance
+        # assertions work.
+
+        self._balance_date = None
+        self._balance_amount = None
+        self._closing_balance_index = -1
+
+        if file_encoding is not None:
+            warnings.warn(
+                dedent(
+                    """
+                    The file_encoding parameter is no longer being used and will be
+                    removed in a future version.
+                    """
+                ),
+                DeprecationWarning,
+            )
+
+    def name(self):
+        return "DKB {}".format(self.__class__.__name__)
+
+    def file_account(self, _):
+        return self.account
+
+    def file_date(self, file):
+        self.extract(file)
+
+        # in case the file contains start/end dates, return the end date
+        # if not, then the file was based on a time period (Zeitraum), so we
+        # return the date of the export instead
+
+        return self._date_to or self._file_date
+
+    def identify(self, file):
+        return self._v1_extractor.identify(file) or self._v2_extractor.identify(file)
+
+    def extract(self, file, existing_entries=None):
+        extractor = None
+
+        if self._v1_extractor.identify(file):
+            extractor = self._v1_extractor
+        elif self._v2_extractor.identify(file):
+            extractor = self._v2_extractor
+        else:
+            raise InvalidFormatError()
+
+        return self._extract(file, extractor)
+
+    def _extract(self, file, extractor):
+        entries = []
+
+        with open(file.name, encoding=extractor.file_encoding) as fd:
+            lines = [line.strip() for line in fd.readlines()]
+
+        line_index = 0
+        header_index = lines.index(extractor.HEADER)
+
+        metadata_lines = lines[0:header_index]
+        transaction_lines = lines[header_index:]
+
+        # Metadata
+
+        metadata = {}
+        reader = csv_reader(metadata_lines)
+
+        for line in reader:
+            line_index += 1
+
+            if not line or line == [""]:
+                continue
+
+            key, value, *_ = line
+
+            metadata[key] = Meta(value, line_index)
+
+        self._update_meta(metadata)
+
+        # Transactions
+
+        reader = csv_dict_reader(transaction_lines)
+
+        for line in reader:
+            line_index += 1
+
+            meta = data.new_metadata(file.name, line_index)
+
+            amount = Amount(fmt_number_de(extractor.get_amount(line)), self.currency)
+
+            date = extractor.get_valuation_date(line)
+
+            description = extractor.get_description(line)
+
+            postings = [data.Posting(self.account, amount, None, None, None, None)]
+
+            if self.description_matcher.account_matches(description):
+                postings.append(
+                    data.Posting(
+                        self.description_matcher.account_for(description),
+                        None,
+                        None,
+                        None,
+                        None,
+                        None,
+                    )
+                )
+
+            entries.append(
+                data.Transaction(
+                    meta,
+                    date,
+                    self.FLAG,
+                    None,
+                    description,
+                    data.EMPTY_SET,
+                    data.EMPTY_SET,
+                    postings,
+                )
+            )
+
+        # Closing Balance
+        meta = data.new_metadata(file.name, self._closing_balance_index)
+        entries.append(
+            data.Balance(
+                meta,
+                self._balance_date,
+                self.account,
+                self._balance_amount,
+                None,
+                None,
+            )
+        )
+
+        return entries
+
+    def _update_meta(self, meta: Dict[str, str]):
+        for key, value in meta.items():
+            if key.startswith("Von"):
+                self._date_from = datetime.strptime(value.value, "%d.%m.%Y").date()
+            elif key.startswith("Bis"):
+                self._date_to = datetime.strptime(value.value, "%d.%m.%Y").date()
+            elif key.startswith("Saldo"):
+                self._balance_amount = Amount(
+                    Decimal(value.value.rstrip(" EUR")), self.currency
+                )
+                self._closing_balance_index = value.line_index
+                if key.startswith("Saldo vom"):
+                    self._balance_date = datetime.strptime(
+                        key.replace("Saldo vom ", "").replace(":", ""),
+                        "%d.%m.%Y",
+                    ).date()
+            elif key.startswith("Datum"):
+                self._file_date = datetime.strptime(value.value, "%d.%m.%Y").date()
+                self._balance_date = self._file_date + timedelta(days=1)
