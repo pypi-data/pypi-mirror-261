a929ad3dbb65c156e7b1acc9bfbd8f58ece5c6de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 03:02:42 +0000

    Add package JscorpPayments/1.0.2/JscorpPayments-1.0.2.tar.gz

---
+++ packages/JscorpPayments/JscorpPayments-1.0.2.tar.gz/JscorpPayments-1.0.2/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+    name="JscorpPayments",
+    version="1.0.2",
+    author="A'zamov Samandar",
+    author_email="azamov.samandar.programmer@gmail.com",
+    description="JscorpPayments",
+    long_description_content_type="text/markdown",
+    python_requires=">=3.5",
+    install_requires=['requests', 'django','djangorestframework'],
+    url="https://github.com/ExcelentProgrammer/JscorpPayments",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ]
+)
