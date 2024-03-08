88f6e45e20b6df5026addc3c53948c3f088fc5c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 18:03:15 +0000

    Add package PoetryStudy.PY/0.1.3/poetrystudy_py-0.1.3-py3-none-any.whl

---
+++ packages/PoetryStudy.PY/poetrystudy_py-0.1.3-py3-none-any.whl/PoetryStudy/data/core.py
@@ -0,0 +1,16 @@
+"""
+ * @file   : core.py
+ * @time   : 15:11
+ * @date   : 2024/3/8
+ * @mail   : 9727005@qq.com
+ * @creator: ShanDong Xiedali
+ * @company: HiLand & RainyTop
+"""
+
+
+def say_hello(someone: str) -> str:
+    return f"Hello,{someone}!"
+
+
+def say_hi(someone: str) -> str:
+    return f"Hello,{someone}!"
