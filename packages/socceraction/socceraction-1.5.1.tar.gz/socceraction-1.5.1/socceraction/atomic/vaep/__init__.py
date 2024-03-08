3039f7700edab8aeda968bf119e0e02226a89d0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 03:02:55 +0000

    Add package socceraction/1.5.1/socceraction-1.5.1.tar.gz

---
+++ packages/socceraction/socceraction-1.5.1.tar.gz/socceraction-1.5.1/socceraction/atomic/vaep/__init__.py
@@ -0,0 +1,5 @@
+"""Implements the Atomic-VAEP framework."""
+from . import features, formula, labels
+from .base import AtomicVAEP
+
+__all__ = ['AtomicVAEP', 'features', 'labels', 'formula']
---
+++ packages/socceraction/socceraction-1.5.1.tar.gz/socceraction-1.5.1/socceraction/vaep/__init__.py
@@ -0,0 +1,5 @@
+"""Implements the VAEP framework."""
+from . import features, formula, labels
+from .base import VAEP
+
+__all__ = ['VAEP', 'features', 'labels', 'formula']
