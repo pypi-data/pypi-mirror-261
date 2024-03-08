b9774104860d22d1ac5ef608012aadd18379e1a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 17:03:29 +0000

    Add package nonebot-plugin-gshisbanner/1.2.5/nonebot_plugin_gshisbanner-1.2.5-py3-none-any.whl

---
+++ packages/nonebot-plugin-gshisbanner/nonebot_plugin_gshisbanner-1.2.5-py3-none-any.whl/nonebot_plugin_gshisbanner/config.py
@@ -0,0 +1,13 @@
+# 插件的配置文件
+from nonebot import get_driver
+from pydantic import Extra, BaseModel
+
+
+class Config(BaseModel, extra=Extra.ignore):
+    gshisbanner_forward_length: int = 10
+    """单次合并转发条数"""
+    gshisbanner_json_url: str = "banners.52v6.com/data"
+    """卡池信息来源网站"""
+
+
+plugin_config = Config.parse_obj(get_driver().config.dict())
