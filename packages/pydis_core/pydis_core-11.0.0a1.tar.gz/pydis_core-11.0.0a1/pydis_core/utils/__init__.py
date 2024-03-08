e7f2af274f5564fc116b914f4d906bb29d72f5c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 04:03:24 +0000

    Add package pydis_core/11.0.0a1/pydis_core-11.0.0a1.tar.gz

---
+++ packages/pydis_core/pydis_core-11.0.0a1.tar.gz/pydis_core-11.0.0a1/pydis_core/utils/__init__.py
@@ -0,0 +1,62 @@
+"""Useful utilities and tools for Discord bot development."""
+
+from pydis_core.utils import (
+    _monkey_patches,
+    caching,
+    channel,
+    checks,
+    commands,
+    cooldown,
+    error_handling,
+    function,
+    interactions,
+    lock,
+    logging,
+    members,
+    messages,
+    pagination,
+    paste_service,
+    regex,
+    scheduling,
+)
+from pydis_core.utils._extensions import unqualify
+
+
+def apply_monkey_patches() -> None:
+    """
+    Applies all common monkey patches for our bots.
+
+    Patches :obj:`discord.ext.commands.Command` and :obj:`discord.ext.commands.Group` to support root aliases.
+        A ``root_aliases`` keyword argument is added to these two objects, which is a sequence of alias names
+        that will act as top-level groups rather than being aliases of the command's group.
+
+        It's stored as an attribute also named ``root_aliases``
+
+    Patches discord's internal ``send_typing`` method so that it ignores 403 errors from Discord.
+        When under heavy load Discord has added a CloudFlare worker to this route, which causes 403 errors to be thrown.
+    """
+    _monkey_patches._apply_monkey_patches()  # noqa: SLF001
+
+
+__all__ = [
+    apply_monkey_patches,
+    caching,
+    channel,
+    checks,
+    commands,
+    cooldown,
+    error_handling,
+    function,
+    interactions,
+    lock,
+    logging,
+    members,
+    messages,
+    pagination,
+    paste_service,
+    regex,
+    scheduling,
+    unqualify,
+]
+
+__all__ = [module.__name__ for module in __all__]
