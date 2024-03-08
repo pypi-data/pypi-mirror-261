defc2a29cfdecfae8798a31fef1eded15ecf1c0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 16:10:18 +0000

    Add package PytatoDIA/0.3.1/PytatoDIA-0.3.1.tar.gz

---
+++ packages/PytatoDIA/PytatoDIA-0.3.1.tar.gz/PytatoDIA-0.3.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="PytatoDIA",
+    version="0.3.1",
+    author="Tyler T. Cooper,Ph.D.",
+    author_email="tcoope2@gmail.com",
+    description="A Toolbox for Proteomic Analyses and Data Visualization",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/TTCooper-PhD/Pytato",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "pandas>=1.1.5",
+        "numpy>=1.19.5",
+        "matplotlib>=3.3.4",
+        "seaborn>=0.11.1",
+        "scipy>=1.5.4",
+        "biopython>=1.78",
+        "requests>=2.25.1",
+    ],
+)
