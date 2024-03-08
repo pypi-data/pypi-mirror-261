e82ba040878430812c74eaa9129895dda5d9b390
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 14:03:01 +0000

    Add package mkdocs-build-plantuml-plugin-ardihikaru/1.9.4/mkdocs-build-plantuml-plugin-ardihikaru-1.9.4.tar.gz

---
+++ packages/mkdocs-build-plantuml-plugin-ardihikaru/mkdocs-build-plantuml-plugin-ardihikaru-1.9.4.tar.gz/mkdocs-build-plantuml-plugin-ardihikaru-1.9.4/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+__version__ = "1.9.4"
+
+setup(
+    name="mkdocs-build-plantuml-plugin-ardihikaru",
+    version=__version__,
+    description="An MkDocs plugin to call plantuml locally or remote (cloned from: "
+                "https://github.com/christo-ph/mkdocs_build_plantuml)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="mkdocs plantuml publishing documentation uml sequence diagram",
+    url=f"https://pypi.org/project/mkdocs-build-plantuml-plugin-ardihikaru/{__version__}/#files",
+    author="Christoph Galler",
+    author_email="galler@quantor.consulting",
+    license="MIT",
+    python_requires=">=3.2",
+    install_requires=["mkdocs>=1.0.4", "httplib2"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    # packages=find_packages(
+    #     exclude=["*.tests", "*.tests.*", "tests.*", "tests", "example", "src"]
+    # ),
+    entry_points={
+        "mkdocs.plugins": [
+            "build_plantuml = src.mkdocs_build_plantuml_plugin_ardihikaru.plantuml:BuildPlantumlPlugin"
+        ]
+    },
+)
