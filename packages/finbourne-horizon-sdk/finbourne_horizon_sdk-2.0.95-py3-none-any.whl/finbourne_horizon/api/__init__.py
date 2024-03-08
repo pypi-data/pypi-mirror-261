b19e29fc86cad0f283b2317438dde44c0b5236fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 03:02:42 +0000

    Add package finbourne-horizon-sdk/2.0.95/finbourne_horizon_sdk-2.0.95-py3-none-any.whl

---
+++ packages/finbourne-horizon-sdk/finbourne_horizon_sdk-2.0.95-py3-none-any.whl/finbourne_horizon/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from finbourne_horizon.api.instrument_api import InstrumentApi
+from finbourne_horizon.api.process_history_api import ProcessHistoryApi
+from finbourne_horizon.api.vendor_api import VendorApi
+
