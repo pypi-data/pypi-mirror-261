5f705155d9be972a88015baaec9d74d853d56247
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 17:03:18 +0000

    Add package zoomeye-hk/2.2.0/zoomeye-hk-2.2.0.tar.gz

---
+++ packages/zoomeye-hk/zoomeye-hk-2.2.0.tar.gz/zoomeye-hk-2.2.0/zoomeye/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+* Filename: __init__.py.py
+* Description:
+* Time: 2020.11.25
+* Author: liuf5
+*/
+"""
+
+__name__ = 'zoomeye'
+__package__ = 'zoomeye'
+__version__ = 'v2.2.0'
+__site__ = "https://www.zoomeye.hk/doc"
