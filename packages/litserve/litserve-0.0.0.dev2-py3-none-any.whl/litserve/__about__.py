efd18ce2d5dfbe045691f7a7d066e034c3d23c4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 14:02:59 +0000

    Add package litserve/0.0.0.dev2/litserve-0.0.0.dev2-py3-none-any.whl

---
+++ packages/litserve/litserve-0.0.0.dev2-py3-none-any.whl/litserve/__about__.py
@@ -0,0 +1,17 @@
+__version__ = "0.0.0.dev2"
+__author__ = "Lightning-AI et al."
+__author_email__ = "community@lightning.ai"
+__license__ = "Apache-2.0"
+__copyright__ = f"Copyright (c) 2024, {__author__}."
+__homepage__ = "https://github.com/Lightning-AI/litserve"
+__docs__ = "Lightweight AI server."
+
+__all__ = [
+    "__author__",
+    "__author_email__",
+    "__copyright__",
+    "__docs__",
+    "__homepage__",
+    "__license__",
+    "__version__",
+]
