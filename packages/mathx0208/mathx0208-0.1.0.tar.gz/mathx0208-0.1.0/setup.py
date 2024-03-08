dffc971f59fcb63a29ec21962ccf0daa2499f450
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 16:10:24 +0000

    Add package mathx0208/0.1.0/mathx0208-0.1.0.tar.gz

---
+++ packages/mathx0208/mathx0208-0.1.0.tar.gz/mathx0208-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='mathx0208',
+version='0.1.0',
+author='xmathx',
+author_email='yayack666@gmail.com',
+description='A short description of your package',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
