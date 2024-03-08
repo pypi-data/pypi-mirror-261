e7d38d967612e67adcf5f259c0e5833c517a5951
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 14:02:48 +0000

    Add package nmbrs-soap-api/0.0.209/nmbrs_soap_api-0.0.209-py3-none-any.whl

---
+++ packages/nmbrs-soap-api/nmbrs_soap_api-0.0.209-py3-none-any.whl/utils/return_list.py
@@ -0,0 +1,27 @@
+def return_list(func):
+    """
+    Decorator ensuring the decorated function always returns a list.
+
+    If result is None, returns an empty list.
+    If result is not a list, wraps it in a list before returning.
+
+    Args:
+        func (callable): The function to be decorated.
+    Returns:
+        callable: The decorated function.
+    """
+
+    def wrapper(*args, **kwargs):
+        try:
+            result = func(*args, **kwargs)
+            if result is None:
+                return []
+            if not isinstance(result, list):
+                return [result]
+            return result
+        except Exception as e:
+            if str(e) == "TypeError: 'NoneType' object is not iterable":
+                return []
+            raise e
+
+    return wrapper
