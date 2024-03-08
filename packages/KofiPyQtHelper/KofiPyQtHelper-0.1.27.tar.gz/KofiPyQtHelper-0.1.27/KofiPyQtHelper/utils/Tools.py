83b75e037058dbeb55504eac5befe0c892b59f5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 16:10:19 +0000

    Add package KofiPyQtHelper/0.1.27/KofiPyQtHelper-0.1.27.tar.gz

---
+++ packages/KofiPyQtHelper/KofiPyQtHelper-0.1.27.tar.gz/KofiPyQtHelper-0.1.27/KofiPyQtHelper/utils/Tools.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+'''
+Author       : Kofi
+Date         : 2024-03-08 10:54:54
+LastEditors  : Kofi
+LastEditTime : 2024-03-08 10:54:55
+Description  : 工具函数
+'''
+import time
+
+def timing_decorator(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f"{func.__name__} 耗时 {end - start} 秒")
+        return result
+
+    return wrapper
