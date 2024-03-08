4edc3b7f244660f106802a45fca1948f29d9c4f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 17:03:54 +0000

    Add package chellow/1709887733.0.0/chellow-1709887733.0.0.tar.gz

---
+++ packages/chellow/chellow-1709887733.0.0.tar.gz/chellow-1709887733.0.0/test/reports/test_report_87.py
@@ -0,0 +1,93 @@
+from io import StringIO
+
+import chellow.e.computer
+from chellow.models import Contract, MarketRole, Participant, User, UserRole
+from chellow.reports.report_87 import content
+from chellow.utils import ct_datetime, to_utc
+
+
+class Sess:
+    def __init__(self, *results):
+        self.it = iter(results)
+
+    def query(self, *arg):
+        return self
+
+    def join(self, *arg):
+        return self
+
+    def order_by(self, *arg):
+        return self
+
+    def filter(self, *arg):
+        return self
+
+    def scalar(self, *arg):
+        return next(self.it)
+
+    def first(self, *arg):
+        return next(self.it)
+
+    def distinct(self, *arg):
+        return next(self.it)
+
+
+def test_summertime(mocker):
+    start_date_ct = ct_datetime(2010, 5)
+    finish_date_ct = ct_datetime(2010, 5, 31, 23, 30)
+    start_date = to_utc(start_date_ct)
+    finish_date = to_utc(finish_date_ct)
+    contract_id = 1
+    user = mocker.Mock()
+    user.email_address = "sfreud"
+
+    mock_c_months_u = mocker.patch(
+        "chellow.reports.report_87.c_months_u", autospec=True
+    )
+
+    MockEra = mocker.patch("chellow.reports.report_87.Era", autospec=True)
+    MockEra.finish_date = finish_date
+    MockEra.start_date = start_date
+
+    mocker.patch(
+        "chellow.reports.report_87.contract_func",
+        autospec=True,
+        return_value=lambda: ["standing_gbp"],
+    )
+
+    f = StringIO()
+    sess = Sess([], [])
+
+    chellow.reports.report_87.create_csv(f, sess, start_date, finish_date, contract_id)
+
+    mock_c_months_u.assert_called_with(
+        start_year=start_date_ct.year,
+        start_month=start_date_ct.month,
+        finish_year=finish_date_ct.year,
+        finish_month=finish_date_ct.month,
+    )
+
+
+def test_content(mocker, sess):
+    mock_file = StringIO()
+    mock_file.close = mocker.Mock()
+    mocker.patch("chellow.reports.report_87.open_file", return_value=mock_file)
+    editor = UserRole.insert(sess, "editor")
+    user = User.insert(sess, "admin@example.com", "xxx", editor, None)
+    user_id = user.id
+    vf = to_utc(ct_datetime(1996, 1, 1))
+    market_role_Z = MarketRole.insert(sess, "Z", "Non-core")
+    participant = Participant.insert(sess, "CALB", "AK Industries")
+    participant.insert_party(sess, market_role_Z, "None core", vf, None, None)
+    market_role_X = MarketRole.insert(sess, "X", "Supplier")
+    participant.insert_party(sess, market_role_X, "Fusion Ltc", vf, None, None)
+
+    contract = Contract.insert_supplier(
+        sess, "Fusion Supplier 2000", participant, "", {}, vf, None, {}
+    )
+    sess.commit()
+
+    start_date = to_utc(ct_datetime(2020, 1, 1))
+    finish_date = to_utc(ct_datetime(2020, 1, 31, 23, 30))
+
+    content(start_date, finish_date, contract.id, user_id)
