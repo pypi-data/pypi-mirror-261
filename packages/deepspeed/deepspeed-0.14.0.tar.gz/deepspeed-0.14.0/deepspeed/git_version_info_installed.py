7881d1326c1f4a9b1e5c7f108c9bf0a4c0770fb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 14:03:13 +0000

    Add package deepspeed/0.14.0/deepspeed-0.14.0.tar.gz

---
+++ packages/deepspeed/deepspeed-0.14.0.tar.gz/deepspeed-0.14.0/deepspeed/git_version_info_installed.py
@@ -0,0 +1,6 @@
+version='0.14.0 '
+git_hash='unknown'
+git_branch='unknown'
+installed_ops={'async_io': False, 'fused_adam': False, 'cpu_adam': False, 'cpu_adagrad': False, 'cpu_lion': False, 'evoformer_attn': False, 'fused_lamb': False, 'fused_lion': False, 'inference_core_ops': False, 'cutlass_ops': False, 'transformer_inference': False, 'quantizer': False, 'ragged_device_ops': False, 'ragged_ops': False, 'random_ltd': False, 'sparse_attn': False, 'spatial_inference': False, 'transformer': False, 'stochastic_transformer': False}
+compatible_ops={'async_io': True, 'fused_adam': True, 'cpu_adam': True, 'cpu_adagrad': True, 'cpu_lion': True, 'evoformer_attn': False, 'fused_lamb': True, 'fused_lion': True, 'inference_core_ops': False, 'cutlass_ops': False, 'transformer_inference': False, 'quantizer': True, 'ragged_device_ops': False, 'ragged_ops': False, 'random_ltd': True, 'sparse_attn': False, 'spatial_inference': False, 'transformer': True, 'stochastic_transformer': True, 'deepspeed_not_implemented': False}
+torch_info={'version': '0.0', 'bf16_support': False, 'cuda_version': '0.0', 'nccl_version': '0.0', 'hip_version': '0.0'}
