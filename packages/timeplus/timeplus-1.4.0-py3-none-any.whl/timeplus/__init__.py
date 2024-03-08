efa82d32936c94e4c78eec9d587b8de23b4b070a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 03:03:31 +0000

    Add package timeplus/1.4.0/timeplus-1.4.0-py3-none-any.whl

---
+++ packages/timeplus/timeplus-1.4.0-py3-none-any.whl/timeplus/__init__.py
@@ -0,0 +1,50 @@
+from .version import __version__  # noqa: F401
+
+from timeplus.env import Environment  # noqa: F401
+from timeplus.query import Query  # noqa: F401
+from timeplus.type import Type  # noqa: F401
+from timeplus.stream import Stream  # noqa: F401
+from timeplus.external_stream import ExternalStream  # noqa: F401
+from timeplus.view import View  # noqa: F401
+
+from timeplus.source import Source  # noqa: F401
+from timeplus.sink import Sink  # noqa: F401
+from timeplus.udf import UDF  # noqa: F401
+from timeplus.dashboard import Dashboard  # noqa: F401
+from timeplus.alert import Alert  # noqa: F401
+from timeplus.dbapi import connect  # noqa: F401
+
+from timeplus.error import (
+    DatabaseError,
+    DataError,
+    Error,
+    IntegrityError,
+    InterfaceError,
+    InternalError,
+    NotSupportedError,
+    OperationalError,
+    ProgrammingError,
+    Warning,
+)
+
+__all__ = [
+    "connect",
+    "apilevel",
+    "threadsafety",
+    "paramstyle",
+    "DataError",
+    "DatabaseError",
+    "Error",
+    "IntegrityError",
+    "InterfaceError",
+    "InternalError",
+    "NotSupportedError",
+    "OperationalError",
+    "ProgrammingError",
+    "Warning",
+]
+
+
+apilevel = "2.0"
+threadsafety = 2
+paramstyle = "pyformat"
