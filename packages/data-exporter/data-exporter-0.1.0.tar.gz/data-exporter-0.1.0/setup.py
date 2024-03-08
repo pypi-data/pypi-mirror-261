6a34c39298987d9c0e1fe1c36de05ee69ae3543e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 14:03:07 +0000

    Add package data-exporter/0.1.0/data-exporter-0.1.0.tar.gz

---
+++ packages/data-exporter/data-exporter-0.1.0.tar.gz/data-exporter-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="data-exporter",
+    version="0.1.0",
+    author="SokinjoNS",
+    author_email="sokinjo.155@gmail.com",
+    description="A module for exporting email data to CSV format.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',  # Specify the format of the long_description
+    url="https://github.com/SokinjoNS/data-exporter",
+    packages=find_packages(),
+    install_requires=[
+        'gmail-message-processor>=0.1.0',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
