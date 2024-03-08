15f82d5b4f1af4867a84c1666b2ec45cd85469e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 04:03:29 +0000

    Add package sapien/3.0.0.dev1/sapien-3.0.0.dev1-cp310-cp310-manylinux2014_x86_64.whl

---
+++ packages/sapien/sapien-3.0.0.dev1-cp310-cp310-manylinux2014_x86_64.whl/sapien/include/svulkan2/common/cuda_helper.h
@@ -0,0 +1,6 @@
+#ifndef CUDA_HELPER_H_
+#define CUDA_HELPER_H_
+
+
+
+#endif // CUDA_HELPER_H_
---
+++ packages/sapien/sapien-3.0.0.dev1-cp310-cp310-manylinux2014_x86_64.whl/sapien/wrapper/articulation_builder.py
@@ -0,0 +1,125 @@
+from .actor_builder import ActorBuilder
+from .. import pysapien as sapien
+import numpy as np
+from dataclasses import dataclass
+from typing import List, Tuple
+
+
+@dataclass
+class JointRecord:
+    joint_type: str = "undefined"  # "fixed", "prismatic", "revolute"
+    limits: Tuple[float] = (-np.inf, np.inf)
+    pose_in_parent: sapien.Pose = sapien.Pose()
+    pose_in_child: sapien.Pose = sapien.Pose()
+    friction: float = 0
+    damping: float = 0
+    name: str = ""
+
+
+class LinkBuilder(ActorBuilder):
+    def __init__(self, index: int, parent):
+        super().__init__()
+        self.parent = parent
+        self.index = index
+        self.joint_record = JointRecord()
+        self.physx_body_type = "link"
+
+    def set_joint_name(self, name):
+        self.joint_record.name = name
+
+    def set_joint_properties(
+        self, type, limits, pose_in_parent, pose_in_child, friction=0, damping=0
+    ):
+        self.joint_record = JointRecord(
+            joint_type=type,
+            limits=limits,
+            pose_in_parent=pose_in_parent,
+            pose_in_child=pose_in_child,
+            friction=friction,
+            damping=damping,
+            name=self.joint_record.name,
+        )
+
+    def _check(self):
+        if self.parent is None:
+            assert self.joint_record.joint_type in ["fixed", "undefined"]
+        else:
+            assert self.joint_record.joint_type in [
+                "fixed",
+                "revolute",
+                "revolute_unwrapped",
+                "prismatic",
+                "continuous",
+            ]
+
+
+class ArticulationBuilder:
+    def __init__(self):
+        self.initial_pose = sapien.Pose()
+        self.link_builders: List[LinkBuilder] = []
+
+    def set_initial_pose(self, pose):
+        self.initial_pose = pose
+
+    def set_scene(self, scene: sapien.Scene):
+        self.scene = scene
+        return self
+
+    def create_link_builder(self, parent: LinkBuilder = None):
+        if self.link_builders:
+            assert parent and parent in self.link_builders
+
+        builder = LinkBuilder(len(self.link_builders), parent)
+        self.link_builders.append(builder)
+
+        return builder
+
+    def build_entities(self, fix_root_link=None):
+        entities = []
+        links = []
+        for b in self.link_builders:
+            b._check()
+            b.physx_body_type = "link"
+
+            entity = sapien.Entity()
+
+            link_component = b.build_physx_component(
+                links[b.parent.index] if b.parent else None
+            )
+
+            entity.add_component(link_component)
+            if b.visual_records:
+                entity.add_component(b.build_render_component())
+            entity.name = b.name
+
+            link_component.name = b.name
+            link_component.joint.name = b.joint_record.name
+            link_component.joint.type = b.joint_record.joint_type
+            link_component.joint.pose_in_child = b.joint_record.pose_in_child
+            link_component.joint.pose_in_parent = b.joint_record.pose_in_parent
+
+            if link_component.joint.type in [
+                "revolute",
+                "prismatic",
+                "revolute_unwrapped",
+            ]:
+                link_component.joint.limit = np.array(b.joint_record.limits).flatten()
+                link_component.joint.set_drive_property(0, b.joint_record.damping)
+
+            links.append(link_component)
+            entities.append(entity)
+
+        if fix_root_link is not None:
+            entities[0].components[0].joint.type = (
+                "fixed" if fix_root_link else "undefined"
+            )
+        entities[0].pose = self.initial_pose
+        return entities
+
+    def build(self, fix_root_link=None) -> sapien.physx.PhysxArticulation:
+        assert self.scene is not None
+        links = self.build_entities(fix_root_link=fix_root_link)
+
+        for l in links:
+            self.scene.add_entity(l)
+        return l.components[0].articulation
