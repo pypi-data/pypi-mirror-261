1b6ff5118549aba7de25a9c0d097f9805e3f66f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 04:03:15 +0000

    Add package atb-lib/0.6.33/atb_lib-0.6.33.tar.gz

---
+++ packages/atb-lib/atb_lib-0.6.33.tar.gz/atb_lib-0.6.33/src/exchange_factory.py
@@ -0,0 +1,10 @@
+﻿from atb_lib.src.binance import CryptoExchangeBinanceWS
+
+
+class CryptoExchangeFactory:
+    @staticmethod
+    def create(exchange_id, market, symbol, callback, logger):
+        if exchange_id.lower() == "binance":
+            return CryptoExchangeBinanceWS(market, symbol, callback, logger)
+        else:
+            raise ValueError(f"Unsupported exchange: '{exchange_id}'")
