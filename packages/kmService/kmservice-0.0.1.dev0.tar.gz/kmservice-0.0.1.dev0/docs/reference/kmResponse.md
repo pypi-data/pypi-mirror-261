865185a577814dca62ec0a5101a1aa8cc2bcf08a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 03:03:11 +0000

    Add package kmService/0.0.1.dev0/kmservice-0.0.1.dev0.tar.gz

---
+++ packages/kmService/kmservice-0.0.1.dev0.tar.gz/kmservice-0.0.1.dev0/docs/reference/kmResponse.md
@@ -0,0 +1,6 @@
+# Reference
+
+::: kmService.KmResponse
+    options:
+      show_root_heading: true
+      show_source: false
---
+++ packages/kmService/kmservice-0.0.1.dev0.tar.gz/kmservice-0.0.1.dev0/kmService/__init__.py
@@ -0,0 +1,10 @@
+from .km_service import KmResponse, KmService, KmServiceBuilder, get_km_service
+
+__version__ = "0.0.1.dev0"
+
+__all__ = [
+    "KmService",
+    "KmResponse",
+    "KmServiceBuilder",
+    "get_km_service",
+]
