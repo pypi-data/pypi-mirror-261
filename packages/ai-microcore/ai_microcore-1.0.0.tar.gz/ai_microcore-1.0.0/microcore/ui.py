7b9f57732c874daa5c0a7e9d4af47b9627196406
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 16:10:28 +0000

    Add package ai-microcore/1.0.0/ai_microcore-1.0.0.tar.gz

---
+++ packages/ai-microcore/ai_microcore-1.0.0.tar.gz/ai_microcore-1.0.0/microcore/ui.py
@@ -0,0 +1,41 @@
+from colorama import Fore, init
+
+init(autoreset=True)
+_input = input
+
+
+def info(*args, color=Fore.LIGHTYELLOW_EX, **kwargs):
+    print(*[color + i for i in args], **kwargs)
+
+
+def debug(msg):
+    info(msg, color=Fore.BLUE)
+
+
+def error(*args, **kwargs):
+    print(*[Fore.RED + i for i in args], **kwargs)
+
+
+def ask_yn(msg):
+    return "y" in input(msg + " (y/n)").lower().strip()
+
+
+def ask_choose(msg, variants: list):
+    i = 0
+    if isinstance(variants, list):
+        for item in variants:
+            i += 1
+            print(f"\t{Fore.MAGENTA}{i}:{Fore.RESET}\t{item}")
+    while True:
+        i = input(f"{msg} {Fore.MAGENTA}[1-{len(variants)}]{Fore.RESET}: ")
+        if not i.isdigit():
+            error("Not a number")
+            continue
+        i = int(i) - 1
+        if i >= len(variants) or i < 0:
+            error("Incorrect number")
+            continue
+        break
+
+    item = variants[int(i)]
+    return item
