c121f31ecc49d6ad107a943c780d8de1a6c01225
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 17:03:29 +0000

    Add package httpbinx/1.6.0/httpbinx-1.6.0.tar.gz

---
+++ packages/httpbinx/httpbinx-1.6.0.tar.gz/httpbinx-1.6.0/httpbinx/main.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from os import path
+
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from starlette.middleware.cors import CORSMiddleware
+
+from httpbinx.meta import tags_metadata
+from httpbinx.routers import router
+
+app = FastAPI(
+    title='httpbinx',
+    description='HTTP Request & Response Service, '
+                'written in Python + FastAPI.',
+    docs_url='/',  # swagger docs page url
+    swagger_ui_parameters={'docExpansion': 'none'},
+    openapi_tags=tags_metadata
+)
+
+app.mount(
+    '/static',
+    StaticFiles(directory=path.join(path.dirname(__file__), 'static')),
+    name='static'
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+# app.openapi_tags = []
+app.include_router(router=router)
