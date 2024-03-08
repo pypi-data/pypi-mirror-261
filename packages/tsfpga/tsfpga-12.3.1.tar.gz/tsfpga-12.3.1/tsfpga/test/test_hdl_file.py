ed7c9c540f81c69b2f71513ad9cedbb02728bfb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 18:02:58 +0000

    Add package tsfpga/12.3.1/tsfpga-12.3.1.tar.gz

---
+++ packages/tsfpga/tsfpga-12.3.1.tar.gz/tsfpga-12.3.1/tsfpga/test/test_hdl_file.py
@@ -0,0 +1,27 @@
+# --------------------------------------------------------------------------------------------------
+# Copyright (c) Lukas Vik. All rights reserved.
+#
+# This file is part of the tsfpga project, a project platform for modern FPGA development.
+# https://tsfpga.com
+# https://github.com/tsfpga/tsfpga
+# --------------------------------------------------------------------------------------------------
+
+# Standard libraries
+from pathlib import Path
+
+# First party libraries
+from tsfpga.hdl_file import HdlFile
+
+
+def test_file_type():
+    assert HdlFile(Path("file.vhd")).is_vhdl
+    assert HdlFile(Path("file.vhdl")).is_vhdl
+    assert not HdlFile(Path("file.vhd")).is_verilog_source
+    assert not HdlFile(Path("file.vhd")).is_verilog_header
+
+    assert HdlFile(Path("file.vh")).is_verilog_header
+    assert HdlFile(Path("file.v")).is_verilog_source
+
+
+def test_can_cast_to_string_without_error():
+    str(HdlFile(Path("file.vhd")))
