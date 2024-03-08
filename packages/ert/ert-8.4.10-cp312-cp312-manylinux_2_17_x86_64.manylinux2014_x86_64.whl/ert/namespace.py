f85d487f6e55e15bba5b8229780855a9e149e361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 04:03:14 +0000

    Add package ert/8.4.10/ert-8.4.10-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/ert/ert-8.4.10-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/ert/namespace.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+import argparse
+from typing import Callable, Optional
+
+from ert.shared.plugins.plugin_manager import ErtPluginManager
+
+
+class Namespace(argparse.Namespace):
+    """
+    ERT argument parser namespace
+    """
+
+    mode: str
+    config: str
+    verbose: bool
+    experimental_mode: bool
+    logdir: str
+    func: Callable[[Namespace, Optional[ErtPluginManager]], None]
