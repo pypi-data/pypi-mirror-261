94eef9fd24e18c9ca0243ed2779c3351c39ae1e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 16:11:00 +0000

    Add package distcrab/0.0.52/distcrab-0.0.52.tar.gz

---
+++ packages/distcrab/distcrab-0.0.52.tar.gz/distcrab-0.0.52/distcrab/logger.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from logging import getLogger, StreamHandler, WARN
+from sys import stdout
+
+logger = getLogger()
+logger.setLevel(WARN)
+handler = StreamHandler(stdout)
+handler.setLevel(WARN)
+logger.addHandler(handler)
