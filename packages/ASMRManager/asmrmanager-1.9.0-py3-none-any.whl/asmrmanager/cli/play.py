d4fb6d6b59018c27bcb41f3d73daac5571baed70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 16:10:57 +0000

    Add package ASMRManager/1.9.0/asmrmanager-1.9.0-py3-none-any.whl

---
+++ packages/ASMRManager/asmrmanager-1.9.0-py3-none-any.whl/asmrmanager/cli/play.py
@@ -0,0 +1,28 @@
+import click
+
+from asmrmanager.cli.core import create_database, fm, rj_argument
+from asmrmanager.common.rj_parse import RJID, id2rj
+from asmrmanager.config import config
+from asmrmanager.logger import logger
+
+
+@click.command()
+@click.pass_context
+@rj_argument
+def play(ctx: click.Context, rj_id: RJID):
+    from asmrmanager.filemanager.utils import folder_chooser
+
+    """play an asmr in the terminal"""
+    from asmrmanager.lrcplayer import MUSIC_SUFFIXES, lrc_play
+
+    rj_path = fm.get_path(rj_id)
+    if rj_path is None:
+        logger.error(f"RJ id {rj_id} not found!")
+        return
+
+    path = folder_chooser(
+        rj_path,
+        lambda _, count: bool(set(count.keys()).intersection(MUSIC_SUFFIXES)),
+    )
+
+    ctx.invoke(lrc_play, path=path)
