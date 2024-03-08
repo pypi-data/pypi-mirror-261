fa5d4b9eacb73a45e71c5bc7878b482645a989b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 17:03:45 +0000

    Add package EasyGCPz/0.1.0/easygcpz-0.1.0-py2.py3-none-any.whl

---
+++ packages/EasyGCPz/easygcpz-0.1.0-py2.py3-none-any.whl/easygcpz/__init__.py
@@ -0,0 +1,19 @@
+"""
+EasyGCPz takes a significant load off of python users of GCP by
+automating query generation, executing queries, and returning
+query results in a number of optional accessible data structures.
+EasyGCPz enables users of any skill level to unlock their querying
+potential by offering multiple degrees of flexibility, while also
+being robust against a wide number of common querying and usage
+problems.
+"""
+
+__author__ = 'MW-OS'
+__version__ = '0.1.0'
+__date__ = '2024.03'
+
+from .main import EasyGCPz as easygcpz
+
+__all__ = ["easygcpz", '__author__', '__version__', '__date__']
+
+# eof
