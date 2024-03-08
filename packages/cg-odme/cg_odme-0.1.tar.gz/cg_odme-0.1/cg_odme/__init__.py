0d8018da9e385691f7db1e9a588883edddf89222
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 17:03:22 +0000

    Add package cg-odme/0.1/cg_odme-0.1.tar.gz

---
+++ packages/cg-odme/cg_odme-0.1.tar.gz/cg_odme-0.1/cg_odme/__init__.py
@@ -0,0 +1,3 @@
+from .calibration import *
+from .data import *
+from .run import *
---
+++ packages/cg-odme/cg_odme-0.1.tar.gz/cg_odme-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name="cg_odme",
+      version="0.1",
+      description="Calibrate optimal path flows using systematic mapping variables",
+      author="Taehooie Kim and Dr. Xuesong (Simon) Zhou",
+      packages=find_packages(),
+      zip_safe=False
+      )
