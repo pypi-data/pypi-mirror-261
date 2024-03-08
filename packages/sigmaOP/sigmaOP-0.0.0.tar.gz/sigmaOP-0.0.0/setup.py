a5e8a4c7b24bc7b13c069241a146b6f4e7802b38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 16:10:34 +0000

    Add package sigmaOP/0.0.0/sigmaOP-0.0.0.tar.gz

---
+++ packages/sigmaOP/sigmaOP-0.0.0.tar.gz/sigmaOP-0.0.0/setup.py
@@ -0,0 +1,12 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='sigmaOP',
+    version='0.0.0',
+    packages=find_packages(),
+    install_requires=[
+        # Specify any dependencies your library may have
+    ],
+)
