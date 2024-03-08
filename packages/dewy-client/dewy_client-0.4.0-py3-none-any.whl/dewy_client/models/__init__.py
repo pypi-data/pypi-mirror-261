67cd2679c8c51dd66cb3eccc1ca9283cb406f2fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 03:03:03 +0000

    Add package dewy-client/0.4.0/dewy_client-0.4.0-py3-none-any.whl

---
+++ packages/dewy-client/dewy_client-0.4.0-py3-none-any.whl/dewy_client/models/__init__.py
@@ -0,0 +1,37 @@
+"""Contains all the data models used in inputs/outputs"""
+
+from .add_document_request import AddDocumentRequest
+from .body_upload_document_content import BodyUploadDocumentContent
+from .collection import Collection
+from .collection_create import CollectionCreate
+from .distance_metric import DistanceMetric
+from .document import Document
+from .document_status import DocumentStatus
+from .http_validation_error import HTTPValidationError
+from .image_chunk import ImageChunk
+from .image_result import ImageResult
+from .ingest_state import IngestState
+from .retrieve_request import RetrieveRequest
+from .retrieve_response import RetrieveResponse
+from .text_chunk import TextChunk
+from .text_result import TextResult
+from .validation_error import ValidationError
+
+__all__ = (
+    "AddDocumentRequest",
+    "BodyUploadDocumentContent",
+    "Collection",
+    "CollectionCreate",
+    "DistanceMetric",
+    "Document",
+    "DocumentStatus",
+    "HTTPValidationError",
+    "ImageChunk",
+    "ImageResult",
+    "IngestState",
+    "RetrieveRequest",
+    "RetrieveResponse",
+    "TextChunk",
+    "TextResult",
+    "ValidationError",
+)
---
+++ packages/dewy-client/dewy_client-0.4.0-py3-none-any.whl/dewy_client/models/add_document_request.py
@@ -0,0 +1,80 @@
+from typing import Any, Dict, List, Type, TypeVar, Union, cast
+
+from attrs import define as _attrs_define
+from attrs import field as _attrs_field
+
+from ..types import UNSET, Unset
+
+T = TypeVar("T", bound="AddDocumentRequest")
+
+
+@_attrs_define
+class AddDocumentRequest:
+    """
+    Attributes:
+        collection (str):
+        url (Union[None, Unset, str]):
+    """
+
+    collection: str
+    url: Union[None, Unset, str] = UNSET
+    additional_properties: Dict[str, Any] = _attrs_field(init=False, factory=dict)
+
+    def to_dict(self) -> Dict[str, Any]:
+        collection = self.collection
+
+        url: Union[None, Unset, str]
+        if isinstance(self.url, Unset):
+            url = UNSET
+        else:
+            url = self.url
+
+        field_dict: Dict[str, Any] = {}
+        field_dict.update(self.additional_properties)
+        field_dict.update(
+            {
+                "collection": collection,
+            }
+        )
+        if url is not UNSET:
+            field_dict["url"] = url
+
+        return field_dict
+
+    @classmethod
+    def from_dict(cls: Type[T], src_dict: Dict[str, Any]) -> T:
+        d = src_dict.copy()
+        collection = d.pop("collection")
+
+        def _parse_url(data: object) -> Union[None, Unset, str]:
+            if data is None:
+                return data
+            if isinstance(data, Unset):
+                return data
+            return cast(Union[None, Unset, str], data)
+
+        url = _parse_url(d.pop("url", UNSET))
+
+        add_document_request = cls(
+            collection=collection,
+            url=url,
+        )
+
+        add_document_request.additional_properties = d
+        return add_document_request
+
+    @property
+    def additional_keys(self) -> List[str]:
+        return list(self.additional_properties.keys())
+
+    def __getitem__(self, key: str) -> Any:
+        return self.additional_properties[key]
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.additional_properties[key] = value
+
+    def __delitem__(self, key: str) -> None:
+        del self.additional_properties[key]
+
+    def __contains__(self, key: str) -> bool:
+        return key in self.additional_properties
