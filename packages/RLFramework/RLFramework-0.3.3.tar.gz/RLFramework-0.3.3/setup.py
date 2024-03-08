f1d2222c7340a7425f39b6ea77a9c80c5f772788
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 16:10:46 +0000

    Add package RLFramework/0.3.3/RLFramework-0.3.3.tar.gz

---
+++ packages/RLFramework/RLFramework-0.3.3.tar.gz/RLFramework-0.3.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='RLFramework',
+    version='0.3.3',
+    packages=find_packages(include=['RLFramework', 'RLFramework.*']),
+    url="https://github.com/Markseo0424/RLFramework.git",
+    install_requires=[
+        'numpy',
+        'torch'
+    ]
+)
