281581bb1c557ee38b4c9f79143d0b97a317c1dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 16:10:28 +0000

    Add package zibai-server/0.10.1/zibai_server-0.10.1-py3-none-any.whl

---
+++ packages/zibai-server/zibai_server-0.10.1-py3-none-any.whl/zibai/__main__.py
@@ -0,0 +1,12 @@
+import sys
+
+from .cli import main, parse_args
+
+
+def command_line() -> None:
+    sys.path.insert(0, ".")
+    main(parse_args(sys.argv[1:]))
+
+
+if __name__ == "__main__":
+    command_line()
