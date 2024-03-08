b7b265da7e490f2d3ac34e387e7d0273e6edd555
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 04:03:08 +0000

    Add package VizAble/0.5.4/vizable-0.5.4-py3-none-any.whl

---
+++ packages/VizAble/vizable-0.5.4-py3-none-any.whl/VizAble/introductions.py
@@ -0,0 +1,22 @@
+from shiny import App, Inputs, Outputs, Session, reactive, render, ui, req, module
+
+def introductions_ui() -> ui.nav_panel:
+    """ user interface for introducing the app
+
+    :return: a nav_panel including all the ui components for introductions
+    :rtype: ui.nav_panel
+    """
+    return ui.nav_panel(
+        "Instructions",
+        ui.tags.header(ui.tags.h1("Instructions")),
+        ui.tags.main(
+            ui.markdown(
+                """
+            This is an web app for **Accessible Data Visualization**.
+            test if v0.5.4 release is triggered
+        
+            Instructions to be added...
+            """
+            ),
+        ),
+    ),
