2fd4cc55ac0082f23154cbc771d43d8cf05984ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 17:03:36 +0000

    Add package arcGisFeatureCache/0.1.0a0/arcgisfeaturecache-0.1.0a0-py3-none-any.whl

---
+++ packages/arcGisFeatureCache/arcgisfeaturecache-0.1.0a0-py3-none-any.whl/arcGisFeatureCache/utils/helpers.py
@@ -0,0 +1,14 @@
+from typing import Any, Protocol
+
+
+class HasTempDataProtocol(Protocol):
+    _data: Any
+
+
+def clear_temp_data(self: HasTempDataProtocol) -> None:
+    del self._data
+    self._data = None
+
+
+if __name__ == "__main__":
+    pass
