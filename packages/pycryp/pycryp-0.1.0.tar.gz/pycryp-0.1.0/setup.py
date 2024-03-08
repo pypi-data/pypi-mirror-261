31534ee6829cc4a41509c87a5b740bf2da95fb5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 17:03:37 +0000

    Add package pycryp/0.1.0/pycryp-0.1.0.tar.gz

---
+++ packages/pycryp/pycryp-0.1.0.tar.gz/pycryp-0.1.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.1.0'
+DESCRIPTION = 'package to encrypt and decrypt messages using AES '
+
+setup(
+    name="pycryp",
+    version=VERSION,
+    author="Suvan Banerjee",
+    author_email="<banerjeesuvan@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['cryptography'],
+    keywords=['python', 'aes', 'encryption', 'decryption', 'aes-256-cbc'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5'
+)
