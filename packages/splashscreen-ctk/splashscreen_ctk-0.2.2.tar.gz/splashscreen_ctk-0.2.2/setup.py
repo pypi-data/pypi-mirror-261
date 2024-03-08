69d75fe48e4b6cd42bef87efb465260e3ccad3ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 04:02:57 +0000

    Add package splashscreen-ctk/0.2.2/splashscreen_ctk-0.2.2.tar.gz

---
+++ packages/splashscreen-ctk/splashscreen_ctk-0.2.2.tar.gz/splashscreen_ctk-0.2.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.txt").read_text()
+
+setup(
+    name='splashscreen_ctk',
+    version='0.2.1',
+    long_description=long_description,
+    long_description_content_type="text/plain",
+    py_modules=[]
+)
