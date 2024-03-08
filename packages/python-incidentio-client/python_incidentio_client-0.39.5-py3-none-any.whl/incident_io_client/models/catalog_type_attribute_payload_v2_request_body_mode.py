d65167f3ea99a240a82acd88c1ff1eec1a3c0b7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 14:02:42 +0000

    Add package python-incidentio-client/0.39.5/python_incidentio_client-0.39.5-py3-none-any.whl

---
+++ packages/python-incidentio-client/python_incidentio_client-0.39.5-py3-none-any.whl/incident_io_client/models/catalog_type_attribute_payload_v2_request_body_mode.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class CatalogTypeAttributePayloadV2RequestBodyMode(str, Enum):
+    EXTERNAL = "external"
+    INTERNAL = "internal"
+    MANUAL = "manual"
+    VALUE_0 = ""
+
+    def __str__(self) -> str:
+        return str(self.value)
