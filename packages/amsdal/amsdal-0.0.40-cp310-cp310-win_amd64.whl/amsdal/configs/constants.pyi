31e0b3dc227071a49277a96c24675a6c5a633cee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 03:02:43 +0000

    Add package amsdal/0.0.40/amsdal-0.0.40-cp310-cp310-win_amd64.whl

---
+++ packages/amsdal/amsdal-0.0.40-cp310-cp310-win_amd64.whl/amsdal/configs/constants.pyi
@@ -0,0 +1,11 @@
+from _typeshed import Incomplete
+from pathlib import Path
+
+BASE_DIR: Incomplete
+TYPE_SCHEMAS_PATH: Path
+CORE_SCHEMAS_PATH: Path
+TESTING_ENVIRONMENT: str
+DEVELOPMENT_ENVIRONMENT: str
+PRODUCTION_ENVIRONMENT: str
+
+def get_default_environment() -> str: ...
