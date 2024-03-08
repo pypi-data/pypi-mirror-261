d233dbcc01c33e3f7b5ef8625c2a86774c6d0bf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 18:03:08 +0000

    Add package vision_oslo_extension/0.6.3/vision_oslo_extension-0.6.3-py3-none-any.whl

---
+++ packages/vision_oslo_extension/vision_oslo_extension-0.6.3-py3-none-any.whl/vision_oslo_extension/__init__.py
@@ -0,0 +1,27 @@
+#
+# -*- coding: utf-8  -*-
+#=================================================================
+# Created by: Jieming Ye
+# Created on: Nov 2023
+# Last Modified: XX/XX/2023
+#=================================================================
+"""
+This module was built to create master GUI for pakcages
+
+This module has been designed requiring some third party packages.
+
+https://realpython.com/python-gui-tkinter/
+
+"""
+#=================================================================
+# VERSION CONTROL
+# V1.0 (Jieming Ye) - Initial Version
+#=================================================================
+# Set Information Variable
+__author__ = "Jieming ye"
+__copyright__ = "Copyright 2023, Network Rail Design Delivery"
+__credits__ = "Jieming Ye"
+__version__ = "1.0"
+__email__ = "jieming.ye@networkrail.co.uk"
+__status__ = "Under Development"
+
