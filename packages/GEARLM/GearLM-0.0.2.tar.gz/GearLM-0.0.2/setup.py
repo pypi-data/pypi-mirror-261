cdcca56209d3ed706d3e1db7f44a6c7611027bd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 17:03:16 +0000

    Add package GEARLM/0.0.2/GearLM-0.0.2.tar.gz

---
+++ packages/GEARLM/GearLM-0.0.2.tar.gz/GearLM-0.0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2' 
+DESCRIPTION = 'GEAR'
+LONG_DESCRIPTION = 'GEAR: An Efficient KV Cache Compression Recipe for Near-Lossless Generative Inference of LLM'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="GearLM", 
+        version=VERSION,
+        author="Hao Kang",
+        author_email="hkang342@gatech.edu",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'AI'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Science/Research",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
