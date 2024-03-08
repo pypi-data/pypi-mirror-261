7289ff1d39e4a4382b8c449c136b21d35b12c38b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 17:03:31 +0000

    Add package abst/2.3.36/abst-2.3.36.tar.gz

---
+++ packages/abst/abst-2.3.36.tar.gz/abst-2.3.36/abst/__version__.py
@@ -0,0 +1,27 @@
+"""
+__version__.py
+~~~~~~~~~~~~~~
+
+Information about the current version of the py-package-template package.
+"""
+
+__title__ = "abst"
+__description__ = (
+    "CLI Command making OCI Bastion and kubernetes usage simple and fast"
+)
+
+__version__ = "2.3.36"
+__author__ = "Jiri Otoupal"
+__author_email__ = "jiri-otoupal@ips-database.eu"
+__license__ = "MIT"
+__url__ = "https://github.com/jiri-otoupal/abst"
+__pypi_repo__ = "https://pypi.org/project/abst/"
+
+__version_name__ = "Formatted-Merged Giraffe"
+__change_log__ = """
+* Added new option to context config so user can input custom ssh arguments to 'ssh-custom-arguments' key in context\n
+* Listing context will show last usage time\n
+* Changed formatting of 'abst context list' and 'abst parallel list'\n
+* Merged Config to Context group\n
+* Fixed parallel contexts fetching wrong path\n
+"""
