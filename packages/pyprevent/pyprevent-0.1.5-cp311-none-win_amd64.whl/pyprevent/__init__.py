4f6d6bd8dab8ffd89ee9820f18a74bc028ef90ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 04:03:25 +0000

    Add package pyprevent/0.1.5/pyprevent-0.1.5-cp311-none-win_amd64.whl

---
+++ packages/pyprevent/pyprevent-0.1.5-cp311-none-win_amd64.whl/pyprevent/__init__.py
@@ -0,0 +1,16 @@
+from .ascvd_calc import (
+    batch_calculate_10_yr_ascvd_risk,
+    batch_calculate_30_yr_ascvd_risk,
+    calculate_10_yr_ascvd_risk,
+    calculate_30_yr_ascvd_risk,
+)
+
+
+from .heart_failure import (
+    batch_calculate_10_yr_heart_failure_risk,
+    batch_calculate_30_yr_heart_failure_risk,
+    calculate_10_yr_heart_failure_risk,
+    calculate_30_yr_heart_failure_risk,
+)
+
+from .cvd import *
