c147317f1925d6f67fad0809d24b51912d587070
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 04:02:57 +0000

    Add package DbConnectTjorven/1.2/DbConnectTjorven-1.2.tar.gz

---
+++ packages/DbConnectTjorven/DbConnectTjorven-1.2.tar.gz/DbConnectTjorven-1.2/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name='DbConnectTjorven',
+    packages=['DbConnectTjorven'],
+    version='1.2',
+    license='GNU',
+    descriptoin='test',
+    author='Tjorven Burdorf',
+    author_email='BurdorfTjorven@gmail.com',
+    url='https://github.com/tj0vtj0v/DbConnect',
+    download_url='https://github.com/tj0vtj0v/DbConnect/archive/refs/tags/Alpha.tar.gz',
+    keywords=['MYSQL', 'Database', 'Connector', 'Private'],
+    install_requires=['mysql-connector-python'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+)
