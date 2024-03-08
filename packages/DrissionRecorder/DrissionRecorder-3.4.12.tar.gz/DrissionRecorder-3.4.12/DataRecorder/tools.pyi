dfe37ce3f8931ca0c1afacc117777541be95d6e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 14:03:15 +0000

    Add package DrissionRecorder/3.4.12/DrissionRecorder-3.4.12.tar.gz

---
+++ packages/DrissionRecorder/DrissionRecorder-3.4.12.tar.gz/DrissionRecorder-3.4.12/DataRecorder/tools.pyi
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+from pathlib import Path
+from typing import Union, Tuple, Any, Optional
+
+from .base import BaseRecorder
+
+
+def align_csv(path: Union[str, Path], encoding: str = 'utf-8', delimiter: str = ',', quotechar: str = '"') -> None: ...
+
+
+def get_usable_path(path: Union[str, Path], is_file: bool = True, parents: bool = True) -> Path: ...
+
+
+def make_valid_name(full_name: str) -> str: ...
+
+
+def get_long(txt) -> int: ...
+
+
+def parse_coord(coord: Optional[int, str, list, tuple] = None, data_col: int = None) -> Tuple[Optional[int], int]: ...
+
+
+def process_content(content: Any, excel: bool = False) -> Optional[int, str, float]: ...
+
+
+def ok_list(data_list: Union[list, dict], excel: bool = False, as_str: bool = False) -> list: ...
+
+
+def get_usable_coord(coord: Union[tuple, list], max_row: int, max_col: int) -> Tuple[int, int]: ...
+
+
+def data_to_list_or_dict(recorder: BaseRecorder, data: Union[list, tuple, dict]) -> Union[list, dict]: ...
