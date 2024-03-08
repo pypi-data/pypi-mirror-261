cfc972deb1234b68920d0734badac99fa87fa8ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 14:02:53 +0000

    Add package python-incidentio-client/0.39.4/python_incidentio_client-0.39.4-py3-none-any.whl

---
+++ packages/python-incidentio-client/python_incidentio_client-0.39.4-py3-none-any.whl/incident_io_client/models/incident_status_v1_response_body_category.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class IncidentStatusV1ResponseBodyCategory(str, Enum):
+    CANCELED = "canceled"
+    CLOSED = "closed"
+    DECLINED = "declined"
+    LEARNING = "learning"
+    LIVE = "live"
+    MERGED = "merged"
+    TRIAGE = "triage"
+
+    def __str__(self) -> str:
+        return str(self.value)
