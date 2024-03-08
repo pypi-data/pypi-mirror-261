ff25589ec076ffbd972d68aec8f1e0c1c14b2cdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 04:03:04 +0000

    Add package mlx/0.6.0/mlx-0.6.0-cp310-cp310-macosx_13_0_arm64.whl

---
+++ packages/mlx/mlx-0.6.0-cp310-cp310-macosx_13_0_arm64.whl/mlx/include/mlx/backend/metal/kernels/steel/utils.h
@@ -0,0 +1,8 @@
+// Copyright © 2024 Apple Inc.
+
+#pragma once
+
+#include <metal_stdlib>
+
+#define STEEL_CONST static constant constexpr const
+#define STEEL_PRAGMA_UNROLL _Pragma("clang loop unroll(full)")
