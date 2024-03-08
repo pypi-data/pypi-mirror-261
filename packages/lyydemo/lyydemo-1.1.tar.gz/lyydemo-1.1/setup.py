2802eac53b90941d458affdc5e57f5ba25cef63b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 17:03:28 +0000

    Add package lyydemo/1.1/lyydemo-1.1.tar.gz

---
+++ packages/lyydemo/lyydemo-1.1.tar.gz/lyydemo-1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='lyydemo',
+    version='1.1',
+    author='yy',
+    author_email='',
+    description='lyydemo for lyy',
+    #packages=find_packages(),
+    license="MIT",
+    install_requires=[
+    ],
+)
