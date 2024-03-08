f257495cb6e21ef0fddf91b7e6f461436c09e243
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 17:03:36 +0000

    Add package nuclia/2.0.5/nuclia-2.0.5.tar.gz

---
+++ packages/nuclia/nuclia-2.0.5.tar.gz/nuclia-2.0.5/nuclia/tests/test_nua/test_predict.py
@@ -0,0 +1,21 @@
+from nuclia.sdk.predict import NucliaPredict
+
+
+def test_predict(testing_config):
+    np = NucliaPredict()
+    embed = np.sentence(text="This is my text", model="multilingual-2023-02-21")
+    assert embed.time > 0
+    assert len(embed.data) == 768
+
+
+def test_rag(testing_config):
+    np = NucliaPredict()
+    generated = np.rag(
+        question="Which is the CEO of Nuclia?",
+        context=[
+            "Nuclia CTO is Ramon Navarro",
+            "Eudald Camprubí is CEO at the same company as Ramon Navarro",
+        ],
+        model="chatgpt-azure-3",
+    )
+    assert "Eudald" in generated.answer
