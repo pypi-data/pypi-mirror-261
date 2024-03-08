b5bc1e3d0478c0200a7eed59dfadbf518c6d5821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 14:03:17 +0000

    Add package reelity/1.0.8/reelity-1.0.8.tar.gz

---
+++ packages/reelity/reelity-1.0.8.tar.gz/reelity-1.0.8/structures/modules/reelity/_status/status.proc.py
@@ -0,0 +1,22 @@
+
+
+
+
+
+def add_paths_to_system (paths):
+	import pathlib
+	from os.path import dirname, join, normpath
+	import sys
+	
+	this_folder = pathlib.Path (__file__).parent.resolve ()	
+	for path in paths:
+		sys.path.insert (0, normpath (join (this_folder, path)))
+
+add_paths_to_system ([
+	'../../../modules',
+	'../../../modules_pip'
+])
+
+
+import reelity._status as reelity_status
+reelity_status.start ()
