4c6d0d387c853e72eacc33cedc40c03c15e5ee3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 14:03:31 +0000

    Add package engcom/0.1.12/engcom-0.1.12.tar.gz

---
+++ packages/engcom/engcom-0.1.12.tar.gz/engcom-0.1.12/README.md
@@ -0,0 +1,8 @@
+![PyPI](https://img.shields.io/pypi/v/engcom?label=pypi%20package)
+![](https://readthedocs.org/projects/engcom/badge/?version=latest&style=flat)
+
+# Engcom
+
+**Engcom** (/enj'com/) is a Python package for engineering computing.
+
+[See the documentation](https://engcom.readthedocs.io) for more information.
---
+++ packages/engcom/engcom-0.1.12.tar.gz/engcom-0.1.12/engcom/publish.py
@@ -0,0 +1,38 @@
+import argparse
+from engcom.publication import Publication
+
+parser = argparse.ArgumentParser(
+    description="Publish a Python script .py or Jupyter .ipynb file."
+)
+parser.add_argument(
+    "source_filename", type=str, help="A Python script .py or Jupyter .ipynb file"
+)
+parser.add_argument("to", type=str, help="Destination type (md, pdf, or docx)")
+parser.add_argument(
+    "--source_kind", type=str, required=False, default="script", help="script or ipynb"
+)
+parser.add_argument(
+    "--title", type=str, required=False, default=None, help="Title of the document"
+)
+parser.add_argument(
+    "--author", type=str, required=False, default=None, help="Author of the document"
+)
+parser.add_argument(
+    "--pdflatex",
+    type=str,
+    required=False,
+    default=True,
+    help="Use pdflatex to build pdf (default: True)",
+)
+
+
+def main():
+    args = parser.parse_args()
+    pub = Publication(
+        title=args.title,
+        author=args.author,
+        source_filename=args.source_filename,
+        source_kind=args.source_kind,
+    )
+    pub.write(to=args.to, pdflatex=args.pdflatex)
+    return 0
