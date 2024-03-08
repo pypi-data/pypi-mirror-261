de304dfbaa88d2fdc398c4f5779c4844a78a1e70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 14:02:52 +0000

    Add package cellmean/1.1.1/cellmean-1.1.1.tar.gz

---
+++ packages/cellmean/cellmean-1.1.1.tar.gz/cellmean-1.1.1/cellmean/apply_fill.py
@@ -0,0 +1,31 @@
+from skimage.restoration import (denoise_tv_chambolle, denoise_bilateral,
+                                 denoise_wavelet, estimate_sigma)
+from skimage import data, img_as_float, io
+
+def bilateral_denoising(image_path, sigma_color=0.05, sigma_spatial=15):
+    image = io.imread(image_path)
+    image_noise = denoise_bilateral(image, sigma_color=sigma_color, sigma_spatial=sigma_spatial, channel_axis=-1)
+    return image_noise
+
+from skimage.filters import gaussian, sobel
+
+def gaussian_filter(image_path, sigma=3):
+    image = io.imread(image_path)
+    image_gaussian = gaussian(image, sigma=sigma, channel_axis=-1)
+    return image_gaussian
+
+def sobel_edge(image):
+    edge_sobel = sobel(image)
+    return edge_sobel
+
+
+import mahotas 
+from scipy import ndimage
+import numpy as np
+
+def mean_filter(image, Bc=None):
+    if Bc is None:
+        Bc = np.ones((3, 3))  # Default structuring element
+    
+    image_mean = mahotas.mean_filter(image, Bc=Bc)
+    return image_mean
