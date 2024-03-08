160cd01ecd93123ce700515c98a6d1137f248f6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 03:03:15 +0000

    Add package t-bug-catcher/0.1.5/t_bug_catcher-0.1.5.tar.gz

---
+++ packages/t-bug-catcher/t_bug_catcher-0.1.5.tar.gz/t_bug_catcher-0.1.5/t_bug_catcher/__init__.py
@@ -0,0 +1,21 @@
+"""Top-level package for t-bug-catcher."""
+
+__author__ = """Thoughtful"""
+__email__ = "support@thoughtful.ai"
+__version__ = '0.1.5'  # fmt: skip
+
+from .bug_catcher import (
+    configure,
+    report_error,
+    report_error_to_jira,
+    report_error_to_bugsnag,
+    attach_file_to_exception,
+)
+
+__all__ = [
+    "configure",
+    "report_error",
+    "report_error_to_jira",
+    "report_error_to_bugsnag",
+    "attach_file_to_exception",
+]
