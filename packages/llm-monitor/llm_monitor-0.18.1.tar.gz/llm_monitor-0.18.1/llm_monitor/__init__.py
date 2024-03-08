7f45e47b15c9bc257ec87344244e3e5250d80898
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 14:03:05 +0000

    Add package llm-monitor/0.18.1/llm_monitor-0.18.1.tar.gz

---
+++ packages/llm-monitor/llm_monitor-0.18.1.tar.gz/llm_monitor-0.18.1/llm_monitor/__init__.py
@@ -0,0 +1,8 @@
+"""LLM Monitor"""
+
+from llm_monitor.handlers import MonitorHandler
+from llm_monitor.monitor import LLMMonitor
+
+__all__ = ["MonitorHandler", "LLMMonitor"]
+
+__version__ = "0.18.1"
