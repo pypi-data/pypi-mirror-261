861dc740e7438fcc048f495dfc5e96af32f76e7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 04:03:16 +0000

    Add package django-spaday/0.1.2/django-spaday-0.1.2.tar.gz

---
+++ packages/django-spaday/django-spaday-0.1.2.tar.gz/django-spaday-0.1.2/tests/test_pagination.py
@@ -0,0 +1,20 @@
+import pytest
+from django.urls import reverse
+from rest_framework import status
+
+from tests.factories import UserFactory
+
+
+@pytest.mark.django_db
+def test_standard_pagination(authenticated_client):
+    UserFactory.create_batch(15)
+    response = authenticated_client.get(reverse("api:users-list"))
+    assert response.status_code == status.HTTP_200_OK
+    assert "current_page" in response.data
+    assert "num_pages" in response.data
+    assert "start_index" in response.data
+    assert "end_index" in response.data
+    assert "num_results" in response.data
+    assert "results" in response.data
+    # print(response.data)
+    assert len(response.data["results"]) == 10  # Default page size
