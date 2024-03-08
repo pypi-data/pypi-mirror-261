9fa3a64cddea52d2a703a97e416b7d59579a54d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-08 03:03:26 +0000

    Add package aq-geometric/2024.3.2.1/aq_geometric-2024.3.2.1-py3-none-any.whl

---
+++ packages/aq-geometric/aq_geometric-2024.3.2.1-py3-none-any.whl/aq_geometric/datasets/__init__.py
@@ -0,0 +1,1 @@
+from aq_geometric.datasets.remote import AqGeometricDataset
---
+++ packages/aq-geometric/aq_geometric-2024.3.2.1-py3-none-any.whl/aq_geometric/models/__init__.py
@@ -0,0 +1,3 @@
+from aq_geometric.models.base_model import BaseModel
+from aq_geometric.models.edge_conv import EdgeConv, EdgeConvModel
+from aq_geometric.models.gcn import GCNModel
