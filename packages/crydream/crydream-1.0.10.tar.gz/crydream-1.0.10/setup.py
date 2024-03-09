0617e96d4f9bf21c40f2edcee8cb30a279a5181f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 15:03:51 +0000

    Add package crydream/1.0.10/crydream-1.0.10.tar.gz

---
+++ packages/crydream/crydream-1.0.10.tar.gz/crydream-1.0.10/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='crydream',
+    version='1.0.10',
+    description='CoreCode',
+    packages=find_packages(),
+    author='CryDream',
+    license='None'
+)
