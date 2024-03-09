82fad89bc2e1a7f752f437c06253ed89575a51c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 14:03:50 +0000

    Add package mpf/0.57.0.dev40/mpf-0.57.0.dev40.tar.gz

---
+++ packages/mpf/mpf-0.57.0.dev40.tar.gz/mpf-0.57.0.dev40/mpf/platforms/fast/fast_dmd.py
@@ -0,0 +1,29 @@
+"""Fast DMD support."""
+from mpf.platforms.interfaces.dmd_platform import DmdPlatformInterface
+
+
+class FASTDMD(DmdPlatformInterface):
+
+    """Object for a FAST DMD."""
+
+    def __init__(self, machine, name, sender):
+        """initialize DMD."""
+        self.machine = machine
+        self.name = name
+        self.send = sender
+
+
+    def set_brightness(self, brightness: float):
+        """Set brightness."""
+        # not supported
+        assert brightness == 1.0
+
+    def update(self, data: bytes):
+        """Update data on the DMD.
+
+        Args:
+        ----
+            data: bytes to send to DMD
+        """
+
+        self.send.send_frame(data)
