736f232c839dc2323527a3fe117388c0bc67b015
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 03:03:56 +0000

    Add package minique/0.9.1/minique-0.9.1-py3-none-any.whl

---
+++ packages/minique/minique-0.9.1-py3-none-any.whl/minique/excs.py
@@ -0,0 +1,26 @@
+class NoSuchJob(Exception):
+    pass
+
+
+class DuplicateJob(Exception):
+    pass
+
+
+class InvalidStatus(Exception):
+    pass
+
+
+class AlreadyAcquired(InvalidStatus):
+    pass
+
+
+class AlreadyResulted(InvalidStatus):
+    pass
+
+
+class InvalidJob(ValueError):
+    pass
+
+
+class MissingJobData(ValueError):
+    pass
