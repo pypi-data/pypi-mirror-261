ee3868585d8d1a95d7c1ad08cd06dedf906e4008
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 06:03:31 +0000

    Add package kgdata/6.5.2/kgdata-6.5.2-cp312-cp312-manylinux_2_35_x86_64.whl

---
+++ packages/kgdata/kgdata-6.5.2-cp312-cp312-manylinux_2_35_x86_64.whl/kgdata/dbpedia/datasets/entity_redirections.py
@@ -0,0 +1,51 @@
+import orjson
+from kgdata.dataset import Dataset
+from kgdata.dbpedia.config import DBpediaDirCfg
+from kgdata.dbpedia.datasets.entities import entities
+from kgdata.misc.ntriples_parser import Triple, ignore_comment, ntriple_loads
+from kgdata.spark.extended_rdd import ExtendedRDD
+from kgdata.splitter import split_a_file
+from rdflib import URIRef
+
+
+def entity_redirections(lang: str = "en"):
+    cfg = DBpediaDirCfg.get_instance()
+    dump_date = cfg.get_dump_date()
+
+    ds: Dataset[tuple[str, str]] = Dataset(
+        cfg.entity_redirections / f"final-{lang}/*.gz",
+        deserialize=orjson.loads,
+        name=f"redirection-dump/{dump_date}-{lang}",
+        dependencies=[entities(lang)],
+    )
+
+    if not ds.has_complete_data():
+        split_a_file(
+            infile=cfg.get_redirection_dump_file(lang),
+            outfile=cfg.entity_redirections / f"raw-{lang}/part.ttl.gz",
+            n_writers=8,
+            override=False,
+        )
+
+        (
+            ExtendedRDD.textFile(cfg.entity_redirections / f"raw-{lang}/*.gz")
+            .filter(ignore_comment)
+            .map(ntriple_loads)
+            .map(norm_redirection)  # extracted redirection (source -> target)
+            .map(lambda x: (x[1], x))
+            .groupByKey()  # group the redirection by targets (target -> [redirections])
+            .join(
+                entities(lang).get_extended_rdd().map(lambda r: (r.id, 1))
+            )  # join with entities to filter out non-existing entities
+            .flatMap(lambda x: x[1][0])  # get back the redirections
+            .map(orjson.dumps)
+            .save_like_dataset(ds, auto_coalesce=True, shuffle=True)
+        )
+
+    return ds
+
+
+def norm_redirection(triple: Triple):
+    assert triple[1] == URIRef("http://dbpedia.org/ontology/wikiPageRedirects")
+    assert isinstance(triple[0], URIRef) and isinstance(triple[2], URIRef)
+    return str(triple[0]), str(triple[2])
