27e156ac60bfc6f9e5e87e61781dc5eeb4611dba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 15:04:23 +0000

    Add package wanmedia/0.0.3/wanmedia-0.0.3.tar.gz

---
+++ packages/wanmedia/wanmedia-0.0.3.tar.gz/wanmedia-0.0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='wanmedia',
+    version='0.0.3',
+    description='Social Media  Analysis ',
+    packages=['wanmedia'],
+    install_requires=[
+        'nltk',
+        'jupyter'
+    ]
+)
