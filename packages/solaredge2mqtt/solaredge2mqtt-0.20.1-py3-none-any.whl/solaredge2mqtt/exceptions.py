4fcd3a82dea4fcd3ca0da322f6d5a59dd559b3a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 06:03:33 +0000

    Add package solaredge2mqtt/0.20.1/solaredge2mqtt-0.20.1-py3-none-any.whl

---
+++ packages/solaredge2mqtt/solaredge2mqtt-0.20.1-py3-none-any.whl/solaredge2mqtt/exceptions.py
@@ -0,0 +1,11 @@
+class ConfigurationException(Exception):
+    def __init__(self, message: str, *args: any) -> None:
+        self.message = message
+        super().__init__(*args)
+
+
+class InvalidDataException(Exception):
+    def __init__(self, message: str, *args: any) -> None:
+        self.message = message
+
+        super().__init__(*args)
