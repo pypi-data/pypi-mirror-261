ba31f10e6d2c37a340e18ec49d1b7a8f05921fdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 05:04:19 +0000

    Add package radge/1.0.0/radge-1.0.0-py3-none-any.whl

---
+++ packages/radge/radge-1.0.0-py3-none-any.whl/radge/__init__.py
@@ -0,0 +1,11 @@
+"""
+Radge: a Python library for generating test cases for algorithmic problems.
+"""
+
+from __future__ import absolute_import
+
+# from .graph import *
+# from .numbers import *
+# from .sequences import *
+# from .string import *
+# from .utils import *
---
+++ packages/radge/radge-1.0.0-py3-none-any.whl/radge/utils.py
@@ -0,0 +1,7 @@
+"""
+Utility functions and constants
+"""
+
+NOISE = 4
+ALPHA_LOWER = "abcdefghijklmnopqrstuvwxyz"
+ALPHA_UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
