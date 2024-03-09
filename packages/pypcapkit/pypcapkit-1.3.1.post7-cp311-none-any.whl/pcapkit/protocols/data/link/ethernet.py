254b6c40cae6cf3f30672c1e08e982211aa7359b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 15:04:54 +0000

    Add package pypcapkit/1.3.1.post7/pypcapkit-1.3.1.post7-cp311-none-any.whl

---
+++ packages/pypcapkit/pypcapkit-1.3.1.post7-cp311-none-any.whl/pcapkit/protocols/data/link/ethernet.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""data models for ethernet protocol"""
+
+from typing import TYPE_CHECKING
+
+from pcapkit.corekit.infoclass import info_final
+from pcapkit.protocols.data.data import Data
+
+if TYPE_CHECKING:
+    from pcapkit.const.reg.ethertype import EtherType
+
+__all__ = ['Ethernet']
+
+
+@info_final
+class Ethernet(Data):
+    """Data model for ethernet packet."""
+
+    #: Destination MAC address.
+    dst: 'str'
+    #: Source MAC address.
+    src: 'str'
+    #: Protocol (internet layer).
+    type: 'EtherType'
+
+    if TYPE_CHECKING:
+        def __init__(self, dst: 'str', src: 'str', type: 'EtherType') -> 'None': ...  # pylint: disable=unused-argument,super-init-not-called,redefined-builtin,multiple-statements
