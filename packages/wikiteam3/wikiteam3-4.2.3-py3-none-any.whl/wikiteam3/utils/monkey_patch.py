6b01910178df49b2f4a3fdb60e48317287b545af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 05:05:01 +0000

    Add package wikiteam3/4.2.3/wikiteam3-4.2.3-py3-none-any.whl

---
+++ packages/wikiteam3/wikiteam3-4.2.3-py3-none-any.whl/wikiteam3/utils/monkey_patch.py
@@ -0,0 +1,138 @@
+import ssl
+import time
+from typing import Optional
+import warnings
+
+import requests
+import requests.adapters
+from urllib3.util import create_urllib3_context
+from urllib3 import PoolManager
+
+from wikiteam3.dumpgenerator.cli.delay import Delay
+from wikiteam3.dumpgenerator.config import Config
+
+def mod_requests_text(requests: requests): # type: ignore
+    """ Monkey patch `requests.Response.text` to handle incorrect encoding """
+    def new_text(_self: requests.Response):
+        # Handle incorrect encoding
+        encoding = _self.encoding
+        if encoding is None or encoding == 'ISO-8859-1':
+            encoding = _self.apparent_encoding
+            if encoding is None:
+                encoding = 'utf-8'
+        if _self.content.startswith(b'\xef\xbb\xbf'):
+            content = _self.content.lstrip(b'\xef\xbb\xbf')
+            encoding = "utf-8"
+        else:
+            content = _self.content
+
+        return content.decode(encoding)
+
+    requests.Response.text = property(new_text) # type: ignore
+
+
+class WakeTLSAdapter(requests.adapters.HTTPAdapter):
+    """
+    Workaround for bad SSL/TLS
+    """
+    def init_poolmanager(self, connections, maxsize, block=False):
+        # https://www.openssl.org/docs/manmaster/man1/openssl-ciphers.html
+        ctx = create_urllib3_context(ciphers="ALL:COMPLMENTOFDEFAULT:eNULL:@SECLEVEL=0")
+
+        ctx.options &= ~ssl.OP_NO_TLSv1_3 & ~ssl.OP_NO_TLSv1_2 & ~ssl.OP_NO_TLSv1_1 & ~ssl.OP_NO_TLSv1
+        with warnings.catch_warnings():
+            warnings.filterwarnings("ignore",category=DeprecationWarning)
+            ctx.minimum_version = ssl.TLSVersion.TLSv1
+
+        ctx.check_hostname = False
+        ctx.verify_mode = ssl.CERT_NONE
+
+        self.poolmanager = PoolManager(
+            num_pools=connections,
+            maxsize=maxsize,
+            block=block,
+            ssl_context=ctx
+        )
+
+class SessionMonkeyPatch:
+    """
+    Monkey patch `requests.Session.send`
+    """
+    hijacked = False
+    def __init__(self,*, session: requests.Session, config: Config,
+                 add_delay: bool=False, delay_msg: Optional[str]=None,
+                 hard_retries: int=0,
+                 free_timeout_connections: bool=True, vaild_lft_sec: int=60 * 3
+        ):
+        """
+        hard_retries: hard retries, default 0 (no retry)
+        free_timeout_connections: regularly(`vaild_lft_sec`) clear connections pool
+        """
+
+        self.session = session
+        self.config = config
+
+        self.add_delay = add_delay
+        self.delay_msg = delay_msg
+
+        self.hard_retries = hard_retries
+
+        self.free_timeout_connections: bool = free_timeout_connections
+        self.vaild_lft_sec = vaild_lft_sec
+        self.last_clear_time = time.time()
+
+    def clear_timeouted_pools(self):
+        for adapter in self.session.adapters.values():
+            adapter: requests.adapters.HTTPAdapter
+            if adapter.poolmanager.pools._container.__len__() > 0 and \
+                time.time() - self.last_clear_time > self.vaild_lft_sec:
+                # TODO: logging this
+                # print('Keep-alived timeout: %d' % adapter.poolmanager.pools._container.__len__(), "connection(s) dropped.")
+                adapter.poolmanager.clear() # clear all
+                self.last_clear_time = time.time()
+
+    def hijack(self):
+        ''' Don't forget to call `release()` '''
+
+        # Monkey patch `requests.Session.send`
+        self.old_send_method = self.session.send
+
+        def new_send(request, **kwargs):
+            hard_retries_left = self.hard_retries + 1
+            if hard_retries_left <= 0:
+                raise ValueError('hard_retries must be positive')
+
+            while hard_retries_left > 0:
+                try:
+                    if self.add_delay:
+                        Delay(msg=self.delay_msg, config=self.config)
+
+                    if self.free_timeout_connections:
+                        self.clear_timeouted_pools()
+
+                    return self.old_send_method(request, **kwargs)
+                except (KeyboardInterrupt, requests.exceptions.ContentDecodingError): # don't retry
+                    raise
+                except Exception as e:
+                    hard_retries_left -= 1
+                    if hard_retries_left <= 0:
+                        raise
+
+                    print('Hard retry... (%d), due to: %s' % (hard_retries_left, e))
+                    time.sleep(3)
+
+        self.session.send = new_send # type: ignore
+        self.hijacked = True
+
+    def release(self):
+        ''' Undo monkey patch '''
+        if not self.hijacked:
+            warnings.warn('Warning: SessionMonkeyPatch.release() called before hijack()', RuntimeWarning)
+            return
+        self.session.send = self.old_send_method
+        del self
+
+    def __del__(self):
+        if self.hijacked:
+            print('Undo monkey patch...')
+            self.release()
---
+++ packages/wikiteam3/wikiteam3-4.2.3-py3-none-any.whl/wikiteam3/utils/wiki_avoid.py
@@ -0,0 +1,24 @@
+import re
+import sys
+from typing import Dict
+
+from wikiteam3.dumpgenerator.config import Config
+
+def avoid_WikiMedia_projects(config: Config=None, other: Dict=None):
+    """Skip Wikimedia projects and redirect to the dumps website"""
+
+    # notice about wikipedia dumps
+    url = ""
+    if config.api:
+        url = url + config.api
+    if config.index:
+        url = url + config.index
+    if re.findall(
+        r"(?i)(wikipedia|wikisource|wiktionary|wikibooks|wikiversity|wikimedia|wikispecies|wikiquote|wikinews|wikidata|wikivoyage)\.org",
+        url,
+    ):
+        print("PLEASE, DO NOT USE THIS SCRIPT TO DOWNLOAD WIKIMEDIA PROJECTS!")
+        print("Download the dumps from http://dumps.wikimedia.org")
+        if not other["force"]:
+            print("Thanks!")
+            sys.exit(2)
