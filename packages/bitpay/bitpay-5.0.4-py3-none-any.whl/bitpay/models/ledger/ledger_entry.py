1b3a2720415411969af240ee3958b1386e1d7ad7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 06:03:39 +0000

    Add package bitpay/5.0.4/bitpay-5.0.4-py3-none-any.whl

---
+++ packages/bitpay/bitpay-5.0.4-py3-none-any.whl/bitpay/models/ledger/ledger_entry.py
@@ -0,0 +1,39 @@
+"""
+LedgerEntry
+"""
+
+from typing import Union
+from pydantic import field_validator
+from bitpay.models.bitpay_model import BitPayModel
+from bitpay.models.ledger.buyer import Buyer
+
+
+class LedgerEntry(BitPayModel):
+    """
+    Ledger entry
+    """
+
+    type: Union[str, None] = None
+    amount: Union[float, None] = None
+    code: Union[int, None] = None
+    timestamp: Union[str, None] = None
+    currency: Union[str, None] = None
+    tx_type: Union[str, None] = None
+    scale: Union[int, None] = None
+    id: Union[str, None] = None
+    support_request: Union[str, None] = None
+    description: Union[str, None] = None
+    invoice_id: Union[str, None] = None
+
+    # Buyer
+    buyer_fields: Buyer = Buyer()
+
+    invoice_amount: Union[float, None] = None
+    invoice_currency: Union[str, None] = None
+    transaction_currency: Union[str, None] = None
+
+    @field_validator("amount")
+    def convert_to_str(cls, value: Union[int, float, None]) -> Union[float, None]:
+        if value is None:
+            return None
+        return float(value)
---
+++ packages/bitpay/bitpay-5.0.4-py3-none-any.whl/bitpay/models/rate/rates.py
@@ -0,0 +1,32 @@
+"""
+Rates
+"""
+
+from bitpay.models.bitpay_model import BitPayModel
+from bitpay.models.rate.rate import Rate
+from bitpay.models.currency import Currency
+from bitpay.exceptions.bitpay_exception import BitPayException
+from typing import List, Optional
+
+
+class Rates(BitPayModel):
+    """
+    Rates:Rates are exchange rates, representing the number of fiat
+    currency units equivalent to one BTC.
+    """
+
+    rates: List[Rate] = []
+
+    def update(self, rate_client) -> None:  # type: ignore
+        rates = rate_client.get_rates()
+        self.rates = rates.rates
+
+    def get_rate(self, currency_code: str) -> Optional[float]:
+        if Currency.is_valid(currency_code) is False:
+            raise BitPayException("currency code must be a type of Model.Currency")
+
+        val = None
+        for rate_obj in self.rates:
+            if rate_obj.code == currency_code:
+                val = rate_obj.rate
+        return val
