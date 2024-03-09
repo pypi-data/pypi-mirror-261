e488e8fb285de37b183c48dbd71d6402cee9c078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 15:04:25 +0000

    Add package pytest-celery/1.0.0b3/pytest_celery-1.0.0b3.tar.gz

---
+++ packages/pytest-celery/pytest_celery-1.0.0b3.tar.gz/pytest_celery-1.0.0b3/src/pytest_celery/vendors/worker/content/app.py
@@ -0,0 +1,33 @@
+"""The pytest-celery plugin provides a set of built-in components called
+:ref:`vendors`. This module is part of the :ref:`built-in-worker` vendor.
+
+Template for Celery worker application.
+"""
+
+from __future__ import annotations
+
+import json
+import logging
+import sys
+
+from celery import Celery
+from celery.signals import after_setup_logger
+
+imports = None
+
+app = Celery("celery_test_app")
+config = None
+
+if config:
+    app.config_from_object(config)
+    print(f"Changed worker configuration: {json.dumps(config, indent=4)}")
+
+
+@after_setup_logger.connect
+def setup_loggers(logger: logging.Logger, *args: tuple, **kwargs: dict) -> None:
+    # https://distributedpython.com/posts/celery-docker-and-the-missing-startup-banner/
+    logger.addHandler(logging.StreamHandler(sys.stdout))
+
+
+if __name__ == "__main__":
+    app.start()
