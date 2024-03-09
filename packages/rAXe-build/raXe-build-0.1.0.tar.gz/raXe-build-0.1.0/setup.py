92259aff5d7855a3113eafa53c1a3e8cfe849bdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 14:05:18 +0000

    Add package rAXe-build/0.1.0/raXe-build-0.1.0.tar.gz

---
+++ packages/rAXe-build/raXe-build-0.1.0.tar.gz/raXe-build-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+setup(
+name='raXe-build',
+version='0.1.0',
+author='Axel',
+author_email='axel.biegalski@edu.esiee.fr',
+description='sL package',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
+
