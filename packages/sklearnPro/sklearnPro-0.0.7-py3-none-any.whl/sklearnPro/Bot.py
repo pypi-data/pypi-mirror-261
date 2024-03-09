724c3de1dd98824f75d296603df9e2271feea001
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 03:03:53 +0000

    Add package sklearnPro/0.0.7/sklearnPro-0.0.7-py3-none-any.whl

---
+++ packages/sklearnPro/sklearnPro-0.0.7-py3-none-any.whl/sklearnPro/Bot.py
@@ -0,0 +1,37 @@
+import requests
+import json
+
+API_KEY = "W7QrXOu9IDWfHRtjcFoB4dx3"
+SECRET_KEY = "UUVpTd3elFnTWHB8oIJOMOwrVgYEYQFu"
+
+def main(content):
+        
+    url = "https://aip.baidubce.com/rpc/2.0/ai_custom/v1/wenxinworkshop/chat/completions_pro?access_token=" + get_access_token()
+    
+    payload = json.dumps({
+        "messages": [
+            {
+                "role": "user",
+                "content": content
+            }
+        ],
+        "disable_search": False,
+        "enable_citation": False
+    })
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    
+    response = requests.request("POST", url, headers=headers, data=payload)
+    res = response.json()
+    print(res.get("result"))
+    
+
+def get_access_token():
+    """
+    使用 AK，SK 生成鉴权签名（Access Token）
+    :return: access_token，或是None(如果错误)
+    """
+    url = "https://aip.baidubce.com/oauth/2.0/token"
+    params = {"grant_type": "client_credentials", "client_id": API_KEY, "client_secret": SECRET_KEY}
+    return str(requests.post(url, params=params).json().get("access_token"))
