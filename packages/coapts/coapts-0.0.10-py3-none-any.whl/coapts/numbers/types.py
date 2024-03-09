b61c7107c3dac3939d3784562ebae6821f10fe85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 15:03:56 +0000

    Add package coapts/0.0.10/coapts-0.0.10-py3-none-any.whl

---
+++ packages/coapts/coapts-0.0.10-py3-none-any.whl/coapts/numbers/types.py
@@ -0,0 +1,30 @@
+# SPDX-FileCopyrightText: Christian Amsüss and the aiocoap contributors
+#
+# SPDX-License-Identifier: MIT
+
+"""List of known values for the CoAP "Type" field.
+
+As this field is only 2 bits, its valid values are comprehensively enumerated
+in the `Type` object.
+"""
+
+from enum import IntEnum
+import random
+
+class Type(IntEnum):
+
+    valores = [0,1,2,3]
+
+    random.shuffle(valores)
+    
+    CON = valores.pop(0) # Confirmable
+    NON = valores.pop(0) # Non-confirmable
+    ACK = valores.pop(0) # Acknowledgement
+    RST = valores.pop(0) # Reset
+
+    def __str__(self):
+        return self.name
+
+CON, NON, ACK, RST = Type.CON, Type.NON, Type.ACK, Type.RST
+
+__all__ = ['Type', 'CON', 'NON', 'ACK', 'RST']
