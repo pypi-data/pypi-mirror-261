a486318f9bdc75b63ffb12163c0fbc8da041b96b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 04:04:51 +0000

    Add package illumio-pylo/0.2.7/illumio_pylo-0.2.7.tar.gz

---
+++ packages/illumio-pylo/illumio_pylo-0.2.7.tar.gz/illumio_pylo-0.2.7/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+__version__ = "0.2.7"
+
+
+setup_readme = "README.md"
+setup_license = "LICENSE"
+
+#setup get dependencies from requirements.txt
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='illumio_pylo',
+    version=__version__,
+    description='API Framework and Utilities for Illumio ASP platform',
+    long_description=setup_readme,
+    author='Christophe Painchaud',
+    author_email='shellescape@gmail.com',
+    url='https://github.com/cpainchaud/pylo',
+    license=setup_license,
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=required,
+    entry_points={
+        'console_scripts': ['pylo-cli=pylo.cli:run'],
+    },
+    package_data={
+        "": ["*.pem"],
+    },
+    python_requires='>=3.11',
+    setup_requires=required
+)
