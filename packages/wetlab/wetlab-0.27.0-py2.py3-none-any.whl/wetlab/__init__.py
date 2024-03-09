923d8b877a31589c32eff1d86985708808d63803
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 03:04:17 +0000

    Add package wetlab/0.27.0/wetlab-0.27.0-py2.py3-none-any.whl

---
+++ packages/wetlab/wetlab-0.27.0-py2.py3-none-any.whl/wetlab/__init__.py
@@ -0,0 +1,44 @@
+"""Lamin's `wetlab` exemplary lab schema.
+
+Import the package::
+
+   import wetlab
+
+ORMs:
+
+.. autosummary::
+   :toctree: .
+
+   Experiment
+   ExperimentType
+   Biosample
+   Techsample
+   Treatment
+   TreatmentTarget
+   Well
+
+"""
+
+__version__ = "0.27.0"
+
+from lamindb_setup import _check_instance_setup
+
+
+# trigger instance loading if users
+# want to access attributes
+def __getattr__(name):
+    if name not in {"models"}:
+        _check_instance_setup(from_lamindb=True)
+    return globals()[name]
+
+
+if _check_instance_setup():
+    from .models import (  # noqa
+        Biosample,
+        Experiment,
+        ExperimentType,
+        Techsample,
+        Treatment,
+        TreatmentTarget,
+        Well,
+    )
