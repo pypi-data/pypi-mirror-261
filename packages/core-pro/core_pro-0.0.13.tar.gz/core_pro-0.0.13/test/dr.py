ec17b18b41be2dbbf7adf9db55edb6a257be7735
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 15:04:46 +0000

    Add package core-pro/0.0.13/core_pro-0.0.13.tar.gz

---
+++ packages/core-pro/core_pro-0.0.13.tar.gz/core_pro-0.0.13/test/dr.py
@@ -0,0 +1,6 @@
+from src.core_pro import Drive
+
+
+url = '1vrcncbs1YGy9rWLBG3s35-mGpcUB8-A7'
+# url = '1SU-YjuibMc1xO4KzDdVTSlODhLHrFXlx'
+a = Drive().get_file_info(url)
---
+++ packages/core-pro/core_pro-0.0.13.tar.gz/core_pro-0.0.13/test/sh.py
@@ -0,0 +1,4 @@
+from src.core_pro import Sheet
+
+sh = '18_xiWXvS3slBghBUUHW4ZZbxTuwxZGAiuYWs7zF_I3Y'
+Sheet(sh)
