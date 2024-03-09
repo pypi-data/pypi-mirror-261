37630bda36a1c073c19fb7c895d48594a79fa1fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 03:03:56 +0000

    Add package omupluginemoji/0.1.6/omupluginemoji-0.1.6-py3-none-any.whl

---
+++ packages/omupluginemoji/omupluginemoji-0.1.6-py3-none-any.whl/omupluginemoji/plugin.py
@@ -0,0 +1,102 @@
+import re
+from dataclasses import dataclass
+from typing import Dict, TypedDict
+
+from omuchat import App, Client, content, Message
+
+APP = App(
+    name="emoji",
+    group="omu.chat.plugins",
+    version="0.1.0",
+)
+client = Client(APP)
+
+
+class Emoji(TypedDict):
+    id: str
+    name: str
+    image_url: str
+    regex: str
+
+
+class registry:
+    emojis: Dict[str, Emoji] = {}
+
+
+@client.omu.registry.listen("emojis")
+async def on_emojis_update(emojis: Dict[str, Emoji]) -> None:
+    registry.emojis = emojis or {}
+
+
+class Directories(TypedDict):
+    data: str
+    assets: str
+    plugins: str
+
+
+@dataclass
+class EmojiMatch:
+    emoji: Emoji
+    match: re.Match
+    start: int
+    end: int
+
+
+def transform(component: content.Component) -> content.Component:
+    if isinstance(component, content.Text):
+        parts = transform_text_content(component)
+        if len(parts) == 1:
+            return parts[0]
+        return content.Root(parts)
+    if isinstance(component, content.Parent):
+        component.set_children(
+            [transform(sibling) for sibling in component.get_children()]
+        )
+    return component
+
+
+def transform_text_content(
+    component: content.Text,
+) -> list[content.Component]:
+    text = component.text
+    parts = []
+    while text:
+        match: EmojiMatch | None = None
+        for emoji in registry.emojis.values():
+            if not emoji["regex"]:
+                continue
+            result = re.search(emoji["regex"], text)
+            if not result:
+                continue
+            if not match or result.start() < match.start:
+                match = EmojiMatch(emoji, result, result.start(), result.end())
+        if not match:
+            parts.append(content.Text.of(text))
+            break
+        if match.start > 0:
+            parts.append(content.Text.of(text[: match.start]))
+        parts.append(
+            content.Image.of(
+                url=match.emoji["image_url"],
+                id=match.emoji["id"],
+                name=match.emoji["name"],
+            )
+        )
+        text = text[match.end :]
+    return parts
+
+
+@client.messages.proxy
+async def on_message(message: Message):
+    if not message.content:
+        return message
+    message.content = transform(message.content)
+    return message
+
+
+async def main():
+    await client.start()
+
+
+if __name__ == "__main__":
+    client.run()
