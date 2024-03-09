e16aa36054b63101f103f959ff63524205400cab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 15:04:22 +0000

    Add package virstrain/1.15/virstrain-1.15.tar.gz

---
+++ packages/virstrain/virstrain-1.15.tar.gz/virstrain-1.15/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+setuptools.setup(
+	name="virstrain",
+	version="1.15",
+	author="Liao Herui",
+	author_email="heruiliao2-c@my.cityu.edu.hk",
+	description="An RNA/DNA virus strain-level identification tool for short reads.",
+	long_description="VirStrain can be applied to identify RNA/DNA viral strains from short reads.",
+	long_description_content_type="text/markdown",
+	url="https://github.com/liaoherui/VirStrain",
+	packages=setuptools.find_packages(),
+	include_package_data=True,
+	zip_safe=True,
+	package_data={
+		"VirStrain":['jellyfish-linux',"*.pl"]
+	},
+	install_requires=[
+	'networkx==2.4',
+	'numpy==1.17.3',
+	'pandas==1.0.1',
+	'biopython==1.74',
+	'plotly==3.10.0',
+	'matplotlib==3.1.2'
+	],
+	entry_points={
+		'console_scripts':[
+		'virstrain = VirStrain.VirStrain:main',
+		'virstrain_build = VirStrain.VirStrain_build:main',
+		'virstrain_contig = VirStrain.VirStrain_contig:main',
+		'virstrain_merge = VirStrain.VirStrain_contigDB_merge:main',
+		]
+		},
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		],
+)
