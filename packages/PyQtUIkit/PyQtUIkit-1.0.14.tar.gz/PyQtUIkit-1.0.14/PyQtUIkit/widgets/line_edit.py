078c7ec33d4186cc58c5e06dd0468027ebb17158
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 04:03:18 +0000

    Add package PyQtUIkit/1.0.14/PyQtUIkit-1.0.14.tar.gz

---
+++ packages/PyQtUIkit/PyQtUIkit-1.0.14.tar.gz/PyQtUIkit-1.0.14/PyQtUIkit/widgets/line_edit.py
@@ -0,0 +1,33 @@
+from PyQt6.QtWidgets import QLineEdit
+
+from PyQtUIkit.core.properties import IntProperty, StringProperty, PaletteProperty
+from PyQtUIkit.widgets._widget import KitWidget as _KitWidget
+
+
+class KitLineEdit(QLineEdit, _KitWidget):
+    main_palette = PaletteProperty('main_palette', 'Main')
+    border = IntProperty('border', )
+    radius = IntProperty('radius', )
+
+    def __init__(self, text=''):
+        super().__init__(text)
+        self.__widgets = []
+        self.border = 1
+        self.radius = 4
+
+    def _apply_theme(self):
+        self.setStyleSheet(f"""
+QLineEdit {{
+    color: {self.main_palette.text};
+    background-color: {self.main_palette.main};
+    border: {self.border}px solid {self._tm.get('Border').main};
+    border-radius: {self.radius}px;
+}}
+QLineEdit:hover {{
+    border: {self.border}px solid {self._tm.get('Border').hover};
+    background-color: {self.main_palette.hover};
+}}
+QLineEdit:focus {{
+    border: {self.border}px solid {self._tm.get('Border').selected};
+    background-color: {self.main_palette.hover};
+}}""")
