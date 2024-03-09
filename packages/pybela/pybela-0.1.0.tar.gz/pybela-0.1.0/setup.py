6e73cbdae34ae7ddc0b1239b01f060fbf3bbd67e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 03:04:03 +0000

    Add package pybela/0.1.0/pybela-0.1.0.tar.gz

---
+++ packages/pybela/pybela-0.1.0.tar.gz/pybela-0.1.0/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+from pathlib import Path
+
+long_description = open(f"{Path(__file__).parent}/readme.md").read()
+
+setuptools.setup(
+    name="pybela",
+    version="0.1.0",
+    author="Teresa Pelinski",
+    author_email="teresapelinski@gmail.com",
+    description="pybela allows interfacing with Bela, the embedded audio platform, using Python. pybela provides a convenient way to stream, log, and monitor sensor data from your Bela device to your laptop, or alternatively, to send values to a Bela program from your laptop.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BelaPlatform/pybela",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "jupyter",
+        "panel",
+        "jupyter-bokeh",
+        "bitarray",
+        "notebook",
+        "websockets",
+        "bokeh",
+        "ipykernel",
+        "nest-asyncio",
+        "aiofiles",
+        "paramiko"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+)
