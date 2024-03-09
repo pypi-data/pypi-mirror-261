d6a51fd257e1dc7004eee0243106e7be2378d599
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 15:04:40 +0000

    Add package xasp/0.8.0/xasp-0.8.0.tar.gz

---
+++ packages/xasp/xasp-0.8.0.tar.gz/xasp-0.8.0/xasp/commands.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+from typing import Optional
+from dumbo_asp.primitives.models import Model
+
+from xasp.entities import Explain
+
+
+def save_explanation_dag(dag: Model, answer_set: Model, atoms_to_explain: Model, target: Path,
+                         distance: Optional[int] = None,
+                         **kwargs):
+    Explain.the_dag(
+        dag,
+        the_answer_set=answer_set,
+        the_atoms_to_explain=atoms_to_explain,
+    ).save_igraph(target, distance, **kwargs)
+
