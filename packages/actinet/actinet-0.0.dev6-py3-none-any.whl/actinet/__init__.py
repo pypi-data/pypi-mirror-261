fd088a066eeae8a277cbf3d99549769bbd8882e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 15:04:23 +0000

    Add package actinet/0.0.dev6/actinet-0.0.dev6-py3-none-any.whl

---
+++ packages/actinet/actinet-0.0.dev6-py3-none-any.whl/actinet/__init__.py
@@ -0,0 +1,12 @@
+name = "actinet"
+__author__ = "Shing Chan, Aidan Acquah, Charilaos Zisou, Aiden Doherty"
+__maintainer__ = "Shing Chan"
+__maintainer_email__ = "shing.chan@ndph.ox.ac.uk"
+__license__ = "See LICENSE file."
+
+__classifier_version__ = "ssl_ukb_c24_rw_20240204"
+__classifier_md5__ = "11c3f36348dae37da4f99bd6d810bbb2"
+
+from . import _version
+
+__version__ = _version.get_versions()["version"]
