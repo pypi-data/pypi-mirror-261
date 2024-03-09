7b3ff6ae897e413506ce7dd3d86ec8df0c0b512e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 03:03:44 +0000

    Add package GPUDTW/0.11/GPUDTW-0.11.tar.gz

---
+++ packages/GPUDTW/GPUDTW-0.11.tar.gz/GPUDTW-0.11/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="GPUDTW",
+    version="0.11",
+    author="Wang Zihao",
+    author_email="qianlkzf@outlook.com",
+    description=" dynamic time warping (DTW) by GPU accelerated",
+    description_file='README.md',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/qianlkzf/GPUDTW",
+    packages=setuptools.find_packages(),
+    package_data={
+        "GPUDTW":['./OpenCL.dll','./opencldtw.cl','./cudadtw.cu']
+    },
+    install_requires=['numpy', 'numba'],
+    keywords='DTW,GPU,OpenCL,CUDA',
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ),
+    license='GNU GENERAL PUBLIC LICENSE Version 3',
+)
