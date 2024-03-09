9026a4f157c5953209637d55c51fe53a7601fa7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 05:03:54 +0000

    Add package yaylib/1.4.12/yaylib-1.4.12.tar.gz

---
+++ packages/yaylib/yaylib-1.4.12.tar.gz/yaylib-1.4.12/yaylib/__init__.py
@@ -0,0 +1,37 @@
+"""
+
+Yay! (nanameue, Inc.) API Client
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+An API wrapper for Yay! (yay.space) written in Python.
+
+:copyright: (c) 2023 qvco
+:license: MIT, see LICENSE for more details.
+
+"""
+
+__title__ = "yaylib"
+__author__ = "qvco"
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2023 qvco"
+__version__ = "1.4.12"
+
+from .client import Client
+from .cookie import Cookie
+from .errors import *
+from .models import *
+from .responses import *
+from .types import *
+from .utils import mention
+from .ws import Intents
+
+__all__ = (
+    "Client",
+    "Cookie",
+    "errors",
+    "models",
+    "responses",
+    "types",
+    "mention",
+    "Intents",
+)
