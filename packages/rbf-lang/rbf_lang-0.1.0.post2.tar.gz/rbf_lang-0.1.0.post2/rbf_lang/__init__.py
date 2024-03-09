ee88bc65aef15596df250061497aedf568372f7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 06:03:16 +0000

    Add package rbf-lang/0.1.0.post2/rbf_lang-0.1.0.post2.tar.gz

---
+++ packages/rbf-lang/rbf_lang-0.1.0.post2.tar.gz/rbf_lang-0.1.0.post2/rbf_lang/__init__.py
@@ -0,0 +1,13 @@
+"""
+Reversible BitFuck (RBF) interpretor
+"""
+
+__version__ = "0.1.0.post2"
+
+from . import program
+from . import tape
+from . import runner
+
+Program = program.Program
+Tape = tape.Tape
+run = runner.run
