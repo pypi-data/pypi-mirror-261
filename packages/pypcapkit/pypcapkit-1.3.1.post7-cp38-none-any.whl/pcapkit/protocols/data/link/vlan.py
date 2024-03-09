9ac9994808ad92b3d3cd3723f16851aef5ad8a11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 15:04:54 +0000

    Add package pypcapkit/1.3.1.post7/pypcapkit-1.3.1.post7-cp38-none-any.whl

---
+++ packages/pypcapkit/pypcapkit-1.3.1.post7-cp38-none-any.whl/pcapkit/protocols/data/link/vlan.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""data models for 802.1Q customer VLAN tag type"""
+
+from typing import TYPE_CHECKING
+
+from pcapkit.corekit.infoclass import info_final
+from pcapkit.protocols.data.data import Data
+
+if TYPE_CHECKING:
+    from pcapkit.const.reg.ethertype import EtherType
+    from pcapkit.const.vlan.priority_level import PriorityLevel
+
+__all__ = ['VLAN', 'TCI']
+
+
+@info_final
+class TCI(Data):
+    """Data model for tag control information."""
+
+    #: Priority code point.
+    pcp: 'PriorityLevel'
+    #: Drop eligible indicator.
+    dei: 'bool'
+    #: VLAN identifier.
+    vid: 'int'
+
+    if TYPE_CHECKING:
+        def __init__(self, pcp: 'PriorityLevel', dei: 'bool', vid: 'int') -> 'None': ...  # pylint: disable=unused-argument,super-init-not-called,multiple-statements
+
+
+@info_final
+class VLAN(Data):
+    """Data model for 802.1Q customer VLAN tag type."""
+
+    #: Tag control information.
+    tci: 'TCI'
+    #: Protocol (Internet Layer).
+    type: 'EtherType'
+
+    if TYPE_CHECKING:
+        def __init__(self, tci: 'TCI', type: 'EtherType') -> 'None': ...  # pylint: disable=unused-argument,super-init-not-called,multiple-statements,redefined-builtin
