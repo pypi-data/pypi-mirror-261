72eedd196c41052e56b1a96cba2a5d29d197b7c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 06:03:10 +0000

    Add package shipyard-domo/0.2.1/shipyard_domo-0.2.1.tar.gz

---
+++ packages/shipyard-domo/shipyard_domo-0.2.1.tar.gz/shipyard_domo-0.2.1/shipyard_domo/cli/refresh_dataset.py
@@ -0,0 +1,90 @@
+import sys
+import time
+import argparse
+import shipyard_bp_utils as shipyard
+from shipyard_domo.utils import exceptions as errs
+from shipyard_domo import DomoClient
+from shipyard_templates import ShipyardLogger, ExitCodeException
+from shipyard_bp_utils.artifacts import Artifact
+
+logger = ShipyardLogger.get_logger()
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--client-id", dest="client_id", required=True)
+    parser.add_argument("--secret-key", dest="secret_key", required=True)
+    parser.add_argument("--dataset-id", dest="dataset_id", required=True)
+    parser.add_argument(
+        "--wait-for-completion",
+        dest="wait_for_completion",
+        required=False,
+        default="FALSE",
+    )
+    args = parser.parse_args()
+
+    return args
+
+
+def determine_execution_status(execution_data):
+    # check if execution has finished first
+    status = execution_data["currentState"]
+    if status == "ABORTED":
+        logger.info("Domo Refresh has been aborted")
+        return errs.EXIT_CODE_FINAL_STATUS_CANCELLED
+    elif status == "ACTIVE":
+        logger.info(f"Domo Refresh is still currently ongoing with status {status}")
+        return errs.EXIT_CODE_STATUS_INCOMPLETE
+    elif status == "INVALID":
+        logger.info("Domo Refresh is invalid either due to system conflict or error")
+        return errs.EXIT_CODE_FINAL_STATUS_INVALID
+    elif status == "SUCCESS":
+        logger.info("Domo has refreshed successfully ")
+        return errs.EXIT_CODE_FINAL_STATUS_SUCCESS
+    else:
+        logger.info(f"Unknown Domo Refresh status: {status}")
+        return errs.EXIT_CODE_UNKNOWN_STATUS
+
+
+def main():
+    try:
+        args = get_args()
+        wait = shipyard.args.convert_to_boolean(args.wait_for_completion)
+        # initialize domo with auth credentials
+        client = DomoClient(client_id=args.client_id, secret_key=args.secret_key)
+
+        # execute dataset refresh
+        results = client.refresh_dataset(args.dataset_id)
+        execution_id = results["id"]
+        logger.info("Sucessfully refreshed dataset")
+
+        # create the artifacts directory and save the execution id (for legacy purposes)
+        domo_artifact = Artifact("domo")
+        domo_artifact.variables.write(
+            filename="execution_id", file_type="pickle", data=execution_id
+        )
+
+        if wait:
+            execution_data = client.get_execution_details(args.dataset_id, execution_id)
+            exit_code_status = determine_execution_status(execution_data)
+            logger.info("Waiting for Domo Refresh to complete")
+            while exit_code_status == errs.EXIT_CODE_STATUS_INCOMPLETE:
+                logger.info("Waiting for 60 seconds before checking again")
+                time.sleep(60)
+                execution_data = client.get_execution_details(
+                    args.dataset_id, execution_id
+                )
+                exit_code_status = determine_execution_status(execution_data)
+            logger.info("Dataset refresh complete")
+            sys.exit(exit_code_status)
+    except ExitCodeException as ec:
+        logger.error(ec.message)
+        sys.exit(ec.exit_code)
+
+    except Exception as e:
+        logger.error(f"Error in refreshing dataset. Response from the API: {str(e)}")
+        sys.exit(errs.EXIT_CODE_UNKNOWN_ERROR)
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/shipyard-domo/shipyard_domo-0.2.1.tar.gz/shipyard_domo-0.2.1/shipyard_domo/cli/upload.py
@@ -0,0 +1,139 @@
+import sys
+import argparse
+import ast
+import re
+import shipyard_bp_utils as shipyard
+from shipyard_domo.domo import DomoClient
+from shipyard_domo.utils import utils
+from shipyard_bp_utils.artifacts import Artifact
+from shipyard_templates import ShipyardLogger, ExitCodeException
+from shipyard_domo.utils import exceptions as errs
+
+logger = ShipyardLogger.get_logger()
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--client-id", dest="client_id", required=True)
+    parser.add_argument("--secret-key", dest="secret_key", required=True)
+    parser.add_argument("--file-name", dest="file_name", required=True)
+    parser.add_argument("--dataset-name", dest="dataset_name", required=True)
+    parser.add_argument(
+        "--dataset-description", dest="dataset_description", required=False, default=""
+    )
+    parser.add_argument("--folder-name", dest="folder_name", required=False, default="")
+    parser.add_argument("--domo-schema", dest="domo_schema", required=False, default="")
+    parser.add_argument(
+        "--insert-method",
+        dest="insert_method",
+        default="REPLACE",
+        choices={"REPLACE", "APPEND"},
+        required=True,
+    )
+    parser.add_argument("--dataset-id", required=False, default="", dest="dataset_id")
+    parser.add_argument(
+        "--source-file-match-type",
+        dest="source_file_match_type",
+        choices={"regex_match", "exact_match"},
+        default="exact_match",
+        required=False,
+    )
+    args = parser.parse_args()
+
+    return args
+
+
+def main():
+    try:
+        args = get_args()
+        client_id = args.client_id
+        secret = args.secret_key
+        file_to_load = args.file_name
+        dataset_name = args.dataset_name
+        dataset_description = args.dataset_description
+        folder_name = args.folder_name
+        insert_method = args.insert_method
+        dataset_id = args.dataset_id
+        match_type = args.source_file_match_type
+        artifacts = Artifact("domo")
+
+        client = DomoClient(client_id=client_id, secret_key=secret)
+        logger.info("Successfully connected to Domo")
+        if args.domo_schema != "":
+            domo_schema = args.domo_schema
+            domo_schema = ast.literal_eval(domo_schema)
+
+        if match_type == "regex_match":
+            file_names = shipyard.files.find_all_local_file_names(folder_name)
+
+            matching_file_names = shipyard.files.find_all_file_matches(
+                file_names, re.compile(file_to_load)
+            )
+            if (n_matches := len(matching_file_names)) == 0:
+                logger.error("No files found")
+                sys.exit(errs.EXIT_CODE_FILE_NOT_FOUND)
+
+            logger.info(f"{n_matches} files found. Preparing to upload...")
+            # if the schema is provided, then use that otherwise infer the schema using sampling
+            if args.domo_schema != "":
+                dataset_schema = utils.make_schema(
+                    domo_schema, matching_file_names, folder_name
+                )
+            else:
+                logger.debug("schema is being inferred")
+                dataset_schema = client.infer_schema(
+                    matching_file_names, folder_name, k=10000
+                )
+            stream_id, execution_id = client.upload_stream(
+                file_name=matching_file_names,
+                dataset_name=dataset_name,
+                insert_method=insert_method,
+                dataset_id=dataset_id,
+                dataset_description=dataset_description,
+                domo_schema=dataset_schema,
+            )
+            logger.info("Successfully loaded all files to Domo")
+            logger.debug("Storing stream_id and execution_id in artifacts")
+            artifacts.variables.write("stream_id", "pickle", stream_id)
+            artifacts.variables.write("execution_id", "pickle", execution_id)
+
+        else:
+            # if the schema is provided, then use that otherwise infer the schema using sampling
+            if folder_name:
+                file_path = shipyard.files.combine_folder_and_file_name(
+                    folder_name=folder_name, file_name=file_to_load
+                )
+            else:
+                file_path = file_to_load
+
+            if not args.domo_schema:
+                # dataset_schema = utils.make_schema(domo_schema, file_path, folder_name)
+                dataset_schema = client.infer_schema(file_path, folder_name, k=10000)
+            else:
+                # dataset_schema = client.infer_schema(file_path, folder_name, k=10000)
+                dataset_schema = utils.make_schema(domo_schema, file_path, folder_name)
+
+            stream_id, execution_id = client.upload_stream(
+                file_name=file_to_load,
+                dataset_name=dataset_name,
+                insert_method=insert_method,
+                dataset_id=dataset_id,
+                dataset_description=dataset_description,
+                domo_schema=dataset_schema,
+            )
+
+            logger.info(f"Successfully loaded {file_path} to Domo")
+            logger.debug("Storing stream_id and execution_id in artifacts")
+            artifacts.variables.write("stream_id", "pickle", stream_id)
+            artifacts.variables.write("execution_id", "pickle", execution_id)
+
+    except ExitCodeException as ec:
+        logger.error(ec.message)
+        sys.exit(ec.exit_code)
+    except Exception as e:
+        logger.error(f"Error in uploading file to Domo: {str(e)}")
+        sys.exit(errs.EXIT_CODE_UNKNOWN_ERROR)
+
+
+if __name__ == "__main__":
+    main()
