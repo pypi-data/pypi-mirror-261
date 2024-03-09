4d0500d57ac538c0ebf01d60a428ad41c19b1d31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 04:03:26 +0000

    Add package zurich-parking/1.0.0/zurich_parking-1.0.0-py3-none-any.whl

---
+++ packages/zurich-parking/zurich_parking-1.0.0-py3-none-any.whl/zurich_parking/__init__.py
@@ -0,0 +1,15 @@
+from importlib import resources
+
+try:
+    import tomllib
+except ModuleNotFoundError:
+    # Third party imports
+    import tomli as tomllib
+
+
+# Version of zurich-parking package
+__version__ = "1.0.0"
+
+# Read URL of the Zurich parking feed from config file
+_cfg = tomllib.loads(resources.read_text("zurich_parking", "config.toml"))
+URL = _cfg["parking_api"]["url"]
