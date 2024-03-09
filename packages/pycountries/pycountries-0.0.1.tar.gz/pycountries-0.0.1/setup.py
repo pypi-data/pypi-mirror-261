1b932fd48f620a973edd8b0b703a99343a8d924c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 06:03:22 +0000

    Add package pycountries/0.0.1/pycountries-0.0.1.tar.gz

---
+++ packages/pycountries/pycountries-0.0.1.tar.gz/pycountries-0.0.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open(
+    "README.md",
+    "r",
+) as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python-country",
+    version="0.0.1",
+    author="author",
+    author_email="author@example.com",
+    description="short package description",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AivGitHub/python-country",
+    project_urls={},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(
+        where="src",
+    ),
+    python_requires=">=3.12"
+)
