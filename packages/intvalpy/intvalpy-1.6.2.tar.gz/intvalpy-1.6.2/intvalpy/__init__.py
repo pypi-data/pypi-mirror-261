bd40c5f0fcc2578dfde429f2587b2a751d8c99b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 03:04:13 +0000

    Add package intvalpy/1.6.2/intvalpy-1.6.2.tar.gz

---
+++ packages/intvalpy/intvalpy-1.6.2.tar.gz/intvalpy-1.6.2/intvalpy/__init__.py
@@ -0,0 +1,9 @@
+from .RealInterval import ClassicalArithmetic, KaucherArithmetic, ArrayInterval, precision, SingleInterval, Interval, ARITHMETICS, INTERVAL_CLASSES, single_type
+from .utils import *
+from .visualization import lineqs, IntLinIncR2, lineqs3D, IntLinIncR3, IPlot
+from .ralgb5 import ralgb5
+
+from .linear import *
+from .nonlinear import *
+
+__version__ = '1.6.2'
