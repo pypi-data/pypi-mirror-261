863eaa00da6217d25b515c26a6059e0e66dfce03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 14:04:25 +0000

    Add package matrix-webhook/3.9.0/matrix_webhook-3.9.0.tar.gz

---
+++ packages/matrix-webhook/matrix_webhook-3.9.0.tar.gz/matrix_webhook-3.9.0/matrix_webhook/__main__.py
@@ -0,0 +1,16 @@
+"""Matrix Webhook module entrypoint."""
+
+import logging
+
+from . import app, conf
+
+
+def main():
+    """Start everything."""
+    log_format = "%(asctime)s - %(name)s - %(lineno)d - %(levelname)s - %(message)s"
+    logging.basicConfig(level=50 - 10 * conf.VERBOSE, format=log_format)
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
