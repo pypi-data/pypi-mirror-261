069a33c968474e4c4b6dac20c975632d14451acc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 04:04:36 +0000

    Add package towbintools/0.1.3/towbintools-0.1.3.tar.gz

---
+++ packages/towbintools/towbintools-0.1.3.tar.gz/towbintools-0.1.3/towbintools/foundation/utils.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+def nan_helper(
+    y,
+):
+    """Helper to handle indices and logical indices of NaNs.
+
+    Parameters:
+        - y (np.ndarray): 1d numpy array with possible NaNs
+
+    Returns:
+        - nans, logical indices of NaNs
+        - index, a function, with signature indices= index(logical_indices),
+          to convert logical indices of NaNs to 'equivalent' indices
+
+    Example:
+        >>> # linear interpolation of NaNs
+        >>> nans, x= nan_helper(y)
+        >>> y[nans]= np.interp(x(nans), x(~nans), y[~nans])
+    """
+
+    return np.isnan(y), lambda z: z.nonzero()[0]
+
+
+# Exception class for the case when a method is not implemented
+class NotImplementedError(Exception):
+    pass
