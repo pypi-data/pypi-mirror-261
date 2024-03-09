fe6c29c9ad476f1ebd9217f6dfa65ad97efe0c5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 15:04:03 +0000

    Add package thread/1.0.0/thread-1.0.0-py3-none-any.whl

---
+++ packages/thread/thread-1.0.0-py3-none-any.whl/thread/utils/config.py
@@ -0,0 +1,14 @@
+class Settings:
+  """
+  # Settings
+  `Non Instantiable`
+  """
+
+  GRACEFUL_EXIT_ENABLED: bool = True
+
+  def __init__(self):
+    raise NotImplementedError('This class is not instantiable')
+
+  @staticmethod
+  def set_graceful_exit(enabled: bool = True):
+    Settings.GRACEFUL_EXIT_ENABLED = enabled
