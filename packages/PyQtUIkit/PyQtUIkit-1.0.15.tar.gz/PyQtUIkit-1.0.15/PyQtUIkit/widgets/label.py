8e855a1e55680c607098564fbef3a086a945bc5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 15:04:56 +0000

    Add package PyQtUIkit/1.0.15/PyQtUIkit-1.0.15.tar.gz

---
+++ packages/PyQtUIkit/PyQtUIkit-1.0.15.tar.gz/PyQtUIkit-1.0.15/PyQtUIkit/widgets/label.py
@@ -0,0 +1,22 @@
+from PyQt6.QtWidgets import QLabel
+
+from PyQtUIkit.core.properties import IntProperty, PaletteProperty
+from PyQtUIkit.widgets._widget import KitWidget as _KitWidget
+
+
+class KitLabel(QLabel, _KitWidget):
+    main_palette = PaletteProperty('main_palette', 'Transparent')
+    border = IntProperty('border', 0)
+    radius = IntProperty('radius', 4)
+
+    def __init__(self, text=''):
+        super().__init__(text)
+
+    def _apply_theme(self):
+        self.setStyleSheet(f"""
+        QWidget {{
+            color: {self.main_palette.text};
+            background-color: {self.main_palette.main};
+            border: {self.border}px solid {self._tm.get('Border').main};
+            border-radius: {self.radius}px;
+        }}""")
