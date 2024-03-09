74e7181e04386f9b31737164f44b70405d0eb4cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 03:03:56 +0000

    Add package teddies/0.0.1/teddies-0.0.1.tar.gz

---
+++ packages/teddies/teddies-0.0.1.tar.gz/teddies-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='teddies',
+    version='0.0.1',
+    description='Super dialect for all the bearish dataframe libraries',
+    url='https://github.com/kszucs',
+    author='kszucs',
+    author_email='kszucs@kszucs.me',
+    packages=[],
+)
