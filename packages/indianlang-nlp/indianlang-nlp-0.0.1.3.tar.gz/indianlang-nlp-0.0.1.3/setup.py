b350c69dde4ecebdf42c29d1447bf64658efd94e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 06:03:32 +0000

    Add package indianlang-nlp/0.0.1.3/indianlang-nlp-0.0.1.3.tar.gz

---
+++ packages/indianlang-nlp/indianlang-nlp-0.0.1.3.tar.gz/indianlang-nlp-0.0.1.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='indianlang-nlp',
+    version='0.0.1.3',
+    packages=find_packages(),
+    install_requires=[
+        'keras',
+        'tensorflow',
+        'gdown',
+    ],
+    author='Amey Pandit',
+    url="https://github.com/panditamey/indianlang-nlp",   
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
