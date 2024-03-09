7727200f97600d01d1d27adf041013031d67bb85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 14:04:57 +0000

    Add package deephaven-plugin-ui/0.9.0/deephaven-plugin-ui-0.9.0.tar.gz

---
+++ packages/deephaven-plugin-ui/deephaven-plugin-ui-0.9.0.tar.gz/deephaven-plugin-ui-0.9.0/src/deephaven/ui/__init__.py
@@ -0,0 +1,20 @@
+"""
+deephaven.ui is a plugin for Deephaven that provides a Python API for creating UIs.
+
+The API is designed to be similar to React, but with some differences to make it more Pythonic.
+"""
+
+from deephaven.plugin import Registration, Callback
+from .components import *
+from .elements import *
+from .hooks import *
+from .object_types import *
+
+__version__ = "0.9.0"
+
+
+class UIRegistration(Registration):
+    @classmethod
+    def register_into(cls, callback: Callback) -> None:
+        callback.register(DashboardType)
+        callback.register(ElementType)
