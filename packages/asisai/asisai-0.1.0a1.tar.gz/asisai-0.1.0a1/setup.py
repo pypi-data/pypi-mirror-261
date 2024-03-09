1778d92b2c6e8b1c9a2bff7ee7db29957f18e937
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 05:04:46 +0000

    Add package asisai/0.1.0a1/asisai-0.1.0a1.tar.gz

---
+++ packages/asisai/asisai-0.1.0a1.tar.gz/asisai-0.1.0a1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='asisai',
+    version='0.1.0-Alpha.1',
+    packages=['ASIS'],
+    install_requires=[
+        # Dependencias si las tienes
+    ],
+)
