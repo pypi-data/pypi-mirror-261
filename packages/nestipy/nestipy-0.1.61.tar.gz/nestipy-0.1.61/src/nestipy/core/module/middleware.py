8ea5baf1c0dccec1cb1a9e7a61842d62f8a4c733
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 03:03:49 +0000

    Add package nestipy/0.1.61/nestipy-0.1.61.tar.gz

---
+++ packages/nestipy/nestipy-0.1.61.tar.gz/nestipy-0.1.61/src/nestipy/core/module/middleware.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+from typing import Callable
+
+
+@dataclass
+class MiddlewareDict:
+    path: str
+    middleware: Callable
+    guard: bool = False
+
+
+class MiddlewareConsumer:
+
+    def __init__(self, compiler):
+        self.compiler = compiler
+
+    def apply_for_route(self, module, path, middleware):
+        self.compiler.apply_middleware_to_path(module, path, [middleware])
+
+    def apply_for_controller(self, module, ctrl, middleware):
+        self.compiler.apply_middleware_to_ctrl(module, ctrl, [middleware])
