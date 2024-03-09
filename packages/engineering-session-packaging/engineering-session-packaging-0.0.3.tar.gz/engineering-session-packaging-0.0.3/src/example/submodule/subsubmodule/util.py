c6425f417862224c0870cb77f2214b97eb4a99f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 04:03:17 +0000

    Add package engineering-session-packaging/0.0.3/engineering-session-packaging-0.0.3.tar.gz

---
+++ packages/engineering-session-packaging/engineering-session-packaging-0.0.3.tar.gz/engineering-session-packaging-0.0.3/src/example/submodule/subsubmodule/util.py
@@ -0,0 +1,10 @@
+location = "I am in the subpackage package"
+list_of_vals = [300, 301, 302]
+
+
+def print_argument(arg):
+    print(f'subpackage arg = {arg}')
+
+
+class Spider:
+    scary = "sometimes"
---
+++ packages/engineering-session-packaging/engineering-session-packaging-0.0.3.tar.gz/engineering-session-packaging-0.0.3/src/example/submodule/util.py
@@ -0,0 +1,10 @@
+location = "I am in package"
+list_of_vals = [200, 201, 202]
+
+
+def print_argument(arg):
+    print(f'package arg = {arg}')
+
+
+class Dogs:
+    also_cool = True
