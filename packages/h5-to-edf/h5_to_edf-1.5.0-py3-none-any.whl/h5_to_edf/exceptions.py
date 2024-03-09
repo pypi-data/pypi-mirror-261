3645fad270d8df8e1fe2e5a7bc75eb1ab29abed2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 05:03:39 +0000

    Add package h5-to-edf/1.5.0/h5_to_edf-1.5.0-py3-none-any.whl

---
+++ packages/h5-to-edf/h5_to_edf-1.5.0-py3-none-any.whl/h5_to_edf/exceptions.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of the EBS-tomo project
+#
+# Copyright (c) 2019-2023 Beamline Control Unit, ESRF
+# Distributed under the GNU LGPLv3. See LICENSE for more info.
+
+from __future__ import annotations
+
+
+class ScanNotTerminated(Exception):
+    """Raised when a processed scan is not yet closed
+
+    Basically there is no `end_time`
+    """
+
+    def __init__(self, filename, group_name=None):
+        self.filename = filename
+        super(ScanNotTerminated, self).__init__(
+            f"Scan {filename} group {group_name} not over"
+        )
+
+
+class ScanNotSuccessed(Exception):
+    """Raised when a processed scan was not properly terminated
+
+    For example the writer have a FAILED status
+    """
+
+    def __init__(self, filename, group_name, status):
+        self.filename = filename
+        super(ScanNotSuccessed, self).__init__(
+            f"Scan {filename} group {group_name} not successed ({status})"
+        )
+
+
+class FileNotProducedByBliss(Exception):
+    """Raised when a processed file was not created by BLISS"""
+
+
+class ScanNotComplete(Exception):
+    """Raised when a the scan was halfly processed"""
