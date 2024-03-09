e96f401f991800ff072ac237a7499b403ad9bab9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 05:03:41 +0000

    Add package i38e-utils/1.0.5/i38e_utils-1.0.5-py3-none-any.whl

---
+++ packages/i38e-utils/i38e_utils-1.0.5-py3-none-any.whl/i38e_utils/df_helper/__init__.py
@@ -0,0 +1,11 @@
+#  Copyright (c) 2024. ISTMO Center S.A.  All Rights Reserved
+#  IBIS is a registered trademark
+#
+from __future__ import annotations
+
+import sys as _sys
+from typing import Any as _Any
+
+from ._df_helper import *
+
+__all__ = ['DfHelper']
