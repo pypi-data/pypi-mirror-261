1b0a5069b32556a1ba7dc93e1888fca55d5b6b3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 14:03:59 +0000

    Add package gifnoc/0.1.5/gifnoc-0.1.5.tar.gz

---
+++ packages/gifnoc/gifnoc-0.1.5.tar.gz/gifnoc-0.1.5/gifnoc/__init__.py
@@ -0,0 +1,19 @@
+from . import config  # noqa: F401
+from .core import (  # noqa: F401
+    Configuration,
+    active_configuration,
+    current_configuration,
+    get,
+    gifnoc,
+    is_loaded,
+    load,
+    load_global,
+    overlay,
+    use,
+)
+from .define import define  # noqa: F401
+from .registry import (  # noqa: F401
+    map_environment_variables,
+    register,
+)
+from .type_wrappers import TaggedSubclass  # noqa: F401
