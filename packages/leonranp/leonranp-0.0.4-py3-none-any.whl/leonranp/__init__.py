9fe87b22bbf66a77c5c48280533fdfb4c1270ea7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 03:04:00 +0000

    Add package leonranp/0.0.4/leonranp-0.0.4-py3-none-any.whl

---
+++ packages/leonranp/leonranp-0.0.4-py3-none-any.whl/leonranp/__init__.py
@@ -0,0 +1,56 @@
+# --coding:UTF-8-- #
+#Code by LeonMMcoset
+'''
+This is Leon Random Plus,that add more functions to the random!
+To use it,code "import leonranp"
+
+Copyright LeonMMcoset.All rights reserved.
+'''
+#Code Running Warning Start
+print('---------Leon Random Plus Warning---------')
+print('This code is not this use for!')
+print('You should use this code on your code!')
+print('To use it,code "import leonranp"')
+print('------------------------------------------')
+#Code Running Warning End
+from random import *
+#Start Code#
+#randstr:just code "randstr()".
+def randstr():
+    randstrlist = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    randstr = choice(randstrlist)
+    print(randstr)
+#randcode:if you want to print code,code "print(randcode())"
+#The randcode() values can be assigned in variables.
+#To print randcode,code "print(randcode())"
+def rcrandstr():
+    randstrlist = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    rcrandstr = choice(randstrlist)
+    return rcrandstr
+def randcode(hmuch):
+    randcode = ''
+    for i in range(hmuch):
+        if randint(0,1)==0:
+            randcode = randcode + str(randint(0,9))
+        else:
+            randcode = randcode + str(rcrandstr())
+    return randcode
+#randbool:if you want to print on,code "print(randbool())"
+#The randbool() values can be assigned in variables.
+def randbool():
+    randbool = randint(0,1)
+    return bool(randbool)
+#End Code#
+#---------------------------------------
+#If your code is commerce,
+#you gonna add this in your code!
+#"leonranp.info()"
+#Thank you!Have a good time.
+def info():
+    print('This code is use Leon Random Plus!')
+#You can use "leonranp.help()" to get help!
+def ranhelp():
+    print('---Leon Random Plus Help Start---')
+    print('randstr() -> Random string')
+    print('randcode(hmuch) -> Random code(such as print a675a or 687dsa or 7s8 and more).')
+    print('---Leon Random Plus Help End---')
