aed9e90a88c52b71c39ec9c414c3920b4b6d9b53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 15:04:39 +0000

    Add package kuzu/0.3.2.dev11/kuzu-0.3.2.dev11.tar.gz

---
+++ packages/kuzu/kuzu-0.3.2.dev11.tar.gz/sdist/kuzu-source/src/include/common/exception/message.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+
+namespace kuzu {
+namespace common {
+
+struct ExceptionMessage {
+    static std::string duplicatePKException(const std::string& pkString);
+    static std::string nonExistentPKException(const std::string& pkString);
+    static std::string invalidPKType(const std::string& type);
+    static inline std::string nullPKException() {
+        return "Found NULL, which violates the non-null constraint of the primary key column.";
+    }
+    static inline std::string notAllowCopyOnNonEmptyTableException() {
+        return "COPY commands can only be executed once on a table.";
+    }
+    static std::string overLargeStringPKValueException(uint64_t length);
+    static std::string overLargeStringValueException(uint64_t length);
+    static std::string violateDeleteNodeWithConnectedEdgesConstraint(
+        const std::string& tableName, const std::string& offset, const std::string& direction);
+    static std::string violateRelMultiplicityConstraint(
+        const std::string& tableName, const std::string& offset, const std::string& direction);
+
+    static inline std::string validateCopyNPYByColumnException() {
+        return "Please use COPY FROM BY COLUMN statement for copying npy files.";
+    }
+    static inline std::string validateCopyCSVParquetByColumnException() {
+        return "Please use COPY FROM statement for copying csv and parquet files.";
+    }
+    static inline std::string validateCopyToCSVParquetExtensionsException() {
+        return "COPY TO currently only supports csv and parquet files.";
+    }
+    static std::string validateCopyNpyNotForRelTablesException(const std::string& tableName);
+};
+
+} // namespace common
+} // namespace kuzu
