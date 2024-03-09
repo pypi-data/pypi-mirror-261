13e44287b249b0bf5cb1ba0126a9026b31e3c20e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 14:03:52 +0000

    Add package message-types/0.1.0/message-types-0.1.0.tar.gz

---
+++ packages/message-types/message-types-0.1.0.tar.gz/message-types-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='message-types',
+    version='0.1.0',
+    url='https://github.com/kaizendevelopments/message-types',
+    author='kaizendevelopments',
+    author_email='kaizen.developments@outlook.com',
+    description='This package defines classes which can be used to apply types, classes to messages used in debugging, message passing, ect.',
+    packages=find_packages(),    
+    install_requires=[],
+)
