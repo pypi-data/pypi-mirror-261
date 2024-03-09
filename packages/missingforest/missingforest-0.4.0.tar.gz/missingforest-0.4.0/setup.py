2f6e9dd1e5c2bd9a3d851f3fc7d80c6b44950bf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 05:03:39 +0000

    Add package missingforest/0.4.0/missingforest-0.4.0.tar.gz

---
+++ packages/missingforest/missingforest-0.4.0.tar.gz/missingforest-0.4.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="missingforest",
+    version="0.4.0",
+    author       = "Richard Tjörnhammar",
+    author_email = "richard.tjornhammar@gmail.com",
+    description  = "Missing Data Imputation for Python : Package was forked from missingpy 0.2.0",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/rictjo/missingpy",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ),
+)
