cb11129df4f082b2a0dbb91f2ca7b33d25d83f46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 15:04:39 +0000

    Add package syrius-sdk/0.0.16/syrius_sdk-0.0.16.tar.gz

---
+++ packages/syrius-sdk/syrius_sdk-0.0.16.tar.gz/syrius_sdk-0.0.16/syrius/commands/CurrencyFormatterCommand.py
@@ -0,0 +1,8 @@
+from syrius.commands.abstract import Command
+from syrius.types import InputType
+
+
+class CurrencyFormatterCommand(Command):
+    """ """
+    id: int = 9
+    quantity: InputType[str]
