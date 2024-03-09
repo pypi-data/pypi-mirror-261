58c15bf3b03c4d8d1aa462b5ef78e9fb5e66829b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 04:03:18 +0000

    Add package energyml-common2-0/1.10.0/energyml_common2_0-1.10.0.tar.gz

---
+++ packages/energyml-common2-0/energyml_common2_0-1.10.0.tar.gz/energyml_common2_0-1.10.0/energyml/eml/v2_0/gts.py
@@ -0,0 +1,83 @@
+from __future__ import annotations
+from dataclasses import dataclass, field
+from typing import Optional, Union
+from energyml.eml.v2_0.mod_3 import NilReasonEnumerationValue
+from energyml.eml.v2_0.xlink import (
+    ActuateValue,
+    ShowValue,
+)
+
+__NAMESPACE__ = "http://www.isotc211.org/2005/gts"
+
+
+@dataclass
+class TmPrimitivePropertyType:
+    class Meta:
+        name = "TM_Primitive_PropertyType"
+
+    type_value: str = field(
+        init=False,
+        default="simple",
+        metadata={
+            "name": "type",
+            "type": "Attribute",
+            "namespace": "http://www.w3.org/1999/xlink",
+        },
+    )
+    href: Optional[str] = field(
+        default=None,
+        metadata={
+            "type": "Attribute",
+            "namespace": "http://www.w3.org/1999/xlink",
+        },
+    )
+    role: Optional[str] = field(
+        default=None,
+        metadata={
+            "type": "Attribute",
+            "namespace": "http://www.w3.org/1999/xlink",
+        },
+    )
+    arcrole: Optional[str] = field(
+        default=None,
+        metadata={
+            "type": "Attribute",
+            "namespace": "http://www.w3.org/1999/xlink",
+        },
+    )
+    title: Optional[str] = field(
+        default=None,
+        metadata={
+            "type": "Attribute",
+            "namespace": "http://www.w3.org/1999/xlink",
+        },
+    )
+    show: Optional[ShowValue] = field(
+        default=None,
+        metadata={
+            "type": "Attribute",
+            "namespace": "http://www.w3.org/1999/xlink",
+        },
+    )
+    actuate: Optional[ActuateValue] = field(
+        default=None,
+        metadata={
+            "type": "Attribute",
+            "namespace": "http://www.w3.org/1999/xlink",
+        },
+    )
+    uuidref: Optional[str] = field(
+        default=None,
+        metadata={
+            "type": "Attribute",
+        },
+    )
+    nil_reason: Optional[Union[str, NilReasonEnumerationValue]] = field(
+        default=None,
+        metadata={
+            "name": "nilReason",
+            "type": "Attribute",
+            "namespace": "http://www.isotc211.org/2005/gco",
+            "pattern": r"other:\w{2,}",
+        },
+    )
