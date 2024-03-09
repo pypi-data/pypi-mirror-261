8abb59bca6a4d2d83fc896cb4eb37a0284aeb86e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 05:05:04 +0000

    Add package madotsuki/1.3/madotsuki-1.3.tar.gz

---
+++ packages/madotsuki/madotsuki-1.3.tar.gz/madotsuki-1.3/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+from setuptools import find_packages
+
+setup(
+    name="madotsuki",
+    version="1.3",
+    description="Библиотека для построения графических CRUD-интерфейсов",
+    author="jqmxaec",
+    packages=find_packages(),
+    install_requires=["PyQt5", "SQLAlchemy"],
+)
