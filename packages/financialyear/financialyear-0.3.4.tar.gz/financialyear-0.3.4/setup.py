cca8bb70be9d75892948ac39fa505ad9993d74f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 15:04:04 +0000

    Add package financialyear/0.3.4/financialyear-0.3.4.tar.gz

---
+++ packages/financialyear/financialyear-0.3.4.tar.gz/financialyear-0.3.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open("README.md", 'r') as f:
+    description = f.read()
+
+setup(
+    name='financialyear',
+    version='0.3.4',
+    packages=find_packages(),
+    install_requires=[],
+    description='It Calculate financial year related dates',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/piidus/FinancialYear',
+    author='Sudiip',
+    author_email='sudiipkumarbasu@gmail.com',
+    entry_point={'comsole_scripts': ['financialyear = src:main:main']}
+)
