7867c63a00db57103fd8cf5bb019e4594f7670d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 15:03:47 +0000

    Add package michen00/0.0.0/michen00-0.0.0.tar.gz

---
+++ packages/michen00/michen00-0.0.0.tar.gz/michen00-0.0.0/README.md
@@ -0,0 +1,11 @@
+# michen00
+
+description
+
+## Usage
+
+...
+
+## Setup
+
+...
---
+++ packages/michen00/michen00-0.0.0.tar.gz/michen00-0.0.0/tests/test_.py
@@ -0,0 +1,8 @@
+"""placeholder test."""
+
+__all__: list = []
+
+
+def test_() -> None:
+    """Test that this test is collected."""
+    assert True
