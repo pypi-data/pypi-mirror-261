eb37a32ca1a6292bf147a253901d87b07dba2178
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 03:03:46 +0000

    Add package HexAeroPy/1.0.4/HexAeroPy-1.0.4-py3-none-any.whl

---
+++ packages/HexAeroPy/HexAeroPy-1.0.4-py3-none-any.whl/HexAeroPy/__init__.py
@@ -0,0 +1,17 @@
+# mypackage/__init__.py
+
+# Package version
+__version__ = "1.0.4"
+
+# Specify what is available to import from the package
+__all__ = ["identify_runways", "choropleth_map", "load_dataset", "add_trajectory"]
+
+# Import from submodules to make available at the package level
+from .runways import identify_runways
+from .runways import load_dataset
+from .h3maps import choropleth_map
+from .h3maps import add_trajectory
+from .setup_module import ensure_data_available
+
+# Initialization package data 
+ensure_data_available(local_install = False)
