247ca1267c617facf2db67d52670517ad926d111
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 15:03:58 +0000

    Add package nonebot-plugin-fishing/0.2.0/nonebot_plugin_fishing-0.2.0-py3-none-any.whl

---
+++ packages/nonebot-plugin-fishing/nonebot_plugin_fishing-0.2.0-py3-none-any.whl/nonebot_plugin_fishing/model.py
@@ -0,0 +1,12 @@
+from nonebot import require
+require("nonebot_plugin_orm")  # noqa
+from nonebot_plugin_orm import Model
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class FishingRecord(Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[str]
+    time: Mapped[int]
+    frequency: Mapped[int]
+    fishes: Mapped[str]
