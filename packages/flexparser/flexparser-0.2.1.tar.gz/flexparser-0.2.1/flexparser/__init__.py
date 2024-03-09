59072ca1739e7cbffa1cca3ac4382b998ef25aea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 06:03:41 +0000

    Add package flexparser/0.2.1/flexparser-0.2.1.tar.gz

---
+++ packages/flexparser/flexparser-0.2.1.tar.gz/flexparser-0.2.1/flexparser/__init__.py
@@ -0,0 +1,59 @@
+"""
+    flexparser
+    ~~~~~~~~~
+
+    Classes and functions to create parsers.
+
+    The idea is quite simple. You write a class for every type of content
+    (called here ``ParsedStatement``) you need to parse. Each class should
+    have a ``from_string`` constructor. We used extensively the ``typing``
+    module to make the output structure easy to use and less error prone.
+
+    :copyright: 2022 by flexparser Authors, see AUTHORS for more details.
+    :license: BSD, see LICENSE for more details.
+"""
+
+from importlib.metadata import version
+
+try:  # pragma: no cover
+    __version__ = version("flexparser")
+except Exception:  # pragma: no cover
+    # we seem to have a local copy not installed without setuptools
+    # so the reported version will be unknown
+    __version__ = "unknown"
+
+
+from .flexparser import (
+    Block,
+    DelimiterAction,
+    DelimiterInclude,
+    IncludeStatement,
+    ParsedStatement,
+    Parser,
+    ParsingError,
+    RootBlock,
+    StatementIterator,
+    UnexpectedEOS,
+    UnknownStatement,
+    parse,
+)
+
+# Deprecate in 0.3
+UnexpectedEOF = UnexpectedEOS
+
+__all__ = (
+    "__version__",
+    "Block",
+    "DelimiterAction",
+    "DelimiterInclude",
+    "IncludeStatement",
+    "ParsedStatement",
+    "Parser",
+    "ParsingError",
+    "RootBlock",
+    "StatementIterator",
+    "UnexpectedEOF",
+    "UnexpectedEOS",
+    "UnknownStatement",
+    "parse",
+)
