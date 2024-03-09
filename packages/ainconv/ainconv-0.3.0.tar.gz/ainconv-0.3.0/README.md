3daeef4bbcf2614540150eed39af3514ee136e5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 06:03:29 +0000

    Add package ainconv/0.3.0/ainconv-0.3.0.tar.gz

---
+++ packages/ainconv/ainconv-0.3.0.tar.gz/ainconv-0.3.0/README.md
@@ -0,0 +1,132 @@
+<!-- omit in toc -->
+# ainconv - Ainu language script converter
+
+[![Version](https://img.shields.io/pypi/v/ainconv)](https://pypi.org/project/ainconv/)
+[![Downloads](https://pepy.tech/badge/ainconv)](https://pepy.tech/project/ainconv)
+[![Read the Docs](https://img.shields.io/readthedocs/ainconv-py)](https://ainconv-py.readthedocs.io/)
+[![GitHub issues](https://img.shields.io/github/issues/mkpoli/ainconv-py)](https://github.com/mkpoli/ainconv-py/issues)
+[![GitHub pull requests](https://img.shields.io/github/issues-pr/mkpoli/ainconv-py)](https://github.com/mkpoli/ainconv-py/issues)
+[![Discord](https://dcbadge.vercel.app/api/server/pkpAdPHzpP?style=flat)](https://discord.aynu.org/)
+
+<!-- omit in toc -->
+## Table of Contents
+
+- [Overview](#overview)
+  - [Important Note](#important-note)
+- [Installation](#installation)
+- [Usage](#usage)
+  - [Word Conversion](#word-conversion)
+  - [Extra Functionality](#extra-functionality)
+    - [Conversion Options](#conversion-options)
+      - [Katakana](#katakana)
+    - [Script Detection](#script-detection)
+    - [Syllable Splitting](#syllable-splitting)
+- [Support](#support)
+- [License](#license)
+- [See also](#see-also)
+
+## Overview
+
+This package provides a comprehensive set of functions for converting text between different writing systems of the [Ainu language](https://en.wikipedia.org/wiki/Ainu_language).
+
+Currently, Latin (Romanization), Katakana and Cyrillic scripts are supported. We are also planning to convert between different romanization systems and Katakana variants. Currently only the more adopted version of Latin script and lossy Katakana script are supported.
+
+Sentence conversion is planned to be supported in the future. For now, only well-formed single word is accepted. The converted string are always in lower case.
+
+### Important Note
+
+Conversion between Latin and Cyrillic script are lossless, however, conversion between Katakana and other scripts are lossy. This means that converting from Katakana to other scripts and then back to Katakana may not give the original string and the result may be ambiguous or even incorrect.
+
+This is because the Katakana script used broadly for the Ainu language is intrinsically ambiguous. For example, it does not distinguish between *tow* and *tu* (both *トゥ*), *iw* and *i.u* (both *イウ*), *ay* and *a.i* (both *アイ*), etc. Some alternative Katakana scripts are proposed to solve this problem, but none of them are widely adopted. We are planning to support some of these alternative scripts in the future.
+
+## Installation
+
+Install the package using pip
+
+```bash
+pip install ainconv
+```
+
+## Usage
+
+> [!NOTE]
+> You can also read the full documentation on [Read the Docs](https://ainconv-py.readthedocs.io/).
+
+### Word Conversion
+
+```python
+from ainconv import (
+    kana2latn,
+    latn2kana,
+    cyrl2latn,
+    latn2cyrl,
+    kana2cyrl,
+    cyrl2kana,
+    # ...
+)
+
+print(kana2latn("イランカラㇷ゚テ")) # "irankarapte"
+print(latn2kana("irankarapte")) # "イランカラㇷ゚テ"
+print(cyrl2latn("иранкараптэ")) # "irankarapte"
+print(latn2cyrl("irankarapte")) # "иранкараптэ"
+print(cyrl2kana("иранкараптэ")) # "イランカラㇷ゚テ"
+print(kana2cyrl("イランカラㇷ゚テ")) # "иранкараптэ"
+```
+
+### Extra Functionality
+
+#### Conversion Options
+
+##### Katakana
+
+```python
+from ainconv import latn2kana
+
+# Use ヰ (wi), ヱ (we) and ヲ (wo) in conversion
+assert latn2kana("wiki") == "ウィキ"  # for loanwords only
+assert latn2kana("wiki", use_wi=True) == "ヰキ"
+assert latn2kana("wenpe") == "ウェンペ"
+assert latn2kana("wenpe", use_we=True) == "ヱンペ"
+assert latn2kana("wose") == "ウォセ"
+assert latn2kana("wose", use_wo=True) == "ヲセ"
+
+assert latn2kana("wiwewo") == "ウィウェウォ"
+assert latn2kana("wiwewo", use_wi=True, use_we=True, use_wo=True) == "ヰヱヲ"
+```
+
+
+#### Script Detection
+
+Detect the script of a given string.
+
+```python
+from ainconv import detect
+
+print(detect("aynu")) # "Latn"
+print(detect("アイヌ")) # "Kana"
+print(detect("айну")) # "Cyrl"
+```
+
+#### Syllable Splitting
+
+```python
+from ainconv import separate
+
+print(separate("eyaykosiramsuypa")) # ["e", "yay", "ko", "si", "ram", "suy", "pa"]
+```
+
+## Support
+
+If you have a question or have found a bug or any other issue, feel free to [open an issue](https://github.com/mkpoli/ainconv-py/issues/new) to let me know. For instructions on how to contribute, see [CONTRIBUTING.md](https://github.com/mkpoli/ainconv-py/blob/master/CONTRIBUTING.md).
+
+You can also [join our Discord](https://discord.aynu.org/) for discussion. We have many projects going on about Ainu, so you may find something interesting there as well.
+
+## License
+
+[MIT License](LICENSE) (c) 2024 mkpoli
+
+## See also
+
+* [ainconv - npm](https://www.npmjs.com/package/ainconv): The JavaScript version of this package
+* [ainconv - crates.io](https://crates.io/crates/ainconv): The Rust version of this package
+* [Module:ain-kana-conv - ウィクショナリー日本語版](https://ja.wiktionary.org/wiki/%E3%83%A2%E3%82%B8%E3%83%A5%E3%83%BC%E3%83%AB:ain-kana-conv): The original Lua Scribunto module in the Japanese Wiktionary
---
+++ packages/ainconv/ainconv-0.3.0.tar.gz/ainconv-0.3.0/src/ainconv/conversion/katakana.py
@@ -0,0 +1,296 @@
+from more_itertools import peekable
+
+# from icecream import ic
+
+# ic.configureOutput(includeContext=True)
+
+from ..conversion.latin import ACCENTED_VOWELS, CONSONANTS, clean
+from ..syllable import separate
+
+KANA_2_LATN = {
+    "ア": "a",
+    "イ": "i",
+    "ウ": "u",
+    "エ": "e",
+    "オ": "o",
+    "カ": "ka",
+    "キ": "ki",
+    "ク": "ku",
+    "ケ": "ke",
+    "コ": "ko",
+    "サ": "sa",
+    "シ": "si",
+    "ス": "su",
+    "セ": "se",
+    "ソ": "so",
+    "タ": "ta",
+    "テ": "te",
+    "ト": "to",
+    "ナ": "na",
+    "ニ": "ni",
+    "ヌ": "nu",
+    "ネ": "ne",
+    "ノ": "no",
+    "ハ": "ha",
+    "ヒ": "hi",
+    "フ": "hu",
+    "ヘ": "he",
+    "ホ": "ho",
+    "パ": "pa",
+    "ピ": "pi",
+    "プ": "pu",
+    "ペ": "pe",
+    "ポ": "po",
+    "マ": "ma",
+    "ミ": "mi",
+    "ム": "mu",
+    "メ": "me",
+    "モ": "mo",
+    "ヤ": "ya",
+    "ユ": "yu",
+    "ヨ": "yo",
+    "ラ": "ra",
+    "リ": "ri",
+    "ル": "ru",
+    "レ": "re",
+    "ロ": "ro",
+    "ワ": "wa",
+    "ヲ": "wo",
+    "ン": "n",
+    "ッ": "t",
+    "ㇳ": "t",
+    "ゥ": "w",
+    "ㇰ": "k",
+    "ㇵ": "x",
+    "ㇶ": "x",
+    "ㇷ": "x",
+    "ㇸ": "x",
+    "ㇹ": "x",
+    "ァ": "a",
+    "ェ": "e",
+    "ォ": "o",
+    "ㇻ": "r",
+    "ㇼ": "r",
+    "ㇽ": "r",
+    "ㇾ": "r",
+    "ㇿ": "r",
+    "ィ": "y",
+    "ㇷ": "h",
+    "ㇱ": "s",
+    "ㇺ": "m",
+    "ㇴ": "n",
+}
+
+KANA_2_LATN_DIAGRAPH = {
+    "イェ": "ye",
+    "ウェ": "we",
+    "ウィ": "wi",
+    "ウォ": "wo",
+    "トゥ": "tu",
+    "ㇷ゚": "p",
+    "ト゚": "tu",
+    "チャ": "ca",
+    "チュ": "cu",
+    "チェ": "ce",
+    "チョ": "co",
+}
+
+
+def kana2latn(kana: str) -> str:
+    """Converts Katakana script to Latin script."""
+    result = []
+    chars = peekable(kana)
+
+    for current_char in chars:
+        latn = None
+        next_char = chars.peek() if chars else None
+
+        # print(current_char, next_char)
+
+        if (
+            current_char
+            and next_char
+            and current_char + next_char in KANA_2_LATN_DIAGRAPH
+        ):
+            # print(f"diagraph: {current_char + next_char = }")
+            next(chars)
+            latn = KANA_2_LATN_DIAGRAPH.get(current_char + next_char)
+        else:
+            latn = KANA_2_LATN.get(current_char)
+
+        # print(f"{latn = }")
+        result.append(latn if latn is not None else current_char)
+
+    return "".join(result)
+
+
+def latn2kana(
+    text: str,
+    use_wi: bool = False,
+    use_we: bool = False,
+    use_wo: bool = False,
+) -> str:
+    """Converts Latin script to Katakana script.
+
+    Args:
+        text (str): The Latin script to be converted.
+        use_wi (bool): Whether to use "ヰ" (wi) instead of "ウィ" (wi).
+        use_we (bool): Whether to use "ヱ" (we) instead of "ウェ" (we).
+        use_wo (bool): Whether to use "ヲ" (wo) instead of "ウォ" (wo).
+
+    Returns:
+        str: The converted Katakana script.
+    """
+    syllables = separate(text)
+    # ic(syllables)
+
+    result = ""
+
+    for syllable in syllables:
+        syllable = clean(syllable.lower())
+
+        if not any(char.isalpha() for char in syllable):
+            result += syllable
+            continue
+
+        for accented, vowel in ACCENTED_VOWELS.items():
+            syllable = syllable.replace(accented, vowel)
+
+        if len(syllable) == 0:
+            continue
+
+        last_char = syllable[-1]
+
+        if last_char in CONSONANTS:
+            (remains, coda) = syllable[:-1], last_char
+        else:
+            (remains, coda) = syllable, ""
+
+        converted_remains = {
+            "a": "ア",
+            "i": "イ",
+            "u": "ウ",
+            "e": "エ",
+            "o": "オ",
+            "'a": "ア",
+            "'i": "イ",
+            "'u": "ウ",
+            "'e": "エ",
+            "'o": "オ",
+            "’a": "ア",
+            "’i": "イ",
+            "’u": "ウ",
+            "’e": "エ",
+            "’o": "オ",
+            "ka": "カ",
+            "ki": "キ",
+            "ku": "ク",
+            "ke": "ケ",
+            "ko": "コ",
+            "sa": "サ",
+            "si": "シ",
+            "su": "ス",
+            "se": "セ",
+            "so": "ソ",
+            "ta": "タ",
+            "tu": "ト゚",
+            "te": "テ",
+            "to": "ト",
+            "ca": "チャ",
+            "ci": "チ",
+            "cu": "チュ",
+            "ce": "チェ",
+            "co": "チョ",
+            "na": "ナ",
+            "ni": "ニ",
+            "nu": "ヌ",
+            "ne": "ネ",
+            "no": "ノ",
+            "ha": "ハ",
+            "hi": "ヒ",
+            "hu": "フ",
+            "he": "ヘ",
+            "ho": "ホ",
+            "pa": "パ",
+            "pi": "ピ",
+            "pu": "プ",
+            "pe": "ペ",
+            "po": "ポ",
+            "ma": "マ",
+            "mi": "ミ",
+            "mu": "ム",
+            "me": "メ",
+            "mo": "モ",
+            "ya": "ヤ",
+            "yi": "イ",
+            "yu": "ユ",
+            "ye": "イェ",
+            "yo": "ヨ",
+            "ra": "ラ",
+            "ri": "リ",
+            "ru": "ル",
+            "re": "レ",
+            "ro": "ロ",
+            "wa": "ワ",
+            "wi": "ヰ",
+            "we": "ヱ",
+            "wo": "ヲ",
+            "nn": "ン",
+            "tt": "ッ",
+        }.get(remains)
+        # ic(converted_remains)
+
+        result += converted_remains or ""
+
+        nucleus = remains[-1] if remains else None
+
+        converted_coda = {
+            "w": "ゥ",
+            "y": "ィ",
+            "m": "ㇺ",
+            "n": "ㇴ",
+            "s": "ㇱ",
+            "p": "ㇷ゚",
+            "t": "ッ",
+            "T": "ㇳ",
+            "k": "ㇰ",
+            "r": {
+                "a": "ㇻ",
+                "i": "ㇼ",
+                "u": "ㇽ",
+                "e": "ㇾ",
+                "o": "ㇿ",
+            }.get(nucleus or "", "ㇽ"),
+            "h": {
+                "a": "ㇵ",
+                "i": "ㇶ",
+                "u": "ㇷ",
+                "e": "ㇸ",
+                "o": "ㇹ",
+            }.get(nucleus or "", "ㇷ"),
+            "x": {
+                "a": "ㇵ",
+                "i": "ㇶ",
+                "u": "ㇷ",
+                "e": "ㇸ",
+                "o": "ㇹ",
+            }.get(nucleus or "", "ㇷ"),
+        }.get(coda, coda)
+
+        result += converted_coda
+
+        # ic(result)
+
+    result = result.replace("ィ", "イ").replace("ゥ", "ウ").replace("ㇴ", "ン")
+
+    # Replace wi, we, wo
+    if not use_wi:
+        result = result.replace("ヰ", "ウィ")
+    if not use_we:
+        result = result.replace("ヱ", "ウェ")
+    if not use_wo:
+        result = result.replace("ヲ", "ウォ")
+
+    # ic(result)
+
+    return result.replace("’", "")
