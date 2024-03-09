840d09e65b0d473682ead7688827613d187991b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 03:03:46 +0000

    Add package lnschema_lamin1/0.26.0/lnschema_lamin1-0.26.0-py2.py3-none-any.whl

---
+++ packages/lnschema_lamin1/lnschema_lamin1-0.26.0-py2.py3-none-any.whl/lnschema_lamin1/__init__.py
@@ -0,0 +1,44 @@
+"""Lamin's `lamin1` exemplary lab schema.
+
+Import the package::
+
+   import lnschema_lamin1
+
+ORMs:
+
+.. autosummary::
+   :toctree: .
+
+   Experiment
+   ExperimentType
+   Biosample
+   Techsample
+   Treatment
+   TreatmentTarget
+   Well
+
+"""
+
+__version__ = "0.26.0"
+
+from lamindb_setup import _check_instance_setup
+
+
+# trigger instance loading if users
+# want to access attributes
+def __getattr__(name):
+    if name not in {"models"}:
+        _check_instance_setup(from_lamindb=True)
+    return globals()[name]
+
+
+if _check_instance_setup():
+    from .models import (  # noqa
+        Biosample,
+        Experiment,
+        ExperimentType,
+        Techsample,
+        Treatment,
+        TreatmentTarget,
+        Well,
+    )
