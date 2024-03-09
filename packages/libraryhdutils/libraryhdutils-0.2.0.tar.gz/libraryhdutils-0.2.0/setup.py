88afede95761698a6feff76ae4e4ebb4c5bc903d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 15:03:50 +0000

    Add package libraryhdutils/0.2.0/libraryhdutils-0.2.0.tar.gz

---
+++ packages/libraryhdutils/libraryhdutils-0.2.0.tar.gz/libraryhdutils-0.2.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+setup(
+name='libraryhdutils',
+version='0.2.0',
+author='Axel',
+author_email='axel.biegalski@edu.esiee.fr',
+description='python package,function available: hackday_flag() (import craftflag from the library)' ,
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
+
