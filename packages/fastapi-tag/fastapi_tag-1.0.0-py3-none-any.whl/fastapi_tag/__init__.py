e69dbdffd62cd803cf4248aa35bc491265a10a97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 15:04:30 +0000

    Add package fastapi-tag/1.0.0/fastapi_tag-1.0.0-py3-none-any.whl

---
+++ packages/fastapi-tag/fastapi_tag-1.0.0-py3-none-any.whl/fastapi_tag/__init__.py
@@ -0,0 +1,16 @@
+"""
+FastAPI Tag is a Module for fastapi help to ensure the Metadata of the API.
+"""
+
+__version__ = "1.0.0"
+
+from fastapi_tag import api, application, metadata, model, routers, type
+
+__all__ = [
+    "api",
+    "application",
+    "metadata",
+    "routers",
+    "type",
+    "model",
+]
