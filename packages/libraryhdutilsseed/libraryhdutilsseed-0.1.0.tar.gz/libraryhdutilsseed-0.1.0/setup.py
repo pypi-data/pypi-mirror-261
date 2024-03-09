d2c96dffc44f31ee512e0de012ec8990bdfe23ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 15:03:51 +0000

    Add package libraryhdutilsseed/0.1.0/libraryhdutilsseed-0.1.0.tar.gz

---
+++ packages/libraryhdutilsseed/libraryhdutilsseed-0.1.0.tar.gz/libraryhdutilsseed-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+setup(
+name='libraryhdutilsseed',
+version='0.1.0',
+author='Axel',
+author_email='axel.biegalski@edu.esiee.fr',
+description='python package,function available: getseed() (import giveseed from the library)' ,
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
+
