b14b7507c6b21d278a61ab72094c5d787cf70e54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 06:03:11 +0000

    Add package maligner/0.0.0/maligner-0.0.0.tar.gz

---
+++ packages/maligner/maligner-0.0.0.tar.gz/maligner-0.0.0/maligner/translations/en.ts
@@ -0,0 +1,159 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.py" line="64"/>
+        <source>File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="65"/>
+        <source>Tools</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="66"/>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="104"/>
+        <source>File opened</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="123"/>
+        <source>File saved</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="128"/>
+        <location filename="../mainwindow.py" line="158"/>
+        <source>No molecule selected</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="136"/>
+        <source>Molecules removed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="150"/>
+        <source>About maligner</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="153"/>
+        <source>maligner is an Open-Source Molecular Alignment Tool.
+
+
+Based on RDKit: http://www.rdkit.org/
+Based on rdeditor: https://github.com/EBjerrum/rdeditor
+Some icons from: http://icons8.com
+Source code: https://github.com/hellmrf/maligner
+
+Released under GPL-v3.0.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="185"/>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="188"/>
+        <source>Open an existing file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="192"/>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="195"/>
+        <source>Save file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="199"/>
+        <source>Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="202"/>
+        <source>Exit the Application</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="206"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="208"/>
+        <source>Displays info about text editor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="211"/>
+        <source>About Qt</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="213"/>
+        <source>Show the Qt library&apos;s About box</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="219"/>
+        <source>Delete</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="222"/>
+        <source>Remove this molecule from canvas Ctrl+X</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="228"/>
+        <source>Anchor current molecule</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="231"/>
+        <source>Set the selected molecule as the anchor for the alignment. (A)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="245"/>
+        <source>Compute MCS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="249"/>
+        <source>Computes and select the Maximum Common Substructure for the loaded molecules.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="254"/>
+        <source>Run Alignment</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="257"/>
+        <source>Align all the molecules.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MolGridViewWidget</name>
+    <message>
+        <location filename="../widgets/MolGridView.py" line="81"/>
+        <source>Choose molecules</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
