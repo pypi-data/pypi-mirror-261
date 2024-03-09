ae5de00d94551d3e2755522fc0bf6c1a87e7564c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-09 04:04:33 +0000

    Add package jgtpy/0.4.9/jgtpy-0.4.9-py3-none-any.whl

---
+++ packages/jgtpy/jgtpy-0.4.9-py3-none-any.whl/jgtpy/JGTBaseRequest.py
@@ -0,0 +1,7 @@
+class JGTBaseRequest:
+    def __init__(self, quiet=True, verbose_level=0):
+        self.quiet = quiet
+        self.verbose_level = verbose_level
+
+    # def __str__(self) -> str:
+    #     pass
