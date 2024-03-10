b7a6951908ffbf5e1cbc0cab287518a4b9d13e35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-10 05:03:16 +0000

    Add package music-checker-micro/0.2.7/music_checker_micro-0.2.7.tar.gz

---
+++ packages/music-checker-micro/music_checker_micro-0.2.7.tar.gz/music_checker_micro-0.2.7/src/music_checker_micro/__init__.py
@@ -0,0 +1,10 @@
+"""
+MusicChecker is a class aimed at scanning a series of media files, 
+extracting tag information from them for later reporting
+"""
+
+VERSION = (0, 2, 7)
+"""Version tuple."""
+
+VERSION_STRING = ".".join(map(str, VERSION))
+"""Version string."""
