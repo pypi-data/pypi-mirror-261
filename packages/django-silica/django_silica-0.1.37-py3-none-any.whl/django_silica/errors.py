f2aad12820b55cedb1d6a0ce715119defeb7919f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-10 14:03:38 +0000

    Add package django-silica/0.1.37/django_silica-0.1.37-py3-none-any.whl

---
+++ packages/django-silica/django_silica-0.1.37-py3-none-any.whl/django_silica/errors.py
@@ -0,0 +1,12 @@
+class SilicaCacheError(Exception):
+    pass
+
+class PropertyDoesNotExist(Exception):
+    pass
+
+class ComponentNotValid(Exception):
+    pass
+
+
+class SingleRootNodeError(Exception):
+    pass
