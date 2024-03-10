2dca37fe66d974749613095e90587da9aecd416b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-10 04:04:05 +0000

    Add package gtsam-stubs/4.2/gtsam-stubs-4.2.tar.gz

---
+++ packages/gtsam-stubs/gtsam-stubs-4.2.tar.gz/gtsam-stubs-4.2/README.md
@@ -0,0 +1,17 @@
+# `gtsam-stubs`
+
+PEP 561 style type annotations for GTSAM.
+
+Note this package may have bugs (wrong annotations), and some return types should not be `Any` but annotated with the actual return types.
+
+If you encounter such types, please help by replacing them with the actual type (like `Union[Isotropic, Unit]`), and make a PR to this repo.
+
+# Install
+
+`pip install gtsam-stubs`
+
+Or, run `setup.py` after cloning this repo.
+
+# LICENSE
+
+BSD 3-Clause
---
+++ packages/gtsam-stubs/gtsam-stubs-4.2.tar.gz/gtsam-stubs-4.2/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+import os
+from os import path
+
+
+def find_stubs(package):
+    stubs = []
+    for root, dirs, files in os.walk(package):
+        for file in files:
+            path = os.path.join(root, file).replace(package + os.sep, "", 1)
+            stubs.append(path)
+    return {package: stubs}
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="gtsam-stubs",
+    maintainer="Fan Jiang",
+    maintainer_email="gtsam@example.com",
+    description="PEP 561 type stubs for GTSAM",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="http://www.gtsam.org",
+    license="BSD",
+    version="4.2",
+    packages=["gtsam-stubs"],
+    # PEP 561 requires these
+    install_requires=[
+        "gtsam>=4.2",
+        'typing_extensions>=3.7.4; python_version>="3.8"',
+    ],
+    package_data=find_stubs("gtsam-stubs"),
+    zip_safe=False,
+)
