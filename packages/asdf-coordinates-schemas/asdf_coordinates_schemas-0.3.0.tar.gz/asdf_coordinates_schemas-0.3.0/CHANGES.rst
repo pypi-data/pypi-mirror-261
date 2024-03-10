0e17c315913994a4d91dc3fd0fb645d49129e137
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-10 03:05:15 +0000

    Add package asdf-coordinates-schemas/0.3.0/asdf_coordinates_schemas-0.3.0.tar.gz

---
+++ packages/asdf-coordinates-schemas/asdf_coordinates_schemas-0.3.0.tar.gz/asdf_coordinates_schemas-0.3.0/CHANGES.rst
@@ -0,0 +1,23 @@
+0.4.0 (unreleased)
+------------------
+
+-
+
+0.3.0 (2024-03-09)
+------------------
+
+- Add ``asdf-astropy`` as a test dependency. [#34]
+- Drop official support for Python 3.8 [#42]
+- Update schemas for ASDF standard 1.6.0 [#55]
+
+0.2.0 (2023-03-21)
+------------------
+
+- Add documentation to package. [#8]
+- Update asdf version and require python 3.8. [#14]
+- Switch to pyproject.toml. [#15]
+
+0.1.0 (2021-12-04)
+------------------
+
+- Initial release.
---
+++ packages/asdf-coordinates-schemas/asdf_coordinates_schemas-0.3.0.tar.gz/asdf_coordinates_schemas-0.3.0/docs/conf.py
@@ -0,0 +1,128 @@
+import datetime
+import os
+import sys
+from pathlib import Path
+
+# Ensure documentation examples are determinstically random.
+import numpy
+import tomli
+from pkg_resources import get_distribution
+
+try:
+    numpy.random.seed(int(os.environ["SOURCE_DATE_EPOCH"]))
+except KeyError:
+    pass
+
+try:
+    from sphinx_astropy.conf.v1 import *  # noqa
+except ImportError:
+    print("ERROR: the documentation requires the sphinx-astropy package to be installed")
+    sys.exit(1)
+
+# Get configuration information from `pyproject.toml`
+with open(Path(__file__).parent.parent / "pyproject.toml", "rb") as configuration_file:
+    conf = tomli.load(configuration_file)
+configuration = conf["project"]
+
+# -- General configuration ----------------------------------------------------
+
+project = configuration["name"]
+author = f"{configuration['authors'][0]['name']} <{configuration['authors'][0]['email']}>"
+copyright = f"{datetime.datetime.now().year}, {configuration['authors'][0]}"
+
+release = get_distribution(configuration["name"]).version
+version = ".".join(release.split(".")[:2])
+
+# If your documentation needs a minimal Sphinx version, state it here.
+# needs_sphinx = '1.2'
+
+intersphinx_mapping["pypa-packaging"] = ("https://packaging.python.org/en/latest/", None)  # noqa
+intersphinx_mapping["asdf"] = ("https://asdf.readthedocs.io/en/latest/", None)  # noqa
+intersphinx_mapping["asdf-standard"] = ("https://asdf-standard.readthedocs.io/en/latest/", None)  # noqa
+intersphinx_mapping["asdf-astropy"] = ("https://asdf-astropy.readthedocs.io/en/latest/", None)  # noqa
+intersphinx_mapping["pytest"] = ("https://docs.pytest.org/en/latest/", None)  # noqa
+
+# To perform a Sphinx version check that needs to be more specific than
+# major.minor, call `check_sphinx_version("x.y.z")` here.
+# check_sphinx_version("1.2.1")
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+exclude_patterns.append("_templates")  # noqa
+
+# This is added to the end of RST files - a good place to put substitutions to
+# be used globally.
+rst_epilog += """"""  # noqa
+
+
+# -- Options for HTML output ---------------------------------------------------
+
+# A NOTE ON HTML THEMES
+# The global astropy configuration uses a custom theme, 'bootstrap-astropy',
+# which is installed along with astropy. A different theme can be used or
+# the options for this theme can be modified by overriding some of the
+# variables set in the global configuration. The variables set in the
+# global configuration are listed below, commented out.
+
+# Add any paths that contain custom themes here, relative to this directory.
+# To use a different custom theme, add the directory containing the theme.
+# html_theme_path = []
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes. To override the custom theme, set this to the
+# name of a builtin theme or the name of a custom theme in html_theme_path.
+html_theme = "sphinx_rtd_theme"
+html_theme_options = {}
+
+html_static_path = ["_static"]
+
+# Custom sidebar templates, maps document names to template names.
+# html_sidebars = {}
+
+# The name of an image file (within the static path) to use as favicon of the
+# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
+# pixels large.
+html_favicon = "_static/logo.ico"
+html_logo = "_static/logo.png"
+
+# If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
+# using the given strftime format.
+# html_last_updated_fmt = ''
+
+# The name for this set of Sphinx documents.  If None, it defaults to
+# "<project> v<release> documentation".
+html_title = f"{project} v{release}"
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = project + "doc"
+
+
+# -- Options for LaTeX output --------------------------------------------------
+
+# Grouping the document tree into LaTeX files. List of tuples
+# (source start file, target name, title, author, documentclass [howto/manual]).
+latex_documents = [("index", project + ".tex", project + " Documentation", author, "manual")]
+
+latex_logo = "_static/logo.pdf"
+
+
+# -- Options for manual page output --------------------------------------------
+
+# One entry per manual page. List of tuples
+# (source start file, name, description, authors, manual section).
+man_pages = [("index", project.lower(), project + " Documentation", [author], 1)]
+
+sys.path.insert(0, os.path.join(os.path.abspath(os.path.dirname("__file__")), "sphinxext"))
+extensions += ["sphinx_asdf"]  # noqa
+
+
+def setup(app):
+    app.add_css_file("custom.css")
+
+
+# -- sphinx_asdf configuration ---------------------------------------------
+
+# Top-level directory containing ASDF schemas (relative to current directory)
+asdf_schema_path = "../resources"
+# This is the prefix common to all schema IDs in this repository
+asdf_schema_standard_prefix = "schemas"
