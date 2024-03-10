f12d5964ae52578cc0f82851d32fd4436222ae76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-10 15:03:26 +0000

    Add package highsbox/1.7.0/highsbox-1.7.0-py3-none-macosx_11_0_arm64.whl

---
+++ packages/highsbox/highsbox-1.7.0-py3-none-macosx_11_0_arm64.whl/highsbox-1.7.0.data/purelib/highsbox/highs_dist/include/highs/HConfig.h
@@ -0,0 +1,22 @@
+#ifndef HCONFIG_H_
+#define HCONFIG_H_
+
+#define FAST_BUILD
+#define ZLIB_FOUND
+/* #undef CUPDLP_CPU */
+#define CMAKE_BUILD_TYPE "Release"
+#define CMAKE_INSTALL_PREFIX "/Users/runner/work/highsbox/highsbox/HiGHS/highs_dist"
+/* #undef HIGHSINT64 */
+/* #undef HIGHS_NO_DEFAULT_THREADS */
+/* #undef HIGHS_HAVE_MM_PAUSE */
+#define HIGHS_HAVE_BUILTIN_CLZ
+/* #undef HIGHS_HAVE_BITSCAN_REVERSE */
+
+#define HIGHS_GITHASH "50670fd"
+#define HIGHS_COMPILATION_DATE "2024-03-10"
+#define HIGHS_VERSION_MAJOR 1
+#define HIGHS_VERSION_MINOR 7
+#define HIGHS_VERSION_PATCH 0
+#define HIGHS_DIR "/Users/runner/work/highsbox/highsbox/HiGHS"
+
+#endif /* HCONFIG_H_ */
