4b188d0f7daa6ee36967b798f22506febc20d43d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-10 04:04:05 +0000

    Add package gbpcli/2.2.0/gbpcli-2.2.0.tar.gz

---
+++ packages/gbpcli/gbpcli-2.2.0.tar.gz/gbpcli-2.2.0/src/gbpcli/subcommands/build.py
@@ -0,0 +1,32 @@
+"""Schedule a build for the given machine in CI/CD"""
+
+import argparse
+
+from gbpcli import GBP
+from gbpcli.subcommands import completers as comp
+from gbpcli.types import Console
+
+HELP = "Schedule a build for the given machine in CI/CD"
+
+
+def handler(args: argparse.Namespace, gbp: GBP, _console: Console) -> int:
+    """Schedule a build for the given machine in CI/CD"""
+    params = {}
+    for param in args.param or []:
+        key, value = param.split("=", 1)
+        params[key] = value
+
+    gbp.build(args.machine, **params)
+
+    return 0
+
+
+def parse_args(parser: argparse.ArgumentParser) -> None:
+    """Set subcommand arguments"""
+    parser.add_argument(
+        "--param", "-p", action="append", help="Build parameter (name=value)"
+    )
+    comp.set(
+        parser.add_argument("machine", metavar="MACHINE", help="name of the machine"),
+        comp.machines,
+    )
---
+++ packages/gbpcli/gbpcli-2.2.0.tar.gz/gbpcli-2.2.0/tests/test_build.py
@@ -0,0 +1,43 @@
+"""Tests for the build subcommand"""
+
+# pylint: disable=missing-function-docstring,protected-access
+from argparse import Namespace
+
+from gbpcli.subcommands.build import handler as build
+
+from . import TestCase
+
+
+class MachinesTestCase(TestCase):
+    """machines() tests"""
+
+    def test(self) -> None:
+        args = Namespace(machine="babette", param=None)
+        self.make_response("schedule_build.json")
+
+        status = build(args, self.gbp, self.console)
+
+        self.assertEqual(status, 0)
+        self.assertEqual(self.console.out.getvalue(), "")
+        self.assert_graphql(
+            self.gbp.query.gbpcli.schedule_build, machine="babette", params=[]
+        )
+
+    def test_with_build_params(self) -> None:
+        args = Namespace(machine="babette", param=["BUILD_TARGET=emptytree"])
+        self.make_response("schedule_build.json")
+
+        status = build(args, self.gbp, self.console)
+
+        self.assertEqual(status, 0)
+        self.assertEqual(self.console.out.getvalue(), "")
+        self.assert_graphql(
+            self.gbp.query.gbpcli.schedule_build,
+            machine="babette",
+            params=[{"name": "BUILD_TARGET", "value": "emptytree"}],
+        )
+        self.assert_graphql(
+            self.gbp.query.gbpcli.schedule_build,
+            machine="babette",
+            params=[{"name": "BUILD_TARGET", "value": "emptytree"}],
+        )
