e8f0c13fea92425b23d8ee81f11b1a03b4427c05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-10 03:04:46 +0000

    Add package retailterms/0.0.9/retailterms-0.0.9.tar.gz

---
+++ packages/retailterms/retailterms-0.0.9.tar.gz/retailterms-0.0.9/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+VERSION = '0.0.9'
+DESCRIPTION = 'NLP Library to find Retail terms'
+LONG_DESCRIPTION = 'This library helps to find Retail Terms that are there in unstructured text. Developers can build on top of it.'
+
+setup(
+    name="retailterms",
+    version=VERSION,
+    author="Marcel Tino",
+    author_email="<marceltino92@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    install_requires=['re','pandas','string','warning','spacy'],
+    keywords=['retail','retailterms'])
