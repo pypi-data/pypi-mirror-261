690f3598bfcf87657b8db7d09812d0c4a1b0c775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-10 03:04:19 +0000

    Add package komodo-sdk/0.0.18/komodo_sdk-0.0.18-py3-none-any.whl

---
+++ packages/komodo-sdk/komodo_sdk-0.0.18-py3-none-any.whl/komodo/framework/komodo_app.py
@@ -0,0 +1,114 @@
+import json
+
+from komodo.framework.komodo_agent import KomodoAgent, data_agent
+from komodo.framework.komodo_datasource import KomodoDataSource
+from komodo.framework.komodo_tool import KomodoTool
+from komodo.framework.komodo_tool_registry import KomodoToolRegistry
+from komodo.framework.komodo_vectorstore import KomodoVectorStore
+
+
+class KomodoApp:
+    def __init__(self, shortcode, name, purpose, agents=None, tools=None, sources=None):
+        self.shortcode = shortcode
+        self.name = name
+        self.purpose = purpose
+        self.agents: [KomodoAgent] = []
+        self.tools: [KomodoTool] = []
+        self.data_sources: [KomodoDataSource] = sources or []
+        self.vector_stores: [KomodoVectorStore] = []
+        self.tools.append(self.search_data_tool())
+        self.agents.append(data_agent(self.search_data_tool()))
+        for agent in agents or []:
+            self.add_agent(agent)
+        for tool in tools or []:
+            self.add_tool(tool)
+
+    def add_agent(self, agent):
+        self.agents += [agent]
+
+    def add_tool(self, tool):
+        if isinstance(tool, str):
+            tool = KomodoToolRegistry.get_tool_by_shortcode(tool)
+        elif isinstance(tool, dict):
+            tool = KomodoTool(**tool)
+        if isinstance(tool, KomodoTool):
+            self.tools.append(tool)
+            return
+        raise ValueError(f"Invalid tool: {tool}")
+
+    def add_data_source(self, ds1):
+        self.data_sources += [ds1]
+
+    def get_data_source(self, source):
+        for a in self.data_sources:
+            if a.id == source or a.name == source:
+                return a
+        return None
+
+    def list_data(self, sources=None, metadata=False):
+        if isinstance(sources, str):
+            sources = sources.split(',')
+        sources = sources or self.data_sources
+
+        dataset = []
+        for source in sources:
+            if isinstance(source, str):
+                source = self.get_data_source(source)
+
+            for item in source.list_items():
+                data = {'source': source.id, 'type': source.type, 'item': item}
+                if metadata:
+                    for doc in source.get_item(item):
+                        data['metadata'] = doc.metadata
+                        print(data)
+                        dataset.append(data)
+                else:
+                    print(data)
+                    dataset.append(data)
+
+        return dataset
+
+    def search_data_definition(self):
+        return {
+            "type": "function",
+            "function": {
+                "name": "appliance_data_search",
+                "description": "Retrieve data from available data sources",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "query": {
+                            "type": "string",
+                            "description": "String to semantic search for in the data sources"
+                        }
+                    },
+                    "required": ["query"]
+                }
+            }
+        }
+
+    def get_data_as_tool_action(self, args):
+        text = args['query']
+        result = []
+        for pc in self.vector_stores:
+            result += pc.search(text, top_k=3)
+
+        if len(result) > 0:
+            return json.dumps(result)
+
+        return "Found: {}. The answer is 42.".format(args['query'])
+
+    def search_data_tool(self):
+        return KomodoTool(shortcode="data_search",
+                          name="Appliance Data Search Tool",
+                          definition=self.search_data_definition(),
+                          action=self.get_data_as_tool_action)
+
+    def add_vector_store(self, store):
+        self.vector_stores += [store]
+
+    def get_vector_store(self, source):
+        for a in self.vector_stores:
+            if a.id == source or a.name == source:
+                return a
+        return None
