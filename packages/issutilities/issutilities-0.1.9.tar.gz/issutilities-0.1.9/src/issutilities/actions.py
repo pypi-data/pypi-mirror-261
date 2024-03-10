0a7ab3999728c0058686cef5cb0b59a11ce431f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-10 15:03:26 +0000

    Add package issutilities/0.1.9/issutilities-0.1.9.tar.gz

---
+++ packages/issutilities/issutilities-0.1.9.tar.gz/issutilities-0.1.9/src/issutilities/actions.py
@@ -0,0 +1,26 @@
+import os, time, asyncio, typing
+from . import colors as colors
+
+
+def clear() -> None:
+    """Clears the console output."""
+    os.system("cls" if os.name == "nt" else "clear")
+    print(colors.RESET, end="")
+
+
+def sleep(x: int | float | None = 0) -> None:
+    """Suspend execution of the calling thread for the given number of seconds."""
+    time.sleep(x)
+
+
+async def sleep_async(
+    delay: int | float | None = 0, result: typing.Any | None = None
+) -> None:
+    """
+    Block for delay seconds.
+
+    If result is provided, it is returned to the caller when the coroutine completes.
+
+    `sleep()` always suspends the current task, allowing other tasks to run.
+    """
+    return await asyncio.sleep(delay, result)
