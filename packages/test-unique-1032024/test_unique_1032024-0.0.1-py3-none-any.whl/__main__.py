08b9280420b7195e27a90ed269a2049109494fa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-10 04:04:04 +0000

    Add package test-unique-1032024/0.0.1/test_unique_1032024-0.0.1-py3-none-any.whl

---
+++ packages/test-unique-1032024/test_unique_1032024-0.0.1-py3-none-any.whl/__main__.py
@@ -0,0 +1,2 @@
+if(__name__ == "__main__"):
+    print("Test project is called as module")
---
+++ packages/test-unique-1032024/test_unique_1032024-0.0.1-py3-none-any.whl/module1.py
@@ -0,0 +1,7 @@
+
+
+def function1():
+    print("function 1 is called")
+
+if(__name__ ==  "__main__"):
+    print("module1 is invoked")
