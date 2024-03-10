b41faf80b2126a8aae9bd5fd4df81c98c016ea3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-10 03:05:01 +0000

    Add package libbids/0.2.0/libbids-0.2.0-cp310-cp310-macosx_14_0_arm64.whl

---
+++ packages/libbids/libbids-0.2.0-cp310-cp310-macosx_14_0_arm64.whl/libbids/utils.py
@@ -0,0 +1,37 @@
+try:
+    from PyQt6.QtWidgets import QMessageBox, QPushButton  # type: ignore
+except ModuleNotFoundError:
+    from PyQt5.QtWidgets import QMessageBox, QPushButton  # type: ignore
+
+
+def qprompt(msg: str, yes_button_text: str = "Yes", no_button_text: str = "No"):
+    """Display a QMessageBox
+
+    Parameters
+    ----------
+    msg : str
+        The message to display
+    yes_button_text : str
+        The text for the yes button
+    no_button_text : no
+        The text for the no button
+
+    Return
+    ------
+    bool
+        Whether the yes button was clicked
+    """
+    # Adjust messagebox class for PyQt6
+    if not hasattr(QMessageBox, "YesRole"):
+        for role in QMessageBox.ButtonRole:
+            setattr(QMessageBox, role.name, role)
+            setattr(QMessageBox, "exec_", QMessageBox.exec)
+
+    msgbox: QMessageBox = QMessageBox()
+    msgbox.setText(msg)
+    yes_button: QPushButton = msgbox.addButton(
+        yes_button_text, QMessageBox.YesRole  # type: ignore
+    )
+    msgbox.addButton(no_button_text, QMessageBox.NoRole)  # type: ignore
+    QMessageBox.exec_(msgbox)  # type: ignore
+    return msgbox.clickedButton() == yes_button
