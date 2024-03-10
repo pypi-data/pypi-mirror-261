0096bce371a2ff88861613a9ba4c19b36e820669
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-10 04:04:15 +0000

    Add package OpenChatGPT/0.1/OpenChatGPT-0.1.tar.gz

---
+++ packages/OpenChatGPT/OpenChatGPT-0.1.tar.gz/OpenChatGPT-0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='OpenChatGPT',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'uuid',
+    ],
+    author='Amir Agassi',
+    author_email='amiragassi04@gmail.com',
+    description='A Python package for integrating with the internal OpenAI Chat API.',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    python_requires='>=3.7',
+)
