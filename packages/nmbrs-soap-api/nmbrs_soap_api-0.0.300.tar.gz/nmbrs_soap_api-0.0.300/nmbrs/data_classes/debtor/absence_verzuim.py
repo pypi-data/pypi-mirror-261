c8c34e98bd8e212efc461ed37627ed15a69461d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-10 04:04:06 +0000

    Add package nmbrs-soap-api/0.0.300/nmbrs_soap_api-0.0.300.tar.gz

---
+++ packages/nmbrs-soap-api/nmbrs_soap_api-0.0.300.tar.gz/nmbrs_soap_api-0.0.300/nmbrs/data_classes/debtor/absence_verzuim.py
@@ -0,0 +1,35 @@
+from nmbrs.data_classes.data_class import DataClass
+from nmbrs.data_classes.utils.xml import parse_xml_to_dict
+
+
+class AbsenceVerzuim(DataClass):
+    """
+    A class representing Absence Verzuim data.
+    """
+
+    def __init__(self, obj: dict) -> None:
+        """
+        Constructor method for AbsenceVerzuim class.
+
+        Initializes instance variables with default values from the provided dictionary.
+
+        Args:
+            obj (dict): A dictionary containing Absence Verzuim data.
+        """
+        self.debtor_id: int | None = obj.get("DebtorID", None)
+        self.company_id: int | None = obj.get("CompanyID", None)
+        self.employee_id: int | None = obj.get("EmployeeID", None)
+        self.xml: str | None = obj.get("XML", None)
+
+    def to_dict(self) -> dict:
+        """
+        Convert the instance to a dictionary.
+
+        :return: A dictionary representation of the Absence Verzuim data.
+        """
+        return {
+            "debtor_id": self.debtor_id,
+            "company_id": self.company_id,
+            "employee_id": self.employee_id,
+            "xml": parse_xml_to_dict(self.xml),
+        }
