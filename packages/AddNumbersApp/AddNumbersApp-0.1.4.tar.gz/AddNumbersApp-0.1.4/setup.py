13c067c2eb60bdc08da9cf6cc699640ec7b31f9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-10 14:04:02 +0000

    Add package AddNumbersApp/0.1.4/AddNumbersApp-0.1.4.tar.gz

---
+++ packages/AddNumbersApp/AddNumbersApp-0.1.4.tar.gz/AddNumbersApp-0.1.4/setup.py
@@ -0,0 +1,18 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='AddNumbersApp',
+    version='0.1.4',  # Increment the version
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Flask',
+    ],
+    entry_points={
+        'console_scripts': [
+            'add-numbers=addnumbersapp.app:main',  # Adjusted to new package name
+        ],
+    },
+)
