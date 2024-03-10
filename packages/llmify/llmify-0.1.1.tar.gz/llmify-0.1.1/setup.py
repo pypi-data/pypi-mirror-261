f9eee0d7dfe52791b5c35dab59b4063a7aa273bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-10 15:03:21 +0000

    Add package llmify/0.1.1/llmify-0.1.1.tar.gz

---
+++ packages/llmify/llmify-0.1.1.tar.gz/llmify-0.1.1/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python\r\n
+import setuptools
+from setuptools import setup
+setup(name='llmify',
+version='0.1.1',
+description='light weight llm utilities for scalable AI apps',
+url='https://github.com/Navan0/llmify',
+author='Navaneeth KT',
+author_email='nktclt@gmail.com',
+long_description=""" light weight llm utilities for scalable AI apps""",
+long_description_content_type='text/markdown',
+packages=setuptools.find_packages(),
+classifiers=[
+    "License :: Free for non-commercial use",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.10",
+    "Development Status :: 1 - Planning",
+],
+zip_safe=False)
