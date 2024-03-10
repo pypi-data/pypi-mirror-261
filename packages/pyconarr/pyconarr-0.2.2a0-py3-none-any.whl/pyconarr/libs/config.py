d96c77669dbe0d70a3d10e7d40f15a622872884f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-10 03:04:16 +0000

    Add package pyconarr/0.2.2a0/pyconarr-0.2.2a0-py3-none-any.whl

---
+++ packages/pyconarr/pyconarr-0.2.2a0-py3-none-any.whl/pyconarr/libs/config.py
@@ -0,0 +1,20 @@
+import os
+from importlib import metadata
+from pathlib import Path
+
+import toml
+import yaml
+
+
+def get_version():
+    try:
+        my_version = metadata.version("conarr-backend")
+    except metadata.PackageNotFoundError:
+        my_version = toml.load(Path(os.getcwd()) / "pyproject.toml")["tool"]["poetry"][
+            "version"
+        ]
+    return my_version
+
+
+with open("config/pyconarr.yml", "r") as f:
+    config = yaml.safe_load(f)
