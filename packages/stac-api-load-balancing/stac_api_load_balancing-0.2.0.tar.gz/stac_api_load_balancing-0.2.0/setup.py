623b51785687bc2b85aa0d8f454c702ae74b773d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-10 03:05:07 +0000

    Add package stac-api-load-balancing/0.2.0/stac_api_load_balancing-0.2.0.tar.gz

---
+++ packages/stac-api-load-balancing/stac_api_load_balancing-0.2.0.tar.gz/stac_api_load_balancing-0.2.0/setup.py
@@ -0,0 +1,35 @@
+"""stac-api-load-balancing setup.py."""
+from setuptools import setup
+
+__version__ = "0.2.0"
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="stac_api_load_balancing",
+    version=__version__,
+    description="Load balancing for stac compliant apis",
+    url="https://github.com/Healy-Hyperspatial/stac-api-load-balancing",
+    include_package_data=True,
+    install_requires=[
+        "click>=7.1.2",
+        "locust",
+        "setuptools",
+        "Cython",
+        "bzt",
+    ],
+    packages=["stac_api_load_balancing"],
+    entry_points={
+        "console_scripts": ["stac-api-load-balancing=stac_api_load_balancing.cli:main"]
+    },
+    author="Jonathan Healy",
+    author_email="jonathan.d.healy@gmail.com",
+    license="MIT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.7",
+    extras_require={
+        "test": ["pytest"],
+    },
+)
