a948362b19e38c587ee5615ee0e193d8aa40a1a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-10 03:04:22 +0000

    Add package flect/0.1.6/flect-0.1.6-py3-none-any.whl

---
+++ packages/flect/flect-0.1.6-py3-none-any.whl/flect/actions.py
@@ -0,0 +1,25 @@
+from typing import Annotated, Literal, Optional
+
+from pydantic import AliasGenerator, BaseModel, ConfigDict, Field
+from pydantic.alias_generators import to_camel
+
+
+class Notify(BaseModel):
+    model_config = ConfigDict(extra="forbid", alias_generator=AliasGenerator(serialization_alias=to_camel))
+
+    action_type: Literal["notify"] = "notify"
+
+    title: str = Field(..., description="The title of the notification.")
+    description: Optional[str] = Field(default=None, description="The description of the notification.")
+    position: Optional[
+        Literal["top-left", "top-right", "bottom-left", "bottom-right", "top-center", "bottom-center"]
+    ] = Field(default=None, description="Position of the toast.")
+    type: Optional[Literal["normal", "action", "success", "info", "warning", "error", "loading", "default"]] = Field(
+        default=None, description="Type of the toast."
+    )
+
+
+AnyAction = Annotated[
+    Notify,
+    Field(discriminator="action_type"),
+]
