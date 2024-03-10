8223c51e2a297e81241f3edef4260ad028fee3fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-10 05:03:16 +0000

    Add package search-query/0.10.0/search_query-0.10.0.tar.gz

---
+++ packages/search-query/search_query-0.10.0.tar.gz/search_query-0.10.0/search_query/node.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+"""Node: This module contains the node logic."""
+
+
+class Node:
+    """Node class."""
+
+    def __init__(self, value: str, operator: bool, search_field: str):
+        """init method"""
+        self.value = value
+        self.operator = operator
+        # flag whether the Node is an operator
+        self.children: list[Node] = []
+        # list of children nodes
+        self.marked = False
+        # marked flag for validation, necessary method
+        self.search_field = search_field
+        # search field to which the node (e.g. search term) should be applied
+        if operator:
+            self.search_field = ""
+
+    def print_node(self) -> str:
+        """returns a string with all information to the node"""
+        return f"value: {self.value} operator: {str(self.operator)} search field: {self.search_field}"
---
+++ packages/search-query/search_query-0.10.0.tar.gz/search_query-0.10.0/search_query/tree.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+"""Tree: This module contains the tree logic."""
+from search_query.node import Node
+
+
+class Tree:
+    """Tree class."""
+
+    def __init__(self, root: Node):
+        """init method"""
+        self.root = root
+
+    def remove_all_marks(self) -> None:
+        """removes all marks from all nodes"""
+        self.root.marked = False
+        for child in self.root.children:
+            child.marked = False
