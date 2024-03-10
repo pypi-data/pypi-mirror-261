e2e07f1ff8c4d23ef703531134999c438f9c6f52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-10 03:04:25 +0000

    Add package SOAPpy-py3/0.52.30/SOAPpy-py3-0.52.30.tar.gz

---
+++ packages/SOAPpy-py3/SOAPpy-py3-0.52.30.tar.gz/SOAPpy-py3-0.52.30/src/SOAPpy/version.py
@@ -0,0 +1,6 @@
+try:
+    import pkg_resources
+
+    __version__ = '0.52.30'
+except:
+    __version__ = '0.52.30'
---
+++ packages/SOAPpy-py3/SOAPpy-py3-0.52.30.tar.gz/SOAPpy-py3-0.52.30/tests/ZeroLengthArray.py
@@ -0,0 +1,9 @@
+import sys
+
+sys.path.insert(1, "..")
+from SOAPpy import *
+
+one = typedArrayType(data=[1], typed=type(1))
+tmp = typedArrayType(data=[], typed=type(1))
+print(buildSOAP(one))
+print(buildSOAP(tmp))
