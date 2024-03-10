7d9542df044259b13af83cea2ef82a52929e3997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-10 03:04:46 +0000

    Add package shrillecho/1.0.7/shrillecho-1.0.7.tar.gz

---
+++ packages/shrillecho/shrillecho-1.0.7.tar.gz/shrillecho-1.0.7/shrillecho/auth/oauth.py
@@ -0,0 +1,17 @@
+import spotipy
+from spotipy import SpotifyOAuth
+import os
+
+from spotipy import SpotifyOAuth
+
+def authenticate():
+
+    client_id = str(os.environ.get("SPOTIPY_CLIENT_ID"))
+    client_secret = str(os.environ.get("SPOTIPY_CLIENT_SECRET"))
+    scope = os.environ.get("SCOPE")
+    redirect_uri = os.environ.get("REDIRECT_URI")
+
+    return spotipy.Spotify(auth_manager=SpotifyOAuth(client_id,
+                                                     client_secret,
+                                                     redirect_uri=redirect_uri,
+                                                     scope=scope))
