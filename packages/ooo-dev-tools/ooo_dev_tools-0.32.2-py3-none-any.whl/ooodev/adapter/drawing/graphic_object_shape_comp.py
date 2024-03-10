5cd34b001fb3972d7c7938953460d32d06fe4578
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-10 14:03:37 +0000

    Add package ooo-dev-tools/0.32.2/ooo_dev_tools-0.32.2-py3-none-any.whl

---
+++ packages/ooo-dev-tools/ooo_dev_tools-0.32.2-py3-none-any.whl/ooodev/adapter/drawing/graphic_object_shape_comp.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+from typing import Any, TYPE_CHECKING
+
+from ooodev.adapter.drawing.generic_shape import GenericShapeComp
+
+if TYPE_CHECKING:
+    from com.sun.star.drawing import GraphicObjectShape  # service
+else:
+    GraphicObjectShape = Any
+
+
+class GraphicObjectShapeComp(GenericShapeComp[GraphicObjectShape]):
+    """
+    Class for managing GraphicObjectShape Component.
+    """
+
+    # pylint: disable=unused-argument
+
+    def __init__(self, component: Any) -> None:
+        """
+        Constructor
+
+        Args:
+            component (Any): UNO component that implements ``com.sun.star.drawing.GraphicObjectShape`` service.
+        """
+        GenericShapeComp.__init__(self, component)
+
+    # region Overrides
+    def _ComponentBase__get_supported_service_names(self) -> tuple[str, ...]:
+        """Returns a tuple of supported service names."""
+        return ("com.sun.star.drawing.GraphicObjectShape",)
+
+    # endregion Overrides
