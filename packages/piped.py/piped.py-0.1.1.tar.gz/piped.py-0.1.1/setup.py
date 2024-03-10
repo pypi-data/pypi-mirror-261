43df8cf2244be7a5b8a5e97c29eed573ed917230
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-10 14:04:06 +0000

    Add package piped.py/0.1.1/piped.py-0.1.1.tar.gz

---
+++ packages/piped.py/piped.py-0.1.1.tar.gz/piped.py-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='piped',
+    version="0.1.1",
+    description="API wrapper for Piped written in Python.",
+    author='vex12853',
+    packages=find_packages(),
+    license='MIT'
+)
