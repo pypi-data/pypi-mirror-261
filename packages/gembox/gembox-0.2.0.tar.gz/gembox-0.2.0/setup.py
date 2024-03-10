42e4b299e73992aba67ec740f8a7a794b174d886
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-10 14:04:02 +0000

    Add package gembox/0.2.0/gembox-0.2.0.tar.gz

---
+++ packages/gembox/gembox-0.2.0.tar.gz/gembox-0.2.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+setup(
+    name='gembox',
+    version='0.2.0',
+    packages=find_packages(),
+    install_requires=[
+    ],
+    url='https://github.com/stevieflyer/gembox',
+    author='Steve Flyer',
+    author_email='steveflyer7@gmail.com',
+    description='This is the gembox toolbox for steve flyer. You can also find your hammers here.',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    entry_points={
+        'console_scripts': [
+            'ls-py=gembox.scripts.ls_py:main'
+        ]
+    },
+)
+# To build
+# python setup.py sdist bdist_wheel
+# To upload
+# twine upload dist/*
