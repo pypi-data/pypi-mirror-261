b0258695335deb652967e470aebfa544c5a47366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-10 14:03:35 +0000

    Add package envarify/0.0.2/envarify-0.0.2.tar.gz

---
+++ packages/envarify/envarify-0.0.2.tar.gz/envarify-0.0.2/setup.py
@@ -0,0 +1,35 @@
+"""Handle module packaging."""
+
+import subprocess
+import textwrap
+
+from setuptools import setup
+
+
+def _get_release_version() -> str:
+    """Get git release tag version."""
+    version = (
+        subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE)
+        .stdout.decode("utf-8")
+        .strip()
+    ) or "0.0.0"
+    print("Release version: ", version)
+    return version
+
+
+def _write_version(version: str) -> None:
+    """Write version to version.py."""
+    with open("src/envarify/version.py", "w") as file:
+        file.write(
+            textwrap.dedent(
+                f'''
+                """Host package version, generated on build."""
+                __version__ = "{version}"
+                '''
+            ).lstrip()
+        )
+
+
+version = _get_release_version()
+_write_version(version)
+setup(version=version)
