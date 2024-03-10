6018d05d9d034e1e327f386d819198e2336e7bf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-10 04:04:36 +0000

    Add package tgpy/0.14.1/tgpy-0.14.1-py3-none-any.whl

---
+++ packages/tgpy/tgpy-0.14.1-py3-none-any.whl/tgpy/_core/utils.py
@@ -0,0 +1,19 @@
+import sys
+import tokenize
+import traceback
+from io import BytesIO
+
+from telethon.tl import TLObject
+
+
+def convert_result(result):
+    if isinstance(result, TLObject):
+        result = result.stringify()
+
+    return result
+
+
+def format_traceback():
+    exc_type, exc_value, exc_traceback = sys.exc_info()
+    exc_traceback = exc_traceback.tb_next.tb_next
+    return traceback.format_exception(exc_type, exc_value, exc_traceback)
