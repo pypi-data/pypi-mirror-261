6b64fcadfa45171d19b5a8247753eefad4031f60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-10 14:03:57 +0000

    Add package laraflask/0.6/laraflask-0.6.tar.gz

---
+++ packages/laraflask/laraflask-0.6.tar.gz/laraflask-0.6/setup.py
@@ -0,0 +1,46 @@
+# laraflask/setup.py
+
+from setuptools import setup, find_packages
+
+import glob
+
+# Get a list of all files in the current directory
+all_files = glob.glob('*')
+
+setup(
+    name='laraflask',
+    version='0.6', # don't forget to change the version number
+    packages=find_packages(),
+    data_files=[('laraflask', all_files)], 
+    install_requires=[
+        'flask',
+        'requests',
+    ],
+    long_description=open('Readme.md').read(),
+    long_description_content_type='text/markdown',
+    author='Teguh Rijanandi',
+    author_email='teguhrijanandi02@gmail.com',
+    description='A flask package for Laraflask',
+    license='MIT',
+    keywords='laraflask flask package',
+    url='https://github.com/laraflask/laraflask',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+)
+
+# Build the Distribution Package:
+# $ python setup.py sdist
+# $ python setup.py sdist bdist_wheel
+# $ python -m build
+
+# Upload the Distribution Package to PyPI:
+# $ twine upload dist/*
+
+# Install the Distribution Package Locally:
+# $ pip install dist/laraflask-0.1.tar.gz
+
+# Uninstall the Distribution Package:
+# $ pip uninstall laraflask
