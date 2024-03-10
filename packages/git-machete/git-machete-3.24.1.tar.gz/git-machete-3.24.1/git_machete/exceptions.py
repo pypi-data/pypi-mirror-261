a38bd3763f829d8ecbe2048cd6ea7f5d18e24053
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-10 03:05:03 +0000

    Add package git-machete/3.24.1/git-machete-3.24.1.tar.gz

---
+++ packages/git-machete/git-machete-3.24.1.tar.gz/git-machete-3.24.1/git_machete/exceptions.py
@@ -0,0 +1,48 @@
+from enum import IntEnum
+
+from git_machete import utils
+
+
+class UnderlyingGitException(Exception):
+    def __init__(self, msg: str, apply_fmt: bool = True) -> None:
+        self.msg: str = utils.fmt(msg) if apply_fmt else msg
+
+    def __str__(self) -> str:
+        return str(self.msg)
+
+
+class MacheteException(Exception):
+    def __init__(self, msg: str, apply_fmt: bool = True) -> None:
+        self.msg: str = utils.fmt(msg) if apply_fmt else msg
+
+    def __str__(self) -> str:
+        return str(self.msg)
+
+
+class UnexpectedMacheteException(MacheteException):
+    def __init__(self, msg: str) -> None:
+        super().__init__(f"{msg}\n\nConsider posting an issue at https://github.com/VirtusLab/git-machete/issues/new")
+
+
+class InteractionStopped(Exception):
+    def __init__(self) -> None:
+        pass
+
+
+class UnprocessableEntityHTTPError(MacheteException):
+    """This exception is raised when GitHub API returns HTTP status code 422 - Unprocessable Entity.
+    Such a situation occurs when trying to do something not allowed by GitHub,
+    e.g. assigning someone from outside organization as a reviewer
+    or creating a pull request for a branch that already has a PR.
+    """
+
+    def __init__(self, msg: str) -> None:
+        super().__init__(msg, apply_fmt=False)
+
+
+class ExitCode(IntEnum):
+    SUCCESS = 0
+    MACHETE_EXCEPTION = 1
+    ARGUMENT_ERROR = 2
+    KEYBOARD_INTERRUPT = 3
+    END_OF_FILE_SIGNAL = 4
