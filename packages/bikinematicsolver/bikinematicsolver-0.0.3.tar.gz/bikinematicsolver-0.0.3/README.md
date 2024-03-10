f882cf94d862ab89659200f6cba88b76e4a861ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-10 14:03:45 +0000

    Add package bikinematicsolver/0.0.3/bikinematicsolver-0.0.3.tar.gz

---
+++ packages/bikinematicsolver/bikinematicsolver-0.0.3.tar.gz/bikinematicsolver-0.0.3/README.md
@@ -0,0 +1,11 @@
+# bikinematicsolver
+
+used in http://bikinematics.herokuapp.com/
+
+to use:
+
+go look at the test_bike.py thats pretty much all it does
+
+get from pypi:
+
+`pip install bikinematicsolver`
---
+++ packages/bikinematicsolver/bikinematicsolver-0.0.3.tar.gz/bikinematicsolver-0.0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.3'
+DESCRIPTION = 'Bike suspension kinematic solver'
+LONG_DESCRIPTION = ''
+
+setup(
+    
+    name = "bikinematicsolver",
+    version = VERSION,
+    author = "mb",
+    author_email = "markbak7@gmail.com",
+    description = DESCRIPTION,
+    long_description = LONG_DESCRIPTION,
+    packages = find_packages(),
+    install_requires=['dijkstar','numpy','scipy'],
+
+    )
