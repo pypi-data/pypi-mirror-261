a35127c242d73dddad2e9f80caba32e8489dce41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-10 14:03:57 +0000

    Add package MukeshAPI/0.5.5/MukeshAPI-0.5.5.tar.gz

---
+++ packages/MukeshAPI/MukeshAPI-0.5.5.tar.gz/MukeshAPI-0.5.5/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup,find_packages
+with open("README.md", encoding="utf8") as readme:
+    long_desc = readme.read()
+VERSION="0.5.5"
+
+# Setting up
+setup(
+    name="MukeshAPI",
+    version=VERSION,
+    author="Mukesh | noob-mukesh",
+    author_email="itzcodermukesh@gmail.com",
+    description="python api hub |mukesh-api",
+    long_description_content_type="text/markdown",
+    long_description=long_desc,
+    packages=find_packages(),
+    license="MIT",
+    install_requires=["pytz>=2023.3"],
+    keywords=['python', "MukeshAPI","flask"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: Implementation",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Internet",
+        "Topic :: Communications",
+        "Topic :: Communications :: Chat",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+    ],
+    
+    project_urls={
+        "Tracker": "https://github.com/noob-mukesh/MukeshAPI/issues",
+        "Community": "https://t.me/mr_sukkun",
+        "Source": "https://github.com/noob-mukesh/MukeshAPI",
+    },
+    python_requires="~=3.7",
+)
