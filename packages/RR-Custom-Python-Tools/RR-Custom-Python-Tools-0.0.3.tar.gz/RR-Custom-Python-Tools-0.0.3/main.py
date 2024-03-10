284e32653e3de3d203df6270c046f96125d62990
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-10 15:03:26 +0000

    Add package RR-Custom-Python-Tools/0.0.3/RR-Custom-Python-Tools-0.0.3.tar.gz

---
+++ packages/RR-Custom-Python-Tools/RR-Custom-Python-Tools-0.0.3.tar.gz/RR-Custom-Python-Tools-0.0.3/main.py
@@ -0,0 +1,7 @@
+from date_manager import DateManager
+from excel_manager import ExcelManager
+from log_manager import LogManager
+
+Date_Manager = DateManager()
+Excel_Manager = ExcelManager()
+Log_Manager = LogManager()
