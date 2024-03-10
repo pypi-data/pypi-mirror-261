133574d0191666108298def78fa1a1d924ef981c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-10 15:03:22 +0000

    Add package nonebot-plugin-rimofun/0.3.0/nonebot_plugin_rimofun-0.3.0-py3-none-any.whl

---
+++ packages/nonebot-plugin-rimofun/nonebot_plugin_rimofun-0.3.0-py3-none-any.whl/nonebot_plugin_rimofun/__init__.py
@@ -0,0 +1,38 @@
+from nonebot import require
+from nonebot.plugin import PluginMetadata, inherit_supported_adapters
+
+require("nonebot_plugin_alconna")
+
+from . import __main__ as __main__  # noqa: E402
+from .config import ConfigModel, config  # noqa: E402
+
+__version__ = "0.3.0"
+__plugin_meta__ = PluginMetadata(
+    name="RimoFun",
+    description="用 RimoChan 开发的工具做的一个有趣的插件",
+    usage=(
+        "指令列表：\n"
+        "注：以下指令后面带不带参数都可以，如果没带参数，Bot 将会询问你\n"
+        " \n"
+        "▶ unvcode\n"
+        "  ▷ 介绍：反和谐超级武器，幼女 Code！\n"
+        f"  ▷ 指令：{'；'.join(config.rimofun_unvcode_commands)}\n"
+        " \n"
+        "▶ bnhhsh\n"
+        "  ▷ 介绍：不对劲的拼音首字母缩写翻译工具！\n"
+        f"  ▷ 指令：{'；'.join(config.rimofun_bnhhsh_commands)}\n"
+        " \n"
+        "▶ yinglish\n"
+        "  ▷ 介绍：淫语翻译机！\n"
+        f"  ▷ 指令：{'；'.join(config.rimofun_yinglish_commands)}\n"
+        " \n"
+        "▶ not translator\n"
+        "  ▷ 介绍：不会翻译机！\n"
+        f"  ▷ 指令：{'；'.join(config.rimofun_translator_commands)}"
+    ),
+    homepage="https://github.com/lgc-NB2Dev/nonebot-plugin-rimofun",
+    type="application",
+    config=ConfigModel,
+    supported_adapters=inherit_supported_adapters("nonebot_plugin_alconna"),
+    extra={"License": "MIT", "Author": "student_2333"},
+)
---
+++ packages/nonebot-plugin-rimofun/nonebot_plugin_rimofun-0.3.0-py3-none-any.whl/nonebot_plugin_rimofun/config.py
@@ -0,0 +1,52 @@
+from typing import List, Set
+
+from nonebot import get_plugin_config
+from pydantic import BaseModel
+
+
+class ConfigModel(BaseModel):
+    nickname: Set[str]
+
+    # unvcode
+    rimofun_unvcode_mse: float = 0.2
+    """unvcode 的全局字符串不同阈值，比如 0.2 代表匹配 80% 相同的字符"""
+
+    rimofun_unvcode_commands: List[str] = ["unvcode", "幼女码"]
+    """手动触发 bnhhsh 的命令"""
+
+    # bnhhsh
+    rimofun_bnhhsh_unvcode: bool = True
+    """bnhhsh 是否使用 unvcode 反和谐"""
+
+    rimofun_bnhhsh_commands: List[str] = ["bnhhsh", "不能好好说话"]
+    """手动触发 bnhhsh 的命令"""
+
+    rimofun_bnhhsh_reply_chance: float = 0.0
+    """bnhhsh 自动回复纯英文消息的触发概率，值应在 0~1 之间"""
+
+    # yinglish
+    rimofun_yinglish_unvcode: bool = False
+    """yinglish 是否使用 unvcode 反和谐"""
+
+    rimofun_yinglish_commands: List[str] = ["yinglish", "淫语"]
+    """手动触发 yinglish 的命令"""
+
+    rimofun_yinglish_reply_chance: float = 0.0
+    """yinglish 自动回复任意文本消息的触发概率，值应在 0~1 之间"""
+
+    rimofun_yinglish_yingness: float = 0.7
+    """yinglish 默认淫乱度，值应在 0~1 之间"""
+
+    # not translator
+    rimofun_translator_commands: List[str] = ["not translator", "不会翻译"]
+    """手动触发 not-translator 的命令"""
+
+    rimofun_translator_reply_chance: float = 0.0
+    """translator 自动回复纯英文消息的触发概率，值应在 0~1 之间"""
+
+    # other
+    rimofun_initiative_priority: int = 99
+    """自动回复 matcher 的优先级"""
+
+
+config = get_plugin_config(ConfigModel)
