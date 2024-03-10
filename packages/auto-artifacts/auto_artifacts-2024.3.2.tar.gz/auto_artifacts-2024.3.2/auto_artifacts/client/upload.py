b8c3cfed9cd7852ac3899a3171f80c1eda374e99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-10 14:03:39 +0000

    Add package auto-artifacts/2024.3.2/auto_artifacts-2024.3.2.tar.gz

---
+++ packages/auto-artifacts/auto_artifacts-2024.3.2.tar.gz/auto_artifacts-2024.3.2/auto_artifacts/client/upload.py
@@ -0,0 +1,40 @@
+import os
+import requests
+
+def upload_files(base_url, file_paths, path, password):
+    """
+    Uploads files to the FastAPI server.
+
+    Args:
+        base_url (str): The base URL where the FastAPI server is running.
+        file_paths (list of str): List of file paths of the files to be uploaded.
+        path (str): The path where the files should be uploaded on the server.
+        password (str): The password for accessing the upload functionality.
+
+    Returns:
+        bool: True if the files were uploaded successfully, False otherwise.
+    """
+
+    # The URL for the upload endpoint
+    url = f"{base_url}/upload/files"
+
+    # Prepare the files for uploading
+    files = [('files', (open(file_path, 'rb'))) for file_path in file_paths]
+
+    # Data to be sent in the form
+    data = {
+        'path': path,
+        'pw': password,
+    }
+
+    # Make the POST request to upload the files
+    response = requests.post(url, files=files, data=data)
+
+    if response.ok:
+        # If the request was successful, return True
+        print(response.json())  # Optionally print the response from the server
+        return True
+    else:
+        # If there was an error, print the error and return False
+        print(f"Error uploading files: {response.text}")
+        return False
---
+++ packages/auto-artifacts/auto_artifacts-2024.3.2.tar.gz/auto_artifacts-2024.3.2/auto_artifacts/server/api_download.py
@@ -0,0 +1,28 @@
+import os
+
+from fastapi import APIRouter, HTTPException, Query
+from fastapi.responses import FileResponse
+
+from auto_artifacts.server.config import BASE_PATH
+from auto_artifacts.server.auth import authenticate
+
+router = APIRouter()
+
+@router.get("/file")
+def download(filename: str = Query(..., description="The name of the file to download"),
+             path: str = Query(..., description="The path to the file, relative to the base path"),
+             pw: str = Query(None, description="Password for accessing private files")):
+
+    full_path = os.path.join(BASE_PATH, path)
+    if not os.path.exists(full_path):
+        raise HTTPException(status_code=404, detail="Invalid file or path")
+
+    if "private" in path:
+        if not authenticate(pw):
+            raise HTTPException(status_code=401, detail="Protected access.")
+
+    file_path = os.path.join(full_path, filename)
+    if not os.path.exists(file_path):
+        raise HTTPException(status_code=404, detail="File does not exist")
+
+    return FileResponse(path=file_path, filename=filename, media_type='application/octet-stream')
