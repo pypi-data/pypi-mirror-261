7f0e8470aecd8d27d62d3d6827e7efdb1a1b52f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 04:04:05 +0000

    Add package MoonlightDB/1.1.1/MoonlightDB-1.1.1-py3-none-any.whl

---
+++ packages/MoonlightDB/MoonlightDB-1.1.1-py3-none-any.whl/Moonlight/cli.py
@@ -0,0 +1,13 @@
+import click
+
+from api import app
+
+@click.command()
+@click.option('--host', default = '127.0.0.1', help = 'database manager IP address')
+@click.option('--port', default = 3000, help = 'database manager port')
+def run(host, port):
+    app.run(
+        host       = host,
+        port       = int(port),
+        access_log = False
+    )
