b01de68bcfa0527c16cc22a034d3c40665557c97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 14:04:56 +0000

    Add package apoplast/1.0.5/apoplast-1.0.5-py3-none-any.whl

---
+++ packages/apoplast/apoplast-1.0.5-py3-none-any.whl/apoplast/shows/ingredient_scan/land/add_measured_ingredient/status_m_eq_and_ba/status_1.py
@@ -0,0 +1,195 @@
+
+'''
+	python3 insurance.py shows/ingredient_scan/land/add_measured_ingredient/status_m_eq_and_ba/status_1.py
+'''
+
+import apoplast.shows.ingredient_scan.land.add_measured_ingredient as add_measured_ingredient
+import apoplast.shows.ingredient_scan.land.build as build_ingredient_scan_land
+import apoplast.shows.ingredient_scan.grove.seek as grove_seek	
+
+import json	
+	
+def check_1 ():
+	land = build_ingredient_scan_land.eloquently ()	
+
+	add_measured_ingredient.beautifully (
+		land = land,
+		
+		amount = 10,
+		source = {
+			"name":	"WALNUTS HALVES & PIECES, WALNUTS",
+			"FDC ID": "1882785",
+			"UPC": "099482434618",
+			"DSLD ID": ""
+		},
+		measured_ingredient = {
+			"name": "Potassium, K",
+			"measures": {
+				"mass + mass equivalents": {
+					"per package": {
+						"listed": [
+							"1947.660",
+							"mg"
+						],
+						"grams": {
+							"decimal string": "1.948",
+							"fraction string": "97383/50000"
+						}
+					}
+				}
+			}
+		}
+	)
+	
+	add_measured_ingredient.beautifully (
+		land = land,
+		
+		amount = 10,
+		source = {
+			"name":	"WALNUTS HALVES & PIECES, WALNUTS",
+			"FDC ID": "1882785",
+			"UPC": "099482434618",
+			"DSLD ID": ""
+		},
+		measured_ingredient = {
+			"name": "Vitamin D (D2 + D3), International Units",
+			"measures": {
+				"biological activity": {
+					"per package": {
+						"listed": [
+							"0.000",
+							"IU"
+						],
+						"IU": {
+							"decimal string": "0.000",
+							"fraction string": "0"
+						}
+					}
+				}
+			}
+		}
+	)
+
+	Potassium = grove_seek.beautifully (
+		grove = land ["grove"],
+		for_each = (
+			lambda entry : True if (
+				"potassium, k" in list (map (
+					lambda name : name.lower (), 
+					entry ["info"] ["names"]
+				))
+			) else False
+		)
+	)
+	assert (len (Potassium ["natures"]) == 1)
+	assert (
+		Potassium ["natures"][0] ==
+		{
+			"amount": "10",
+			"source": {
+				"name": "WALNUTS HALVES & PIECES, WALNUTS",
+				"FDC ID": "1882785",
+				"UPC": "099482434618",
+				"DSLD ID": ""
+			},
+			"ingredient": {
+				"name": "Potassium, K"
+			},
+			"measures": {
+				"mass + mass equivalents": {
+					"per package": {
+						"listed": [
+							"1947.660",
+							"mg"
+						],
+						"grams": {
+							"decimal string": "1.948",
+							"fraction string": "97383/50000"
+						}
+					}
+				}
+			}
+		}
+	)	
+	assert (
+		Potassium ["measures"]["mass + mass equivalents"] ==
+		{
+			"per recipe": {
+				"grams": {
+					"fraction string": "97383/5000"
+				}
+			}
+		}
+	)
+
+
+	Vitamin_D = grove_seek.beautifully (
+		grove = land ["grove"],
+		for_each = (
+			lambda entry : True if (
+				"Vitamin D (D2 + D3), International Units".lower () in list (map (
+					lambda name : name.lower (), 
+					entry ["info"] ["names"]
+				))
+			) else False
+		)
+	)
+	assert (len (Vitamin_D ["natures"]) == 1)
+	assert (
+		Vitamin_D ["natures"][0] ==
+		{
+			"amount": "10",
+			"source": {
+				"name": "WALNUTS HALVES & PIECES, WALNUTS",
+				"FDC ID": "1882785",
+				"UPC": "099482434618",
+				"DSLD ID": ""
+			},
+			"ingredient": {
+				"name": "Vitamin D (D2 + D3), International Units",
+			},
+			"measures": {
+				"biological activity": {
+					"per package": {
+						"listed": [
+							"0.000",
+							"IU"
+						],
+						"IU": {
+							"decimal string": "0.000",
+							"fraction string": "0"
+						}
+					}
+				}
+			}
+		}
+	), Vitamin_D ["natures"]
+	assert (
+		Vitamin_D ["measures"]["biological activity"] ==
+		{
+			"per recipe": {
+				"IU": {
+					"fraction string": "0"
+				}
+			}
+		}
+	)
+	
+	assert (
+		land ["measures"] ["mass + mass equivalents"] ==
+		{
+			"per recipe": {
+				"grams": {
+					"fraction string": "0"
+				}
+			}
+		}
+	)
+	
+
+	return;
+	
+	
+checks = {
+	'check 1': check_1
+}
---
+++ packages/apoplast/apoplast-1.0.5-py3-none-any.whl/apoplast/shows/ingredient_scan/measures/merge/__init__.py
@@ -0,0 +1,42 @@
+
+'''
+	import apoplast.shows.ingredient_scan.measures.merge as merge_measures
+	merge_measures.calc (
+		aggregate_measures,
+		new_measures
+	)
+'''
+
+from fractions import Fraction
+
+def calc (
+	aggregate_measures, 
+	new_measures
+):
+	for measure in new_measures:
+		pers = new_measures [ measure ]
+		
+		for per in pers:
+			units = pers [per]
+			
+			if (per == "portion of grove"):
+				continue;
+			
+			if (per not in [ "per recipe" ]):
+				raise Exception (f"The divisor found, '{ per }', was not accounted for.");
+			
+			for unit in units:
+				#print ("unit:", unit)
+			
+				if (measure not in aggregate_measures):
+					aggregate_measures [ measure ] = {}
+					aggregate_measures [ measure ] [per] = {}
+					aggregate_measures [ measure ] [per] [unit] = {}
+					aggregate_measures [ measure ] [per] [unit] ["fraction string"] = "0"
+
+			
+				aggregate_measures [ measure ] [per] [unit] ["fraction string"] = str (
+					Fraction (new_measures [ measure ] [per] [unit] ["fraction string"]) + 
+					Fraction (aggregate_measures [ measure ] [per] [unit] ["fraction string"])
+				)
+				
