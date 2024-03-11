175ea07a695686743fa3693bb1cef495deba7690
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 15:04:45 +0000

    Add package EasyFlowQ/1.4.9/easyflowq-1.4.9.tar.gz

---
+++ packages/EasyFlowQ/easyflowq-1.4.9.tar.gz/easyflowq-1.4.9/release/pyi_mac.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# delete and add folder
+rm -r release/EasyFlowQ_release_mac/*
+mkdir release/EasyFlowQ_release_mac/dmg
+
+# package with pyinstaller and codesign
+# conda activate easyflow_env
+pyinstaller --noconfirm --distpath ./release/EasyFlowQ_release_mac/ --workpath ./_temp/ ./release/pyi_mac.spec
+codesign -f -s EasyFlowQ_YMa release/EasyFlowQ_release_mac/EasyFlowQ_MACOS.app
+
+# Copy the app bundle to the dmg folder.
+cp -r release/EasyFlowQ_release_mac/EasyFlowQ_MACOS.app release/EasyFlowQ_release_mac/dmg
+
+# If the DMG already exists, delete it.
+test -f release/EasyFlowQ_release_mac/EasyFlowQ_MACOS.dmg && rm release/EasyFlowQ_release_mac/EasyFlowQ_MACOS.dmg
+
+create-dmg \
+  --volname EasyFlowQ_MACOS.dmg \
+  --icon EasyFlowQ_MACOS.app 120 120 \
+  --app-drop-link 360 120 \
+  release/EasyFlowQ_release_mac/EasyFlowQ_MACOS.dmg \
+  release/EasyFlowQ_release_mac/dmg/
