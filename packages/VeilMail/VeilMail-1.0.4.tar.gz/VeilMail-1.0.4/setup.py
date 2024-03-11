15f4105ca53fee01883e792c58be66c7005945d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 04:04:00 +0000

    Add package VeilMail/1.0.4/VeilMail-1.0.4.tar.gz

---
+++ packages/VeilMail/VeilMail-1.0.4.tar.gz/VeilMail-1.0.4/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+setup(
+    name='VeilMail',
+    version='1.0.4',
+    py_modules=['veilmail'],
+    install_requires=[
+        'requests',
+        'rich',
+        'pyfiglet',
+        'yaspin',
+        'alive-progress',
+        'pyperclip',
+        'questionary',
+        'html2text'
+    ],
+    entry_points={
+        'console_scripts': [
+            'veilmail=VeilMail.main:main',
+        ],
+    },
+    author='Ethen Dixon',
+    author_email='ethendixon@outlook.com',
+    description='A simple command-line email client using the 1secmail API.',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Zadeson/veilmail',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
