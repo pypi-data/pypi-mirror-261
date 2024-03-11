1f2ede464d68692f791daa9a1d9edfb93de21813
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 15:04:34 +0000

    Add package test-package-kosareva-ea/0.0.1/test_package_kosareva_ea-0.0.1.tar.gz

---
+++ packages/test-package-kosareva-ea/test_package_kosareva_ea-0.0.1.tar.gz/test_package_kosareva_ea-0.0.1/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+    name='test_package_kosareva_ea', # Имя пакета
+    version='0.0.1',  # Версия пакета
+    description='My Package',
+    packages=setuptools.find_packages(),
+)
