86d2b73836a187ebd21480d72bc1a14d64152a5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 14:05:01 +0000

    Add package generate-core/0.4.2/generate_core-0.4.2.tar.gz

---
+++ packages/generate-core/generate_core-0.4.2.tar.gz/generate_core-0.4.2/generate/utils.py
@@ -0,0 +1,62 @@
+from __future__ import annotations
+
+import asyncio
+from typing import Any, AsyncIterator, Awaitable, Generator, Generic, Iterable, TypeVar
+
+from generate.types import OrIterable
+
+T = TypeVar('T')
+P = TypeVar('P')
+R = TypeVar('R')
+
+
+def ensure_iterable(obj: OrIterable[T]) -> Iterable[T]:
+    if isinstance(obj, Iterable):
+        return obj
+    return [obj]
+
+
+class CatchReturnGenerator(Generic[T, P, R]):
+    value: R
+
+    def __init__(self, generator: Generator[T, P, R]) -> None:
+        self.generator = generator
+
+    def __iter__(self) -> Generator[T, P, R]:
+        self.value = yield from self.generator
+        return self.value
+
+
+def sync_await(awaitable: Awaitable[T]) -> T:
+    loop = asyncio.get_event_loop()
+    try:
+        return loop.run_until_complete(awaitable)
+    except RuntimeError:
+        import nest_asyncio
+
+        nest_asyncio.apply()
+        return loop.run_until_complete(awaitable)
+
+
+def sync_aiter(aiterator: AsyncIterator[T]) -> Generator[T, None, None]:
+    loop = asyncio.get_event_loop()
+    try:
+        while True:
+            yield loop.run_until_complete(aiterator.__anext__())
+    except StopAsyncIteration:
+        return
+    except RuntimeError:
+        import nest_asyncio
+
+        nest_asyncio.apply()
+        yield from sync_aiter(aiterator)
+
+
+def unwrap_model(model: Any) -> Any:
+    from generate.model import GenerateModel
+
+    if hasattr(model, 'model'):
+        if isinstance(model.model, GenerateModel):
+            return unwrap_model(model.model)
+        return model
+    return model
