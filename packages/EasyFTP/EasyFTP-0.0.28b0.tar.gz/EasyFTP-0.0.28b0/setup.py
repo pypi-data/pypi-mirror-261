702b94ca8a87a9d28371525aeb13346525017075
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 14:04:43 +0000

    Add package EasyFTP/0.0.28b0/EasyFTP-0.0.28b0.tar.gz

---
+++ packages/EasyFTP/EasyFTP-0.0.28b0.tar.gz/EasyFTP-0.0.28b0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert_file('README.md', 'rst', encoding = "utf-8")
+except(IOError, ImportError):
+    long_description = open('README.md', encoding = "utf-8").read()
+
+setup(
+    name='EasyFTP',
+    version='0.0.28b',
+    description='Easy usage of FTP operation',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='ZustFancake',
+    author_email='ZustFancake@dimigo.hs.kr',
+    url='https://github.com/ZustFancake/EasyFTP',
+    install_requires=[],
+    packages=find_packages(exclude=[], where = "./src"),
+    package_dir = {'' : "./src"},
+    keywords=['zustfancake', 'ftp', 'easyftp', 'EasyFTP', 'pypi'],
+    python_requires='>=3.6',
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+)
