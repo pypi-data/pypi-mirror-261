5351fb513a1d40321dc537a84a938389e563fdc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 16:05:28 +0000

    Add package populate-synapse-nb/1.48/populate_synapse_nb-1.48-py3-none-any.whl

---
+++ packages/populate-synapse-nb/populate_synapse_nb-1.48-py3-none-any.whl/populate_synapse_nb/test_notebook.py
@@ -0,0 +1,24 @@
+
+
+
+
+from populate_synapse_nb import PopulateAzureSynapseNotebook
+PopulateAzureSynapseNotebook(source_path='notebook/test_notebook.py').run(force=True)
+
+
+
+
+print('Hello world')
+
+print('1')
+
+print('2')
+
+print('3')
+
+print('4')
+
+
+
+
+
