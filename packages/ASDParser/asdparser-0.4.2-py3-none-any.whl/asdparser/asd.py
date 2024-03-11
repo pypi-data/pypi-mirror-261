a0b5a98a8d2d893e9999a35ff1731c677aa5f629
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 05:04:54 +0000

    Add package ASDParser/0.4.2/asdparser-0.4.2-py3-none-any.whl

---
+++ packages/ASDParser/asdparser-0.4.2-py3-none-any.whl/asdparser/asd.py
@@ -0,0 +1,33 @@
+import os 
+import untangle
+import pandas as pd
+
+from .model import ASDProtein
+
+class AsdDB: 
+    def __init__(self, directory):
+        self.dir = directory
+        self.proteins = {}
+
+        for file in os.listdir(self.dir ):
+            file_path = f'{self.dir }/{file}'
+            obj = untangle.parse(file_path)
+            protein = ASDProtein.from_xml_obj(obj)
+            self.proteins[protein.id] = protein
+    
+    def get(self,id):
+        return self.proteins[id]
+    
+    def to_df(self):
+        data = [[iid, 
+                 self.proteins[iid].organism, 
+                 self.proteins[iid].uniprot_id,
+                 len(self.proteins[iid].allosteric_sites),
+                 len(self.proteins[iid].inhibitors),
+                 len(self.proteins[iid].activators),
+                 len(self.proteins[iid].regulators),
+                 len(self.proteins[iid].pdbs),
+                 len(self.proteins[iid].ptms)] for iid in self.proteins]
+        
+        return pd.DataFrame(data, 
+                            columns=['id','organism', 'uniprot', 'n_allosteric_sites','inihibitors', 'activators', 'regulators', 'pdbs', 'ptms']) 
