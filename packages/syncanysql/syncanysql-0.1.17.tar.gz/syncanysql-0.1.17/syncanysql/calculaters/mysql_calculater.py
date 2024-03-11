98369bcaa18b1ba86f2719e3ec8a177cb3e8dd72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 16:05:12 +0000

    Add package syncanysql/0.1.17/syncanysql-0.1.17.tar.gz

---
+++ packages/syncanysql/syncanysql-0.1.17.tar.gz/syncanysql-0.1.17/syncanysql/calculaters/mysql_calculater.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# 2023/3/2
+# create by: snower
+
+import traceback
+from syncany.logger import get_logger
+from syncany.calculaters.calculater import Calculater
+from syncany.calculaters.import_calculater import parse_final_filter
+
+
+class MysqlCalculater(Calculater):
+    funcs = None
+
+    @classmethod
+    def find_func(cls, name):
+        if cls.funcs is None:
+            from . import mysql_funcs
+            cls.funcs = mysql_funcs.funcs
+        return cls.funcs.get(name)
+
+    def __init__(self, *args, **kwargs):
+        super(MysqlCalculater, self).__init__(*args, **kwargs)
+
+        self.func = self.find_func(self.name[7:])
+
+    def calculate(self, *args):
+        try:
+            return self.func(*args)
+        except (ValueError, KeyError) as e:
+            return None
+        except Exception as e:
+            get_logger().warning("mysql calculater execute %s(%s) error: %s\n%s", self.name[7:], args, e,
+                                 traceback.format_exc())
+            return None
+
+    def get_final_filter(self):
+        if hasattr(self.func, "get_final_filter"):
+            return self.func.get_final_filter()
+        return parse_final_filter(self.func)
---
+++ packages/syncanysql/syncanysql-0.1.17.tar.gz/syncanysql-0.1.17/syncanysql/calculaters/window_calculater.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+# 2023/6/1
+# create by: snower
+
+from syncany.calculaters.calculater import Calculater
+from syncany.filters import IntFilter, FloatFilter
+from ..errors import SyncanySqlExecutorException
+
+
+class WindowAggregateCalculater(Calculater):
+    def __init__(self, *args, **kwargs):
+        super(WindowAggregateCalculater, self).__init__(*args, **kwargs)
+
+        if self.name.endswith("::aggregate"):
+            self.calculate = self.aggregate
+        elif self.name.endswith("::order_aggregate"):
+            self.calculate = self.order_aggregate
+
+    def aggregate(self, state_value, data_value, context):
+        raise SyncanySqlExecutorException("window caculate require order by")
+
+    def order_aggregate(self, state_value, data_value, context):
+        return context.current_index + 1
+
+    def calculate(self, *args):
+        if self.name.endswith("::aggregate"):
+            return self.aggregate(*args)
+        if self.name.endswith("::order_aggregate"):
+            return self.order_aggregate(*args)
+        return None
+
+
+class WindowStateAggregateCalculater(WindowAggregateCalculater):
+    def __init__(self, *args, **kwargs):
+        super(WindowStateAggregateCalculater, self).__init__(*args, **kwargs)
+
+        if self.name.endswith("::final_value"):
+            self.calculate = self.final_value
+
+    def final_value(self, state_value):
+        return state_value
+
+    def calculate(self, *args):
+        if self.name.endswith("::final_value"):
+            return self.final_value(*args)
+        return super(WindowStateAggregateCalculater, self).calculate(*args)
+
+
+class WindowStateAggregateRowNumberCalculater(WindowAggregateCalculater):
+    def order_aggregate(self, state_value, data_value, context):
+        return context.current_index + 1
+
+    def get_final_filter(self):
+        return IntFilter.default()
+
+
+class WindowStateAggregateRankCalculater(WindowStateAggregateCalculater):
+    def order_aggregate(self, state_value, data_value, context):
+        order_value = context.order_value
+        if state_value is None:
+            return {"order_value": order_value, "rank": 1, "row_number": 1}
+        state_value["row_number"] += 1
+        if order_value != state_value["order_value"]:
+            state_value["rank"] += 1
+        return state_value
+
+    def final_value(self, state_value):
+        if state_value is None:
+            return 1
+        return state_value["rank"]
+
+    def get_final_filter(self):
+        return IntFilter.default()
+
+
+class WindowStateAggregateDenseRankCalculater(WindowStateAggregateCalculater):
+    def order_aggregate(self, state_value, data_value, context):
+        order_value = context.order_value
+        if state_value is None:
+            return {"order_value": order_value, "rank": 1}
+        if order_value != state_value["order_value"]:
+            state_value["rank"] += 1
+        return state_value
+
+    def final_value(self, state_value):
+        if state_value is None:
+            return 1
+        return state_value["rank"]
+
+    def get_final_filter(self):
+        return IntFilter.default()
+
+
+class WindowStateAggregatePercentRankCalculater(WindowStateAggregateCalculater):
+    def order_aggregate(self, state_value, data_value, context):
+        order_value = context.order_value
+        if state_value is None:
+            return {"order_value": order_value, "rank": 1, "row_number": 1}
+        state_value["row_number"] += 1
+        if order_value != state_value["order_value"]:
+            state_value["rank"] += 1
+        return state_value
+
+    def final_value(self, state_value):
+        if state_value is None:
+            return 1
+        if state_value["row_number"] <= 1:
+            return 0
+        return state_value["rank"] - 1 / state_value["row_number"] - 1
+
+    def get_final_filter(self):
+        return FloatFilter.default()
+
+
+class WindowStateAggregateCumeDistCalculater(WindowAggregateCalculater):
+    def order_aggregate(self, state_value, data_value, context):
+        if not context.datas:
+            return 0
+        return context.current_index + 1 / len(context.datas) + 1
+
+    def get_final_filter(self):
+        return FloatFilter.default()
