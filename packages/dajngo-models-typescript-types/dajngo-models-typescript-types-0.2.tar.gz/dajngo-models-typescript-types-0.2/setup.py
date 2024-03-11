a1079f074a424adefef48b06b0f2e6a732e2ee7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 04:04:02 +0000

    Add package dajngo-models-typescript-types/0.2/dajngo-models-typescript-types-0.2.tar.gz

---
+++ packages/dajngo-models-typescript-types/dajngo-models-typescript-types-0.2.tar.gz/dajngo-models-typescript-types-0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='dajngo-models-typescript-types',
+    version='0.2',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'django',
+    ],
+    description='A package to create your Typescript Types/Models from your Django Models.',
+    long_description=long_description,
+     long_description_content_type="text/markdown",
+    author='Enzo_frnt',
+    url='https://github.com/enzofrnt/Django-Model-to-TypeScript',
+    classifiers=[
+        'Framework :: Django',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    entry_points={
+        'console_scripts': [
+            'django2ts=django_model_to_typescript_types:main'
+        ],
+    },
+)
