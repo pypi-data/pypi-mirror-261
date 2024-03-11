10c1ffb2ec1a009aea1efea7324ae49f7a66cf70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 03:03:40 +0000

    Add package offlinesec-client/1.0.31/offlinesec_client-1.0.31.tar.gz

---
+++ packages/offlinesec-client/offlinesec_client-1.0.31.tar.gz/offlinesec_client-1.0.31/offlinesec_client/req_patch_day.py
@@ -0,0 +1,52 @@
+import argparse
+import yaml
+import offlinesec_client.func
+from offlinesec_client.const import FILE
+from offlinesec_client.multi_systems import process_yaml_file
+
+
+SUPPORTED_SYSTEMS = ["ABAP", "JAVA", "BO", "HANA"]
+UPLOAD_URL = "/sec-notes"
+
+
+def check_file_arg(s):
+    res = offlinesec_client.func.check_file_arg(s, ['yaml'], 200000)
+    with open(s, 'r') as file:
+        yaml_content = yaml.safe_load(file)
+        if "sap_systems" not in yaml_content:
+            raise argparse.ArgumentTypeError("Wrong the YAML file (%s) structure. Key 'sap_systems' not found" % (s,))
+    return res
+
+
+def init_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file", action="store", type=check_file_arg,
+                        help="File Name (SAP systems and their software components)", required=False)
+    parser.add_argument('--wait', action='store_true', help="Wait 5 minutes and download the report")
+    parser.parse_args()
+    return vars(parser.parse_args())
+
+
+def print_errors(errors):
+    for error in errors:
+        print(error)
+
+
+def process_it(args):
+    systems = process_yaml_file(args)
+    additional_keys = dict()
+    additional_keys["patch_day"] = True
+    offlinesec_client.func.send_to_server(systems, UPLOAD_URL, additional_keys)
+
+
+def main():
+    args = init_args()
+    if (FILE in args and args[FILE]):
+        process_it(args)
+    else:
+        print("You need to specify input file(s) (-f option)")
+
+
+if __name__ == '__main__':
+    main()
+
