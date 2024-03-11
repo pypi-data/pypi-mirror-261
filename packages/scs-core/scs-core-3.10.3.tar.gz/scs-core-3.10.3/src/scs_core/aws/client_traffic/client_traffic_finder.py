23aaa63c6f9da162f3abde2e316ec3005baa18e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 16:05:12 +0000

    Add package scs-core/3.10.3/scs-core-3.10.3.tar.gz

---
+++ packages/scs-core/scs-core-3.10.3.tar.gz/scs-core-3.10.3/src/scs_core/aws/client_traffic/client_traffic_finder.py
@@ -0,0 +1,54 @@
+"""
+Created on 8 Aug 2023
+
+@author: Bruno Beloff (bruno.beloff@southcoastscience.com)
+"""
+
+from scs_core.aws.client.api_client import APIClient
+from scs_core.aws.client_traffic.client_traffic_intercourse import ClientTrafficResponse
+from scs_core.aws.config.aws_endpoint import AWSEndpoint
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+class Endpoint(AWSEndpoint):
+    @classmethod
+    def configuration(cls):
+        return cls('CliTrfAPI/ClientTraffic',
+                   'https://tduyom430a.execute-api.us-west-2.amazonaws.com/default/ClientTraffic')
+
+
+# --------------------------------------------------------------------------------------------------------------------
+
+class ClientTrafficFinder(APIClient):
+    """
+    classdocs
+    """
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def __init__(self):
+        super().__init__()
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+
+    def find_for_users(self, token, request):
+        url = Endpoint.url('users')
+
+        for item in self._get_blocks(url, token, ClientTrafficResponse, payload=request):
+            yield item
+
+
+    def find_for_organisations(self, token, request):
+        url = Endpoint.url('organisations')
+
+        for item in self._get_blocks(url, token, ClientTrafficResponse, payload=request):
+            yield item
+
+
+    def find_for_organisations_users(self, token, request):
+        url = Endpoint.url('organisations-users')
+
+        for item in self._get_blocks(url, token, ClientTrafficResponse, payload=request):
+            yield item
