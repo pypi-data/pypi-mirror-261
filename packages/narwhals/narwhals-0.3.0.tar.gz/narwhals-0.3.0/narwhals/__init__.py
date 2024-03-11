07b0590acbfe4d213306c00fbddac1842a567730
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 04:04:01 +0000

    Add package narwhals/0.3.0/narwhals-0.3.0.tar.gz

---
+++ packages/narwhals/narwhals-0.3.0.tar.gz/narwhals-0.3.0/narwhals/__init__.py
@@ -0,0 +1,25 @@
+from narwhals.containers import get_implementation
+from narwhals.containers import is_dataframe
+from narwhals.containers import is_pandas
+from narwhals.containers import is_polars
+from narwhals.containers import is_series
+from narwhals.translate import get_namespace
+from narwhals.translate import to_native
+from narwhals.translate import translate_any
+from narwhals.translate import translate_frame
+from narwhals.translate import translate_series
+
+__version__ = "0.3.0"
+
+__all__ = [
+    "translate_frame",
+    "translate_series",
+    "translate_any",
+    "is_dataframe",
+    "is_series",
+    "is_polars",
+    "is_pandas",
+    "get_implementation",
+    "get_namespace",
+    "to_native",
+]
