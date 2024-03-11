d706daab50b9cee442fadca13278cbeb3813bbcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 14:05:23 +0000

    Add package blossoms-sayak/0.16.0/blossoms_sayak-0.16.0.tar.gz

---
+++ packages/blossoms-sayak/blossoms_sayak-0.16.0.tar.gz/blossoms_sayak-0.16.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="blossoms_sayak",
+    version="0.16.0",
+    description="A simple Python package for math operations and geometry calculations.",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Sayak Paul",
+    author_email="spsayakpaul@gmail.com",
+    package_dir={"": "src"},
+    packages=find_packages("src"),
+    package_data={"blossoms_sayak": ["py.typed"]},
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
---
+++ packages/blossoms-sayak/blossoms_sayak-0.16.0.tar.gz/blossoms_sayak-0.16.0/src/blossoms_sayak/__init__.py
@@ -0,0 +1,4 @@
+__version__ = "0.16.0"
+
+from .geometry import circle_area
+from .operations import add, subtract, multiply, divide 
