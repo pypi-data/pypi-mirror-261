338e2454f9239a68a63fd5ab7330bbc3493e2396
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 15:04:34 +0000

    Add package oeahr-package/0.0.9/oeahr_package-0.0.9.tar.gz

---
+++ packages/oeahr-package/oeahr_package-0.0.9.tar.gz/oeahr_package-0.0.9/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+PACKAGE_NAME = "oeahr_package"
+
+setup(
+    name=PACKAGE_NAME,
+    version="0.0.9",
+    description="This is my tools package",
+    packages=find_packages(),
+    entry_points={
+        "package_tools": ["test_dyna = oeahr_package.utils:list_package_tools"],
+    },
+    install_requires=[
+        "promptflow",
+        "promptflow-tools"
+    ]
+)
