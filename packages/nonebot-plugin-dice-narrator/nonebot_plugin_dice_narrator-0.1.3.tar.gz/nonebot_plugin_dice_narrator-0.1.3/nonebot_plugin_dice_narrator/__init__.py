d84af4dd37efe807e7c491f6252df6cb1870345b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 15:04:43 +0000

    Add package nonebot-plugin-dice-narrator/0.1.3/nonebot_plugin_dice_narrator-0.1.3.tar.gz

---
+++ packages/nonebot-plugin-dice-narrator/nonebot_plugin_dice_narrator-0.1.3.tar.gz/nonebot_plugin_dice_narrator-0.1.3/nonebot_plugin_dice_narrator/__init__.py
@@ -0,0 +1,17 @@
+from nonebot import get_driver
+from nonebot.plugin import PluginMetadata
+
+from nonebot_plugin_dice_narrator.config import config
+from nonebot_plugin_dice_narrator.matchers import register_matcher
+
+__plugin_meta__ = PluginMetadata(
+    name="nonebot-plugin-dice-narrator",
+    description="",
+    usage="检定 逃离触手怪的追捕",
+    type="application",
+    homepage="https://github.com/KroMiose/nonebot-plugin-dice-narrator",
+)
+
+global_config = get_driver().config
+
+register_matcher()
