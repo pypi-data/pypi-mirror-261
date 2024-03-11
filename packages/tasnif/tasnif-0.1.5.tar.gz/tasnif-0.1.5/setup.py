d3c781dff22f1e4111f7eab8fa1eabab780b1427
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 16:05:16 +0000

    Add package tasnif/0.1.5/tasnif-0.1.5.tar.gz

---
+++ packages/tasnif/tasnif-0.1.5.tar.gz/tasnif-0.1.5/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="tasnif",
+    version="0.1.5",
+    install_requires=[
+        "numpy",
+        "scikit-learn",
+        "matplotlib",
+        "Pillow",
+        "scipy",
+        "tqdm",
+        "torch",
+        "torchvision",
+        "img2vec_pytorch",
+    ],
+    python_requires=">=3.9",
+    author="Mert Cobanov",
+    author_email="mertcobanov@gmail.com",
+    description="A simple library for unsupervised image clustering",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/cobanov/tasnif",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
