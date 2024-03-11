5f3965a716fc47cb550f4f38c4c178b92bae15fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 16:05:18 +0000

    Add package textpack2/0.1.3/textpack2-0.1.3.tar.gz

---
+++ packages/textpack2/textpack2-0.1.3.tar.gz/textpack2-0.1.3/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="textpack2",
+    version="0.1.3",
+    author="Luke Whyte",
+    author_email="lukeawhyte@gmail.com",
+    maintainer="Mikuláš Poul",
+    maintainer_email="mikulaspoul@gmail.com",
+    description="Quickly identify and group similar text strings in a large dataset",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/xelixdev/textpack",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "pandas",
+        "scikit-learn",
+        "scipy",
+        "numpy",
+        "cython",
+        "sparse-dot-topn",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
