c246fa3483e0cb95d9a77be5ee1f908c57b5f3a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 14:04:47 +0000

    Add package types-zstd/1.5.0.20240311/types-zstd-1.5.0.20240311.tar.gz

---
+++ packages/types-zstd/types-zstd-1.5.0.20240311.tar.gz/types-zstd-1.5.0.20240311/setup.py
@@ -0,0 +1,51 @@
+from setuptools import setup
+
+name = "types-zstd"
+description = "Typing stubs for zstd"
+long_description = '''
+## Typing stubs for zstd
+
+This is a [PEP 561](https://peps.python.org/pep-0561/)
+type stub package for the [`zstd`](https://github.com/sergey-dryabzhinsky/python-zstd) package.
+It can be used by type-checking tools like
+[mypy](https://github.com/python/mypy/),
+[pyright](https://github.com/microsoft/pyright),
+[pytype](https://github.com/google/pytype/),
+PyCharm, etc. to check code that uses
+`zstd`.
+
+This version of `types-zstd` aims to provide accurate annotations
+for `zstd==1.5.*`.
+The source for this package can be found at
+https://github.com/python/typeshed/tree/main/stubs/zstd. All fixes for
+types and metadata should be contributed there.
+
+See https://github.com/python/typeshed/blob/main/README.md for more details.
+This package was generated from typeshed commit `3802899a01269df575ea32a21534c5400fb9218a` and was tested
+with mypy 1.9.0, pyright 1.1.350, and
+pytype 2024.2.27.
+'''.lstrip()
+
+setup(name=name,
+      version="1.5.0.20240311",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      project_urls={
+          "GitHub": "https://github.com/python/typeshed",
+          "Changes": "https://github.com/typeshed-internal/stub_uploader/blob/main/data/changelogs/zstd.md",
+          "Issue tracker": "https://github.com/python/typeshed/issues",
+          "Chat": "https://gitter.im/python/typing",
+      },
+      install_requires=[],
+      packages=['zstd-stubs'],
+      package_data={'zstd-stubs': ['__init__.pyi', 'METADATA.toml']},
+      license="Apache-2.0 license",
+      python_requires=">=3.8",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 3",
+          "Typing :: Stubs Only",
+      ]
+)
