3d4ee3213c47ef7240dfc6f572b545987718bab4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 16:05:14 +0000

    Add package waldur-slurm-agent/0.2.2/waldur_slurm_agent-0.2.2.tar.gz

---
+++ packages/waldur-slurm-agent/waldur_slurm_agent-0.2.2.tar.gz/waldur_slurm_agent-0.2.2/waldur_slurm/__init__.py
@@ -0,0 +1,69 @@
+import os
+from enum import Enum
+from importlib.metadata import version
+
+from waldur_client import WaldurClient
+
+from waldur_slurm.slurm_client import logger
+from waldur_slurm.slurm_client.backend import SlurmBackend
+
+
+# "pull" stands for sync from Waldur to SLURM cluster
+# "push" stands for sync from SLURM cluster to Waldur
+class WaldurSyncDirection(Enum):
+    PULL = "pull"
+    PUSH = "push"
+
+
+WALDUR_API_URL = os.environ["WALDUR_API_URL"]
+WALDUR_API_TOKEN = os.environ["WALDUR_API_TOKEN"]
+
+WALDUR_SYNC_DIRECTION = os.environ["WALDUR_SYNC_DIRECTION"]
+
+if WALDUR_SYNC_DIRECTION not in [
+    WaldurSyncDirection.PULL.value,
+    WaldurSyncDirection.PUSH.value,
+]:
+    logger.error(
+        "SLURM_DEPLOYMENT_TYPE has invalid value: %s. Possible values are %s and %s",
+        WALDUR_SYNC_DIRECTION,
+        WaldurSyncDirection.PULL.value,
+        WaldurSyncDirection.PUSH.value,
+    )
+    exit(1)
+
+WALDUR_OFFERING_UUID = os.environ.get("WALDUR_OFFERING_UUID")
+
+if not WALDUR_OFFERING_UUID:
+    logger.error("WALDUR_OFFERING_UUID is empty")
+    exit(1)
+
+ENABLE_USER_HOMEDIR_ACCOUNT_CREATION = os.environ.get(
+    "ENABLE_USER_HOMEDIR_ACCOUNT_CREATION", "false"
+)
+
+ENABLE_USER_HOMEDIR_ACCOUNT_CREATION = ENABLE_USER_HOMEDIR_ACCOUNT_CREATION.lower() in [
+    "yes",
+    "true",
+]
+waldur_slurm_agent_version = version("waldur-slurm-agent")
+
+user_agent_dict = {
+    "pull": f"waldur-slurm-service-pull/{waldur_slurm_agent_version}",
+    "push": f"waldur-slurm-service-push/{waldur_slurm_agent_version}",
+}
+
+user_agent = user_agent_dict.get(WALDUR_SYNC_DIRECTION)
+
+waldur_rest_client = WaldurClient(WALDUR_API_URL, WALDUR_API_TOKEN, user_agent)
+
+slurm_backend = SlurmBackend()
+
+sentry_dsn = os.environ.get("SENTRY_DSN")
+
+if sentry_dsn:
+    import sentry_sdk
+
+    sentry_sdk.init(
+        dsn=sentry_dsn,
+    )
