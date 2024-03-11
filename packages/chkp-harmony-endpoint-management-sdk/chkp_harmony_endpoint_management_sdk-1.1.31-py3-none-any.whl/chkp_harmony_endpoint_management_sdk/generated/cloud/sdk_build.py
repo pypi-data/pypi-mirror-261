880cd007827a917899fe2539ddef674c59a7c252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 03:03:53 +0000

    Add package chkp-harmony-endpoint-management-sdk/1.1.31/chkp_harmony_endpoint_management_sdk-1.1.31-py3-none-any.whl

---
+++ packages/chkp-harmony-endpoint-management-sdk/chkp_harmony_endpoint_management_sdk-1.1.31-py3-none-any.whl/chkp_harmony_endpoint_management_sdk/generated/cloud/sdk_build.py
@@ -0,0 +1,5 @@
+
+from chkp_harmony_endpoint_management_sdk.classes.harmony_endpoint_sdk_info import HarmonyEndpointSDKInfo
+
+def sdk_build_info() -> HarmonyEndpointSDKInfo:
+    return HarmonyEndpointSDKInfo(sdk_build="8221583761",sdk_version="1.1.31",spec="web-mgmt-external-api-production",spec_version="1.9.212",released_on="2024-03-10T11:20:47.780337")
