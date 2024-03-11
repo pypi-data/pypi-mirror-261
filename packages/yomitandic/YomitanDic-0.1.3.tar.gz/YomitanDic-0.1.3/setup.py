603033b14c50a45623afe1d862d7b95893424fde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 03:03:58 +0000

    Add package yomitandic/0.1.3/YomitanDic-0.1.3.tar.gz

---
+++ packages/yomitandic/YomitanDic-0.1.3.tar.gz/YomitanDic-0.1.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='YomitanDic',
+    version='0.1.3',
+    description='A library for easily creating dictionary files importable into Yomichan for Japanese popup dictionaries',
+    author='JawGBoi',
+    packages=find_packages(),
+    python_requires='>=3.6',
+)
