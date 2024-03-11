d267b5c00aea1c505a12bf534ac3e36e8778c9fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 14:05:25 +0000

    Add package mylocale/0.3.0/mylocale-0.3.0.tar.gz

---
+++ packages/mylocale/mylocale-0.3.0.tar.gz/mylocale-0.3.0/mylocale/read.py
@@ -0,0 +1,20 @@
+import csv
+
+
+def read(
+    fp: str, stringname: str, locale: str, variables: list = []
+):  # fp for path to translationfile, locale for wanted locale and variables for eventual variables
+    KEYS = []
+    STRINGS = []
+    with open(file=fp, newline="", encoding="utf-8", mode="r") as csvfile:
+        csvreader = csv.DictReader(csvfile, delimiter=",")
+        for item in csvreader:
+            KEYS.append(item[stringname])
+        for item in csvreader:
+            STRINGS.append(item["de_DE"])
+
+        return STRINGS
+
+
+if __name__ == "__main__":
+    print(read(fp="localisation.csv", stringname="stringname", locale="de_de"))
