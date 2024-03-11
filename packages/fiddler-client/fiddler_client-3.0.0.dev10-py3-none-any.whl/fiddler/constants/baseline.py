640053188f3cd927faca923ad5d021e6a404dff7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 15:05:02 +0000

    Add package fiddler-client/3.0.0.dev10/fiddler_client-3.0.0.dev10-py3-none-any.whl

---
+++ packages/fiddler-client/fiddler_client-3.0.0.dev10-py3-none-any.whl/fiddler/constants/baseline.py
@@ -0,0 +1,18 @@
+import enum
+
+
+@enum.unique
+class BaselineType(str, enum.Enum):
+    STATIC = 'STATIC'
+    ROLLING = 'ROLLING'
+
+
+@enum.unique
+class WindowSize(enum.IntEnum):
+    ONE_HOUR = 3600
+    ONE_DAY = 86400
+    ONE_WEEK = 604800
+    ONE_MONTH = 2592000
+
+    def __str__(self) -> str:
+        return self.name
