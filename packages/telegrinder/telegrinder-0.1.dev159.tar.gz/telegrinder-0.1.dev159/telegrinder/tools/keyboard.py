e3de3ed67a547bdfc57b365dc196fae6a3d76d47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 04:03:31 +0000

    Add package telegrinder/0.1.dev159/telegrinder-0.1.dev159.tar.gz

---
+++ packages/telegrinder/telegrinder-0.1.dev159.tar.gz/telegrinder-0.1.dev159/telegrinder/tools/keyboard.py
@@ -0,0 +1,137 @@
+import dataclasses
+import typing
+from abc import ABC, abstractmethod
+from types import NoneType
+
+from fntypes.option import Nothing, Some
+
+from telegrinder.msgspec_utils import Option
+from telegrinder.types.objects import (
+    InlineKeyboardMarkup,
+    ReplyKeyboardMarkup,
+    ReplyKeyboardRemove,
+)
+
+from .buttons import Button, ButtonT, InlineButton, RowButtons
+
+DictStrAny = dict[str, typing.Any]
+AnyMarkup = InlineKeyboardMarkup | ReplyKeyboardMarkup
+
+
+def keyboard_remove(*, selective: bool | None = None) -> ReplyKeyboardRemove:
+    return ReplyKeyboardRemove(
+        remove_keyboard=True,
+        selective=Nothing() if selective is None else Some(selective),
+    )
+
+
+@dataclasses.dataclass
+class KeyboardModel:
+    resize_keyboard: bool | Option[bool]
+    one_time_keyboard: bool | Option[bool]
+    selective: bool | Option[bool]
+    is_persistent: bool | Option[bool]
+    keyboard: list[list[dict[str, typing.Any]]]
+
+
+class ABCMarkup(ABC, typing.Generic[ButtonT]):
+    BUTTON: type[ButtonT]
+    keyboard: list[list[dict[str, typing.Any]]]
+
+    @abstractmethod
+    def dict(self) -> DictStrAny:
+        pass
+
+    @abstractmethod
+    def get_markup(self) -> AnyMarkup:
+        pass
+
+    @classmethod
+    def empty(cls) -> AnyMarkup:
+        return cls().get_markup()
+
+    def add(self, row_or_button: RowButtons[ButtonT] | ButtonT) -> typing.Self:
+        if not len(self.keyboard):
+            self.row()
+        
+        if isinstance(row_or_button, RowButtons):
+            self.keyboard[-1].extend(row_or_button.get_data())
+            if row_or_button.auto_row:
+                self.row()
+            return self
+
+        self.keyboard[-1].append(row_or_button.get_data())
+        return self
+
+    def row(self) -> typing.Self:
+        if len(self.keyboard) and not len(self.keyboard[-1]):
+            raise RuntimeError("Last row is empty!")
+
+        self.keyboard.append([])
+        return self
+
+    def format(self, **format_data: typing.Dict[str, str]) -> "ABCMarkup":
+        copy_keyboard = self.__class__()
+        for row in self.keyboard:
+            for button in row:
+                copy_button = button.copy()
+                copy_button["text"] = copy_button["text"].format(**format_data)
+                copy_keyboard.add(self.BUTTON(**copy_button))
+            copy_keyboard.row()
+        return copy_keyboard
+
+    def merge(self, other: typing.Self) -> typing.Self:
+        self.keyboard.extend(other.keyboard)
+        return self
+
+
+class Keyboard(ABCMarkup[Button], KeyboardModel):
+    BUTTON = Button
+
+    def __init__(
+        self,
+        *,
+        resize_keyboard: bool = True,
+        one_time_keyboard: bool = False,
+        selective: bool = False,
+        is_persistent: bool = False,
+    ):
+        self.keyboard = [[]]
+        self.resize_keyboard = resize_keyboard
+        self.one_time_keyboard = one_time_keyboard
+        self.selective = selective
+        self.is_persistent = is_persistent
+
+    def dict(self) -> DictStrAny:
+        self.keyboard = [row for row in self.keyboard if row]
+        return {
+            k: v.unwrap() if v and isinstance(v, Some) else v
+            for k, v in self.__dict__.items()
+            if type(v) not in (NoneType, Nothing)
+        }
+
+    def get_markup(self) -> ReplyKeyboardMarkup:
+        return ReplyKeyboardMarkup(**self.dict())
+
+
+class InlineKeyboard(ABCMarkup[InlineButton]):
+    BUTTON = InlineButton
+
+    def __init__(self) -> None:
+        self.keyboard = [[]]
+
+    def dict(self) -> DictStrAny:
+        self.keyboard = [row for row in self.keyboard if row]
+        return dict(inline_keyboard=self.keyboard)
+
+    def get_markup(self) -> InlineKeyboardMarkup:
+        return InlineKeyboardMarkup(**self.dict())
+
+
+__all__ = (
+    "ABCMarkup",
+    "InlineKeyboard",
+    "Keyboard",
+    "KeyboardModel",
+    "keyboard_remove",
+)
