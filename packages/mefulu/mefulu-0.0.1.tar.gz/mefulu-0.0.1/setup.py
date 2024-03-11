ff9bd080d734bb50d062921a8fbd0fb0744b4545
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 15:04:29 +0000

    Add package mefulu/0.0.1/mefulu-0.0.1.tar.gz

---
+++ packages/mefulu/mefulu-0.0.1.tar.gz/mefulu-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='mefulu',  # Имя пакета
+    version='0.0.1',  # Версия пакета
+    author='Zagumennov U.I.',
+    author_email='your@domain.com',
+    description='My Package',
+    packages=setuptools.find_packages(),
+)
