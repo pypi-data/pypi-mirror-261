dc65032e7cbbf4955ad5905d3814bb05fa530430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 04:03:28 +0000

    Add package thinkerforge2mqtt/0.0.1.dev0/thinkerforge2mqtt-0.0.1.dev0-py3-none-any.whl

---
+++ packages/thinkerforge2mqtt/thinkerforge2mqtt-0.0.1.dev0-py3-none-any.whl/thinkerforge2mqtt/__init__.py
@@ -0,0 +1,7 @@
+"""
+    thinkerforge2mqtt
+    Emit MQTT events from thinkerforge
+"""
+
+__version__ = '0.0.1.dev0'
+__author__ = 'Jens Diemer <git@jensdiemer.de>'
---
+++ packages/thinkerforge2mqtt/thinkerforge2mqtt-0.0.1.dev0-py3-none-any.whl/thinkerforge2mqtt/constants.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+import thinkerforge2mqtt
+
+
+CLI_EPILOG = 'Project Homepage: https://github.com/jedie/thinkerforge2mqtt'
+
+BASE_PATH = Path(thinkerforge2mqtt.__file__).parent
