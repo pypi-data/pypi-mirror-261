c5c8af9827b0d1e558d4cadabe0217f0a67e1ced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 15:04:18 +0000

    Add package apoplast/1.0.6/apoplast-1.0.6.tar.gz

---
+++ packages/apoplast/apoplast-1.0.6.tar.gz/apoplast-1.0.6/structures/modules/apoplast/shows/ingredient_scan/DB/scan/seek/__init__.py
@@ -0,0 +1,35 @@
+
+'''
+#
+#	find name
+#
+import apoplast.shows.ingredient_scan.DB.scan.seek as seek_nutrient
+nutrient = seek_nutrient.presently (
+	for_each = lambda essential : True if "thiamin" in essential ["names"] else False
+)
+'''
+
+'''
+#
+#	find region
+#
+import apoplast.shows.ingredient_scan.DB.scan.seek as seek_nutrient
+nutrient = seek_nutrient.presently (
+	for_each = lambda essential : True if essential ["region"] == 1 else False
+)
+'''
+
+import apoplast.shows.ingredient_scan.DB.scan.list as ingredients_DB_list_scan
+
+def for_each ():
+	return False
+	
+def presently (
+	for_each = for_each
+):
+	essentials = ingredients_DB_list_scan.retrieve ()
+	for essential in essentials:
+		if (for_each (essential)):
+			return essential
+	
+	return None
