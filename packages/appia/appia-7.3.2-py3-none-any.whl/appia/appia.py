8a61aa4dc7e51a8d3f725d3a2ebd74c6d9188efa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 14:04:31 +0000

    Add package appia/7.3.2/appia-7.3.2-py3-none-any.whl

---
+++ packages/appia/appia-7.3.2-py3-none-any.whl/appia/appia.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+import argparse
+import logging
+
+from appia.parsers.process_parser import parser as process_parser
+from appia.parsers.database_parser import parser as db_parser
+from appia.parsers.utilities_parser import parser as util_parser
+
+main_parser = argparse.ArgumentParser(
+    description="Process chromatography data and visualize it on the web."
+)
+subparsers = main_parser.add_subparsers()
+
+verbosity = main_parser.add_argument_group("Verbosity")
+vxg = verbosity.add_mutually_exclusive_group()
+vxg.add_argument(
+    "-q",
+    "--quiet",
+    help="Print Errors only",
+    action="store_const",
+    dest="verbosity",
+    const="q",
+)
+vxg.add_argument(
+    "-v",
+    "--verbose",
+    help="Print Info, Warnings, and Errors. Default state.",
+    action="store_const",
+    dest="verbosity",
+    const="v",
+)
+vxg.add_argument(
+    "--debug",
+    help="Print debug output.",
+    action="store_const",
+    dest="verbosity",
+    const="d",
+)
+
+subparsers.add_parser(name="process", help="Process data", parents=[process_parser])
+subparsers.add_parser(name="database", help="Manage CouchDB", parents=[db_parser])
+subparsers.add_parser(name="utils", help="Utilities", parents=[util_parser])
+
+
+def main():
+    args = main_parser.parse_args()
+
+    levels = {"q": logging.ERROR, "v": logging.INFO, "d": logging.DEBUG}
+    try:
+        level = levels[args.verbosity]
+    except KeyError:
+        level = logging.INFO
+
+    logging.basicConfig(
+        level=level, format="{levelname}: {message} ({filename})", style="{"
+    )
+
+    if "func" in args:
+        args.func(args)
+    else:
+        main_parser.print_help()
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/appia/appia-7.3.2-py3-none-any.whl/appia/parsers/database_parser.py
@@ -0,0 +1,74 @@
+import argparse
+import os
+from appia.processors.core import three_column_print
+
+
+def main(args):
+    from appia.processors.database import db
+
+    if args.list or args.check_versions:
+        list = db.update_experiment_list()
+        if args.check_versions:
+            out_of_date = False
+            for exp_id in list:
+                try:
+                    version = db.db.get(exp_id)["version"]
+                except KeyError:
+                    version = 0
+                if version != db.version:
+                    out_of_date = True
+                    print(f"Experiment {exp_id} is version {version}")
+            if not out_of_date:
+                print("All versions match.")
+        if args.list:
+            three_column_print(list)
+
+    if args.delete:
+        for id in args.delete:
+            db.remove_experiment(id)
+
+    if args.inspect:
+        for id in args.inspect:
+            exp = db.pull_experiment(id)
+            print(exp)
+            exp.show_tables()
+
+    if args.download:
+        for id in args.download:
+            exp = db.pull_experiment(id)
+            exp.save_csvs(os.getcwd())
+
+    if args.migrate:
+        db.migrate()
+
+
+parser = argparse.ArgumentParser(description="Database management", add_help=False)
+parser.set_defaults(func=main)
+parser.add_argument(
+    "-l",
+    "--list",
+    help="Print list of all experiments in database",
+    action="store_true",
+)
+parser.add_argument(
+    "-d", "--delete", help="Delete experiment(s) by name", type=str, nargs="+"
+)
+parser.add_argument(
+    "-i", "--inspect", help="Print information about experiments", type=str, nargs="+"
+)
+parser.add_argument(
+    "--download",
+    help="Save experiments from the database as a .csv. Note that these may have been downsampled.",
+    type=str,
+    nargs="+",
+)
+parser.add_argument(
+    "--check-versions",
+    help="List experiments, categorized by version.",
+    action="store_true",
+)
+parser.add_argument(
+    "--migrate",
+    help="Download and upload all experiments to migrate them to a new version. Back up first!!!",
+    action="store_true",
+)
