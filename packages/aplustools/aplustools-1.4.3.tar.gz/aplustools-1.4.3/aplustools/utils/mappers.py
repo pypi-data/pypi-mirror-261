e08ffe5ba04db88383b0975228a382a71ac3def4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 03:04:13 +0000

    Add package aplustools/1.4.3/aplustools-1.4.3.tar.gz

---
+++ packages/aplustools/aplustools-1.4.3.tar.gz/aplustools-1.4.3/aplustools/utils/mappers.py
@@ -0,0 +1,25 @@
+# utils/mappers.py
+
+class EmptyType:
+    pass
+
+def reverse_map(functions, input_value=EmptyType()):
+    if type(input_value) == EmptyType:
+        return [func() for func in functions]
+    return [func(input_value) for func in functions]
+
+# Implement map with generator like arguments?
+
+def local_test():
+    try:
+        reverse_map([print, print], "Hello World")
+    except Exception as e:
+        print(f"Exception occurred {e}.")
+        return False
+    else:
+        print("Test completed successfully.")
+        return True
+
+
+if __name__ == "__main__":
+    local_test()
