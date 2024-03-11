10514803cc6c75749baaf4ddd9e87a78d72f03cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 04:04:05 +0000

    Add package MapCoords/0.1.8/MapCoords-0.1.8-py3-none-any.whl

---
+++ packages/MapCoords/MapCoords-0.1.8-py3-none-any.whl/MapCoords/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# @Time     : 2018/5/16 16:49
+# @Author   : Run
+# @File     : __init__.py
+# @Software : PyCharm
+
+"""
+todo 1. 整理文件夹crawl_city_ring_coords
+"""
+
+from MapCoords.coord_transform import *
+from MapCoords.dis_compute import *
+from MapCoords.rings import *
+
+
