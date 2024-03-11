53fff67d855359145c2aaed27a02c2caeffc9602
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 03:03:53 +0000

    Add package loopgpt/0.1.1/loopgpt-0.1.1-py3-none-any.whl

---
+++ packages/loopgpt/loopgpt-0.1.1-py3-none-any.whl/loopgpt/tools/math.py
@@ -0,0 +1,17 @@
+from loopgpt.tools import BaseTool
+
+from simpleeval import simple_eval
+
+
+class EvaluateMath(BaseTool):
+    """Evaluate simple math expressions.
+
+    Args:
+        expression (str): The expression to evaluate.
+
+    Returns:
+        str: The result of the expression.
+    """
+
+    def run(self, expression: str):
+        return str(simple_eval(expression))
