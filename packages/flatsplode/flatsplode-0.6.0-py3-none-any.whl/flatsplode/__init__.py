0884b5b82d5ddcb423af905164e8fea245424549
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 03:04:18 +0000

    Add package flatsplode/0.6.0/flatsplode-0.6.0-py3-none-any.whl

---
+++ packages/flatsplode/flatsplode-0.6.0-py3-none-any.whl/flatsplode/__init__.py
@@ -0,0 +1,9 @@
+"""
+flatsplode
+"""
+
+from .flatsplode import explode  # noqa: F401
+from .flatsplode import flatsplode  # noqa: F401
+from .flatsplode import flatten  # noqa: F401
+
+__version__ = "0.6.0"
