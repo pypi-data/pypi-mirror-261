3a1bf8bd4944f6b67f2de84e6795b77034a97a20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 15:04:51 +0000

    Add package QuenserLibrary/0.0.9/QuenserLibrary-0.0.9.tar.gz

---
+++ packages/QuenserLibrary/QuenserLibrary-0.0.9.tar.gz/QuenserLibrary-0.0.9/QuenserLibrary/QuenserLibrary.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+#QuenserLibrary.py
+
+from robot.api.deco import keyword
+
+class QuenserLibrary:
+    def PrintTEST(self):
+        """ พิมพ์ข้อความ 'Hello, world!' ลงในคอนโซล """
+        print("Hello, world!")
