0e9e74910d2f1a1d133398bd59b6953bbedfe93d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 16:05:24 +0000

    Add package richie/2.25.0b2.dev136/richie-2.25.0b2.dev136.tar.gz

---
+++ packages/richie/richie-2.25.0b2.dev136.tar.gz/richie-2.25.0b2.dev136/src/frontend/js/utils/test/expectBanner.ts
@@ -0,0 +1,34 @@
+import { screen, waitFor } from '@testing-library/react';
+import { BannerType, getBannerTestId } from 'components/Banner';
+
+export const expectBannerError = async (message: string, rootElement: ParentNode = document) => {
+  return expectBanner(BannerType.ERROR, message, rootElement);
+};
+export const expectBannerInfo = async (message: string, rootElement: ParentNode = document) => {
+  return expectBanner(BannerType.INFO, message, rootElement);
+};
+
+export const expectBanner = async (
+  type: BannerType,
+  message: string,
+  rootElement: ParentNode = document,
+) => {
+  await waitFor(async () => {
+    const banner = rootElement.querySelector('.banner--' + type) as HTMLElement;
+    expect(banner).not.toBeNull();
+    expect(banner).toHaveTextContent(message);
+  });
+};
+
+export const expectNoBannerError = async (message: string) => {
+  return expectNoBanner(BannerType.ERROR, message);
+};
+export const expectNoBannerInfo = async (message: string) => {
+  return expectNoBanner(BannerType.INFO, message);
+};
+
+export const expectNoBanner = async (type: BannerType, message: string) => {
+  await waitFor(() => {
+    expect(screen.queryByTestId(getBannerTestId(message, type))).toBeNull();
+  });
+};
