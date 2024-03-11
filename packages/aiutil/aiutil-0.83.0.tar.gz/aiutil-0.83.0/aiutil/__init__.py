132d7faa4d323bf091a6dabb3aa820056f40977c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 14:05:02 +0000

    Add package aiutil/0.83.0/aiutil-0.83.0.tar.gz

---
+++ packages/aiutil/aiutil-0.83.0.tar.gz/aiutil-0.83.0/aiutil/__init__.py
@@ -0,0 +1,6 @@
+"""A utils Python package for data scientists.
+"""
+
+from . import poetry
+
+__version__ = "0.83.0"
---
+++ packages/aiutil/aiutil-0.83.0.tar.gz/aiutil-0.83.0/aiutil/hadoop/__init__.py
@@ -0,0 +1,5 @@
+"""Sub-package for Hadoop.
+"""
+
+from .hdfs import Hdfs
+from .log import LogFilter
