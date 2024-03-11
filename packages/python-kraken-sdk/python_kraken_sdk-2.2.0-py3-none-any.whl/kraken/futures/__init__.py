0fc552a2957bbe86cd86012a97d8ab74ede05446
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 03:03:58 +0000

    Add package python-kraken-sdk/2.2.0/python_kraken_sdk-2.2.0-py3-none-any.whl

---
+++ packages/python-kraken-sdk/python_kraken_sdk-2.2.0-py3-none-any.whl/kraken/futures/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2023 Benjamin Thomas Schwertfeger
+# GitHub: https://github.com/btschwertfeger
+# pylint: disable=unused-import
+
+"""This module provides the Kraken Futures clients"""
+
+from kraken.futures.funding import Funding
+from kraken.futures.market import Market
+from kraken.futures.trade import Trade
+from kraken.futures.user import User
+from kraken.futures.ws_client import KrakenFuturesWSClient
+
+__all__ = ["Funding", "KrakenFuturesWSClient", "Market", "Trade", "User"]
