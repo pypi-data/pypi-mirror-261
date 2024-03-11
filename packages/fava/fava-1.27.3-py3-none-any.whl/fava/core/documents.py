6991da2e734fe57ccae084824e96f520cafbb030
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 15:04:33 +0000

    Add package fava/1.27.3/fava-1.27.3-py3-none-any.whl

---
+++ packages/fava/fava-1.27.3-py3-none-any.whl/fava/core/documents.py
@@ -0,0 +1,68 @@
+"""Document path related helpers."""
+
+from __future__ import annotations
+
+from os import altsep
+from os import sep
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+from fava.helpers import FavaAPIError
+
+if TYPE_CHECKING:  # pragma: no cover
+    from fava.core import FavaLedger
+
+
+def is_document_or_import_file(filename: str, ledger: FavaLedger) -> bool:
+    """Check whether the filename is a document or in an import directory.
+
+    Args:
+        filename: The filename to check.
+        ledger: The FavaLedger.
+
+    Returns:
+        Whether this is one of the documents or a path in an import dir.
+    """
+    if any(
+        filename == d.filename for d in ledger.all_entries_by_type.Document
+    ):
+        return True
+    file_path = Path(filename).resolve()
+    return any(
+        str(file_path).startswith(str(ledger.join_path(d)))
+        for d in ledger.fava_options.import_dirs
+    )
+
+
+def filepath_in_document_folder(
+    documents_folder: str,
+    account: str,
+    filename: str,
+    ledger: FavaLedger,
+) -> Path:
+    """File path for a document in the folder for an account.
+
+    Args:
+        documents_folder: The documents folder.
+        account: The account to choose the subfolder for.
+        filename: The filename of the document.
+        ledger: The FavaLedger.
+
+    Returns:
+        The path that the document should be saved at.
+    """
+    if documents_folder not in ledger.options["documents"]:
+        raise FavaAPIError(f"Not a documents folder: {documents_folder}.")
+
+    if account not in ledger.attributes.accounts:
+        raise FavaAPIError(f"Not a valid account: '{account}'")
+
+    for separator in sep, altsep:
+        if separator:
+            filename = filename.replace(separator, " ")
+
+    return ledger.join_path(
+        documents_folder,
+        *account.split(":"),
+        filename,
+    )
