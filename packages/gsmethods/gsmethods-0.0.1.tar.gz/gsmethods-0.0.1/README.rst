43dbf46042ded4b976c839c3e84fae70301fc71a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 04:04:05 +0000

    Add package gsmethods/0.0.1/gsmethods-0.0.1.tar.gz

---
+++ packages/gsmethods/gsmethods-0.0.1.tar.gz/gsmethods-0.0.1/README.rst
@@ -0,0 +1,4 @@
+# GSMethods
+
+## What is it?
+**gsmethods** is a python package that provides tools for creating and analysing group sequential statistical tests.
---
+++ packages/gsmethods/gsmethods-0.0.1.tar.gz/gsmethods-0.0.1/gsmethods/main.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+import numpy as np
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    a = np.array([1, 2, 3, 4])
+    print(a)
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
