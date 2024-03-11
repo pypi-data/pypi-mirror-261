71a7023593d84e7eed620485ecc52809b734de93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 15:04:35 +0000

    Add package stereosite/1.1.3/stereosite-1.1.3.tar.gz

---
+++ packages/stereosite/stereosite-1.1.3.tar.gz/stereosite-1.1.3/setup.py
@@ -0,0 +1,30 @@
+from  setuptools import setup, find_packages
+
+with open("PYPI.rst", "r") as f:
+    long_description = f.read()
+
+setup(
+    name='stereosite',
+    version='1.1.3',
+    author='LiuXing',
+    author_email='liuxing2@genomics.cn',
+    description=('Analysis spatial transcriptomics data'),
+    long_description=long_description,
+    license='GPL-3 License',
+    keywords='spatial cell interaction intensity',
+    url="https://github.com/STOmics/StereoSiTE",
+
+    packages=['stereosite'], #需要打包的目录列表
+
+    include_package_data=True,
+    platforms='any',
+    #需要安装的依赖包
+    install_requires = [
+        'anndata>=0.8.0',
+        'scanpy>=1.9.1',
+        'squidpy>=1.1.2',
+        'decoupler>=1.4.0',
+        'pydeseq2>=0.3.6',
+        'networkx>=3.1'
+    ],
+)
