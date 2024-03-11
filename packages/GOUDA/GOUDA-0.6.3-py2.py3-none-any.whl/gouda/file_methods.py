8d02286da54b30e31f93e1afccc5ce882ad3b0d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 05:04:46 +0000

    Add package GOUDA/0.6.3/GOUDA-0.6.3-py2.py3-none-any.whl

---
+++ packages/GOUDA/GOUDA-0.6.3-py2.py3-none-any.whl/gouda/file_methods.py
@@ -0,0 +1,320 @@
+"""General file method and JSON handling"""
+import copy
+import glob
+import gzip
+import imghdr
+import json
+import numpy as np
+import os
+import warnings
+
+from gouda.data_methods import num_digits
+
+__author__ = "Sean Mullan"
+__copyright__ = "Sean Mullan"
+__license__ = "mit"
+
+
+def ensure_dir(*paths):
+    """Check if a given directory exists, and create it if it doesn't. Multiple directories can be passed as a top-to-bottom path structure.
+
+    Parameters
+    ----------
+    *paths : iterable
+        One or more nested directories to ensure
+
+    Returns
+    -------
+    str
+        Joined filepath of all ensured directories
+
+    """
+    # TODO - Can this all be replaced with `os.makedirs(os.path.join([str(path) for path in paths]))`? The only difference is the non-dir check
+    full_path = ''
+    for path in paths:
+        full_path = os.path.join(full_path, str(path))
+        if os.path.exists(full_path) and os.path.isdir(full_path):
+            continue
+        elif os.path.exists(full_path):
+            raise ValueError("A file without an extension is blocking directory creation at {}".format(full_path))
+        else:
+            os.makedirs(full_path, exist_ok=True)
+    return full_path
+
+
+# ! TODO - Update tests for new formatting
+def next_filename(filename: str, first_idx: int = 2, path_fmt: str = '{base_name}_{idx:01d}{ext}'):
+    """Check if a given file exists, and return a new filename for a numbered copy if it does.
+
+    Parameters
+    ----------
+    filename : str
+        The base filename to iterate on
+    first_idx : int, optional
+        Index of the first numbered copy to check, by default 2
+    path_fmt : str, optional
+        Format for subsequent filepaths, by default '{base_name}_{idx:01d}{ext}'
+
+    NOTE
+    ----
+    For path_fmt, base_name is the basename of the file (i.e. the filename without the extension), idx is the index of the numbered copy, and ext is the extension of the file including the period(s) (see :func:`~gouda.file_methods.fullsplit` for path/basename/extension split).
+    The default path_fmt assumes a maximum of 9 numbered copies. If you need more, change the 01d to 02d, 03d, etc. with the center digit indicating the number of digits in the maximum expected index.
+    """
+    filename = str(filename)
+    if os.path.isfile(filename):
+        path, base, extension = fullsplit(filename)
+        i = first_idx
+        while True:
+            next_check = os.path.join(path, path_fmt.format(base_name=base, idx=i, ext=extension))
+            if os.path.isfile(next_check):
+                i += 1
+            else:
+                return next_check
+    else:
+        return filename
+
+
+# JSON methods
+def load_json(filename):
+    """Load a JSON file, and re-form any numpy arrays if :func:`~gouda.save_json` was used to write them."""
+    with open(filename, 'r') as f:
+        data = json.load(f)
+    if isinstance(data, dict) and 'slice_start' in data:
+        data = slice(data['slice_start'], data['slice_stop'], data['slice_step'])
+    elif isinstance(data, list):
+        if data[-1] == 'numpy':
+            np_filename = filename.rsplit('.', 1)[0] + '_array.npz'
+            arrays = np.load(np_filename)
+            data = data[0]
+        elif data[-1] == 'numpy_zip':
+            np_filename = filename.rsplit('.', 1)[0] + '_arrayzip.npz'
+            arrays = np.load(np_filename)
+            data = data[0]
+        elif data[-1] == 'numpy_embed':
+            data = data[0]
+        # else:
+        #     return data
+
+    def renumpy(_data):
+        if isinstance(_data, list):
+            if len(_data) == 2 and isinstance(_data[0], str):
+                if 'numpy.' in _data[0]:
+                    _data = np.dtype(_data[0][6:]).type(_data[1])
+                elif 'set.' in _data[0]:
+                    _data = set(renumpy(_data[1]))
+            else:
+                for i in range(len(_data)):
+                    _data[i] = renumpy(_data[i])
+        elif isinstance(_data, dict):
+            if 'numpy_array' in _data:
+                if isinstance(_data['numpy_array'], list):
+                    new_data = np.array(_data['numpy_array'], dtype=_data['dtype']).reshape(_data['shape'])
+                else:
+                    new_data = arrays[_data['numpy_array']]
+                    if new_data.dtype != _data['dtype'] or list(new_data.shape) != _data['shape']:
+                        raise ValueError("Numpy array file doesn't match expected stored numpy array data")
+                return new_data
+            elif 'slice_start' in _data:
+                _data = slice(_data['slice_start'], _data['slice_stop'], _data['slice_step'])
+            else:
+                for key in _data.keys():
+                    _data[key] = renumpy(_data[key])
+        return _data
+
+        # if len(data) == 1:
+        #     data = data[0]
+    data = renumpy(data)
+    return data
+
+
+def is_jsonable(data):
+    """Check to see if data is JSON serializable"""
+    try:
+        json.dumps(data)
+        return True
+    except (TypeError, OverflowError):
+        return False
+
+
+def save_json(data, filename, embed_arrays=True, compressed=False):
+    """Save a list/dict/numpy.ndarray as a JSON file.
+
+    Parameters
+    ----------
+    data : [list, dict, numpy.ndarray]
+        Data to save as a JSON file
+    filename : string
+        Path to write the JSON to
+    embed_arrays : bool [defaults to True]
+        Whether to embed any numpy arrays into the JSON as lists with metadata. If false saves them to a separate file with placeholders in the JSON.
+    compressed : bool [defaults to False]
+        If saving numpy arrays in a separate file, this determines if they are zipped or not.
+
+    NOTE
+    ----
+    JSON files saved this way can be read with any JSON reader, but will have an extra numpy tag at the end that is used to tell :func:`~gouda.load_json` how to read the arrays back in.
+    """
+    out_arrays = {}
+    used_arrays = [False]
+    if embed_arrays and compressed:
+        warnings.warn('Cannot compress an array that is embedded in a JSON', UserWarning)
+        compressed = False
+
+    def unnumpy(_data):
+        if isinstance(_data, (list, tuple)):
+            new_data = []
+            for i in range(len(_data)):
+                new_data.append(unnumpy(_data[i]))
+        elif isinstance(_data, set):
+            new_data = ['set.', unnumpy(list(_data))]
+        elif isinstance(_data, dict):
+            new_data = {}
+            for key in _data.keys():
+                new_data[key] = unnumpy(_data[key])
+        elif isinstance(_data, slice):
+            new_data = {'slice_start': _data.start, 'slice_stop': _data.stop, 'slice_step': _data.step}
+        elif isinstance(_data, np.ndarray):
+            used_arrays[0] = True
+            if embed_arrays:
+                new_data = {"numpy_array": _data.tolist(), "dtype": str(_data.dtype), "shape": _data.shape}
+            else:
+                new_data = {"numpy_array": 'array_{}'.format(len(out_arrays)), 'dtype': str(_data.dtype), 'shape': _data.shape}
+                out_arrays['array_{}'.format(len(out_arrays))] = _data
+        elif 'numpy' in str(type(_data)):
+            dtype = str(_data.dtype)
+            if np.issubdtype(_data.dtype, np.integer):
+                _data = int(_data)
+            elif np.issubdtype(_data.dtype, np.floating):
+                _data = float(_data)
+            else:
+                raise ValueError('Un-JSON-able dtype {} found'.format(dtype))
+            new_data = ['numpy.' + dtype, _data]
+        else:
+            new_data = copy.copy(_data)
+        return new_data
+
+    data = unnumpy(data)
+    if used_arrays[0]:
+        data = [data]
+        if compressed:
+            data.append('numpy_zip')
+        elif embed_arrays:
+            data.append('numpy_embed')
+        else:
+            data.append('numpy')
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=2, sort_keys=True)
+    if len(out_arrays) != 0:
+        np_filename = filename.rsplit('.', 1)[0]
+        if compressed:
+            np.savez_compressed(np_filename + '_arrayzip.npz', **out_arrays)
+        else:
+            np.savez(np_filename + '_array.npz', **out_arrays)
+
+
+def is_image(path):
+    """Check if the path is an image file"""
+    path = str(path)
+    try:
+        return imghdr.what(path) is not None
+    except IsADirectoryError:
+        return False
+
+
+def fullsplit(path):
+    """Split the path into head, basename, and extension
+
+    NOTE
+    ----
+    * This splits at the first non-leading period of the basename, compared to os.path.splitext which splits the whole path at the last period.
+    * Leading periods are considered to be part of the basename
+    """
+    # return os.path.split(path)
+    head, tail = os.path.split(path)
+    splitpath = tail.split('.')
+    to_add = ''
+    while splitpath[0] == '':
+        splitpath = splitpath[1:]
+        to_add += '.'
+    splitpath[0] = to_add + splitpath[0]
+    if len(splitpath) == 1:
+        return head, splitpath[0], ''
+    elif len(splitpath) > 2:
+        return head, splitpath[0], '.' + '.'.join(splitpath[1:])
+    else:
+        return head, splitpath[0], '.' + splitpath[1]
+
+
+def basicname(path):
+    """Return the basename of the path without the extension"""
+    return fullsplit(path)[1]
+
+
+def get_sorted_filenames(pattern, sep='_', ending=True, reverse=False):
+    """Sort filenames based on ending digits
+
+    Parameters
+    ----------
+    pattern : str
+        The pattern used with glob to find files
+    sep : str
+        The separator between the filename and the indexing value (the default is '_')
+    ending_index : bool
+        Whether the indexing value is at the end of the filename or the start (the default is True)
+    reverse : bool
+        Whether to reverse the order of the returned filenames
+
+    NOTESs
+    -----
+    ending_index=True with sep='_' would look like 'filename_1.txt', and ending_index=False would look like '1_filename.txt'
+
+    This method is only useful in the case where you have file_2.txt and file_10.txt where file_10 would be sorted first with other methods because the 1 is at the same index as the 2.
+    """
+    def get_copy_num(x):
+        x = basicname(x)
+        item = x.rsplit(sep, 1) if ending else x.split(sep, 1)
+        if len(item) != 2:
+            return -1
+        item = item[-1 if ending else 0]
+        if str.isdigit(item):
+            return int(item)
+        else:
+            return -1
+
+    pattern = str(pattern)
+    files = glob.glob(pattern)
+    max_num = -1
+    for item in files:
+        max_num = max(max_num, get_copy_num(item))
+    digits = num_digits(max_num)
+    key_string = "{:0" + str(digits) + "d}"
+
+    def get_copy_key(x):
+        x = basicname(x)
+        item = x.rsplit(sep, 1) if ending else x.split(sep, 1)
+        if len(item) != 2:
+            return x
+        key = item[int(ending)]
+        path = item[int(~ending)]
+        if str.isdigit(key):
+            key = key_string.format(int(key))
+        return sep.join([path, key]) if ending else sep.join([key, path])
+
+    return sorted(files, key=get_copy_key, reverse=False)
+
+
+def save_arr(path, arr):
+    if path.endswith('.gz'):
+        with gzip.open(path, 'wb') as f:
+            np.save(f, arr)
+    else:
+        np.save(path, arr)
+
+
+def read_arr(path):
+    if path.endswith('.gz'):
+        with gzip.open(path, 'rb') as f:
+            data = np.load(f)
+    else:
+        data = np.load(path)
+    return data
---
+++ packages/GOUDA/GOUDA-0.6.3-py2.py3-none-any.whl/gouda/plot_methods.py
@@ -0,0 +1,170 @@
+"""These are all matplotlib helper methods, and are still being developed."""
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.patches import PathPatch
+from matplotlib.path import Path
+
+from gouda.typing import ColorType
+
+
+def parse_color(color: ColorType, float_cmap='viridis', int_cmap='Set1') -> tuple[float, float, float]:
+    """Convert the input to a rgb color
+
+    NOTE
+    ----
+    Recognizes all formats that can be used with matplotlib in addition to rgb/rgba tuples as strings [ie. '(0.1, 0.2, 0.5)'] and single values.
+    Single values will be mapped to the given matplotlib colormap. Ints will wrap-around and floats will be clipped to [0, 1].
+
+    TODO - add lookup to get hexcodes from colors.py
+    """
+    try:
+        return matplotlib.colors.to_rgb(color)  # type: ignore  # NOTE - same as to_rgba[:3]
+    except ValueError:
+        if isinstance(color, str):
+            # Format is comma- and/or space-separated values
+            bad_chars = str.maketrans({item: '' for item in '()[]'})
+            color.translate(bad_chars)
+            if ', ' in color:
+                divided = color.split(', ')
+            elif ',' in color:
+                divided = color.split(',')
+            else:
+                divided = color.split(' ')
+            rgb = np.array(divided).astype(np.float32)
+            return matplotlib.colors.to_rgb(rgb / 255 if rgb.max() > 1.0 else rgb)  # type: ignore
+        elif isinstance(color, float):
+            return matplotlib.colormaps.get_cmap(float_cmap)
+        elif isinstance(color, int):
+            return matplotlib.colormaps.get_cmap(int_cmap)(color % 9)
+        else:
+            # Format is any array-like set of values
+            rgb = np.array(color).astype(np.float32)
+            return matplotlib.colors.to_rgb(rgb / 255 if rgb.max() > 1.0 else rgb)  # type: ignore
+
+
+def plot_accuracy_curve(acc, thresh, label_height=0.5, line_args={}, thresh_args={}):
+    """Plot the accuracy for the given accuracy and threshold values.
+
+    Parameters
+    ----------
+    acc : numpy.ndarray
+        An array of accuracy values for each given threshold
+    thresh : numpy.ndarray
+        An array of threshold values matching each accuracy value
+    label_height : float
+        The value between 0 and 1 for the y-position of the threshold label on the graph. Set to None for no label (default is 0.5)
+    line_args : dict
+        A dictionary of keyword arguments to pass when plotting the accuracy line
+    thresh_args : dict
+        A dictionary of keyword arguments to pass when plotting the threshold vline
+    """
+    line_defaults = {'color': 'black', 'label': 'accuracy'}
+    for key in line_defaults:
+        if key not in line_args:
+            line_args[key] = line_defaults[key]
+    thresh_defaults = {'color': 'r', 'label': 'best threshold'}
+    for key in thresh_defaults:
+        if key not in thresh_args:
+            thresh_args[key] = thresh_defaults[key]
+
+    best_idx = np.argmax(acc)
+    best_thresh = thresh[best_idx]
+    best_acc = acc[best_idx]
+
+    plt.plot(thresh, acc, **line_args)
+    plt.vlines(best_thresh, 0, 1, **thresh_args)
+    plt.ylim(0, 1)
+    plt.xlim(0, 1)
+    if label_height is not None:
+        plt.text(best_thresh + 0.01, label_height, "{:.2f}% @ {:.2f}".format(best_acc * 100, best_thresh))
+    plt.xlabel('Prediction Threshold')
+    plt.ylabel('Accuracy')
+
+
+def quick_line(x1, x2, y1, ytop, y2, lw=1):
+    """Generate a path in matplotlib"""
+    verts = [
+        (x1, y1),
+        (x1, ytop),
+        (x2, ytop),
+        (x2, y2)
+    ]
+    codes = [
+        Path.MOVETO,
+        Path.LINETO,
+        Path.LINETO,
+        Path.LINETO
+    ]
+    path = Path(verts, codes)
+    patch = PathPatch(path, fill=False, lw=1)
+    return patch
+
+
+def annotate_arrows(ax,
+                    text,
+                    start_point,
+                    end_points,
+                    y_top=None,
+                    line_width=1,
+                    direction='left',
+                    x_spacing=0.05,
+                    y_spacing=0.07,
+                    y_top_spacing=0.02,
+                    repeat_text=True,
+                    text_spacing=0.02):
+    """Add arrows that start at a point, go upwards, go over, and then descend to another point - mostly useful for bar graphs
+
+    Parameters
+    ----------
+    ax : plt.Axis (?)
+        Axis to plot on
+    text : str
+        text to annotate over lines
+    start_point : (float, float)
+        x, y coordinates of the base location
+    end_points : list of (float, float)
+        x, y end-points for the lines
+    y_top : float
+        The height of the top of the lines. If none, automatically selected above the highest point
+    line_width : float
+        line width
+    direction : 'left' | 'right'
+        The direction of the end-points from the base
+    x_spacing : float
+        horizontal distance between starting points
+    y_spacing : float
+        vertical distance from point to line start
+    y_top_spacing : float
+        vertical distance between top lines
+    repeat_text : bool
+        if true, text added over top line. If false, text added over each line and vertical spacing increased
+    text_spacing : bool
+        extra spacing between lines if text repeated
+    """
+    end_points = sorted(end_points, key=lambda x: x[0], reverse=direction == 'left')
+    if not repeat_text:
+        text_spacing = 0.
+    if y_top is None:
+        y_top = max([point[1] for point in end_points] + [start_point[1]]) + y_top_spacing + y_spacing
+
+    x_jitter = np.arange(len(end_points)) * x_spacing
+    x_jitter -= x_jitter[-1] / 2.
+
+    # best_mid = [0, None]
+    x_start, y_start = start_point
+    y_start = y_start + y_spacing
+    max_y = 0
+    for i in range(len(end_points)):
+        x_end, y_end = end_points[i]
+        y_end = y_end + y_spacing
+        max_y = y_top + (y_top_spacing + text_spacing) * i
+        line = quick_line(x_start + x_jitter[i], x_end, y_start, max_y, y_end, lw=line_width)
+        if repeat_text:
+            ax.annotate(text, xy=((x_start + x_end) / 2., max_y), ha='center', va='bottom', zorder=10)
+        ax.add_patch(line)
+
+    x_vals = [point[0] for point in end_points] + [x_start]
+    mid_x = (min(x_vals) + max(x_vals)) / 2.
+    if not repeat_text:
+        ax.annotate(text, xy=(mid_x, y_top + y_top_spacing * (len(end_points) - 1)), ha='center', va='bottom', zorder=10)
