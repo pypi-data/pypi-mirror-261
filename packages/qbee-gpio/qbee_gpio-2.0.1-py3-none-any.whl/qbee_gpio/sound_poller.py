76805fa4cdc5044c6274781a1584a321f7f557e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 16:05:40 +0000

    Add package qbee-gpio/2.0.1/qbee_gpio-2.0.1-py3-none-any.whl

---
+++ packages/qbee-gpio/qbee_gpio-2.0.1-py3-none-any.whl/qbee_gpio/sound_poller.py
@@ -0,0 +1,37 @@
+import asyncio
+import logging
+from dataclasses import dataclass
+from pathlib import Path
+from typing import AsyncIterator, Optional
+
+from asyncinotify import Inotify, Mask
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class SoundPoller:
+    """Poll sound driver activity and expose whether something is playing."""
+
+    # Path to the sound driver.
+    driver_path: Path
+    currently_in_use_command: Optional[str]
+
+    async def poll(self) -> AsyncIterator[bool]:
+        with Inotify() as inotify:
+            inotify.add_watch(self.driver_path, Mask.OPEN | Mask.CLOSE_WRITE)
+            logger.debug("started sound poller")
+            # Check whether it's currently playing.
+            if self.currently_in_use_command:
+                process = await asyncio.create_subprocess_shell(
+                    self.currently_in_use_command,
+                    stdout=asyncio.subprocess.PIPE,
+                    stderr=asyncio.subprocess.PIPE,
+                )
+                stdout, _ = await process.communicate()
+                if stdout:
+                    yield True
+            async for event in inotify:
+                output = event.mask == Mask.OPEN
+                logger.debug("sound driver %sin use", "" if output else "not ")
+                yield output
