d676747cb1a58382e4566835c7889b5cfcf30391
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 15:04:51 +0000

    Add package paibox/1.0.0a4/paibox-1.0.0a4-py3-none-any.whl

---
+++ packages/paibox/paibox-1.0.0a4-py3-none-any.whl/paibox/__init__.py
@@ -0,0 +1,35 @@
+from .backend import BACKEND_CONFIG as BACKEND_CONFIG
+from .backend import Mapper as Mapper
+from .base import *
+from .context import FRONTEND_ENV as FRONTEND_ENV
+from .network import DynSysGroup as DynSysGroup
+from .network import Network as Network
+from .neuron import *
+from .node import NodeDict as NodeDict
+from .node import NodeList as NodeList
+from .projection import InputProj as InputProj
+from .simulator import Probe as Probe
+from .simulator import Simulator as Simulator
+from .synapses import NoDecay as NoDecay
+from .synapses import SynConnType as SynConnType
+
+__all__ = [
+    "Mapper",
+    "DynSysGroup",
+    "Network",
+    "NodeDict",
+    "NodeList",
+    "InputProj",
+    "Simulator",
+    "SynConnType",
+    "Probe",
+    "BACKEND_CONFIG",
+    "FRONTEND_ENV",
+]
+
+from importlib.metadata import version
+
+try:
+    __version__ = version("paibox")
+except Exception:
+    __version__ = None
