b6241303bb18e4058530de2826beb79b86ec7e30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 14:04:38 +0000

    Add package teaxyzz/0.0.2/teaxyzz-0.0.2.tar.gz

---
+++ packages/teaxyzz/teaxyzz-0.0.2.tar.gz/teaxyzz-0.0.2/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+setup(
+    author='User',
+    author_email='example@mail.com',
+    name='teaxyzz',
+    version='0.0.2',
+    description='A simple package for https://app.tea.xyz/. Example tea-xyz1 - https://github.com/madest92/tea-xyz1 and tea-xyz2 - https://github.com/madest92/tea-xyz2',
+    url='https://github.com/madest92/teaxyz',
+    project_urls={
+        'Homepage': 'https://github.com/madest92/teaxyz',
+        'Source': 'https://github.com/madest92/teaxyz',
+    },
+    py_modules=['hi_tea'],
+    entry_points={
+        'console_scripts': [
+            'hi-tea=hi_tea:hello_tea_xyz'
+        ]
+    },
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'tea-xyz1',
+        'tea-xyz2',
+        'teaxyz',
+        'teaxyzzz',
+        'teaxyzzzz',
+        'teaxyzzzzz',
+        'teaxyzzzzzz',
+        # add your projects
+    ],
+)
