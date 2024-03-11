e6edf4407465b52c70adc1926343270d7a4627d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 15:04:29 +0000

    Add package new-papka/0.0.1/new_papka-0.0.1.tar.gz

---
+++ packages/new-papka/new_papka-0.0.1.tar.gz/new_papka-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='new_papka', # Имя пакета
+    version='0.0.1',  # Версия пакета
+    author='Polina',
+    author_email='your@domain.com',
+    description='My Package',
+    packages=setuptools.find_packages(),
+)
