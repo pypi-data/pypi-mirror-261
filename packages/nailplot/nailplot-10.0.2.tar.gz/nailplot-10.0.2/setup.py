352c7129ad53bd69015902fb6f4a5eef12a601e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 14:04:58 +0000

    Add package nailplot/10.0.2/nailplot-10.0.2.tar.gz

---
+++ packages/nailplot/nailplot-10.0.2.tar.gz/nailplot-10.0.2/setup.py
@@ -0,0 +1,10 @@
+# setup.py
+
+from setuptools import setup
+
+setup(
+    name='nailplot',
+    version='10.0.2',
+    packages=[''],
+    install_requires=['requests', 'google', 'google.generativeai'],
+)
