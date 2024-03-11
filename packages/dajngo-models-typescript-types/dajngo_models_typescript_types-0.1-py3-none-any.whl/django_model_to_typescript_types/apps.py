80e8377321d982cecab98767f1455679aad0026b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 03:03:40 +0000

    Add package dajngo-models-typescript-types/0.1/dajngo_models_typescript_types-0.1-py3-none-any.whl

---
+++ packages/dajngo-models-typescript-types/dajngo_models_typescript_types-0.1-py3-none-any.whl/django_model_to_typescript_types/apps.py
@@ -0,0 +1,19 @@
+import os
+
+from django.apps import AppConfig
+from .modeltotypescriptconverter import ModelToTypeScriptConverter
+
+class DjangoModelToTypescriptTypesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'django_model_to_typescript_types'
+
+    def ready(self):
+        if os.environ.get('RUN_MAIN') :
+            print('Lets convert the models to typescript types')
+            # converter = ModelToTypeScriptConverter(os.environ.get('TS_APP_TO_INCLUDE', 'app'), os.environ.get('TS_PATH', '/tmp/tsinterface/interfaces.ts'), os.environ.get('TS_SEPERATED_FILES', False))
+            print("TS_SEPERATED_FILES "+os.environ.get('TS_SEPERATED_FILES', True))
+            converter = ModelToTypeScriptConverter(os.environ.get('TS_APP_TO_INCLUDE', 'app'), os.environ.get('TS_PATH', '/tmp/tsinterface/'), os.environ.get('TS_SEPERATED_FILES', True))
+            converter.generate_interfaces()
+        
+
+    
