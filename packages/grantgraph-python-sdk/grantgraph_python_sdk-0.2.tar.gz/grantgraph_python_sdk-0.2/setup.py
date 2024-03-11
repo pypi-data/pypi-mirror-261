eb39a7f4780de8680474c9a43c18751f02c023eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 04:06:20 +0000

    Add package grantgraph-python-sdk/0.2/grantgraph_python_sdk-0.2.tar.gz

---
+++ packages/grantgraph-python-sdk/grantgraph_python_sdk-0.2.tar.gz/grantgraph_python_sdk-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='grantgraph_python_sdk',
+    version='0.2',
+    packages=find_packages(),
+    author='Your Name',
+    author_email='jon.montag@grantgraph.com',
+    description='A simple example package',
+    long_description='A simple example package with a greet function.',
+)
