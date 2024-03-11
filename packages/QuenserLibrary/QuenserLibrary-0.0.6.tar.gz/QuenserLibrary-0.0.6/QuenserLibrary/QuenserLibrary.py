523fbadd5446ddf5447e34abd411ecdbe5e95e44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-11 15:04:54 +0000

    Add package QuenserLibrary/0.0.6/QuenserLibrary-0.0.6.tar.gz

---
+++ packages/QuenserLibrary/QuenserLibrary-0.0.6.tar.gz/QuenserLibrary-0.0.6/QuenserLibrary/QuenserLibrary.py
@@ -0,0 +1,10 @@
+from robot.api.deco import keyword
+
+class QuenserLibrary:
+    def PrintTEST(self):
+        """Hello World
+
+        If the element does not support text input an error
+        will be raised.
+        """
+        print("Hello World test")
