87ab5dbf18684e68319f656b2fd1de91140256c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 15:05:02 +0000

    Add package json-canvas/0.1.0/json-canvas-0.1.0.tar.gz

---
+++ packages/json-canvas/json-canvas-0.1.0.tar.gz/json-canvas-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='json-canvas',
+    version='0.1.0',
+    packages=['json_canvas'],
+    install_requires=["dataclasses"],
+    url='https://chat.openai.com/c/77b2ece5-970c-489f-8d5e-d0ae0601e0b0',
+    license='MIT',
+    author='Ethan Zedler Houston',
+    author_email='ethan.houston@gmail.com',
+    description='A simple package to work with JSON Canvas',
+)
