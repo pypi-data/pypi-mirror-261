64029bb3077dddef2832cd8c8dd5d60903646ef2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 04:04:01 +0000

    Add package reactpy-table/0.0.10/reactpy_table-0.0.10-py3-none-any.whl

---
+++ packages/reactpy-table/reactpy_table-0.0.10-py3-none-any.whl/reactpy_table/features/table_search.py
@@ -0,0 +1,28 @@
+from ..types import ITable, TableSearch, TData, TFeatureFactory, Updater, update_state
+
+
+class DefaultTableSearch(TableSearch[TData]):
+
+    @update_state
+    def table_search(self, search_term: str, case_sensitive: bool = False):
+        if not case_sensitive:
+            search_term = search_term.lower()
+
+        def _filter(row: TData):
+            row_text = " ".join([str(val) for val in row.model_dump().values()])
+
+            if not case_sensitive:
+                row_text = row_text.lower()
+
+            return search_term in row_text
+
+        result = filter(_filter, self.initial_values)
+        self.data.rows = list(result)
+
+
+def getDefaultTableSearch() -> TFeatureFactory[TData, TableSearch[TData]]:
+
+    def wrapper(table: ITable[TData], updater: Updater[TData]) -> TableSearch[TData]:
+        return DefaultTableSearch(table=table, updater=updater)
+
+    return wrapper
