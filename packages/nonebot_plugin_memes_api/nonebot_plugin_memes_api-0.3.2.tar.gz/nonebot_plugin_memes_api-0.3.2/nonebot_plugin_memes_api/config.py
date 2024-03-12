9dfe45639274bcc7335b83ac0ffff6abdc27e532
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 16:04:28 +0000

    Add package nonebot_plugin_memes_api/0.3.2/nonebot_plugin_memes_api-0.3.2.tar.gz

---
+++ packages/nonebot_plugin_memes_api/nonebot_plugin_memes_api-0.3.2.tar.gz/nonebot_plugin_memes_api-0.3.2/nonebot_plugin_memes_api/config.py
@@ -0,0 +1,18 @@
+from typing import List
+
+from nonebot import get_plugin_config
+from pydantic import BaseModel
+
+
+class Config(BaseModel):
+    meme_generator_base_url: str = "http://127.0.0.1:2233"
+    memes_command_start: List[str] = []
+    memes_command_force_whitespace: bool = True
+    memes_disabled_list: List[str] = []
+    memes_prompt_params_error: bool = False
+    memes_use_sender_when_no_image: bool = False
+    memes_use_default_when_no_text: bool = False
+    memes_random_meme_show_info: bool = False
+
+
+memes_config = get_plugin_config(Config)
