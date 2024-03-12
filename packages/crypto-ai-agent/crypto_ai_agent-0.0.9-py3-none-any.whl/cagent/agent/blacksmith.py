5d11c4771ad9e5cd2f1746f51041295af78aec5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 07:04:18 +0000

    Add package crypto-ai-agent/0.0.9/crypto_ai_agent-0.0.9-py3-none-any.whl

---
+++ packages/crypto-ai-agent/crypto_ai_agent-0.0.9-py3-none-any.whl/cagent/agent/blacksmith.py
@@ -0,0 +1,6 @@
+class Blacksmith:
+    def __init__(self):
+        self.attribute = "dummy"
+
+    def dummy_method(self):
+        return "This is a dummy method"
---
+++ packages/crypto-ai-agent/crypto_ai_agent-0.0.9-py3-none-any.whl/cagent/agent/professor.py
@@ -0,0 +1,6 @@
+class Professor:
+    def __init__(self):
+        self.attribute = "dummy"
+
+    def dummy_method(self):
+        return "This is a dummy method"
