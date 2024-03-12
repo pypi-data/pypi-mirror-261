2dd147a4f3ab19998f186b2bf81deb1cc98d5742
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 03:05:35 +0000

    Add package kindwise-api-client/0.4.0/kindwise_api_client-0.4.0-py3-none-any.whl

---
+++ packages/kindwise-api-client/kindwise_api_client-0.4.0-py3-none-any.whl/kindwise/__init__.py
@@ -0,0 +1,6 @@
+from kindwise.models import Result, Identification, Input, UsageInfo
+from kindwise.plant import PlantIdentification, HealthAssessment, RawPlantIdentification
+from kindwise.plant import PlantApi
+from kindwise.insect import InsectApi
+from kindwise.mushroom import MushroomApi
+from kindwise.crop_health import CropHealthApi
