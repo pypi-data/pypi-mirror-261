ef8537f8c2205a51fe5b9a657dec0e21ed5e5002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 04:04:41 +0000

    Add package The-best-quote/0.1.2/The_best_quote-0.1.2.tar.gz

---
+++ packages/The-best-quote/The_best_quote-0.1.2.tar.gz/The_best_quote-0.1.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+  with open('README.md', 'r') as f:
+    return f.read()
+
+
+setup(
+  name='The_best_quote',
+  version='0.1.2',
+  author='agagarkin',
+  author_email='eshkereholskill@gmail.com',
+  description='This is the simplest module for quick work with files.',
+  long_description=readme(),
+  long_description_content_type='text/markdown',
+  url='https://github.com/Agagarkin/The_best_quote.git',
+  packages=find_packages(),
+  install_requires=['requests>=2.25.1'],
+  classifiers=[
+    'Programming Language :: Python :: 3.11',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent'
+  ],
+  keywords='files speedfiles ',
+  project_urls={
+    'GitHub': 'https://github.com/Agagarkin/The_best_quote.git'
+  },
+  python_requires='>=3.6'
+)
