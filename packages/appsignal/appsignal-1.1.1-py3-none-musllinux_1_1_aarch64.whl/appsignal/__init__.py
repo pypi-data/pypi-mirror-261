926c5d31b277d742d2c4f2dc80aa574c27ca347e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 04:04:29 +0000

    Add package appsignal/1.1.1/appsignal-1.1.1-py3-none-musllinux_1_1_aarch64.whl

---
+++ packages/appsignal/appsignal-1.1.1-py3-none-musllinux_1_1_aarch64.whl/appsignal/__init__.py
@@ -0,0 +1,48 @@
+from .client import Client as Appsignal
+from .metrics import add_distribution_value, increment_counter, set_gauge
+from .tracing import (
+    send_error,
+    send_error_with_context,
+    set_body,
+    set_category,
+    set_custom_data,
+    set_error,
+    set_header,
+    set_name,
+    set_namespace,
+    set_params,
+    set_root_name,
+    set_session_data,
+    set_sql_body,
+    set_tag,
+)
+
+
+__all__ = [
+    "Appsignal",
+    "set_body",
+    "set_sql_body",
+    "set_category",
+    "set_custom_data",
+    "set_header",
+    "set_name",
+    "set_namespace",
+    "set_params",
+    "set_root_name",
+    "set_session_data",
+    "set_tag",
+    "set_error",
+    "send_error",
+    "send_error_with_context",
+    "increment_counter",
+    "set_gauge",
+    "add_distribution_value",
+]
+
+
+# Try and load the appsignal-beta package. If it's present and imported, it
+# will print a message about switching to the `appsignal` package.
+try:
+    import appsignal_beta  # type:ignore # noqa: F401
+except ImportError:
+    pass
---
+++ packages/appsignal/appsignal-1.1.1-py3-none-musllinux_1_1_aarch64.whl/appsignal/client.py
@@ -0,0 +1,72 @@
+from __future__ import annotations
+
+import logging
+import sys
+from logging import DEBUG, ERROR, INFO, WARNING, Logger
+from typing import TYPE_CHECKING, ClassVar
+
+from .agent import agent
+from .config import Config, Options
+from .opentelemetry import start_opentelemetry
+
+
+if TYPE_CHECKING:
+    from typing_extensions import Unpack
+
+
+class Client:
+    _logger: Logger
+    _config: Config
+
+    LOG_LEVELS: ClassVar[dict[str, int]] = {
+        "error": ERROR,
+        "warning": WARNING,
+        "info": INFO,
+        "debug": DEBUG,
+        "trace": DEBUG,
+    }
+
+    def __init__(self, **options: Unpack[Options]) -> None:
+        self._config = Config(options)
+        self.start_logger()
+
+        if not self._config.is_active():
+            self._logger.info("AppSignal not starting: no active config found")
+
+    def start(self) -> None:
+        if self._config.is_active():
+            self._logger.info("Starting AppSignal")
+            agent.start(self._config)
+            start_opentelemetry(self._config)
+
+    def start_logger(self) -> None:
+        self._logger = logging.getLogger("appsignal")
+        self._logger.setLevel(self.LOG_LEVELS[self._config.option("log_level")])
+
+        if self._config.option("log") == "file":
+            log_file_path = self._config.log_file_path()
+            if log_file_path:
+                handler = logging.FileHandler(log_file_path)
+                handler.setFormatter(
+                    logging.Formatter(
+                        "[%(asctime)s (process) #%(process)d][%(levelname)s] "
+                        "%(message)s",
+                        "%Y-%m-%dT%H:%M:%S",
+                    )
+                )
+                self._logger.addHandler(handler)
+            else:
+                self._start_stdout_logger()
+        else:
+            self._start_stdout_logger()
+
+    def _start_stdout_logger(self) -> None:
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setFormatter(
+            logging.Formatter(
+                "[%(asctime)s (process) #%(process)d][appsignal][%(levelname)s] "
+                "%(message)s",
+                "%Y-%m-%dT%H:%M:%S",
+            )
+        )
+        self._logger.addHandler(handler)
