35cbad16b4ecc61c1ba20ad0f9fb0055a1d68760
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 06:05:05 +0000

    Add package v4l2py/3.0.0/v4l2py-3.0.0.tar.gz

---
+++ packages/v4l2py/v4l2py-3.0.0.tar.gz/v4l2py-3.0.0/setup.py
@@ -0,0 +1,11 @@
+#
+# This file is part of the v4l2py project
+#
+# Copyright (c) 2021 Tiago Coutinho
+# Distributed under the GPLv3 license. See LICENSE for more info.
+
+"""The setup script."""
+
+from setuptools import setup
+
+setup()
---
+++ packages/v4l2py/v4l2py-3.0.0.tar.gz/v4l2py-3.0.0/v4l2py/device.py
@@ -0,0 +1,10 @@
+#
+# This file is part of the v4l2py project
+#
+# Copyright (c) 2021 Tiago Coutinho
+# Distributed under the GPLv3 license. See LICENSE for more info.
+
+# ruff: noqa: F401, F403
+
+from linuxpy.device import device_number
+from linuxpy.video.device import *
