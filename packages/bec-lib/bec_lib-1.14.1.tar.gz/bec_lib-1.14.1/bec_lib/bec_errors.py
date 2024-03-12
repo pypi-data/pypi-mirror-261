a96f68a84397e8dc09af21cc5f6cb0f1eae177cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 06:05:01 +0000

    Add package bec-lib/1.14.1/bec_lib-1.14.1.tar.gz

---
+++ packages/bec-lib/bec_lib-1.14.1.tar.gz/bec_lib-1.14.1/bec_lib/bec_errors.py
@@ -0,0 +1,14 @@
+class ScanAbortion(Exception):
+    """Scan abortion exception"""
+
+
+class ScanInterruption(Exception):
+    """Scan interruption exception"""
+
+
+class ServiceConfigError(Exception):
+    """Service config error"""
+
+
+class DeviceConfigError(Exception):
+    """Device config error"""
