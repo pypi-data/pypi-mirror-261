891a8131da8b3201a44445af6b5dd6f0442c2936
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 06:04:53 +0000

    Add package supriya/24.3b1/supriya-24.3b1-cp311-cp311-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/supriya/supriya-24.3b1-cp311-cp311-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl/supriya/assets/synthdefs/simple_sine.py
@@ -0,0 +1,16 @@
+from ...synthdefs import SynthDef, SynthDefBuilder
+from ...ugens import Out, SinOsc
+
+
+def _build_synthdef() -> SynthDef:
+    with SynthDefBuilder(amplitude=0, bus=0, frequency=440) as builder:
+        Out.ar(
+            bus=builder["bus"],
+            source=SinOsc.ar(frequency=builder["frequency"]) * builder["amplitude"],
+        )
+    return builder.build(name="simple_sine")
+
+
+simple_sine = _build_synthdef()
+
+__all__ = ["simple_sine"]
