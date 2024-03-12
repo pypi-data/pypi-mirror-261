91f97909e5e2b00c9839ee5d65f3f6aaecc931cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 16:05:11 +0000

    Add package testproxima1/0.0.1/testproxima1-0.0.1.tar.gz

---
+++ packages/testproxima1/testproxima1-0.0.1.tar.gz/testproxima1-0.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name='testproxima1',
+    version='0.0.1',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages()
+)
