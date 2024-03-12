851fb12fd2f407b8183941800b809ce0ca1d317e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 06:04:27 +0000

    Add package pdf-tools-sdk-test/0.1.0/pdf_tools_sdk_test-0.1.0.tar.gz

---
+++ packages/pdf-tools-sdk-test/pdf_tools_sdk_test-0.1.0.tar.gz/pdf_tools_sdk_test-0.1.0/setup.py
@@ -0,0 +1,21 @@
+import platform
+
+from setuptools import find_packages, setup
+
+setup(
+    name="pdf_tools_sdk_test",
+    version="0.1.0",
+    packages=find_packages(),
+    description="Python package for Pdftools SDK",
+    package_data={
+        "pdf_tools_sdk_test": [
+            "lib/linux-x64/*.so",
+            "lib/osx-arm64/*.dylib",
+            "lib/osx-x64/*.dylib",
+            "lib/win-x64/*.dll",
+            "lib/win-x86/*.dll",
+        ],
+    },
+    include_package_data=True,
+    zip_safe=False,
+)
