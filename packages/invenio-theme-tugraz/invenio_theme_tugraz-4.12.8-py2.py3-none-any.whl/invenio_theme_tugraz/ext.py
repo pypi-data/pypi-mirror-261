445dea140933b6e1c218844669d664cf883da153
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 06:04:16 +0000

    Add package invenio-theme-tugraz/4.12.8/invenio_theme_tugraz-4.12.8-py2.py3-none-any.whl

---
+++ packages/invenio-theme-tugraz/invenio_theme_tugraz-4.12.8-py2.py3-none-any.whl/invenio_theme_tugraz/ext.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020-2024 Graz University of Technology.
+#
+# invenio-theme-tugraz is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""invenio module for TUGRAZ theme."""
+
+from . import config
+from .views import index, locked
+
+
+class InvenioThemeTugraz(object):
+    """invenio-theme-tugraz extension."""
+
+    def __init__(self, app=None):
+        """Extension initialization."""
+        if app:
+            self.init_app(app)
+
+    def init_app(self, app):
+        """Flask application initialization."""
+        # add index route rule
+        # https://flask.palletsprojects.com/en/1.1.x/api/#flask.Flask.add_url_rule
+        app.add_url_rule("/", "index", index)
+        self.init_config(app)
+
+        app.register_error_handler(423, locked)
+
+        app.extensions["invenio-theme-tugraz"] = self
+
+    def init_config(self, app):
+        """Initialize configuration."""
+        for k in dir(config):
+            if k.startswith("INVENIO_THEME_TUGRAZ_") or k.startswith("THEME_TUGRAZ_"):
+                app.config.setdefault(k, getattr(config, k))
