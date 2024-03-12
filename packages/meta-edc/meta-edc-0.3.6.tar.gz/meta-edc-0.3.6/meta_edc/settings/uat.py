1a748dc76406131a84fd7f93be31a4ce5fa7d0a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 07:03:46 +0000

    Add package meta-edc/0.3.6/meta-edc-0.3.6.tar.gz

---
+++ packages/meta-edc/meta-edc-0.3.6.tar.gz/meta-edc-0.3.6/meta_edc/settings/uat.py
@@ -0,0 +1,24 @@
+from importlib.metadata import version
+
+from multisite import SiteID
+
+from .defaults import *  # noqa
+
+print(f"Settings file {__file__}")
+print(f"Version: {version('meta_edc')}")
+
+SITE_ID = SiteID(default=1)
+EDC_SITES_UAT_DOMAIN = True
+EDC_CONSENT_BYPASS_CONSENT_DATETIME_VALIDATION = True
+ALLOWED_HOSTS = [
+    "amana.uat.tz.meta3.clinicedc.org",
+    "hindu-mandal.uat.tz.meta3.clinicedc.org",
+    "mbagala.uat.tz.meta3.clinicedc.org",
+    "mnazi-moja.uat.tz.meta3.clinicedc.org",
+    "mwananyamala.uat.tz.meta3.clinicedc.org",
+    "temeke.uat.tz.meta3.clinicedc.org",
+]
+
+# edc_model_admin
+EDC_MODEL_ADMIN_CSS_THEME = "edc_purple"
+LIVE_SYSTEM = False
