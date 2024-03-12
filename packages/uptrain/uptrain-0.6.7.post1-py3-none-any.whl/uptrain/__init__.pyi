728e92826c9522579211bb97b7108ac2fc9cf8e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 07:04:04 +0000

    Add package uptrain/0.6.7.post1/uptrain-0.6.7.post1-py3-none-any.whl

---
+++ packages/uptrain/uptrain-0.6.7.post1-py3-none-any.whl/uptrain/__init__.pyi
@@ -0,0 +1,29 @@
+__all__ = [
+    "EvalLLM",
+    "APIClient",
+    "Evals",
+    "ConversationSatisfaction",
+    "CritiqueTone",
+    "GuidelineAdherence",
+    "ResponseMatching",
+    "Settings",
+    "EvalLlamaIndex",
+    "CustomPromptEval",
+    "RcaTemplate",
+    "JailbreakDetection",
+]
+
+from .framework.base import Settings
+from .framework.remote import APIClient
+from .framework.evals import (
+    Evals,
+    CritiqueTone,
+    GuidelineAdherence,
+    ResponseMatching,
+    ConversationSatisfaction,
+    CustomPromptEval,
+    JailbreakDetection,
+)
+from .framework.evalllm import EvalLLM
+from .framework.rca_templates import RcaTemplate
+from .integrations.llama_index import EvalLlamaIndex
---
+++ packages/uptrain/uptrain-0.6.7.post1-py3-none-any.whl/uptrain/integrations/__init__.pyi
@@ -0,0 +1,3 @@
+__all__ = ["EvalLlamaIndex"]
+
+from .llama_index import EvalLlamaIndex
