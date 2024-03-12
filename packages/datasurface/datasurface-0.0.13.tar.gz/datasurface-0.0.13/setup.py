ecef6de635734f28fb87074d6b55fd1f598f6d44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 07:03:54 +0000

    Add package datasurface/0.0.13/datasurface-0.0.13.tar.gz

---
+++ packages/datasurface/datasurface-0.0.13.tar.gz/datasurface-0.0.13/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='datasurface',
+    version='0.0.13',
+    license='Apache License 2.0',
+    description='Automate the governance, management and movement of data within your enterprise',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Billy Newport',
+    author_email='billy@billynewport.com',
+    url='https://github.com/billynewport/datasurface',
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: Database :: Database Engines/Servers',
+    ],
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    package_data={"datasurface": ["*.pyi", "**/*.pyi", "py.typed"]},  # Include .pyi files and py.typed file
+    install_requires=requirements
+)
---
+++ packages/datasurface/datasurface-0.0.13.tar.gz/datasurface-0.0.13/src/datasurface/md/Azure.py
@@ -0,0 +1,98 @@
+from enum import Enum
+from datasurface.md import Documentation
+from datasurface.md.Governance import DataContainer
+from .Governance import CloudVendor, Credential, DataPlatform, EncryptionSystem, Ecosystem, GovernanceZone, HostPortSQLDatabase, InfrastructureLocation, Team
+from .Lint import NameHasBadSynthax, ValidationTree
+from .utils import is_valid_azure_key_vault_name
+
+
+class AzureVaultObjectType(Enum):
+    HSM_KEYS = 1
+    SOFTWARE_KEYS = 2
+    SECRETS = 3
+    CERTIFICATES = 4
+    STORAGE_ACCOUNT_KEYS = 5
+
+
+class AzureKeyVaultCredential(Credential):
+    """This allows a secret to be read from Azure Key Vault. The secret should be in the
+    form of 2 lines, first line is user name, second line is password"""
+    def __init__(self, keyVaultName: str, objectName: str) -> None:
+        super().__init__()
+        self.keyVaultName: str = keyVaultName
+        self.objectName: str = objectName
+        self.objectType: AzureVaultObjectType = AzureVaultObjectType.SECRETS
+
+    def __eq__(self, __value: object) -> bool:
+        return super().__eq__(__value) and type(__value) is AzureKeyVaultCredential and self.keyVaultName == __value.keyVaultName and \
+            self.objectName == __value.objectName
+
+    def lint(self, eco: 'Ecosystem', tree: ValidationTree) -> None:
+        super().lint(eco, tree)
+        if (not is_valid_azure_key_vault_name(self.keyVaultName)):
+            tree.addRaw(NameHasBadSynthax(f"Azure Key Vault name <{self.keyVaultName}> needs to match [a-z0-9]{3,24}"))
+
+    def __str__(self) -> str:
+        return f"AzureKeyVaultCredential({self.keyVaultName}/{self.objectName})"
+
+    def getURL(self) -> str:
+        return f"https://{self.keyVaultName}.vault.azure.net/{self.objectType}/{self.objectName}"
+
+
+class AzureKeyVault(EncryptionSystem):
+    pass
+
+
+class AzureDataplatform(DataPlatform):
+    """This platform manages pipelines for resources within Azure"""
+    def __init__(self, name: str, doc: Documentation, platformCredential: AzureKeyVaultCredential):
+        super().__init__(name, doc)
+        self.platformCredential = platformCredential
+
+    def __hash__(self) -> int:
+        return hash(self.name)
+
+    def lint(self, eco: 'Ecosystem', tree: ValidationTree):
+        cTree = tree.addSubTree(self)
+        self.platformCredential.lint(eco, cTree)
+
+    def getSupportedVendors(self, eco: Ecosystem) -> set[CloudVendor]:
+        rc: set[CloudVendor] = set()
+        rc.add(CloudVendor.AZURE)
+        return rc
+
+    def __eq__(self, __value: object) -> bool:
+        return super().__eq__(__value) and isinstance(__value, AzureDataplatform)
+
+    def _str__(self) -> str:
+        return f"AzureDataPlatform({self.name})"
+
+    def isContainerSupported(self, eco: Ecosystem, dc: DataContainer) -> bool:
+        return dc.areLocationsOwnedByTheseVendors(eco, {CloudVendor.AZURE})
+
+    def getInternalDataContainers(self) -> set[DataContainer]:
+        # TODO: Implement this method
+        return set()
+
+
+class AzureBatchDataPlatform(AzureDataplatform):
+    def __init__(self, name: str, doc: Documentation, platformCredential: AzureKeyVaultCredential):
+        super().__init__(name, doc, platformCredential)
+
+
+class AzureSQLDatabase(HostPortSQLDatabase):
+    """This is an Azure SQL Database resource. """
+    def __init__(self, name: str, hostName: str, port: int, databaseName: str, loc: InfrastructureLocation):
+        super().__init__(name, loc, hostName, port, databaseName)
+
+    def __str__(self) -> str:
+        return f"AzureDatabaseResource({self.name})"
+
+    def __hash__(self) -> int:
+        return hash(self.name)
+
+    def lint(self, eco: Ecosystem, gz: GovernanceZone, t: Team, tree: ValidationTree) -> None:
+        super().lint(eco, gz, t, tree)
+
+    def __eq__(self, o: object) -> bool:
+        return super().__eq__(o) and isinstance(o, AzureSQLDatabase)
