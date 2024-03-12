f7e85d301f89c001ed790cb9382840ec9c78adff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 16:04:45 +0000

    Add package library-kostya/0.0.1/library_kostya-0.0.1.tar.gz

---
+++ packages/library-kostya/library_kostya-0.0.1.tar.gz/library_kostya-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='library_kostya', # Имя пакета
+    version='0.0.1',  # Версия пакета
+    author='Kostya',
+    author_email='your@domain.com',
+    description='My Package',
+    packages=setuptools.find_packages(),
+)
