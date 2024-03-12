b719020605330c5660f74876bd6bb8b5c2ad548b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 06:05:05 +0000

    Add package afeng-py-tools/0.0.126/afeng_py_tools-0.0.126-py3-none-any.whl

---
+++ packages/afeng-py-tools/afeng_py_tools-0.0.126-py3-none-any.whl/afeng_tools/fastapi_tool/common/po_service/resource_po_service_.py
@@ -0,0 +1,9 @@
+from afeng_tools.fastapi_tool.common.po_service.po_service import PoService
+
+
+class ResourcePoService(PoService):
+    """
+    使用示例：resource_po_service = ResourcePoService(app_info.db_code, ResourceInfoPo)
+    """
+
+    pass
