c142fd615e440480287463bfcd11a25bef653a7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 04:04:02 +0000

    Add package wildberries-api-client/0.1.0/wildberries_api_client-0.1.0-py3-none-any.whl

---
+++ packages/wildberries-api-client/wildberries_api_client-0.1.0-py3-none-any.whl/wildberries_api_client/content/api/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from content.api._api import DefaultApi
+from content.api.analytics_api import AnalyticsApi
+from content.api.configurator_api import ConfiguratorApi
+from content.api.media_files_api import MediaFilesApi
+from content.api.tags_api import TagsApi
+from content.api.trash_api import TrashApi
+from content.api.upload_api import UploadApi
+from content.api.view_api import ViewApi
---
+++ packages/wildberries-api-client/wildberries_api_client-0.1.0-py3-none-any.whl/wildberries_api_client/tariffs/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from tariffs.api.refund_value_toseller_api import RefundValueTosellerApi
+from tariffs.api.warehouse_factors_api import WarehouseFactorsApi
