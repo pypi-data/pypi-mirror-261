9a890e58131ffd7034705350643855ba2366d950
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 03:05:46 +0000

    Add package torchaddition/0.1.0/torchaddition-0.1.0.tar.gz

---
+++ packages/torchaddition/torchaddition-0.1.0.tar.gz/torchaddition-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='torchaddition',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'torch',
+    ],
+    author='Your Name',
+    description='A simple PyTorch package for additions',
+    license='MIT',
+)
