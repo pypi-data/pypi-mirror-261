f890f0313dc3b37fe2e7c7981ddf9de1331f3269
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 04:04:13 +0000

    Add package lago-python-client/1.0.0/lago-python-client-1.0.0.tar.gz

---
+++ packages/lago-python-client/lago-python-client-1.0.0.tar.gz/lago-python-client-1.0.0/lago_python_client/models/plan.py
@@ -0,0 +1,51 @@
+from typing import List, Optional
+
+from lago_python_client.base_model import BaseModel
+
+from .charge import Charges, ChargesResponse, ChargesOverrides
+from .tax import Taxes, TaxesResponse
+from ..base_model import BaseResponseModel
+
+
+class Plan(BaseModel):
+    name: Optional[str]
+    invoice_display_name: Optional[str]
+    code: Optional[str]
+    interval: Optional[str]
+    description: Optional[str]
+    amount_cents: Optional[int]
+    amount_currency: Optional[str]
+    trial_period: Optional[float]
+    pay_in_advance: Optional[bool]
+    bill_charges_monthly: Optional[bool]
+    charges: Optional[Charges]
+    tax_codes: Optional[List[str]]
+
+
+class PlanResponse(BaseResponseModel):
+    lago_id: str
+    name: str
+    invoice_display_name: Optional[str]
+    created_at: str
+    code: str
+    interval: Optional[str]
+    description: Optional[str]
+    amount_cents: Optional[int]
+    amount_currency: Optional[str]
+    trial_period: Optional[float]
+    pay_in_advance: Optional[bool]
+    bill_charges_monthly: Optional[bool]
+    charges: Optional[ChargesResponse]
+    active_subscriptions_count: int
+    draft_invoices_count: int
+    taxes: Optional[TaxesResponse]
+
+class PlanOverrides(BaseModel):
+    name: Optional[str]
+    invoice_display_name: Optional[str]
+    description: Optional[str]
+    amount_cents: Optional[int]
+    amount_currency: Optional[str]
+    trial_period: Optional[float]
+    charges: Optional[ChargesOverrides]
+    tax_codes: Optional[List[str]]
