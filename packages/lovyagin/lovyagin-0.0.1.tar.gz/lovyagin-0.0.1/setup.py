4a5c52e90f6da5f0a390d65aae805c4e73904ae9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 04:04:28 +0000

    Add package lovyagin/0.0.1/lovyagin-0.0.1.tar.gz

---
+++ packages/lovyagin/lovyagin-0.0.1.tar.gz/lovyagin-0.0.1/setup.py
@@ -0,0 +1,11 @@
+# setup.py
+import setuptools
+
+setuptools.setup(
+    name='lovyagin', # Имя пакета
+    version='0.0.1',  # Версия пакета
+    author='Степан',
+    author_email='your@domain.com',
+    description='My Package',
+    packages=setuptools.find_packages(),
+)
