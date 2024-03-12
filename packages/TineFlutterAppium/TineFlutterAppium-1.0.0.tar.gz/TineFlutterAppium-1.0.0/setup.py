edaeb9df97d711c5eb01361462a9c4572280dd85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 16:04:44 +0000

    Add package TineFlutterAppium/1.0.0/TineFlutterAppium-1.0.0.tar.gz

---
+++ packages/TineFlutterAppium/TineFlutterAppium-1.0.0.tar.gz/TineFlutterAppium-1.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="TineFlutterAppium",
+    version="1.0.0",
+    author="Pornpawit Suttha",
+    author_email="pornpawit14suttha@gmail.com",
+    description="This Library is support to appiumlibrary and appiumflutterlibrary",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/pornpawit08/TineFlutterAppium.git",
+    packages=find_packages(),
+    install_requires=[
+        'robotframework>=3.0', 
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
