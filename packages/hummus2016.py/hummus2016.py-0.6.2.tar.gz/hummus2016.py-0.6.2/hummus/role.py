fc217ee791cd603b155930c27b95dfb4249e2c62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 06:04:58 +0000

    Add package hummus2016.py/0.6.2/hummus2016.py-0.6.2.tar.gz

---
+++ packages/hummus2016.py/hummus2016.py-0.6.2.tar.gz/hummus2016.py-0.6.2/hummus/role.py
@@ -0,0 +1,14 @@
+class Role:
+  def __init__(self,data):
+    self.id = data['id']
+    self.name = data['name']
+    self.color = data['color']
+    from .permissions import Permissions #circular import begone
+    self.permissions = Permissions(data['permissions'])
+    self.mentionable = data['mentionable']
+    self.hoist = data['hoist']
+    self.position = data['position']
+
+class FakeRole: #this is really used to provide a placeholder for top role comparison in funcs.py
+  def __init__(self,position):
+    self.position = position
