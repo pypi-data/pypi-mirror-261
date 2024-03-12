1b56e9c79dfb28e0ff7c77505ef2b52206ca4448
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 16:05:01 +0000

    Add package python-timetools/1.0.0/python_timetools-1.0.0-py3-none-any.whl

---
+++ packages/python-timetools/python_timetools-1.0.0-py3-none-any.whl/timetools/timeutil.py
@@ -0,0 +1,11 @@
+from datetime import datetime, timedelta
+
+class TimeUtil:
+    
+    @staticmethod
+    def get_start_of_date(date: datetime) -> datetime:
+        return date.replace(hour=0, minute=0, second=0, microsecond=0)
+
+    @staticmethod
+    def get_end_of_date(date: datetime) -> datetime:
+        return (date + timedelta(days=1)).replace(hour=0, minute=0, second=0, microsecond=0) - timedelta(microseconds=1)
