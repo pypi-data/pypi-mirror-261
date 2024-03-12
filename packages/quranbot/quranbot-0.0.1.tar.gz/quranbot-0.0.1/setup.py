a45bc8c1bb4bfa74ec94ef6189434b2d442dd198
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 03:05:51 +0000

    Add package quranbot/0.0.1/quranbot-0.0.1.tar.gz

---
+++ packages/quranbot/quranbot-0.0.1.tar.gz/quranbot-0.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'A package developed by SaudiBits to facilitate the process of making a quran bot'
+LONG_DESCRIPTION = 'A package developed by SaudiBits to facilitate the process of making a quran bot'
+
+# Setting up
+setup(
+    name="quranbot",
+    version=VERSION,
+    author="Ahmed Alnasser",
+    author_email="itzzmega2005@gmail.com",
+    license="MIT",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['python-telegram-bot', 'Pillow', 'requests'],
+    keywords=['Quran', 'Bot'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "License :: OSI Approved :: MIT License"
+    ]
+)
