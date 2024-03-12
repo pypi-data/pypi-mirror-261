f7b67e45feaa71c7a1e944d283f083905a11a20d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 06:04:50 +0000

    Add package waifuvault/1.0.7/waifuvault-1.0.7.tar.gz

---
+++ packages/waifuvault/waifuvault-1.0.7.tar.gz/waifuvault-1.0.7/src/waifuvault/waifuvault.py
@@ -0,0 +1,94 @@
+import json
+import os
+from io import BytesIO
+
+import requests
+from requests_toolbelt import MultipartEncoder
+from .waifumodels import FileResponse, FileUpload
+
+
+# Upload File
+def upload_file(file_obj: FileUpload):
+    parameters = {}
+    if file_obj.password:
+        parameters['password'] = file_obj.password
+    if file_obj.expires:
+        parameters['expires'] = file_obj.expires
+    if file_obj.hidefilename:
+        parameters['hide_filename'] = file_obj.hidefilename
+
+    if file_obj.is_url():
+        response = requests.put(
+            "https://waifuvault.moe/rest",
+            params=parameters,
+            data={'url': file_obj.target}
+        )
+    else:
+        multipart_data = MultipartEncoder(
+            fields={
+                'file': (os.path.basename(file_obj.target), open(file_obj.target, 'rb'))
+            }
+        )
+        response = requests.put(
+            "https://waifuvault.moe/rest",
+            params=parameters,
+            data=multipart_data,
+            headers={'Content-Type': multipart_data.content_type})
+    return dict_to_obj(json.loads(response.text))
+
+
+# Get File Info
+def file_info(token: str, formatted: bool):
+    url = f"https://waifuvault.moe/rest/{token}"
+    response = requests.get(
+        url,
+        params={'formatted': 'true' if formatted else 'false'}
+    )
+    check_error(response)
+    return dict_to_obj(json.loads(response.text))
+
+
+# Delete File
+def delete_file(token: str):
+    url = f"https://waifuvault.moe/rest/{token}"
+    response = requests.delete(url)
+    check_error(response)
+    return True if response.text == "true" else False
+
+
+# Get File
+def get_file(file_obj: FileResponse, password: str = None):
+    headers = {}
+    if password:
+        headers["x-password"] = password
+    if not file_obj.url and file_obj.token:
+        url = file_info(file_obj.token, False).url
+    else:
+        url = file_obj.url
+    response = requests.get(url, headers=headers)
+    check_error(response)
+    return BytesIO(response.content)
+
+
+# Check Error
+def check_error(response: requests.models.Response):
+    if not response.ok:
+        try:
+            err = json.loads(response.text)
+            status = err["status"]
+            name = err["name"]
+            message = err['message']
+        except:
+            status = response.text
+            name = ""
+            message = ""
+        raise Exception(f"Error {status} ({name}): {message}")
+    return
+
+
+def dict_to_obj(dict_obj: any):
+    return FileResponse(
+        dict_obj["token"],
+        dict_obj["url"],
+        True if dict_obj["protected"] == "true" else False,
+        dict_obj["retentionPeriod"])
