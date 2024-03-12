c2656659e916447c0430522e888fba0b4e4ee54b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 05:04:30 +0000

    Add package PythonTestLJC/3.0.0/PythonTestLJC-3.0.0-py3-none-any.whl

---
+++ packages/PythonTestLJC/PythonTestLJC-3.0.0-py3-none-any.whl/sample/simple.py
@@ -0,0 +1,25 @@
+import node
+import odict
+import certifi
+import charset_normalizer
+import docutils
+import idna
+import importlib_metadata
+import jaraco
+import keyring
+import markdown_it
+import mdurl
+import more_itertools
+import nh3
+import odict
+import pip
+import pkginfo
+import plumber
+import pygments
+import requests_toolbelt
+import readme_renderer
+import requests
+
+
+def add_one(number):
+    return number + 1
