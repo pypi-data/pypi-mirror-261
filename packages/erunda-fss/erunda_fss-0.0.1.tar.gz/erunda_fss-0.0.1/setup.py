0320b5c09c41cccead9355f24e8bcdc2e7b843d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 04:04:07 +0000

    Add package erunda-fss/0.0.1/erunda_fss-0.0.1.tar.gz

---
+++ packages/erunda-fss/erunda_fss-0.0.1.tar.gz/erunda_fss-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='erunda_fss', # Имя пакета
+    version='0.0.1',  # Версия пакета
+    author='STF',
+    author_email='impmail@vk.com',
+    description='My first package',
+    packages=setuptools.find_packages(),
+)
