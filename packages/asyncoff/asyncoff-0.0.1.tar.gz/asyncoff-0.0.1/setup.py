3a69517caeb316d4bb3a9405ae908ed4f0937c13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 04:04:01 +0000

    Add package asyncoff/0.0.1/asyncoff-0.0.1.tar.gz

---
+++ packages/asyncoff/asyncoff-0.0.1.tar.gz/asyncoff-0.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Run async functions in synchronous mode'
+LONG_DESCRIPTION = DESCRIPTION
+
+# Setting up
+setup(
+    name="asyncoff",
+    version=VERSION,
+    author="Maryimana",
+    author_email="mudjaycker@gmail.com",
+    description=DESCRIPTION,
+    # long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    # install_requires=[],
+    keywords=['python', 'async', 'asyncoff', 'sync', 'async to sync'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
