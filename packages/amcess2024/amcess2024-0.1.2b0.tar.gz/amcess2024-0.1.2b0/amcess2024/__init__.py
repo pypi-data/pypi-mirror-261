287c1ad2aa39f7e42968b437f59b15536cf3d595
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 14:04:17 +0000

    Add package amcess2024/0.1.2b0/amcess2024-0.1.2b0.tar.gz

---
+++ packages/amcess2024/amcess2024-0.1.2b0.tar.gz/amcess2024-0.1.2b0/amcess2024/__init__.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of the
+#   AMCESS Project (https://gitlab.com/ADanianZE/amcess).
+# Copyright (c) 2021, Edison Florez
+# License: GPLv3
+#   Full Text: https://gitlab.com/ADanianZE/amcess/-/blob/main/LICENSE
+
+# =============================================================================
+# DOCS
+# =============================================================================
+
+"""
+AMCESS
+Atomic and Molecular Cluster Energy Surface Sampler.
+"""
+
+# =============================================================================
+# META
+# =============================================================================
+
+__version__ = "0.1.2"
+
+
+# =============================================================================
+# IMPORTS
+# =============================================================================
+
+from .base_molecule import Molecule, Cluster  # noqa
+from .search_engine import SearchConfig as engine  # noqa
