d42385365c9894123e85be208838932ade4775f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 04:04:17 +0000

    Add package matej-libs/0.2/matej-libs-0.2.tar.gz

---
+++ packages/matej-libs/matej-libs-0.2.tar.gz/matej-libs-0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+version = "0.2"
+
+setup(
+	name='matej-libs',
+	version=version,
+	author="Matej Vitek",
+	author_email="matej.vitek.business@gmail.com",
+	description="Collection of Python utility libraries.",
+	long_description=open('README.md', encoding='utf-8').read(),
+	url='https://github.com/MatejVitek/Python-Libraries',
+	packages=find_packages(),
+	python_requires='>=3.12',
+	install_requires=['numpy'],
+	extras_require={
+		'google-drive': ['requests'],
+		'config': ['ruamel.yaml'],
+		'parallel': ['joblib']
+	}
+)
