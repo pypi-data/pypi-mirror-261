2742cca4728c45cec2956b27bb14f96550268f98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 14:03:54 +0000

    Add package vinyl/0.0.16/vinyl-0.0.16-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/vinyl/vinyl-0.0.16-cp310-cp310-macosx_11_0_arm64.whl/vinyl/__init__.pyi
@@ -0,0 +1,14 @@
+from _typeshed import Incomplete
+from ibis import _ as _
+from vinyl.lib.asset import metric as metric, model as model, resource as resource
+from vinyl.lib.definitions import load_defs as load_defs
+from vinyl.lib.enums import FillOptions as FillOptions
+from vinyl.lib.field import Field as Field
+from vinyl.lib.metric import MetricStore as MetricStore
+from vinyl.lib.operations import case as case, if_else as if_else
+from vinyl.lib.set import join as join
+from vinyl.lib.source import source as source
+from vinyl.lib.table import VinylTable as VinylTable
+from vinyl.lib.utils.graphics import rich_print as rich_print
+
+original_print: Incomplete
