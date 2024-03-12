b7568c5625a30fb8d960782fdb4e4aa365beea2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 03:05:00 +0000

    Add package gardener-cicd-libs/1.2364.0/gardener_cicd_libs-1.2364.0-py3-none-any.whl

---
+++ packages/gardener-cicd-libs/gardener_cicd_libs-1.2364.0-py3-none-any.whl/dso/model.py
@@ -0,0 +1,336 @@
+import dataclasses
+import datetime
+import enum
+
+import gci.componentmodel as cm
+
+import dso.cvss
+import dso.labels
+import unixutil.model
+
+
+@dataclasses.dataclass
+class ScanArtifact:
+    name: str
+    label: dso.labels.SourceScanLabel
+    component: cm.Component
+    source: cm.ComponentSource
+
+
+class Datasource:
+    ARTEFACT_ENUMERATOR = 'artefact-enumerator'
+    BDBA = 'bdba' # formerly protecode
+    CHECKMARX = 'checkmarx'
+    CLAMAV = 'clamav'
+    CC_UTILS = 'cc-utils'
+
+
+@dataclasses.dataclass(frozen=True)
+class LocalArtefactId:
+    artefact_name: str | None
+    artefact_version: str | None
+    artefact_type: str
+    artefact_extra_id: dict
+
+
+@dataclasses.dataclass(frozen=True)
+class ComponentArtefactId:
+    component_name: str | None
+    component_version: str | None
+    artefact: LocalArtefactId
+    artefact_kind: str = 'artefact' # artefact | resource | source
+
+    def as_frozenset(self) -> frozenset[str]:
+        return frozenset((
+            self.component_name,
+            self.component_version,
+            self.artefact_kind,
+            self.artefact.artefact_name,
+            self.artefact.artefact_version,
+            self.artefact.artefact_type,
+            frozenset(self.artefact.artefact_extra_id.items()),
+        ))
+
+
+def component_artefact_id_from_ocm(
+    component: cm.Component,
+    artefact: cm.Resource | cm.ComponentSource
+) -> ComponentArtefactId:
+    local_artefact = LocalArtefactId(
+        artefact_name=artefact.name,
+        artefact_version=artefact.version,
+        artefact_type=artefact.type,
+        artefact_extra_id=artefact.extraIdentity,
+    )
+
+    if isinstance(artefact, cm.Resource):
+        artefact_kind = 'resource'
+    elif isinstance(artefact, cm.ComponentSource):
+        artefact_kind = 'source'
+    else:
+        # should not occur
+        raise TypeError(artefact)
+
+    return ComponentArtefactId(
+        component_name=component.name,
+        component_version=component.version,
+        artefact=local_artefact,
+        artefact_kind=artefact_kind,
+    )
+
+
+class Datatype:
+    CODECHECKS_AGGREGATED = 'codechecks/aggregated'
+    VULNERABILITIES_CVE = 'vulnerabilities/cve'
+    MALWARE = 'malware'
+    LICENSE = 'license'
+    COMPONENTS = 'components'
+    FILESYSTEM_PATHS = 'filesystem/paths'
+    OS_IDS = 'os_ids'
+    RESCORING_VULNERABILITIES = 'rescoring/vulnerabilities'
+    RESCORING = 'rescorings'
+    COMPLIANCE_SNAPSHOTS = 'compliance/snapshots'
+
+
+class RelationKind:
+    RESCORE = 'rescore'
+
+
+@dataclasses.dataclass(frozen=True)
+class Relation:
+    '''
+    Describes relation between artefact_metadata.
+    This is necessary as "rescorings" (type: "rescoring/vulnerabilities") are stored as
+    artefact_metadata, but relate to artefact_metadata (of type "vulnerabilities/cve") as they
+    rescore vulnerability findings.
+    '''
+    refers_to: str # see `Datatype` for supported values
+    relation_kind: str # see `RelationKind` for supported values
+
+
+@dataclasses.dataclass(frozen=True)
+class Metadata:
+    datasource: str
+    type: str
+    relation: Relation | None = None
+    creation_date: datetime.datetime | str = None
+
+    def __post_init__(self):
+        # only determine creation date on/post init because otherwise the creation date will
+        # be the date when this module was imported
+        if not self.creation_date:
+            # since the dataclass is frozen, `__setattr__` must be used
+            # [ref](https://docs.python.org/3/library/dataclasses.html#frozen-instances)
+            object.__setattr__(self, 'creation_date', datetime.datetime.now())
+
+
+@dataclasses.dataclass(frozen=True)
+class CVE:
+    cve: str | None
+    cvss3Score: float
+    cvss: dso.cvss.CVSSV3 | dict | None
+    affected_package_name: str | None
+    affected_package_version: str | None
+    reportUrl: str
+    product_id: int
+    group_id: int
+    base_url: str
+    bdba_cfg_name: str
+
+
+@dataclasses.dataclass(frozen=True)
+class OsID:
+    os_info: unixutil.model.OperatingSystemId
+
+
+@dataclasses.dataclass(frozen=True)
+class License:
+    name: str
+    reportUrl: str
+    productId: int
+
+
+@dataclasses.dataclass(frozen=True)
+class ComponentVersion:
+    name: str
+    version: str
+
+
+@dataclasses.dataclass(frozen=True)
+class ComponentSummary:
+    components: list[ComponentVersion]
+
+
+@dataclasses.dataclass(frozen=True)
+class FilesystemPath:
+    path: str
+    digest: str
+
+
+@dataclasses.dataclass(frozen=True)
+class FilesystemPaths:
+    paths: list[FilesystemPath]
+
+
+@dataclasses.dataclass(frozen=True)
+class ClamAVMetadata:
+    clamav_version_str: str
+    signature_version: int
+    virus_definition_timestamp: datetime.datetime
+
+
+@dataclasses.dataclass
+class MalwareFindingMeta:
+    scanned_octets: int
+    receive_duration_seconds: float
+    scan_duration_seconds: float
+    scanned_content_digest: str | None = None
+
+
+@dataclasses.dataclass
+class MalwareFinding:
+    status: str
+    details: str
+    malware_status: str
+    meta: MalwareFindingMeta | None
+    name: str
+
+
+@dataclasses.dataclass(frozen=True)
+class MalwareSummary:
+    '''
+    empty list of findings states "no malware found"
+    '''
+    findings: list[MalwareFinding]
+    metadata: ClamAVMetadata
+
+
+@dataclasses.dataclass(frozen=True)
+class CodecheckFindings:
+    high: int
+    medium: int
+    low: int
+    info: int
+
+
+@dataclasses.dataclass(frozen=True)
+class CodecheckSummary:
+    findings: CodecheckFindings
+    risk_rating: int
+    risk_severity: int
+    overview_url: str
+    report_url: str | None
+
+
+@dataclasses.dataclass(frozen=True)
+class Vulnerability:
+    cve: str
+    rescored_severity: dso.cvss.CVESeverity
+    matching_rules: list[str]
+    comment: str | None # optional additional assessment message
+
+
+@dataclasses.dataclass(frozen=True)
+class BDBAComponent:
+    name: str
+    version: str | None # bdba might be unable to determine a version
+    source: str
+
+
+@dataclasses.dataclass(frozen=True)
+class Rescoring:
+    bdba_component: BDBAComponent
+    vulnerabilities: list[Vulnerability]
+
+
+@dataclasses.dataclass(frozen=True)
+class RescoringData:
+    rescorings: list[Rescoring]
+
+
+@dataclasses.dataclass(frozen=True)
+class RescoringVulnerabilityFinding:
+    cve: str
+    affected_package_name: str
+
+
+@dataclasses.dataclass(frozen=True)
+class RescoringLicenseFinding:
+    name: str
+
+
+@dataclasses.dataclass(frozen=True)
+class CustomRescoring:
+    finding: (
+        RescoringVulnerabilityFinding
+        | RescoringLicenseFinding
+    )
+    severity: str
+    matching_rules: list[str]
+    user: dict
+    comment: str | None = None
+
+
+class ComplianceSnapshotStatuses(enum.StrEnum):
+    ACTIVE = 'active'
+    INACTIVE = 'inactive'
+
+
+@dataclasses.dataclass(frozen=True)
+class ComplianceSnapshotState:
+    timestamp: datetime.datetime
+    status: ComplianceSnapshotStatuses | str | int | None = None
+    service: str | None = None
+
+
+@dataclasses.dataclass(frozen=True)
+class ComplianceSnapshot:
+    cfg_name: str
+    latest_processing_date: datetime.date
+    correlation_id: str
+    state: list[ComplianceSnapshotState]
+
+    def current_state(
+        self,
+        service: str = None,
+    ) -> ComplianceSnapshotState | None:
+        for state in sorted(self.state, key=lambda s: s.timestamp, reverse=True):
+            if service == state.service:
+                return state
+        return None
+
+    def purge_old_states(
+        self,
+        service: str = None,
+    ):
+        current_state = None
+        for state in sorted(self.state, key=lambda s: s.timestamp, reverse=True):
+            if not service == state.service:
+                continue
+
+            if not current_state:
+                current_state = state
+                continue
+
+            self.state.remove(state)
+
+
+@dataclasses.dataclass(frozen=True)
+class ArtefactMetadata:
+    artefact: ComponentArtefactId
+    meta: Metadata
+    data: (
+        CodecheckSummary
+        | ComponentSummary
+        | FilesystemPaths
+        | CVE
+        | License
+        | MalwareSummary
+        | OsID
+        | RescoringData
+        | CustomRescoring
+        | ComplianceSnapshot
+        | dict # fallback, there should be a type
+    )
+    id: int | None = None
+    discovery_date: datetime.date | None = None
