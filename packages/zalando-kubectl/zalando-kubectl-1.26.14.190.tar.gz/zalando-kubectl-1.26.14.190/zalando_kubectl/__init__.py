c24e5446eb6ff766e707275b6b7fef79a787de73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 04:04:01 +0000

    Add package zalando-kubectl/1.26.14.190/zalando-kubectl-1.26.14.190.tar.gz

---
+++ packages/zalando-kubectl/zalando-kubectl-1.26.14.190.tar.gz/zalando-kubectl-1.26.14.190/zalando_kubectl/__init__.py
@@ -0,0 +1,27 @@
+# This is replaced during release process.
+__version_suffix__ = '190'
+
+APP_NAME = "zalando-kubectl"
+
+KUBECTL_VERSION = "v1.26.14"
+KUBECTL_SHA512 = {
+    "linux": "8d49db3e90fbcd08b8391b5d0b60d1b0d56f049686512d3596f7f0e40b48b2e55e98d34cab02ba6daa256a539c86f9dd34cfb2deb117ecde3c713f3cf307bbd6",
+    "darwin": "179878b42f9f7f52bdc983901e5770ee7bfe1ca2595e079e855779e18866336918bb33b4a23021f6ed79eed88a76edf1ae83a1e5727bd79e68f2884c30c81876",
+}
+STERN_VERSION = "1.26.0"
+STERN_SHA256 = {
+    "linux": "de79474d9197582e38da0dccc8cd14af23d6b52b72bee06b62943c19ab95125e",
+    "darwin": "f89631ea73659e1db4e9d8ef94c58cd2c4e92d595e5d2b7be9184f86e755ee95",
+}
+KUBELOGIN_VERSION = "v1.28.0"
+KUBELOGIN_SHA256 = {
+    "linux": "83282148fcc70ee32b46edb600c7e4232cbad02a56de6dc17e43e843fa55e89e",
+    "darwin": "8169c6e85174a910f256cf21f08c4243a4fb54cd03a44e61b45129457219e646",
+}
+ZALANDO_AWS_CLI_VERSION = "0.4.3"
+ZALANDO_AWS_CLI_SHA256 = {
+    "linux": "bf0c32087985629c8694f4153230cbb7d627ae1794a942752f5cd1d76e118bf4",
+    "darwin": "725d7262fb6c8e8705e1c3b59b53ccd78a59e9361711dc584b401d88cfd3fa69",
+}
+
+APP_VERSION = KUBECTL_VERSION + "." + __version_suffix__
