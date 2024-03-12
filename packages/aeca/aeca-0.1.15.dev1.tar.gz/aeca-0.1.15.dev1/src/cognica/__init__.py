bc3c98a881a8755f761f07c683fb98305d51c3b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 03:05:05 +0000

    Add package aeca/0.1.15.dev1/aeca-0.1.15.dev1.tar.gz

---
+++ packages/aeca/aeca-0.1.15.dev1.tar.gz/aeca-0.1.15.dev1/src/cognica/__init__.py
@@ -0,0 +1,31 @@
+#
+# Cognica
+#
+# Copyright (c) 2023 Cognica, Inc.
+#
+
+# pylint: disable=missing-module-docstring
+
+from cognica.channel import Channel
+from cognica.document_db import DocumentDB
+from cognica.fts_analysis_pipeline import FTSAnalysisPipeline
+from cognica.key_value_db import KeyValueDB, KeyspaceManager
+from cognica.sentence_transformer import (
+    SentenceTransformerEncoder,
+    SentenceTransformerCrossEncoder,
+    SentenceTransformerCLIPEncoder,
+    SentenceTransformerQAEncoder,
+)
+
+
+__all__ = [
+    "Channel",
+    "DocumentDB",
+    "FTSAnalysisPipeline",
+    "KeyValueDB",
+    "KeyspaceManager",
+    "SentenceTransformerEncoder",
+    "SentenceTransformerCrossEncoder",
+    "SentenceTransformerCLIPEncoder",
+    "SentenceTransformerQAEncoder",
+]
