07ac3c081d7615ea2b1dfc891a8e89cbc9ec78f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 03:05:03 +0000

    Add package laser-learning-environment/0.1.13/laser_learning_environment-0.1.13-cp312-cp312-manylinux_2_12_i686.manylinux2010_i686.whl

---
+++ packages/laser-learning-environment/laser_learning_environment-0.1.13-cp312-cp312-manylinux_2_12_i686.manylinux2010_i686.whl/lle/action.pyi
@@ -0,0 +1,29 @@
+from typing import List, Tuple, final
+
+@final
+class Action:
+    """Enumeration of all possible actions."""
+
+    NORTH: "Action"
+    SOUTH: "Action"
+    EAST: "Action"
+    WEST: "Action"
+    STAY: "Action"
+
+    N: int
+    """Enum cardinality"""
+
+    ALL: List["Action"]
+    """List of all variants"""
+
+    def __init__(self, action_num: int):
+        """Create an action from an integer identifier. Invalid values (< 0 or > 4) raise a ValueError."""
+    @property
+    def delta(self) -> Tuple[int, int]:
+        """The delta (i, j) in coordinates for this action."""
+    @property
+    def value(self) -> int:
+        """The integer value of this action."""
+    @property
+    def name(self) -> str:
+        """The string name of this action."""
