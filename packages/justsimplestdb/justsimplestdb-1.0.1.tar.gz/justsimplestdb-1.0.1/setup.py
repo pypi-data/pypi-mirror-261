2abb7724e12a29d5fd4ee9c8a02295edf004a747
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 07:03:56 +0000

    Add package justsimplestdb/1.0.1/justsimplestdb-1.0.1.tar.gz

---
+++ packages/justsimplestdb/justsimplestdb-1.0.1.tar.gz/justsimplestdb-1.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='justsimplestdb',
+    version='1.0.1',
+    description='Simplified DataBase Management System using raw python, can also read ".txt" files into database like format',
+    url='https://github.com/M1ch5lsk1/JustSimplestDB/tree/main',
+    author='Jakub Aleksander Michalski',
+    author_email='jakub.michalski.aleksander@gmail.com',
+    license='MIT',
+    packages=['justsimplestdb'],
+    zip_safe=False
+)
