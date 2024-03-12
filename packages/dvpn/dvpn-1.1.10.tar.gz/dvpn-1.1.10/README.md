0336d42113daaff18a655aa04a9268fe3f78fc9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 16:04:50 +0000

    Add package dvpn/1.1.10/dvpn-1.1.10.tar.gz

---
+++ packages/dvpn/dvpn-1.1.10.tar.gz/dvpn-1.1.10/README.md
@@ -0,0 +1,95 @@
+# DieVPN
+
+### Tool that allows to switch between VPNs with ease and speed
+
+[![image](https://img.shields.io/pypi/v/dvpn.svg)](https://pypi.org/project/dvpn/)
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/dvpn)](https://pypi.org/project/dvpn/)
+[![Downloads](https://pepy.tech/badge/dvpn)](https://pepy.tech/project/dvpn)
+### Supported
+
+#### VPNs
+
+* `AnyConnectVPN` - Cisco
+* `Tunnelblick`
+* `Viscosity`
+
+#### OS
+
+* MacOS
+* Linux (Debian, Ubuntu, ...)
+* Windows
+
+#### Python
+
+* 3.7+
+
+## Installing
+
+Install and update using [pip](https://pip.pypa.io/en/stable/quickstart/):
+
+```bash
+pip install dvpn
+
+or
+
+pip3 install dvpn
+```
+
+
+
+## How to set up with GUI
+
+* Use `dvpn gui` in terminal to add your VPNs, passwords and usernames are stored in plaintext so be sure to have disk
+  encrypted.
+* If it seems applications is freezing, it is just side effect of compatibility solution on OSX and not using threaded
+  connection/disconnection currently **#TODO** just wait it will finish
+* **Please be patient and don't click multiple times on buttons in same time**
+
+## How to set up with CLI
+
+* Copy `template_secret.json` to `package/config/secret.json`
+* Edit secret accordingly to json format and your credentials
+* **Now you can use cli commands**
+* _Future Release will contain auto vpn resolve `dvpn autoresolve`_
+
+For Tunnelblick you must install tunnelblickctl
+
+on macOS use:
+
+```
+brew tap benwebber/tunnelblickctl
+brew install tunnelblickctl
+```
+
+
+## How to set up Manually
+
+* Clone repository with `git clone https://github.com/jiri-otoupal/dievpn.git`
+* Install requirements with `pip install -r requirements.txt` 
+_(ignore / delete windows requirements pywin32 & wexpect if
+  your pip is trying to install them)_
+* Copy `template_secret.json` to `package/config/secret.json`
+* **Edit secret accordingly to json format and your credentials**
+
+## Usage
+
+All commands need to be launched in os terminal not in Pycharm terminal
+
+Access all functions through gui
+```
+dvpn gui
+```
+
+or CLI 
+
+Connect to VPN specified in `config/secret.json`
+
+```
+dvpn connect {name_in_credentials}
+```
+
+Disconnect from any currently connected VPN
+
+```
+dvpn disconnect
+```
---
+++ packages/dvpn/dvpn-1.1.10.tar.gz/dvpn-1.1.10/dvpn/vpns/tunnelblick.py
@@ -0,0 +1,87 @@
+import os
+import subprocess
+import sys
+from time import sleep
+
+from dvpn.vpns.base import VpnCli
+
+if sys.platform == "win32":
+    import wexpect
+else:
+    import pexpect as wexpect
+
+
+class TunnelblickCLI(VpnCli):
+    fields = [
+        {"name": "VPN Name", "placeholderText": "Name of VPN configuration"},
+    ]
+    cli_path_win = "NOT EXISTENT ! MAC OS ONLY"
+
+    cli_path_osx = "tunnelblickctl"
+
+    def get_state(self, vpn_name: str) -> str:
+        all_vpns = subprocess.check_output(
+            [self.get_default_cli_path(), "status"]
+        ).decode()
+        for line in all_vpns.split("\n"):
+            info = line.split()
+            if vpn_name == info[0]:
+                return info[1]
+        return None
+
+    def get_connected_vpn(self):
+        pass
+
+    def reset(self, cli_path=None, host: str = None):
+        print("...Disconnecting")
+
+        os.system(
+            f"{self.get_default_cli_path()} launch"
+        )
+
+        while host is not None and not (
+            "disconnected" in self.get_state(host).lower()
+            or "exiting" in self.get_state(host).lower()
+        ):
+            sleep(0.1)
+
+    def __connect(self, vpn_name) -> dict:
+        print(f">> Connecting to {vpn_name}")
+        os.system(
+            f"{self.get_default_cli_path()} connect {vpn_name}"
+        )
+
+        while not (
+            "connected" in (state_conn := self.get_state(vpn_name).lower())
+            or "disconnected" in state_conn
+            or "network_access" in state_conn
+            or "exiting" in state_conn
+        ):
+            sleep(0.1)
+
+        state = self.get_state(vpn_name).lower()
+        connected = "connected" in state or "network_access" in state
+        return {"connected": connected, "reason": "VPN Error"}
+
+    def connect(self, creds: dict) -> (bool, dict):
+        print("~ Resetting connection")
+        try:
+            self.reset()
+        except subprocess.CalledProcessError:
+            print("Launching Tunnelblick")
+            os.system(
+                f"{self.get_default_cli_path()} launch"
+            )
+
+        try:
+            stat = self.__connect(creds["VPN Name"])
+        except wexpect.TIMEOUT as ex:
+            stat = {"reason": "invalid credentials"}
+
+        if stat.get("connected", False):
+            print("<Successfully Connected>")
+            return True, stat
+        else:
+            print("Connection failed :(")
+            print(f"Reason {''.join(stat.get('log', ['Unable to read stdout']))}")
+            return False, stat
