c31afdd845d3256ca4b507bacc7541a5d32def3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 16:04:44 +0000

    Add package miau-mak/0.0.1/miau_mak-0.0.1.tar.gz

---
+++ packages/miau-mak/miau_mak-0.0.1.tar.gz/miau_mak-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name = 'miau_mak', # Имя пакета
+    version = '0.0.1',  # Версия пакета
+    author = 'Marina',
+    author_email = 'marina@icloud.com',
+    description = 'My Package',
+    packages = setuptools.find_packages(),
+)
