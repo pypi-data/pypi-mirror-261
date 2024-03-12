cea76069024d7f0c8dd1fcebaf4ace640bdc94d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 03:05:37 +0000

    Add package The-best-quote/0.0.17/The_best_quote-0.0.17.tar.gz

---
+++ packages/The-best-quote/The_best_quote-0.0.17.tar.gz/The_best_quote-0.0.17/The_best_quote/quote.py
@@ -0,0 +1,10 @@
+class quote():
+    def __init__(self, item):
+        if item == 0:
+            print("Делу время потехехех")
+        if item == 1:
+            print("ээде2де ыыыыы")
+        if item == 2:
+            print("Много пить - недолго жить")
+        if item == 3:
+            print("Мотивацию поднял, школа - дом, сегодня в зал")
