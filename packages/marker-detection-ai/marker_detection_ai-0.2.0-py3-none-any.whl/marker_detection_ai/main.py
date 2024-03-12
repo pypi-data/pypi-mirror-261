1b560241e2771138b624649cd4be05a63c25cf27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 15:05:25 +0000

    Add package marker-detection-ai/0.2.0/marker_detection_ai-0.2.0-py3-none-any.whl

---
+++ packages/marker-detection-ai/marker_detection_ai-0.2.0-py3-none-any.whl/marker_detection_ai/main.py
@@ -0,0 +1,15 @@
+from ultralytics import YOLO
+from importlib.resources import files
+
+def process_image(image_path):
+
+     # Initialize the YOLO model
+    weights_path = files('marker_detection_ai').joinpath('best_yolov8m.pt')
+    model = YOLO(str(weights_path))
+    
+    # Run inference
+    results = model(image_path)  # results list
+
+    # Return the results
+    return results
+
