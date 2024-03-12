a32297022bd5cd2454828ff02fed87f7fbff1c92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 16:04:44 +0000

    Add package difflib-chubak/0.0.1/difflib_chubak-0.0.1.tar.gz

---
+++ packages/difflib-chubak/difflib_chubak-0.0.1.tar.gz/difflib_chubak-0.0.1/setup.py
@@ -0,0 +1,11 @@
+# setup.py
+import setuptools
+
+setuptools.setup(
+    name='difflib_chubak', # Имя пакета
+    version='0.0.1',  # Версия пакета
+    author='Chubak',
+    author_email='104@mail.com',
+    description='My Package',
+    packages=setuptools.find_packages(),
+)
