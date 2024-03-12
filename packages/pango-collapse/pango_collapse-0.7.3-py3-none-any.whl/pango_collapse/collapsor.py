e5fbf0607e52b74a0b0e1fab5c7e0b458ebdc204
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 15:04:41 +0000

    Add package pango-collapse/0.7.3/pango_collapse-0.7.3-py3-none-any.whl

---
+++ packages/pango-collapse/pango_collapse-0.7.3-py3-none-any.whl/pango_collapse/collapsor.py
@@ -0,0 +1,78 @@
+from typing import List
+from urllib.error import URLError
+from pango_aliasor.aliasor import Aliasor
+import pandas as pd
+
+
+class Collapsor(Aliasor):
+    def __init__(self, alias_file=None):
+        try:
+            super().__init__(alias_file=alias_file)
+        except URLError:
+            from warnings import warn
+            from pathlib import Path
+            warn("""\n\nCould not download the alias_key file!\n\nUsing default the alias_key file which may be out of date. This may result in lineages that cannot be collapsed.\n\nThe alias_key is should be kept up to date because it contains the hierarchical information about lineages designations.\n\nTo specify a custom alias file use the `--alias-file` option.\n""")
+            alias_file = Path(__file__).parent / "alias_key.json"
+            super().__init__(alias_file=alias_file)
+
+
+
+    def collapse(
+        self, compressed_lineage: str, potential_parents: List[str], strict=False
+    ):
+        if compressed_lineage in potential_parents:
+            return compressed_lineage
+
+        uncompressed_lineage = self.uncompress(compressed_lineage)
+
+        parts = uncompressed_lineage.split(".")
+
+        for i in range(1, len(parts)):
+            compressed_parent_lineage = self.compress(".".join(parts[:-i]))
+            if compressed_parent_lineage in potential_parents:
+                return compressed_parent_lineage
+
+        if uncompressed_lineage.startswith("X") and "Recombinant" in potential_parents:
+            # special case for Recombinant
+            return "Recombinant"
+
+        if strict:
+            return None
+
+        return compressed_lineage
+
+    def collapse_column(
+        self, array_of_uncompress_lineages, potential_parents, strict=False
+    ):
+        return [
+            self.collapse(compressed_lineage, potential_parents, strict=strict)
+            if pd.notna(compressed_lineage)
+            else None
+            for compressed_lineage in array_of_uncompress_lineages
+        ]
+
+    def uncompress_column(self, array_of_compressed_lineages):
+        return [
+            self.uncompress(lineage) if pd.notna(lineage) else None
+            for lineage in array_of_compressed_lineages
+        ]
+
+    def expand(self, lineage, delimiter=":"):
+        uncompressed_lineage = self.uncompress(lineage)
+
+        parts = uncompressed_lineage.split(".")
+        levels = len(parts) - 1  # (remove starting B/A)
+        indirections = (levels) % 3 if (levels) % 3 != 0 else 3
+
+        expanded_lineage = [self.compress(uncompressed_lineage)]
+        for i in range(indirections, levels, 3):
+            compressed_parent_lineage = self.compress(".".join(parts[:-i]))
+            expanded_lineage.append(compressed_parent_lineage)
+
+        return delimiter.join(expanded_lineage[::-1])
+
+    def expand_column(self, array_of_lineages, delimiter=":"):
+        return [
+            self.expand(lineage, delimiter=delimiter) if pd.notna(lineage) else None
+            for lineage in array_of_lineages
+        ]
---
+++ packages/pango-collapse/pango_collapse-0.7.3-py3-none-any.whl/pango_collapse/main.py
@@ -0,0 +1,133 @@
+from pathlib import Path
+from typing import Optional
+import typer
+
+from .utils import load_potential_parents_from_file, load_potential_parents_from_url
+from .collapsor import Collapsor
+from rich import print
+
+app = typer.Typer()
+
+
+def get_version():
+    import pkg_resources
+
+    return pkg_resources.get_distribution("pango-collapse").version
+
+
+def version_callback(value: bool):
+    if value:
+        version = get_version()
+        print(f"pango-collapse {version}")
+        raise typer.Exit()
+
+
+@app.command(context_settings={"help_option_names": ["-h", "--help"]})
+def main(
+    input: Path = typer.Argument(
+        ...,
+        help="Path to input CSV/TSV with Lineage column.",
+        dir_okay=False,
+        exists=True,
+    ),
+    output: Path = typer.Option(
+        ...,
+        "-o",
+        "--output",
+        help="Path to output CSV/TSV with Lineage column.",
+        dir_okay=False,
+    ),
+    collapse_file: Optional[Path] = typer.Option(
+        f"{Path(__file__).parent.resolve()}/collapse.txt",
+        "-c",
+        "--collapse-file",
+        help="Path to collapse file with lineages (one per line) to collapse up to. Defaults to collapse file shipped with this version of pango-collapse.",
+    ),
+    lineage_column: Optional[str] = typer.Option(
+        "Lineage",
+        "-l",
+        "--lineage-column",
+        help="Column to extract from input file for lineage.",
+    ),
+    full_column: Optional[str] = typer.Option(
+        "Lineage_full",
+        "-f",
+        "--full-column",
+        help="Column to use for the uncompressed output.",
+    ),
+    collapse_column: Optional[str] = typer.Option(
+        "Lineage_family",
+        "-k",
+        "--collapse-column",
+        help="Column to use for the collapsed output.",
+    ),
+    expand_column: Optional[str] = typer.Option(
+        "Lineage_expanded",
+        "-e",
+        "--expand-column",
+        help="Column to use for the expanded output.",
+    ),
+    alias_file: Optional[Path] = typer.Option(
+        None,
+        "-a",
+        "--alias-file",
+        help="Path to Pango Alias file for pango_aliasor. Will download latest file if not supplied.",
+    ),
+    strict: Optional[bool] = typer.Option(
+        False,
+        "-s",
+        "--strict",
+        help="If a lineage is not in the collapse file return None instead of the compressed lineage.",
+    ),
+    latest: Optional[bool] = typer.Option(
+        False,
+        "-u",
+        "--latest",
+        help="Load the collapse from from a url (--url).",
+    ),
+    collapse_file_url: Optional[str] = typer.Option(
+        None,
+        "--url",
+        help="Url to use when loading the collapse file with --latest. https://raw.githubusercontent.com/MDU-PHL/pango-collapse/main/pango_collapse/collapse.txt",
+    ),
+    version: Optional[bool] = typer.Option(
+        None,
+        "-v",
+        "--version",
+        callback=version_callback,
+        is_eager=True,
+        help="Print the current version number and exit.",
+    ),
+):
+    """
+    Collapse Pango sublineages up to user defined parent lineages.
+    """
+    version = get_version()
+    print(f"\n[bold green]pango-collapse {version}[bold green]\n")
+    import pandas as pd
+
+    collapsor = Collapsor(alias_file=alias_file)
+
+    sep = ","
+    if input.suffix == ".tsv":
+        sep = "\t"
+    df = pd.read_csv(input, low_memory=False, sep=sep)
+    
+    df[full_column] = collapsor.uncompress_column(df[lineage_column])
+    df[expand_column] = collapsor.expand_column(df[full_column])
+
+    if latest or collapse_file_url is not None:
+        if collapse_file_url is None:
+            collapse_file_url = "https://raw.githubusercontent.com/MDU-PHL/pango-collapse/main/pango_collapse/collapse.txt"
+        print(f"Loading collapse file from {collapse_file_url}\n")
+        potential_parents = load_potential_parents_from_url(url=collapse_file_url)
+    else:
+        potential_parents = load_potential_parents_from_file(collapse_file=collapse_file)
+    print("[yellow]Collapsing up to the following lineages:[yellow]")
+    print(" -", "\n - ".join(potential_parents))
+    df[collapse_column] = collapsor.collapse_column(df[lineage_column], potential_parents=potential_parents, strict=strict)
+    
+    sep = ","
+    if output.suffix == ".tsv":
+        sep = "\t"
+    df.to_csv(output, index=False, sep=sep)
