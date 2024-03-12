a3ce44a7f9e55ace4c704b595eea0c6417a480f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 03:05:36 +0000

    Add package The-best-quote/0.0.16/The_best_quote-0.0.16.tar.gz

---
+++ packages/The-best-quote/The_best_quote-0.0.16.tar.gz/The_best_quote-0.0.16/The_best_quote/quote.py
@@ -0,0 +1,8 @@
+class quote():
+    def __init__(self, item):
+        if item == 0:
+            print("Делу время потехехех")
+        if item == 1:
+            print("ээде2де ыыыыы")
+        if item == 2:
+            print("Хуй пизда сковорода")
