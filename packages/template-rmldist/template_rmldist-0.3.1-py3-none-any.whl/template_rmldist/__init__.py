ea081a3ce0c1a1eb78dc05c48656091f67dd6fb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 16:04:38 +0000

    Add package template-rmldist/0.3.1/template_rmldist-0.3.1-py3-none-any.whl

---
+++ packages/template-rmldist/template_rmldist-0.3.1-py3-none-any.whl/template_rmldist/__init__.py
@@ -0,0 +1,14 @@
+import rich
+__version__ = "0.3.1"
+
+
+def hello(name):
+    return f"Hello, {name}!"
+
+
+def goodbye(name):
+    return f"Goodbye, {name}!"
+
+
+def print_bluehello(name):
+    rich.print(f"Hello, [blue]{name}[/blue]! (in blue)")
