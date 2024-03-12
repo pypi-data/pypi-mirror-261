d3a6c91eac119fc48dcc99c28e2647972a254298
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 05:04:26 +0000

    Add package TensorFlowPlus/0.1.0/TensorFlowPlus-0.1.0.tar.gz

---
+++ packages/TensorFlowPlus/TensorFlowPlus-0.1.0.tar.gz/TensorFlowPlus-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="TensorFlowPlus",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "tensorflow",
+        # Add any additional dependencies here
+    ],
+)
