c3706f9a9dd1b524e98c3125f81e93c045be37af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 04:04:15 +0000

    Add package usd-lde/0.0.1/usd_lde-0.0.1.tar.gz

---
+++ packages/usd-lde/usd_lde-0.0.1.tar.gz/usd_lde-0.0.1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name='usd_lde', # Имя пакета
+    version='0.0.1',  # Версия пакета
+    author_email='your@domain.com',
+    description='My Package',
+    packages=setuptools.find_packages(),
+)
