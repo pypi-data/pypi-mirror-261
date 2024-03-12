5f35adc3f79c0ab07a96803d9f9bf12bcd89cefe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 04:04:24 +0000

    Add package bioflow-insight/0.0.24/bioflow-insight-0.0.24.tar.gz

---
+++ packages/bioflow-insight/bioflow-insight-0.0.24.tar.gz/bioflow-insight-0.0.24/tests/test_outils.py
@@ -0,0 +1,41 @@
+import unittest
+from src.outils import *
+
+class TestOutils(unittest.TestCase):
+
+    #TODO -> finish this
+
+    def test_get_next_element_caracter(self):
+        test = """This is a test\n!"""
+        val, index = get_next_element_caracter(test, 3)
+        self.assertEqual(val, 'i')
+        self.assertEqual(index, 5)
+        val, index = get_next_element_caracter(test, 13)
+        self.assertEqual(val, '!')
+        self.assertEqual(index, 15)
+        val, index = get_next_element_caracter(test, 15)
+        self.assertEqual(val, -1)
+        self.assertEqual(index, -1)
+    
+    def test_get_before_element_caracter(self):
+        test = """This is a test\n!"""
+        val, index = get_before_element_caracter(test, 0)
+        self.assertEqual(val, -1)
+        self.assertEqual(index, -1)
+        val, index = get_before_element_caracter(test, 5)
+        self.assertEqual(val, 's')
+        self.assertEqual(index, 3)
+        val, index = get_before_element_caracter(test, 15)
+        self.assertEqual(val, 't')
+        self.assertEqual(index, 13)
+
+    def test_remove_comments(self):
+        code_with_comments = ''
+        with open("tests/ressources/outils/remove_comments_with.nf", 'r') as f:
+            code_with_comments = f.read()
+    
+        with open("tests/ressources/outils/remove_comments_wo.nf", 'r') as f:
+            code_wo_comments = f.read()
+        self.assertEqual(remove_comments(code_with_comments), code_wo_comments)
+
+    
