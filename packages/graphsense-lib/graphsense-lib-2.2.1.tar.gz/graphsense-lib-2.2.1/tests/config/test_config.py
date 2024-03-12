88ed9b0cfd1c8b35880edff5dfab8ccbb610f15c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 03:04:54 +0000

    Add package graphsense-lib/2.2.1/graphsense-lib-2.2.1.tar.gz

---
+++ packages/graphsense-lib/graphsense-lib-2.2.1.tar.gz/graphsense-lib-2.2.1/tests/config/test_config.py
@@ -0,0 +1,13 @@
+from graphsenselib.config import config, get_approx_reorg_backoff_blocks
+
+
+def test_config_is_not_loaded_by_default():
+    # But real config should not be automatically loaded.
+    assert config.is_loaded() is False
+
+
+def test_get_approx_reorg_backoff_blocks():
+    assert get_approx_reorg_backoff_blocks("btc") == 8
+    assert get_approx_reorg_backoff_blocks("bch", 1.5) == 9
+    assert get_approx_reorg_backoff_blocks("eth") == 288
+    assert get_approx_reorg_backoff_blocks("ltc") == 29
