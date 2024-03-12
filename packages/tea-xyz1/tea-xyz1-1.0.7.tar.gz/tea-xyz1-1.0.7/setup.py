6609d67209caae82b8fdad46bde07fb7a35cd6bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 03:04:48 +0000

    Add package tea-xyz1/1.0.7/tea-xyz1-1.0.7.tar.gz

---
+++ packages/tea-xyz1/tea-xyz1-1.0.7.tar.gz/tea-xyz1-1.0.7/setup.py
@@ -0,0 +1,55 @@
+from setuptools import setup
+
+setup(
+    author='Alex',
+    author_email='madest92@mail.com',
+    name='tea-xyz1',
+    version='1.0.7',
+    description='A simple package for https://app.tea.xyz/. Example tea-xyz1 - https://github.com/madest92/tea-xyz1 and tea-xyz2 - https://github.com/madest92/tea-xyz2',
+    url='https://github.com/madest92/tea-xyz1',
+    project_urls={
+        'Homepage': 'https://github.com/madest92/tea-xyz1',
+        'Source': 'https://github.com/madest92/tea-xyz1',
+    },
+    py_modules=['hello_tea'],
+    entry_points={
+        'console_scripts': [
+            'hello-tea=hello_tea:hello_tea_func'
+        ]
+    },
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'requests>=2.20.0',
+        'tea-xyz2',
+        'teaxyz',
+        'teaxyzz',
+        'teaxyzzz',
+        'teaxyzzzz',
+        'teaxyzzzzz',
+        'teaxyzzzzzz',
+        'teaxyzzz1',
+        'teaxyzzz2',
+        'teaxyzzz3',
+        'teaxyzzz4',
+        'teaxyzzz5',
+        'teaxyzzz6',
+        'teaxyzzz7',
+        'teaxyzzz8',
+        'teaxyzzz9',
+        'teaxyzzz10',
+        'teaxyzzz11',
+        'teaxyzzz12',
+        'teaxyzzz13',
+        'teaxyzzz14',
+        'teaxyzzz15',
+        'teaxyzzz16',
+        'teaxyzzz17',
+        'teaxyzzz18',
+        'teaxyzzz19',
+        'teaxyzzz20',
+    ],
+)
