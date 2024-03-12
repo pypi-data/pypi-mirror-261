3a8fd00a411364ed3239f8d96bef97eb750b5433
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 07:03:46 +0000

    Add package pixegami/0.2/pixegami-0.2.tar.gz

---
+++ packages/pixegami/pixegami-0.2.tar.gz/pixegami-0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pixegami',
+    version='0.2',
+    packages=find_packages(),
+    install_requires=[
+        'numpy>=1.11.1'
+    ],
+    entry_points={
+        "console_scripts": [
+            "pixegami-hello = pixegami:hello",
+        ],
+    }
+)
