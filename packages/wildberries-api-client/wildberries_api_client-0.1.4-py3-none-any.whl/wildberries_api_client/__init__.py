cd482ae2a09b21f72d2f5effbeace032e021dd58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 16:04:54 +0000

    Add package wildberries-api-client/0.1.4/wildberries_api_client-0.1.4-py3-none-any.whl

---
+++ packages/wildberries-api-client/wildberries_api_client-0.1.4-py3-none-any.whl/wildberries_api_client/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+
+import analytics
+import content
+import feedbacks_questions
+import marketplace
+import prices
+import promotion
+import recommendations
+import statistics
+import tariffs
