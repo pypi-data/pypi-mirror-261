8460634f9f8da366992f6611446dbfa35a8f1d85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 05:04:26 +0000

    Add package mp-basic-calculator/0.1/mp_basic_calculator-0.1.tar.gz

---
+++ packages/mp-basic-calculator/mp_basic_calculator-0.1.tar.gz/mp_basic_calculator-0.1/mp_basic_calculator/calculator.py
@@ -0,0 +1,66 @@
+from typing import Union
+
+
+class Calculator:
+    """It's basic calculator made with python"""
+
+    def __init__(self):
+        """Initialize the calculator and internal memory"""
+        self.memory = 0
+
+    def addition(self, x: Union[int, float]) -> Union[int, float]:
+        """Accepts value as float or integer and adds to existing value in the memory
+
+        Parameters:
+        x: Should be integer or float
+        """
+        self.memory = self.memory + x
+        print(self.memory)
+        return self.memory
+
+    def subtract(self, x: Union[int, float]) -> Union[int, float]:
+        """Accepts value as float or integer and subtracts to existing value in the memory
+
+        Parameters:
+        x: Should be integer or float
+        """
+        self.memory = self.memory - x
+        print(self.memory)
+        return self.memory
+
+    def multiply(self, x: Union[int, float]) -> Union[int, float]:
+        """Accepts value as float or integer and multiplies to existing value in the memory
+
+        Parameters:
+        x: Should be integer or float
+        """
+        self.memory *= x
+        print(self.memory)
+        return self.memory
+
+    def divide(self, x: Union[int, float]) -> Union[int, float]:
+        """Accepts value as float or integer and divides to existing value in the memory
+
+        Parameters:
+        x: Should be integer or float
+        """
+        self.memory /= x
+        print(self.memory)
+        return self.memory
+
+    def take_nth_root(self, x: Union[int, float], y: float) -> Union[int, float]:
+        """Accepts value as float or integer and takes nth root, which given as second parameter as integer
+
+        Parameters:
+        x: Should be integer or float
+        y: Should be integer
+        """
+        self.memory = x ** (1 / y)
+        print(self.memory)
+        return self.memory
+
+    def clean_memory(self):
+        """Cleans the memory"""
+        self.memory = 0
+        print(self.memory)
+        return self.memory
---
+++ packages/mp-basic-calculator/mp_basic_calculator-0.1.tar.gz/mp_basic_calculator-0.1/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup, find_packages
+
+setup(name='mp_basic_calculator', version='0.1', packages=find_packages(), install_requires=['pytest'])
