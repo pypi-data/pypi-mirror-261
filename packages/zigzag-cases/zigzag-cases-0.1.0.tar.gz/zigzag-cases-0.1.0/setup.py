150044cb50214f00e6b5b54a9150170675c930f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 15:04:48 +0000

    Add package zigzag-cases/0.1.0/zigzag-cases-0.1.0.tar.gz

---
+++ packages/zigzag-cases/zigzag-cases-0.1.0.tar.gz/zigzag-cases-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = "zigzag-cases",
+  version = "0.1.0",
+  author = "Avinash R",
+  description = "This is short description",
+  long_description = open("README.md").read(),
+  long_description_content_type="text/markdown"
+)
