b91ceeedac4f272c6b7d199ea50e005e0350ef1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 04:04:28 +0000

    Add package sf2-baso-kontol/0.2.0/sf2_baso_kontol-0.2.0.tar.gz

---
+++ packages/sf2-baso-kontol/sf2_baso_kontol-0.2.0.tar.gz/sf2_baso_kontol-0.2.0/setup.py
@@ -0,0 +1,34 @@
+import pathlib
+import setuptools
+
+
+setuptools.setup(
+        name="sf2_baso_kontol",
+        version="0.2.0",
+        description="pypi test",
+        long_description=pathlib.Path("README.md").read_text(),
+        long_description_content_type="text/markdown",
+        url="https://github.com/IrfanDect/baso_kontol_clic",
+        author="IrfanDect",
+        author_email="fasy@gmail.com",
+        license="MIT",
+        install_requires=["rich","requests","prompt-toolkit","baso_kontol"],
+        extras_require={
+            "excel": ["openpyxl"],
+            },
+        packages=setuptools.find_packages(),
+        include_package_data=True,
+        classifiers=[
+        'Intended Audience :: Developers', 'Topic :: Utilities',
+        'License :: Public Domain', 'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+        ],
+        keywords="super_baso_kontol",
+        entry_points={"console_script": ["sf2_baso_kontol = sf2_baso_kontol.cli:main"]}
+        )
