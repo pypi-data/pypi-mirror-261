d31a4eb7bffdf7da592f9f36d8c2c89ebc313044
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 03:04:52 +0000

    Add package skymap-index-monitoring/0.0.0/skymap_index_monitoring-0.0.0.tar.gz

---
+++ packages/skymap-index-monitoring/skymap_index_monitoring-0.0.0.tar.gz/skymap_index_monitoring-0.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='skymap_index_monitoring',
+    version='0.0.0',
+    packages=find_packages(),
+    install_requires=[
+        # "requests",
+    ],
+)
