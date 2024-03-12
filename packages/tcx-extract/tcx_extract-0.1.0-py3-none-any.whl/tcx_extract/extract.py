94860b53565e8994c9f8f6112c5fb3949272eb25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 07:03:54 +0000

    Add package tcx-extract/0.1.0/tcx_extract-0.1.0-py3-none-any.whl

---
+++ packages/tcx-extract/tcx_extract-0.1.0-py3-none-any.whl/tcx_extract/extract.py
@@ -0,0 +1,11 @@
+import subprocess
+import os
+
+def get_tag(filepath: str, tag_name: str) -> str:
+    cwd = os.path.abspath(os.path.dirname(__file__))
+    abs_path = os.path.join(cwd, 'zig', 'extract')
+    return subprocess.check_output([abs_path, filepath, tag_name]).decode('utf-8')
+
+def extract(filepath: str, tag_name: str) -> list[str]:
+    result = get_tag(filepath=filepath, tag_name=tag_name)
+    return result.split('\n')
