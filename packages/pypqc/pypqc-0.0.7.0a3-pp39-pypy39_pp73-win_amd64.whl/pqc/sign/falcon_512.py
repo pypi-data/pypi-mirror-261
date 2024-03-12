f25685b3807ff93e36fad73a987fa909233f1bd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 07:03:57 +0000

    Add package pypqc/0.0.7.0a3/pypqc-0.0.7.0a3-pp39-pypy39_pp73-win_amd64.whl

---
+++ packages/pypqc/pypqc-0.0.7.0a3-pp39-pypy39_pp73-win_amd64.whl/pqc/sign/falcon_512.py
@@ -0,0 +1,17 @@
+from .._common import _VarSigAlg
+
+from pathlib import Path
+import os
+
+
+if os.environ.get('LICENSED_FALCON', '0') == '0':
+	# fmt: off
+	from .._util import patent_notice
+	patent_notice(['US7308097B2'],
+	            'the Falcon cryptosystem', 2,
+	            ['https://csrc.nist.gov/csrc/media/Projects/post-quantum-cryptography/documents/selected-algos-2022/final-ip-statements/Falcon-Statements-final.pdf#page=20']
+	)
+	# fmt: on
+
+
+_VarSigAlg._init_module(Path(__file__).stem, globals())
