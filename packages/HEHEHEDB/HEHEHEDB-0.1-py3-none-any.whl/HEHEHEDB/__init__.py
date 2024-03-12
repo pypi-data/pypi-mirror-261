d031b47c97f8209c8325f4405d1b22f0bc0047b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 04:04:14 +0000

    Add package HEHEHEDB/0.1/HEHEHEDB-0.1-py3-none-any.whl

---
+++ packages/HEHEHEDB/HEHEHEDB-0.1-py3-none-any.whl/HEHEHEDB/__init__.py
@@ -0,0 +1,26 @@
+# __init__.py
+
+import mysql.connector
+
+# Assuming you have 'mydb' already initialized somewhere in your code
+# Replace 'mydb' with the correct connection object
+
+def CDB(text,mydb):
+    mycursor = mydb.cursor()
+    mycursor.execute("CREATE DATABASE " + text)
+
+def CTB(text,mydb):
+    mycursor = mydb.cursor()
+    mycursor.execute("CREATE TABLE " + text)
+
+def ITBV(text, text1,text2,mydb):
+    sqlFormula = "INSERT INTO " + text + text1
+    mycursor = mydb.cursor()
+    mycursor.execute(sqlFormula, text2)
+    mydb.commit()
+
+def ITBVS(text, text1,text2,mydb):
+    sqlFormula = "INSERT INTO " + text + text1
+    mycursor = mydb.cursor()
+    mycursor.executemany(sqlFormula, text2)
+    mydb.commit()
