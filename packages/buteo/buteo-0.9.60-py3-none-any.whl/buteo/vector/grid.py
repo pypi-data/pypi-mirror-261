cec1d5e926c796d7f090b05bbd50aa5218f8dbcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 05:04:26 +0000

    Add package buteo/0.9.60/buteo-0.9.60-py3-none-any.whl

---
+++ packages/buteo/buteo-0.9.60-py3-none-any.whl/buteo/vector/grid.py
@@ -0,0 +1,10 @@
+"""
+Create vector grids from references or hardcode
+"""
+
+# TODO: Implement grid functions
+# TODO: create_grid, create_grid_points, create_grid_linestrings
+
+def _vector_create_grid():
+    """ Not implemented."""
+    raise RuntimeError("Not implemented")
