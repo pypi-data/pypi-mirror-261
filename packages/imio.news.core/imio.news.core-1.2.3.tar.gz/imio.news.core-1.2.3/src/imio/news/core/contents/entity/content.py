d816eaa0c5893f59766ff2a247b5325b94c673e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 16:04:28 +0000

    Add package imio.news.core/1.2.3/imio.news.core-1.2.3.tar.gz

---
+++ packages/imio.news.core/imio.news.core-1.2.3.tar.gz/imio.news.core-1.2.3/src/imio/news/core/contents/entity/content.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from imio.smartweb.common.interfaces import ILocalManagerAware
+from imio.smartweb.locales import SmartwebMessageFactory as _
+from plone import schema
+from plone.app.z3cform.widget import SelectFieldWidget
+from plone.autoform import directives
+from plone.dexterity.content import Container
+from plone.supermodel import model
+from zope.interface import implementer
+
+
+class IEntity(model.Schema):
+    """Marker interface and Dexterity Python Schema for Entity"""
+
+    directives.widget(zip_codes=SelectFieldWidget)
+    zip_codes = schema.List(
+        title=_("Zip codes and cities"),
+        description=_("Choose zip codes for this entity"),
+        value_type=schema.Choice(vocabulary="imio.smartweb.vocabulary.Cities"),
+    )
+
+    model.fieldset("categorization", fields=["local_categories"])
+    local_categories = schema.Text(
+        title=_("Specific news categories"),
+        description=_(
+            "List of news categories values available for this entity (one per line)"
+        ),
+        required=False,
+    )
+
+
+@implementer(IEntity, ILocalManagerAware)
+class Entity(Container):
+    """Entity content type"""
