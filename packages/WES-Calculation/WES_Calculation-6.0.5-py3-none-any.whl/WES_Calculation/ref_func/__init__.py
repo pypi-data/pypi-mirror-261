dee7bc41d702d588a4e2e1c921e8298c2f5dc51b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 04:04:16 +0000

    Add package WES-Calculation/6.0.5/WES_Calculation-6.0.5-py3-none-any.whl

---
+++ packages/WES-Calculation/WES_Calculation-6.0.5-py3-none-any.whl/WES_Calculation/ref_func/__init__.py
@@ -0,0 +1,12 @@
+'''
+Date         : 2023-01-24 11:30:46
+Author       : BDFD,bdfd2005@gmail.com
+Github       : https://github.com/bdfd
+LastEditTime : 2023-01-24 11:46:52
+LastEditors  : BDFD
+Description  : 
+FilePath     : \WES_Calculation\ref_func\__init__.py
+Copyright (c) 2023 by BDFD, All Rights Reserved. 
+'''
+from .common_func import data_conv, data_conv_int, return_img_stream
+from .windspeed_func import phi1f, psi1f, psi2f, A, B, att
