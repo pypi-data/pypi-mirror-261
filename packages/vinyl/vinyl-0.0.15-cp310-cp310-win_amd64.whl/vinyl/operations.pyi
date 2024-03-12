4c3b8626b3ebb492bde7c38a4373a49170a83496
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 07:04:00 +0000

    Add package vinyl/0.0.15/vinyl-0.0.15-cp310-cp310-win_amd64.whl

---
+++ packages/vinyl/vinyl-0.0.15-cp310-cp310-win_amd64.whl/vinyl/operations.pyi
@@ -0,0 +1,1 @@
+from vinyl.lib.constructors import date as date, interval as interval, literal as literal, map as map, now as now, random as random, struct as struct, time as time, timestamp as timestamp
