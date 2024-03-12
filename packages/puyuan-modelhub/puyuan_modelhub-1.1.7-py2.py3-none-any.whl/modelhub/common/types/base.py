1a440aacd1af45b596dc4964799cf4a44e0ef123
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 16:05:11 +0000

    Add package puyuan-modelhub/1.1.7/puyuan_modelhub-1.1.7-py2.py3-none-any.whl

---
+++ packages/puyuan-modelhub/puyuan_modelhub-1.1.7-py2.py3-none-any.whl/modelhub/common/types/base.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel as PydanticBaseModel
+
+
+class BaseModel(PydanticBaseModel):
+    def to_event(self, prefix="data: ") -> str:
+        return f"{prefix}{self.json()}\r\n\r\n"
+
+    class Config:
+        arbitrary_types_allowed = True
