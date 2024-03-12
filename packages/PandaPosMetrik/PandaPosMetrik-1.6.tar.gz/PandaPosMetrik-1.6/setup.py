1ceaac51d414454a95561bc5333f5b514b3c07b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 04:04:11 +0000

    Add package PandaPosMetrik/1.6/PandaPosMetrik-1.6.tar.gz

---
+++ packages/PandaPosMetrik/PandaPosMetrik-1.6.tar.gz/PandaPosMetrik-1.6/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="PandaPosMetrik",
+    version="1.6",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        
+        "pyodbc",
+    ]
+)
