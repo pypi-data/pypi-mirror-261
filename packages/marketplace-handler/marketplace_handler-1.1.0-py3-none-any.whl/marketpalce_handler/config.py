e273361be7fa30448527f302ad9d6a65ff5a9ec4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 16:05:02 +0000

    Add package marketplace-handler/1.1.0/marketplace_handler-1.1.0-py3-none-any.whl

---
+++ packages/marketplace-handler/marketplace_handler-1.1.0-py3-none-any.whl/marketpalce_handler/config.py
@@ -0,0 +1,7 @@
+class Settings:
+    wb_api_url: str = "https://suppliers-api.wildberries.ru/"
+    wb_price_url: str = "https://discounts-prices-api.wb.ru/"
+    WB_ITEMS_REFRESH_LIMIT: int = 1000
+
+
+settings = Settings()
