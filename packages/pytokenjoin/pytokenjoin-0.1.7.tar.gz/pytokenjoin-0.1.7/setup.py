ce7a4587bc0b8db392d2af1a4149a703223b926c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 16:04:36 +0000

    Add package pytokenjoin/0.1.7/pytokenjoin-0.1.7.tar.gz

---
+++ packages/pytokenjoin/pytokenjoin-0.1.7.tar.gz/pytokenjoin-0.1.7/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name="pytokenjoin",
+    version="0.1.7",
+    description="pyTokenJoin is a library containing efficient algorithms that solve the set similarity join problem with maximum weighted bipartite matching.",
+    author="Alexandros Zeakis",
+    author_email="azeakis@athenarc.gr",
+    url="https://github.com/alexZeakis/pyTokenJoin",
+    packages=["pytokenjoin", "pytokenjoin.jaccard", "pytokenjoin.edit", "pytokenjoin.utils"],
+    install_requires=[
+        "editdistance==0.6.2",
+        "networkx==3.1",
+        "pandas==1.5.3",
+        "numpy==1.23.5"
+    ],
+)
+
