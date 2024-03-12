8559fdb155e0bd8a1634acbf5d88b5e337cce6a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 16:04:40 +0000

    Add package aiosalesforce/0.1.0/aiosalesforce-0.1.0-py3-none-any.whl

---
+++ packages/aiosalesforce/aiosalesforce-0.1.0-py3-none-any.whl/aiosalesforce/auth/base.py
@@ -0,0 +1,182 @@
+import asyncio
+import logging
+
+from abc import ABC, abstractmethod
+from typing import final
+
+from httpx import AsyncClient
+
+from aiosalesforce.events import EventBus
+
+logger = logging.getLogger(__name__)
+
+
+class Auth(ABC):
+    def __init__(self) -> None:
+        self.__access_token: str | None = None
+        self.__lock = asyncio.Lock()
+
+    @final
+    async def get_access_token(
+        self,
+        client: AsyncClient,
+        base_url: str,
+        version: str,
+        event_bus: EventBus,
+    ) -> str:
+        """
+        Get access token.
+
+        If this is the first time this method is called, it will acquire a new
+        access token from Salesforce.
+
+        Parameters
+        ----------
+        client : httpx.AsyncClient
+            HTTP client.
+        base_url : str
+            Salesforce base URL.
+            E.g., "https://mydomain.my.salesforce.com".
+        version : str
+            REST API version.
+            E.g., "57.0".
+        event_bus : aiosalesforce.events.EventBus
+            Event bus.
+
+        Returns
+        -------
+        str
+            Access token
+
+        """
+        if self.__access_token is not None:
+            return self.__access_token
+        async with self.__lock:
+            if self.__access_token is None:
+                logger.debug(
+                    "Acquiring new access token using %s for %s",
+                    self.__class__.__name__,
+                    base_url,
+                )
+                self.__access_token = await self._acquire_new_access_token(
+                    client=client,
+                    base_url=base_url,
+                    version=version,
+                    event_bus=event_bus,
+                )
+            return self.__access_token
+
+    @final
+    async def refresh_access_token(
+        self,
+        client: AsyncClient,
+        base_url: str,
+        version: str,
+        event_bus: EventBus,
+    ) -> str:
+        """
+        Refresh the access token.
+
+        Parameters
+        ----------
+        client : httpx.AsyncClient
+            HTTP client.
+        base_url : str
+            Salesforce base URL.
+            E.g., "https://mydomain.my.salesforce.com".
+        version : str
+            REST API version.
+            E.g., "57.0".
+        event_bus : aiosalesforce.events.EventBus
+            Event bus.
+
+        Returns
+        -------
+        str
+            Access token
+
+        """
+        if self.__access_token is None:
+            raise RuntimeError("No access token to refresh")
+        token_before_refresh = self.__access_token
+        async with self.__lock:
+            if self.__access_token == token_before_refresh:
+                logger.debug(
+                    "Refreshing access token using %s for %s",
+                    self.__class__.__name__,
+                    base_url,
+                )
+                self.__access_token = await self._refresh_access_token(
+                    client=client,
+                    base_url=base_url,
+                    version=version,
+                    event_bus=event_bus,
+                )
+            return self.__access_token
+
+    @abstractmethod
+    async def _acquire_new_access_token(
+        self,
+        client: AsyncClient,
+        base_url: str,
+        version: str,
+        event_bus: EventBus,
+    ) -> str:
+        """
+        Acquire a new access token from Salesforce.
+
+        Parameters
+        ----------
+        client : httpx.AsyncClient
+            HTTP client.
+        base_url : str
+            Salesforce base URL.
+            E.g., "https://mydomain.my.salesforce.com".
+        version : str
+            REST API version.
+            E.g., "57.0".
+        event_bus : aiosalesforce.events.EventBus
+            Event bus.
+
+        Returns
+        -------
+        str
+            Access token
+
+        """
+
+    async def _refresh_access_token(
+        self,
+        client: AsyncClient,
+        base_url: str,
+        version: str,
+        event_bus: EventBus,
+    ) -> str:
+        """
+        Refresh the access token.
+
+        Parameters
+        ----------
+        client : httpx.AsyncClient
+            HTTP client.
+        base_url : str
+            Salesforce base URL.
+            E.g., "https://mydomain.my.salesforce.com".
+        version : str
+            REST API version.
+            E.g., "57.0".
+        event_bus : aiosalesforce.events.EventBus
+            Event bus.
+
+        Returns
+        -------
+        str
+            Access token
+
+        """
+        return await self._acquire_new_access_token(
+            client=client,
+            base_url=base_url,
+            version=version,
+            event_bus=event_bus,
+        )
---
+++ packages/aiosalesforce/aiosalesforce-0.1.0-py3-none-any.whl/aiosalesforce/auth/client_credentials_flow.py
@@ -0,0 +1,48 @@
+from httpx import AsyncClient
+
+from aiosalesforce.auth.base import Auth
+from aiosalesforce.events import EventBus, RestApiCallConsumptionEvent
+from aiosalesforce.exceptions import AuthenticationError
+
+
+class ClientCredentialsFlow(Auth):
+    """
+    Authenticate using the OAuth 2.0 Client Credentials Flow.
+
+    https://help.salesforce.com/s/articleView?id=sf.remoteaccess_oauth_client_credentials_flow.htm&type=5
+
+    """
+
+    def __init__(self, client_id: str, client_secret: str) -> None:
+        super().__init__()
+        self.client_id = client_id
+        self.client_secret = client_secret
+
+    async def _acquire_new_access_token(
+        self,
+        client: AsyncClient,
+        base_url: str,
+        version: str,
+        event_bus: EventBus,
+    ) -> str:
+        del version  # not used in this flow (this line is for linter)
+        response = await client.post(
+            f"{base_url}/services/oauth2/token",
+            headers={
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+            data={
+                "grant_type": "client_credentials",
+                "client_id": self.client_id,
+                "client_secret": self.client_secret,
+            },
+        )
+        await event_bus.publish_event(
+            RestApiCallConsumptionEvent(
+                type="rest_api_call_consumption",
+                response=response,
+            )
+        )
+        if not response.is_success:
+            raise AuthenticationError(response.text, response)
+        return response.json()["access_token"]
