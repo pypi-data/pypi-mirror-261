7161f210214b8dbff46fdacfa1a7250e9d1f6347
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 05:05:27 +0000

    Add package ENGR131-Util-2024/0.2.20/ENGR131_Util_2024-0.2.20.tar.gz

---
+++ packages/ENGR131-Util-2024/ENGR131_Util_2024-0.2.20.tar.gz/ENGR131_Util_2024-0.2.20/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="ENGR131_Util_2024",
+    version="0.2.20",
+    author="Joshua C. Agar",
+    description="Drexel Midterm Utility Functions",
+    packages=["ENGR131_Util_2024"],
+    install_requires=["cryptography", "drexel_jupyter_logger", "ipywidgets"],
+)
