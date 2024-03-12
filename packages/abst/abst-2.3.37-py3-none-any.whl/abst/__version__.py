009162aef404cd2b073255f60eeaeec4146c739a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 03:04:49 +0000

    Add package abst/2.3.37/abst-2.3.37-py3-none-any.whl

---
+++ packages/abst/abst-2.3.37-py3-none-any.whl/abst/__version__.py
@@ -0,0 +1,23 @@
+"""
+__version__.py
+~~~~~~~~~~~~~~
+
+Information about the current version of the py-package-template package.
+"""
+
+__title__ = "abst"
+__description__ = (
+    "CLI Command making OCI Bastion and kubernetes usage simple and fast"
+)
+
+__version__ = "2.3.37"
+__author__ = "Jiri Otoupal"
+__author_email__ = "jiri-otoupal@ips-database.eu"
+__license__ = "MIT"
+__url__ = "https://github.com/jiri-otoupal/abst"
+__pypi_repo__ = "https://pypi.org/project/abst/"
+
+__version_name__ = "Formatted-Merged Giraffe"
+__change_log__ = """
+* Fixed try except for response being None\n
+"""
