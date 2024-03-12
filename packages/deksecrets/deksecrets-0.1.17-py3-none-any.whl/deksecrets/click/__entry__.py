304ef52db6de86b34e1676e267a4e108891bffce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-12 06:05:18 +0000

    Add package deksecrets/0.1.17/deksecrets-0.1.17-py3-none-any.whl

---
+++ packages/deksecrets/deksecrets-0.1.17-py3-none-any.whl/deksecrets/click/__entry__.py
@@ -0,0 +1,12 @@
+from . import app
+from .infisical import app as infisical_app
+from .k8s import app as k8s_app
+from .gitea import app as gitea_app
+
+app.add_typer(infisical_app, name='infisical')
+app.add_typer(k8s_app, name='k8s')
+app.add_typer(gitea_app, name='gitea')
+
+
+def main():
+    app()
