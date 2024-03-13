e74430524312ed32ba712a1964aec7bb84ef424f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 05:04:56 +0000

    Add package aquacalc/0.0.5/aquacalc-0.0.5-py3-none-any.whl

---
+++ packages/aquacalc/aquacalc-0.0.5-py3-none-any.whl/aquacalc/aquacalc.py
@@ -0,0 +1,15 @@
+def calculate_tank_volume(length, width, height):
+    """Calculates the volume of a rectangular tank.
+
+    Args:
+        length (float): Length of the tank.
+        width (float): Width of the tank.
+        height (float): Height of the tank.
+
+    Returns:
+        float: The volume of the tank.
+    """
+    volume = length * width * height
+    return volume
+
+    
