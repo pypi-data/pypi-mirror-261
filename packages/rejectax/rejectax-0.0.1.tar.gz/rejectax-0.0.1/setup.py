5836e006d35666369e9657fdcb7db3095064e0c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 05:04:51 +0000

    Add package rejectax/0.0.1/rejectax-0.0.1.tar.gz

---
+++ packages/rejectax/rejectax-0.0.1.tar.gz/rejectax-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+name = 'rejectax'
+version = '0.0.1'
+
+with open('README.md' ,'r') as f:
+    long_description = f.read().strip()
+
+setup(
+    name=name,
+    version=version,
+    description=name,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url=f'https://github.com/mdmould/{name}',
+    author='Matthew Mould',
+    author_email='mattdmould@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=['jax'],
+    python_requires='>=3.10',
+    )
