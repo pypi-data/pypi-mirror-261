9f4e64e18b7a443facefd87a63aa1f4cd1306010
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 16:04:53 +0000

    Add package emploid/0.0.7/emploid-0.0.7.tar.gz

---
+++ packages/emploid/emploid-0.0.7.tar.gz/emploid-0.0.7/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.6'
+DESCRIPTION = 'automates stuff'
+LONG_DESCRIPTION = 'A package for spaghettification.'
+
+# Setting up
+setup(
+    name="emploid",
+    version=VERSION,
+    author="ayoub almontaser",
+    author_email="twyyre1996@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['opencv-python', 'pyautogui', 'pyaudio', 'Appium-Python-Client'],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Windows",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    package_data={
+        'scribe_templates' : ['modules/scribe/template/*.html']
+    }
+)
