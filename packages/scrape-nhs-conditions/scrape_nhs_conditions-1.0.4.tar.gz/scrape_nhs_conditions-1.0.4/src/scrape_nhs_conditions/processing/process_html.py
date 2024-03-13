c6babd251b4b92c8d1f62369d0565ff13868522b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 03:04:40 +0000

    Add package scrape-nhs-conditions/1.0.4/scrape_nhs_conditions-1.0.4.tar.gz

---
+++ packages/scrape-nhs-conditions/scrape_nhs_conditions-1.0.4.tar.gz/scrape_nhs_conditions-1.0.4/src/scrape_nhs_conditions/processing/process_html.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+import jsonlines
+import scrape_nhs_conditions.data_ingestion.simple_nhs_conditions_scrape as snhscs
+
+
+def process_nhs_conditions_json(docs_directory, json_file):
+    """Extracts text from the json file and writes it to a text files in the docs_directory
+    Args:
+        docs_directory (str): The directory to write the text files to
+        json_file (str): The json file to extract the text from
+    """
+    Path(docs_directory).mkdir(parents=True, exist_ok=True)
+
+    with jsonlines.open(json_file) as reader:
+        for index, obj in enumerate(reader):
+            extracted_text = snhscs.process_nhs_conditions_json(obj)
+
+            page_name = obj["source_url"].split("/")[-2]
+
+            if extracted_text != "":
+                with open(
+                    f"{docs_directory}/{page_name}.txt", "w", encoding="utf-8"
+                ) as file:
+                    file.write(extracted_text)
+
+            print(index, ": ", obj["source_url"])
