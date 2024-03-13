bddcda2d0303503330018fa67bf92198d85d4a03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 16:04:54 +0000

    Add package wzy-learn/0.1.0/wzy_learn-0.1.0.tar.gz

---
+++ packages/wzy-learn/wzy_learn-0.1.0.tar.gz/wzy_learn-0.1.0/setup.py
@@ -0,0 +1,21 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='wzy_learn',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+    ],
+    description='Just a demo',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Dr. Zhiyuan Wang',
+    author_email='wang1399@e.ntu.edu.sg',
+    url='https://github.com/Dr-WangZhiyuan/wzy_learn',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
