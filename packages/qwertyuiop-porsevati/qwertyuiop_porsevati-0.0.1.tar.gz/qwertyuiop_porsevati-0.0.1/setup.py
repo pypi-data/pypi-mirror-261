28f96cac939833565b2adae39be96f396af9557e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 04:05:20 +0000

    Add package qwertyuiop-porsevati/0.0.1/qwertyuiop_porsevati-0.0.1.tar.gz

---
+++ packages/qwertyuiop-porsevati/qwertyuiop_porsevati-0.0.1.tar.gz/qwertyuiop_porsevati-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='qwertyuiop_porsevati',  # Имя пакета
+    version='0.0.1',  # Версия пакета
+    author='PorsevaTatyana',
+    author_email='tporseva@inbox.ru',
+    description='my package',
+    packages=setuptools.find_packages(),
+)
