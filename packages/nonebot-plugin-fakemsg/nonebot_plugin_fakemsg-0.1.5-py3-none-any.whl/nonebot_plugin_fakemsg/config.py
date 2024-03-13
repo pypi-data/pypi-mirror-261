4d7f128990e4ca4dae7266539c8838e537f7f2fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 16:04:37 +0000

    Add package nonebot-plugin-fakemsg/0.1.5/nonebot_plugin_fakemsg-0.1.5-py3-none-any.whl

---
+++ packages/nonebot-plugin-fakemsg/nonebot_plugin_fakemsg-0.1.5-py3-none-any.whl/nonebot_plugin_fakemsg/config.py
@@ -0,0 +1,10 @@
+from pydantic import Extra, BaseModel
+from nonebot import get_driver
+
+
+class Config(BaseModel, extra=Extra.ignore):
+    user_split: str = "|"
+    message_split: str = " "
+
+
+config = Config.parse_obj(get_driver().config.dict())
