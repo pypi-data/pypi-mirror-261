e352fc96674a10448fe87824e825fa41749faa2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 04:05:16 +0000

    Add package misha-lib/0.0.1/misha_lib-0.0.1.tar.gz

---
+++ packages/misha-lib/misha_lib-0.0.1.tar.gz/misha_lib-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='misha_lib', # Имя пакета
+    version='0.0.1',  # Версия пакета
+    author='Misha Astanin',
+    author_email='misha100904@gmail.com',
+    description='My Package',
+    packages=setuptools.find_packages(),
+)
