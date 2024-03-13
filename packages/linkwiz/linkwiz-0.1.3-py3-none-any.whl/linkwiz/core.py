f115cf86db7425315bc69bef852e675917b6f5b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 04:04:44 +0000

    Add package linkwiz/0.1.3/linkwiz-0.1.3-py3-none-any.whl

---
+++ packages/linkwiz/linkwiz-0.1.3-py3-none-any.whl/linkwiz/core.py
@@ -0,0 +1,31 @@
+from urllib.parse import urlparse
+from linkwiz.browser import get_installed_browsers
+from linkwiz.match import find_matching_browser
+from linkwiz.gui import LinkwizGUI
+from linkwiz.launch import launch_browser
+import logging
+
+
+def process_url(url):
+    """Process the provided URL."""
+    parsed_url = urlparse(url)
+
+    if parsed_url.scheme not in ("http", "https"):
+        raise ValueError("Invalid URL.")
+
+    browsers = get_installed_browsers()
+
+    try:
+        launch_browser_command = find_matching_browser(
+            browsers, parsed_url.geturl(), parsed_url.netloc
+        )
+        if launch_browser_command:
+            launch_browser(*launch_browser_command)
+            return
+
+        app = LinkwizGUI(browsers, url)
+        launch_browser_command = app.run()
+        launch_browser(*launch_browser_command)
+
+    except Exception as e:
+        logging.error(f"Error processing URL: {e}")
