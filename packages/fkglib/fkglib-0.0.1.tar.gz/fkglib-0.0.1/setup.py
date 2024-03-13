b48d1bacd60bc4dc499dc4118e20b89f71d517a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 03:04:17 +0000

    Add package fkglib/0.0.1/fkglib-0.0.1.tar.gz

---
+++ packages/fkglib/fkglib-0.0.1.tar.gz/fkglib-0.0.1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+setuptools.setup(
+name='fkglib', # Имя пакета
+version='0.0.1', # Версия пакета
+author="Filippov Konstantin Grigor'evich",
+author_email='40381@gmail.com',
+description='Legend',
+packages=setuptools.find_packages(),
+)
