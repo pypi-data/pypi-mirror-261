275597d0703a2a49584551dab1d11dc50bff0cb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 04:04:55 +0000

    Add package Btc-price1/0.1.3/Btc_price1-0.1.3.tar.gz

---
+++ packages/Btc-price1/Btc_price1-0.1.3.tar.gz/Btc_price1-0.1.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup,find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+# Get the long description from the README file
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+	name='Btc_price1',
+	description="基于Paddle构建简单神经网络比特币价格预测",
+	version='0.1.3',
+	long_description=long_description,
+    long_description_content_type='text/markdown',
+	packages=find_packages(),
+	author="toy1v",
+	url="https://github.com/toy1v/calculator",
+	license='MIT License',
+    python_requires='>=3',
+)
+
