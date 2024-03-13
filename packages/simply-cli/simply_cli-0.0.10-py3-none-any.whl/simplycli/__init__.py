ee06ca9eb04dc7e534307e593c2b34095a664787
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 15:06:36 +0000

    Add package simply-cli/0.0.10/simply_cli-0.0.10-py3-none-any.whl

---
+++ packages/simply-cli/simply_cli-0.0.10-py3-none-any.whl/simplycli/__init__.py
@@ -0,0 +1,15 @@
+from simplycli.cli import AbstractFunctionCommandWrapper, AbstractCommandClass, Command, CLI
+from simplycli.arg import ArgumentError, CommandSignatureError, ArgParser, ArgMatcher, Arg, __ARG_PARSING_IMPLEMENTATION__
+
+__all__ = [
+    "AbstractFunctionCommandWrapper",
+    "AbstractCommandClass",
+    "Command",
+    "CLI",
+    "ArgumentError",
+    "CommandSignatureError",
+    "ArgParser",
+    "ArgMatcher",
+    "Arg",
+    "__ARG_PARSING_IMPLEMENTATION__"
+]
