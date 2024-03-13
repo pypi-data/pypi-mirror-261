10598468b38c00ca438658e5e21fd2217fd1f0a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 03:05:09 +0000

    Add package autohelper/0.0.2/autohelper-0.0.2.tar.gz

---
+++ packages/autohelper/autohelper-0.0.2.tar.gz/autohelper-0.0.2/autohelper/gui/Communicate.py
@@ -0,0 +1,15 @@
+import numpy as np
+from PySide6.QtCore import Signal, QObject
+
+
+class Communicate(QObject):
+    frame = Signal(np.ndarray)
+    log = Signal(int, str)
+    fps = Signal(int)
+    frame_time = Signal(int)
+    scene = Signal(str)
+    draw_box = Signal(str, list)
+    tasks = Signal()
+
+
+communicate = Communicate()
