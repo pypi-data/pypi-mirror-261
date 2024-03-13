c19c2e89660e76c0ed91d577ec2412d804590ca9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 05:04:19 +0000

    Add package dumbo-runlim/0.2.1/dumbo_runlim-0.2.1.tar.gz

---
+++ packages/dumbo-runlim/dumbo_runlim-0.2.1.tar.gz/dumbo_runlim-0.2.1/dumbo_runlim/experiments/ucorexplain.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+import typer
+
+from dumbo_runlim.utils import run_external_command
+
+
+def command(
+        output_file: Path = typer.Option(
+            "output.csv", "--output-file", "-o",
+            help="File to store final results",
+        ),
+) -> None:
+    """
+    Experiment for the ICLP 2024 paper.
+    """
+    # git clone...
+    # git pull...
+    run_external_command("../dumbo-runlim-ucorexplain", [
+        "poetry", "run", "python", "-m", "ucorexplain",
+        "-o", output_file,
+    ])
