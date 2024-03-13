5ee4fb8c487f9d283d19d7c0955416494388c547
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 04:04:51 +0000

    Add package a2gpipelines/0.0.4/a2gpipelines-0.0.4.tar.gz

---
+++ packages/a2gpipelines/a2gpipelines-0.0.4.tar.gz/a2gpipelines-0.0.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "a2gpipelines",
+    version = "0.0.4",
+    author = "DaniloAraneda",
+    author_email = "danilo@alert2gain.com",
+    description = "short package description",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir = {"": "src"},
+    packages = setuptools.find_packages(where="src"),
+    include_dirs=[],
+    python_requires = ">=3.10.7",
+    requires=[]
+)
