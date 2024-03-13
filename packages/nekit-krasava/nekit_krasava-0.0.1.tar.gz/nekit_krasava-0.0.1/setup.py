a6a4919f8f037c2cbcc2dfa317bac8d8cb36785b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 04:05:18 +0000

    Add package nekit-krasava/0.0.1/nekit_krasava-0.0.1.tar.gz

---
+++ packages/nekit-krasava/nekit_krasava-0.0.1.tar.gz/nekit_krasava-0.0.1/setup.py
@@ -0,0 +1,11 @@
+# setup.py
+import setuptools
+
+setuptools.setup(
+    name='nekit_krasava',  # Имя пакета
+    version='0.0.1',  # Версия пакета
+    author='Nekit Yo',
+    author_email='your@gmail.com',
+    description='So Package',
+    packages=setuptools.find_packages(),
+)
