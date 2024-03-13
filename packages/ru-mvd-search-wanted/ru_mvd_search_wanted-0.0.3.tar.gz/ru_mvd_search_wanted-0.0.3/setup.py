89bfc427349fc2c970171b54d11c15e2ff585304
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 06:06:26 +0000

    Add package ru-mvd-search-wanted/0.0.3/ru_mvd_search_wanted-0.0.3.tar.gz

---
+++ packages/ru-mvd-search-wanted/ru_mvd_search_wanted-0.0.3.tar.gz/ru_mvd_search_wanted-0.0.3/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="ru_mvd_search_wanted",
+    version="0.0.3",
+    author="Daniil Agniashvili",
+    author_email="dortos123456@gmail.com",
+    description="Parser of information from the website of the Ministry of Internal Affairs Search",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/daniilak/ru_mvd_search_wanted",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux"
+    ],
+    install_requires=['httpx','beautifulsoup4', 'pip_system_certs', 'fake-useragent'],
+    python_requires='>3.7',
+)
