d5b6aa4c2aa492ef30d59fd8ba7c48d1b51df7b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 03:04:26 +0000

    Add package ou-container-builder/3.0.0b8/ou_container_builder-3.0.0b8-py3-none-any.whl

---
+++ packages/ou-container-builder/ou_container_builder-3.0.0b8-py3-none-any.whl/ou_container_builder/cli/__init__.py
@@ -0,0 +1,3 @@
+"""The core CLI Typer app."""
+from ou_container_builder.cli import clean, build, generate, version  # noqa: I001, F401
+from ou_container_builder.cli.base import app  # noqa: I001, F401
