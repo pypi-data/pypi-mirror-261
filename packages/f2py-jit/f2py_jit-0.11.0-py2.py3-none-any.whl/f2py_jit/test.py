ac7b3f0017a107998a9179670896dbf998edde08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 03:05:01 +0000

    Add package f2py-jit/0.11.0/f2py_jit-0.11.0-py2.py3-none-any.whl

---
+++ packages/f2py-jit/f2py_jit-0.11.0-py2.py3-none-any.whl/f2py_jit/test.py
@@ -0,0 +1,16 @@
+class Test:
+
+    def __init__(self, arg=None):
+        #: Value argument
+        self.arg = arg
+
+        #: Value x
+        self.x = 1
+
+    def test(self):
+        """
+        das
+        """
+        return self.x
+        
+
