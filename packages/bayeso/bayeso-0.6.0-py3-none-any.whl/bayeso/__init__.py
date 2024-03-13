380fd2436ae334d2787201152f14c0a3b993289d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 14:05:40 +0000

    Add package bayeso/0.6.0/bayeso-0.6.0-py3-none-any.whl

---
+++ packages/bayeso/bayeso-0.6.0-py3-none-any.whl/bayeso/__init__.py
@@ -0,0 +1,9 @@
+#
+# author: Jungtaek Kim (jungtaek.kim.mail@gmail.com)
+# last updated: January 4, 2024
+#
+"""BayesO is a simple, but essential Bayesian optimization
+package, implemented in Python."""
+
+
+__version__ = '0.6.0'
