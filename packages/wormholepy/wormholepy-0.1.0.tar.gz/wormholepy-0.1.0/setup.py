956f107cb14796147256ba59ef2d96aa06e882de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 14:04:36 +0000

    Add package wormholepy/0.1.0/wormholepy-0.1.0.tar.gz

---
+++ packages/wormholepy/wormholepy-0.1.0.tar.gz/wormholepy-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='wormholepy',
+    version='0.1.0',    
+    description='A package to check Wormhole airdrop',
+    url='https://github.com/bomsauro/wormholepy',
+    author='Bomsauro',
+    author_email='bomsauro@gmail.com',
+    license='BSD 2-clause',
+    packages=['wormholepy'],
+    install_requires=['requests']
+)
---
+++ packages/wormholepy/wormholepy-0.1.0.tar.gz/wormholepy-0.1.0/wormholepy/__init__.py
@@ -0,0 +1,9 @@
+"""
+wormholepy.
+
+A package to check Wormhole airdrop.
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Bomsauro'
+__credits__ = 'Gangue do Macaco'
