0776728cff7e13dbceddd5c6073c243d1334aaa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 03:04:58 +0000

    Add package pyanasolution/0.1.1/pyanasolution-0.1.1.tar.gz

---
+++ packages/pyanasolution/pyanasolution-0.1.1.tar.gz/pyanasolution-0.1.1/pyanasolution/__init__.py
@@ -0,0 +1,7 @@
+from .channel_cos_flow import ChannelCosFlow
+from .gaussian_hill import GaussianHill
+from .poiseuille_flow import PoiseuilleFlow
+from .reinjection_1d import Reinjection1D
+from .reinjection_2d import Reinjection2D
+from .theis import Theis
+from .uniform_flow import UniformFlow
