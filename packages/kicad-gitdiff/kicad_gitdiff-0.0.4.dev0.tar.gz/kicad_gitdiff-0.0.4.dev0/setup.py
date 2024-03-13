1b72e0fff654b796f918c535dac9ed06809cffbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 05:04:44 +0000

    Add package kicad-gitdiff/0.0.4.dev0/kicad_gitdiff-0.0.4.dev0.tar.gz

---
+++ packages/kicad-gitdiff/kicad_gitdiff-0.0.4.dev0.tar.gz/kicad_gitdiff-0.0.4.dev0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+setup(
+    name='kicad_gitdiff',
+    version='0.0.4-dev',
+    url='https://github.com/maersdal/kicad_gitdiff_cli',
+    license='Apache-2.0',
+    license_files=["LICENSE"],
+    author='magnus ersdal',
+    author_email='magnus@resani.com',
+    description='A tool for a visual diff with git and kicad',
+    long_description="""
+Simple visual diff for kicad 8 or higher. one can easily extend this tool to build a diff-er for anything that outputs svg.
+
+This is not an official kicad tool or anything, just a friday night project.
+
+My goal was just to get some `git` integration with the Kicad file formats.
+
+[Documentation on github](https://github.com/maersdal/kicad_gitdiff_cli#readme)
+""",
+    long_description_content_type="text/markdown",
+    package_dir={"": "src"},
+    packages=['git_config_template', 'kicad_export', 'sch_diff', 'svg_diff', 'checkers'],
+    package_data={"git_config_template": ["*_template"]},
+    include_package_data=True,
+    entry_points={
+        'console_scripts':
+            ['kdiff_initialize = git_config_template:append_custom_git_config',
+             'kicad_sch_diff = sch_diff:git_diff',
+             ]
+        },
+    )
---
+++ packages/kicad-gitdiff/kicad_gitdiff-0.0.4.dev0.tar.gz/kicad_gitdiff-0.0.4.dev0/src/svg_diff/svg_difftool.py
@@ -0,0 +1,42 @@
+"""
+Brute-force diffing svgs
+"""
+opacity_1 = '0.3'
+opacity_2 = '0.3'
+color_1 = '#FF4C51'
+color_2 = '#8D78FF'
+
+
+def kdiff_svg(fname_1, fname_2, fout):
+    """
+    Eeschema-SVG format from kicad has 11 lines of header and its </svg> tag is on the last line
+    so no parsing is needed, just some array indexing. Might be fragile
+    """
+    with open(fname_1, 'r') as f:
+        lines1 = f.readlines()
+    with open(fname_2, 'r') as f:
+        lines2 = f.readlines()
+    result_svg_lines = []
+    svg_header_len = 11
+    result_svg_lines.extend(lines1[:svg_header_len])
+
+    result_svg_lines.extend([replace1(line) for line in lines1[svg_header_len:-1]])
+    result_svg_lines.extend([replace2(line) for line in lines2[svg_header_len:]])
+    with open(fout, 'w') as f:
+        f.writelines(result_svg_lines)
+
+
+def replace_with_color(line, color, opacity):
+    """Only works if the export is black and white"""
+    line = line.replace('#000000', color)
+    line = line.replace('stroke-opacity:1', 'stroke-opacity:' + opacity)
+    line = line.replace('fill-opacity:1.0', 'fill-opacity:' + opacity)
+    return line
+
+
+def replace1(line):
+    return replace_with_color(line, color_1, opacity_1)
+
+
+def replace2(line):
+    return replace_with_color(line, color_2, opacity_2)
