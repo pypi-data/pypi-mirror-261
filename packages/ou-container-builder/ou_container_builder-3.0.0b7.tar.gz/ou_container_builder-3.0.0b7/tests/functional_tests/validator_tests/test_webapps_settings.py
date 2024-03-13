10929a8bd80973ed422da7f903a18a80d542caf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 03:05:00 +0000

    Add package ou-container-builder/3.0.0b7/ou_container_builder-3.0.0b7.tar.gz

---
+++ packages/ou-container-builder/ou_container_builder-3.0.0b7.tar.gz/ou_container_builder-3.0.0b7/tests/functional_tests/validator_tests/test_webapps_settings.py
@@ -0,0 +1,38 @@
+"""Test the sources settings validation."""
+from pydantic import ValidationError
+from pytest import raises
+
+from ou_container_builder.settings import WebApp
+
+from .util import error_locations
+
+
+def test_valid_web_app_settings():
+    """Test that a valid sources configuration passes."""
+    WebApp(
+        path="test",
+        options={
+            "command": ["python", "-m", "http.server", "{port}"],
+            "port": 80,
+            "timeout": 120,
+            "absolute_url": True,
+            "launcher_entry": {},
+        },
+    )
+    WebApp(
+        path="test",
+        options={
+            "command": "python -m http.server {port}",
+            "port": 80,
+            "timeout": 120,
+            "absolute_url": True,
+            "launcher_entry": {},
+        },
+    )
+
+
+def test_default_web_app_settings():
+    """Test that the default web_app settings are correct."""
+    settings = WebApp(path="test", options={"command": ["python", "-m", "http.server", "{port}"]})
+    assert settings.path == "test"
+    assert settings.options["command"] == ["python", "-m", "http.server", "{port}"]
