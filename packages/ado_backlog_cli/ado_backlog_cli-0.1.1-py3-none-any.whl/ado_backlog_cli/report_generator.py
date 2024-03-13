5a3db2500fb8dd26ea7346b7e8ee4ad888b6fbc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 03:04:40 +0000

    Add package ado_backlog_cli/0.1.1/ado_backlog_cli-0.1.1-py3-none-any.whl

---
+++ packages/ado_backlog_cli/ado_backlog_cli-0.1.1-py3-none-any.whl/ado_backlog_cli/report_generator.py
@@ -0,0 +1,15 @@
+class ReportGenerator:
+    @staticmethod
+    def output_to_csv(records, filename):
+        # Output records to a CSV file
+        pass
+
+    @staticmethod
+    def print_tabulated(records):
+        # Print records in a tabulated format
+        pass
+
+    @staticmethod
+    def group_by_criteria(records, criteria):
+        # Group records based on specified criteria
+        pass
