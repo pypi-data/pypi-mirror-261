7b485276dbab951daac63eed8fec969c987690fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 04:04:33 +0000

    Add package aquacalc/0.0.2/aquacalc-0.0.2.tar.gz

---
+++ packages/aquacalc/aquacalc-0.0.2.tar.gz/aquacalc-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aquacalc',
+    version='0.0.2',
+    packages=find_packages(),
+    description='A package for water and sanitation calculations',
+    long_description='This package provides functions and utilities for performing calculations related to water and sanitation. It includes modules for water quality analysis, water treatment design, wastewater treatment, and more.',
+    author='Bhuwan Panday',
+    author_email='bhuwanpanday1@gmail.com',
+    
+)
