34197e554fcaaae1f319cb7096af1b8a2cef89d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 03:05:08 +0000

    Add package lamin_validator/0.1.0/lamin_validator-0.1.0-py2.py3-none-any.whl

---
+++ packages/lamin_validator/lamin_validator-0.1.0-py2.py3-none-any.whl/lamin_validator/__init__.py
@@ -0,0 +1,21 @@
+"""Validators built on LaminDB.
+
+Import the package::
+
+   from lamin_validator import Validator, AnnDataValidator
+
+This is the complete API reference:
+
+.. autosummary::
+   :toctree: .
+
+   Validator
+   AnnDataValidator
+   datasets
+"""
+
+__version__ = "0.1.0"
+
+from . import datasets
+from ._anndata_validator import AnnDataValidator
+from ._validator import Validator
