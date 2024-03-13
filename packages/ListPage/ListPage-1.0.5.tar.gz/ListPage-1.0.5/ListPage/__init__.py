cb7d32fd44103e7dc9776acaea61ab19f14bfd02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 04:05:27 +0000

    Add package ListPage/1.0.5/ListPage-1.0.5.tar.gz

---
+++ packages/ListPage/ListPage-1.0.5.tar.gz/ListPage-1.0.5/ListPage/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+from .list_page import ListPage
+from .scrolling_page import ScrollingPage
+from .paths import Xpaths, CssPaths, Paths
+from .targets import Targets
+from DataRecorder import Recorder
+
+__version__ = '1.0.5'
