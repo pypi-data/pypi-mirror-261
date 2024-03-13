7abf6d884f1fa1a4a8cb6620c6b10ff1c84089c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 15:06:39 +0000

    Add package django-polymorphic-tree-for-garpix-page/2.2.1/django_polymorphic_tree_for_garpix_page-2.2.1-py3-none-any.whl

---
+++ packages/django-polymorphic-tree-for-garpix-page/django_polymorphic_tree_for_garpix_page-2.2.1-py3-none-any.whl/polymorphic_tree/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.join(path.abspath(path.dirname(__file__)), 'polymorphic_tree')
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='django-polymorphic-tree-for-garpix-page',
+    version='2.2.1',
+    description='',
+    long_description=long_description,
+    url='https://github.com/garpixcms/garpix_page',
+    author='Garpix LTD',
+    author_email='info@garpix.com',
+    license='Apache 2.0',
+    packages=find_packages(exclude=['testproject', 'testproject.*']),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Framework :: Django',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 3.0',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'django-polymorphic>=1.3',  # Latest to support Django 1.8 and 1.10
+        'django-mptt>=0.8.0',
+        'django-tag-parser>=2.1',
+        'future>=0.12.2',
+    ],
+)
