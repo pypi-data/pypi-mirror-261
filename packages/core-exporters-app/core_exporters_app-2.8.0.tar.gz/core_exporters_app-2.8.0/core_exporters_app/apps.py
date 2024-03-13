f8484ccac6f70cd4042851022fb3d107fe6957a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 06:05:38 +0000

    Add package core-exporters-app/2.8.0/core_exporters_app-2.8.0.tar.gz

---
+++ packages/core-exporters-app/core_exporters_app-2.8.0.tar.gz/core_exporters_app-2.8.0/core_exporters_app/apps.py
@@ -0,0 +1,25 @@
+""" Core exporters apps config
+"""
+import sys
+
+from django.apps import AppConfig
+
+
+# TODO: loaded two times (not a problem and may not happen in production)
+# see http://stackoverflow.com/a/16111968
+class CoreExportersAppConfig(AppConfig):
+    """Exporters configuration"""
+
+    name = "core_exporters_app"
+    verbose_name = "Core Exporters App"
+
+    def ready(self):
+        """Run once at startup"""
+        if "migrate" not in sys.argv:
+            import core_exporters_app.components.exporter.watch as exporter_watch
+            from core_exporters_app.exporters import discover
+            from core_exporters_app.exporters import urls
+
+            discover.init_periodic_tasks()
+            discover.discover_exporter(urls.urlpatterns)
+            exporter_watch.init()
