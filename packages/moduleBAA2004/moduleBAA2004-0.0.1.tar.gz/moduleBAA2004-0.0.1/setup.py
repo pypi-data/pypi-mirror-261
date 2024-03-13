d01d2d6163a2f80704be3ba713d85b332a468ecc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 04:05:28 +0000

    Add package moduleBAA2004/0.0.1/moduleBAA2004-0.0.1.tar.gz

---
+++ packages/moduleBAA2004/moduleBAA2004-0.0.1.tar.gz/moduleBAA2004-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='moduleBAA2004',
+    version='0.0.1',
+    author='Bultsev Alexander',
+    author_email='bultsevalexander@yandex.ru',
+    description='useless module',
+    packages=setuptools.find_packages()
+)
