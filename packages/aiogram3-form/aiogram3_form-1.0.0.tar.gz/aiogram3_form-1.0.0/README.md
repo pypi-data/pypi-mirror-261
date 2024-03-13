c2613203308b4e9c87ded71bdef7b82fe88b08ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 14:04:28 +0000

    Add package aiogram3-form/1.0.0/aiogram3_form-1.0.0.tar.gz

---
+++ packages/aiogram3-form/aiogram3_form-1.0.0.tar.gz/aiogram3_form-1.0.0/README.md
@@ -0,0 +1,53 @@
+# aiogram3-form
+A library to create forms in aiogram3
+
+```shell
+pip install aiogram3-form
+```
+
+# Example
+```Python
+import asyncio
+
+from aiogram import Bot, Dispatcher, F, Router, types
+from aiogram3_form import Form, FormField
+from aiogram.fsm.context import FSMContext
+
+bot = Bot(token="YOUR_TOKEN")
+dispatcher = Dispatcher()
+router = Router()
+dispatcher.include_router(router)
+
+
+class NameForm(Form, router=router):
+    first_name: str = FormField(enter_message_text="Enter your first name please")
+    second_name: str = FormField(
+        enter_message_text="Enter your second name please",
+        filter=(F.text.len() > 10) & F.text,
+    )
+    age: int = FormField(
+        enter_message_text="Enter age as integer",
+        error_message_text="Age should be numeric!",
+    )
+
+
+@NameForm.submit()
+async def name_form_submit_handler(form: NameForm, event_chat: types.Chat):
+    # handle form data
+    # also supports aiogram standart DI (e. g. middlewares, filters, etc)
+    await form.answer(
+        f"{form.first_name} {form.second_name} of age {form.age} in chat {event_chat.title}"
+    )
+
+
+@router.message(F.text == "/form")
+async def form_handler(_, state: FSMContext):
+    await NameForm.start(bot, state)  # start your form
+
+
+async def main():
+    await dispatcher.start_polling(bot)
+
+
+asyncio.run(main())
+```
---
+++ packages/aiogram3-form/aiogram3_form-1.0.0.tar.gz/aiogram3_form-1.0.0/aiogram3_form/field.py
@@ -0,0 +1,53 @@
+from dataclasses import dataclass
+from typing import Any, Awaitable, Callable, Dict, Optional, Type, Union
+
+from aiogram import types
+
+from .filters import FormFilter, _FormFilter
+
+Markup = Union[
+    types.ReplyKeyboardMarkup, types.InlineKeyboardMarkup, types.ReplyKeyboardRemove
+]
+
+EnterCallback = Callable[[int, int, Dict[str, Any]], Awaitable[types.Message]]
+
+
+@dataclass(frozen=True)
+class FormFieldInfo:
+    enter_message_text: Optional[str]
+    error_message_text: Optional[str]
+    filter: Optional[FormFilter]
+    reply_markup: Optional[Markup]
+    enter_callback: Optional[EnterCallback]
+
+    def __post_init__(self):
+        if self.enter_message_text is None and self.enter_callback is None:
+            raise ValueError("enter_message_text or enter_callback should be set")
+
+
+@dataclass(frozen=True)
+class FormFieldData:
+    name: str
+    type: Type
+    info: FormFieldInfo
+    filter: _FormFilter
+
+
+def FormField(
+    *,
+    enter_message_text: Optional[str] = None,
+    filter: Optional[FormFilter] = None,
+    error_message_text: Optional[str] = None,
+    reply_markup: Optional[Markup] = None,
+    enter_callback: Optional[EnterCallback] = None
+) -> Any:
+    if enter_message_text is None and enter_callback is None:
+        raise ValueError("enter_message_text or enter_callback should be set")
+
+    return FormFieldInfo(
+        enter_message_text=enter_message_text,
+        error_message_text=error_message_text,
+        filter=filter,
+        reply_markup=reply_markup,
+        enter_callback=enter_callback,
+    )
