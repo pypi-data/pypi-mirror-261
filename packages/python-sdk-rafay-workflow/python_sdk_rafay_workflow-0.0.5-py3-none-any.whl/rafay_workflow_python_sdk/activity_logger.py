42e1ab2fb3f2eda908b7cfa5bb6c558ab7840c2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 15:06:33 +0000

    Add package python-sdk-rafay-workflow/0.0.5/python_sdk_rafay_workflow-0.0.5-py3-none-any.whl

---
+++ packages/python-sdk-rafay-workflow/python_sdk_rafay_workflow-0.0.5-py3-none-any.whl/rafay_workflow_python_sdk/activity_logger.py
@@ -0,0 +1,30 @@
+from logging import Handler, StreamHandler
+from logging.handlers import MemoryHandler
+import requests
+from .const import WorkflowTokenHeader
+
+
+class ActivityLogHandler(MemoryHandler):
+    def __init__(self, endpoint, token, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.token = token
+        self.endpoint = endpoint
+
+    def flush(self) -> None:
+        self.acquire()
+        try:
+            buf = [self.format(record) for record in self.buffer]
+            resp = requests.post(self.endpoint, headers={
+                WorkflowTokenHeader: self.token,
+            }, data='\n'.join(buf))
+
+            if resp.status_code != 200:
+                raise Exception(f"Failed to send logs to {self.endpoint}, status code: {resp.status_code}, response: {resp.text}")
+            self.buffer.clear()
+        finally:
+            self.release()    
+
+
+
+
+
