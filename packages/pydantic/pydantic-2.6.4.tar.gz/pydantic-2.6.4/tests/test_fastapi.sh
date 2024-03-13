069e66ae15a94763b4ba33f1a11d3a42ab8b1f2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 03:05:09 +0000

    Add package pydantic/2.6.4/pydantic-2.6.4.tar.gz

---
+++ packages/pydantic/pydantic-2.6.4.tar.gz/pydantic-2.6.4/tests/test_fastapi.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/env bash
+
+set -x
+set -e
+
+cd fastapi
+git fetch --tags
+
+pip install -r requirements.txt
+# Install the version of pydantic from the current branch, not the released version used by fastapi
+pip uninstall -y pydantic
+cd .. && pip install . && cd fastapi
+
+# ./scripts/test.sh accepts arbitrary arguments and passes them to the pytest call.
+# This may be necessary if we make low-consequence changes to pydantic, such as minor changes the details of a JSON
+# schema or the contents of a ValidationError
+#
+# To skip a specific test, add '--deselect path/to/test.py::test_name' to the end of this command
+#
+# To update the list of deselected tests, remove all deselections, run the tests, and re-add any remaining failures
+./scripts/test.sh -vv
