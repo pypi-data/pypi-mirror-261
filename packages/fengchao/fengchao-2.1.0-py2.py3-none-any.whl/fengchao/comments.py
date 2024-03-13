5df431b546edfd7a304dc8b5092f8f737d53934a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 16:04:25 +0000

    Add package fengchao/2.1.0/fengchao-2.1.0-py2.py3-none-any.whl

---
+++ packages/fengchao/fengchao-2.1.0-py2.py3-none-any.whl/fengchao/comments.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+"""
+# File       : comments.py
+# Time       ：2024/2/23 13:48
+# Author     ：andy
+# version    ：python 3.9
+"""
+from dataclasses import dataclass
+from .utils import Retry
+
+
+@dataclass
+class Comments:
+    BASE_URL = "http://124.126.18.4:6000"
+    LOCAL_URL = "/aigc/local_chat/"
+    ONLINE_URL = "/aigc/chat/"
+    MODELS_URL = "/aigc/models/"
+    PROMPTS_URL = "/aigc/prompts/"
+    KGS_URL = "/aigc/kgs"
+
+    TIMEOUT = 600
+    RETRY = Retry()
