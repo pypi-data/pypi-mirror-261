35f355b359c4774a55e827434a68a19183591663
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 04:04:53 +0000

    Add package semanticsdp/0.1.0b4/semanticsdp-0.1.0b4-py3-none-any.whl

---
+++ packages/semanticsdp/semanticsdp-0.1.0b4-py3-none-any.whl/semanticsdp/rtcp_feedback_info.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+from ._dataclass_fix import dataclass, field
+
+from semanticsdp import BaseSdp
+
+
+@dataclass(slots=True, eq=True)
+class RTCPFeedbackInfo(BaseSdp):
+    id: str
+    params: list[str] = field(default_factory=list)
+
+    def to_dict(self) -> dict:
+        return {
+            "id": self.id,
+            "params": self.params.copy(),
+        }
+
+    @classmethod
+    def from_dict(cls, data: dict):
+        data = data.copy()
+        data["params"] = data.pop("params", []).copy()
+        return super(RTCPFeedbackInfo, cls).from_dict(data)
+
+    def __hash__(self):
+        return hash(self.id)
---
+++ packages/semanticsdp/semanticsdp-0.1.0b4-py3-none-any.whl/semanticsdp/stream_info.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+from ._dataclass_fix import dataclass, field
+
+from semanticsdp import TrackInfo, BaseSdp
+
+
+@dataclass(slots=True, eq=True)
+class StreamInfo(BaseSdp):
+    id: str
+    tracks: dict[str, TrackInfo] = field(default_factory=dict)
+
+    def to_dict(self) -> dict:
+        return {
+            "id": self.id,
+            "tracks": {key: track.to_dict() for key, track in self.tracks.items()},
+        }
+
+    @classmethod
+    def from_dict(cls, data: dict) -> StreamInfo:
+        data = data.copy()
+        data["tracks"] = {key: TrackInfo.from_dict(track) for key, track in data.pop("tracks", {}).items()}
+        return super(StreamInfo, cls).from_dict(data)
