bfc0d205df1db392c1ee55d567e47e83279cebba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 04:04:58 +0000

    Add package lcsvec/0.0.1/lcsvec-0.0.1-cp38-cp38-win32.whl

---
+++ packages/lcsvec/lcsvec-0.0.1-cp38-cp38-win32.whl/lcsvec/__init__.py
@@ -0,0 +1,7 @@
+"""Main LCSvec module."""
+
+from .lcsvec_ext import lccs, lccs_length, lcs, lcs_length, lcs_table
+
+__version__ = "0.0.1"
+
+__all__ = ["lccs", "lccs_length", "lcs", "lcs_length", "lcs_table"]
