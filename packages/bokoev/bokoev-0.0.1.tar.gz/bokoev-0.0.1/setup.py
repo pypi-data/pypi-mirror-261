56dd225fe376093b5dcb27fe49c8118f76affc76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 04:05:28 +0000

    Add package bokoev/0.0.1/bokoev-0.0.1.tar.gz

---
+++ packages/bokoev/bokoev-0.0.1.tar.gz/bokoev-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='bokoev',  # Имя пакета
+    version='0.0.1',  # Версия пакета
+    author='Bokoev George',
+    author_email='goshik04@gmail.com',
+    description='My Package',
+    packages=setuptools.find_packages(),
+)
