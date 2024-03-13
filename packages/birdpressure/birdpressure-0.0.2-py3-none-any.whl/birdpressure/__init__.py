0abad690fc7126e47b77dff3172d5d32ea75d228
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 04:04:38 +0000

    Add package birdpressure/0.0.2/birdpressure-0.0.2-py3-none-any.whl

---
+++ packages/birdpressure/birdpressure-0.0.2-py3-none-any.whl/birdpressure/__init__.py
@@ -0,0 +1,11 @@
+"""
+Bird impact pressure (birdpressure) is a Python module intended to
+approximately calculate the peak pressure produced by a bird impact.
+
+@Jonas, improve this
+
+.. automodule:: birdpressure.AnalBarber
+    :members:
+
+"""
+pass
