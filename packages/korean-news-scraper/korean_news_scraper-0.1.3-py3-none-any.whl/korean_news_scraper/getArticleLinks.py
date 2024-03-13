4d09ee423ebf0cad356b324f0f49f7831feecfdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 16:04:45 +0000

    Add package korean-news-scraper/0.1.3/korean_news_scraper-0.1.3-py3-none-any.whl

---
+++ packages/korean-news-scraper/korean_news_scraper-0.1.3-py3-none-any.whl/korean_news_scraper/getArticleLinks.py
@@ -0,0 +1,35 @@
+"""
+# getArticleLinks.py
+
+When storing a lot of data in the Python list data type, 
+there is a disadvantage of slowing down the speed, 
+but a separate data type can be introduced later.
+"""
+from bs4 import BeautifulSoup
+import requests
+import urllib
+
+from scrollDownPage import scrollDownPage
+
+
+def getArticleLinks(lang: str, keyword: str) -> list[str]:
+
+    print("\nExtract articles links")
+
+    # select language
+    if lang == "ko-KR":
+        url = f"https://news.google.com/search?q={urllib.parse.quote(keyword)}&hl=ko&gl=KR&ceid=KR%3Ako"
+    elif lang == "en-EN":
+        url = f"https://news.google.com/search?q={urllib.parse.quote(keyword)}&hl=en-US&gl=US&ceid=US:en"
+    else:
+        raise ValueError("Unsupported Language. Try to use 'ko-KR' or 'en-EN'")
+
+    scrollDownPage(url)
+
+    # get articles's url
+    re = requests.get(url)
+    soup = BeautifulSoup(re.text, "html.parser")
+    news_tags = soup.find_all('a', class_="WwrzSb")
+    news_links = ["https://news.google.com/" + tag["href"][2:] for tag in news_tags]  # Links to articles
+
+    return news_links
