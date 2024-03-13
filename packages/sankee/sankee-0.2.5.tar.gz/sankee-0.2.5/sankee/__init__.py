6397b4a594bd3044c8949d158156db05cfb5b570
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 16:04:37 +0000

    Add package sankee/0.2.5/sankee-0.2.5.tar.gz

---
+++ packages/sankee/sankee-0.2.5.tar.gz/sankee-0.2.5/sankee/__init__.py
@@ -0,0 +1,11 @@
+from sankee import datasets
+from sankee.plotting import sankify
+from sankee.themes import Theme
+
+__version__ = "0.2.5"
+
+__all__ = [
+    "sankify",
+    "datasets",
+    "Theme",
+]
