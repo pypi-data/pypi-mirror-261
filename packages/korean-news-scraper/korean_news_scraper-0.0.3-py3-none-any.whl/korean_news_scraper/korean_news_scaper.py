b98a4a2c892c834d28c5389d4a5bc6f48a724cc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 05:04:57 +0000

    Add package korean-news-scraper/0.0.3/korean_news_scraper-0.0.3-py3-none-any.whl

---
+++ packages/korean-news-scraper/korean_news_scraper-0.0.3-py3-none-any.whl/korean_news_scraper/korean_news_scaper.py
@@ -0,0 +1,47 @@
+from bs4 import BeautifulSoup
+import pandas as pd
+import requests
+import urllib
+import os
+
+from .korean_news_scaper import scrollDownPage
+
+
+def googleNewsScraper(keywords: list = None):
+
+    keyword = "문재인"
+    data = getArticleLinks("ko-KR", keyword) 
+
+    df = pd.DataFrame(data)
+    print(df)
+
+    df.columns = ["News Links"]
+    df.to_csv(f"{os.getcwd()}/data/collected_data/{keyword}.csv")
+
+    return data
+
+
+
+
+
+def getArticleLinks(lang: str, keyword: str) -> list[str]:
+
+    print("Extract articles links")
+
+    # select language
+    if lang == "ko-KR":
+        url = f"https://news.google.com/search?q={urllib.parse.quote(keyword)}&hl=ko&gl=KR&ceid=KR%3Ako"
+    elif lang == "en-EN":
+        url = f"https://news.google.com/search?q={urllib.parse.quote(keyword)}&hl=en-US&gl=US&ceid=US:en"
+    else:
+        raise ValueError("Unsupported Language. Try to use 'ko-KR' or 'en-EN'")
+
+    scrollDownPage(url)
+
+    # get articles's url
+    re = requests.get(url)
+    soup = BeautifulSoup(re.text, "html.parser")
+    news_tags = soup.find_all('a', class_="WwrzSb")
+    news_links = ["https://news.google.com/" + tag["href"][2:] for tag in news_tags]  # Links to articles
+
+    return news_links
