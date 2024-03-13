87474967fb207d3214e43a332d47a88d23bba10c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 15:06:07 +0000

    Add package crawler-analytics/0.1.5/crawler_analytics-0.1.5-py3-none-any.whl

---
+++ packages/crawler-analytics/crawler_analytics-0.1.5-py3-none-any.whl/crawler_analytics/server.py
@@ -0,0 +1,35 @@
+import json
+import argparse
+from aiohttp import web
+from crawler_analytics.main import Analytics
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "project_name",
+    metavar="Project Name",
+    type=str,
+    nargs="+",
+    help="Project Name",
+)
+
+args = parser.parse_args()
+analytics = Analytics(project_name=args.project_name)
+
+
+async def handle(request):
+    return web.Response(
+        text=json.dumps(await analytics.data(), indent=4),
+        content_type="application/json",
+    )
+
+
+def server():
+    app = web.Application()
+    app.router.add_get("/", handle)
+
+    web.run_app(app, host="localhost", port=8081)
+
+
+if __name__ == "__main__":
+    server()
