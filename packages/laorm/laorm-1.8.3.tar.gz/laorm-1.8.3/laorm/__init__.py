e5407801abd17ccdf84c4faa0e96a99201718bc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 16:04:27 +0000

    Add package laorm/1.8.3/laorm-1.8.3.tar.gz

---
+++ packages/laorm/laorm-1.8.3.tar.gz/laorm-1.8.3/laorm/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "1.8.3"
+
+from .PPA import PPA as PPA
+from .stream import LaModel, FieldDescriptor, table, sql
+
+__all__ = [
+    "PPA",
+    "LaModel",
+    "FieldDescriptor",
+    "table",
+    "sql",
+]
