644b2ac0c8d66fea721985163ddb8a06d882d1c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 03:05:16 +0000

    Add package leonranp/0.0.9/leonranp-0.0.9.tar.gz

---
+++ packages/leonranp/leonranp-0.0.9.tar.gz/leonranp-0.0.9/leonranp/test.py
@@ -0,0 +1,19 @@
+from __init__ import *
+randstr()
+print('---')
+print(randcode(120))
+print('---')
+a = randcode(6)
+print(a)
+b = input("aa")
+
+if a == b:
+    print("Yes")
+else:
+    print("No")
+print('---')
+a = randbool()
+print(a)
+upgrade()
+print('---')
+print(randlistcode(10))
