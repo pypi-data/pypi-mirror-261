a82108d1d43d8b49dc3e15f44c762c021936efc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 14:04:35 +0000

    Add package geoglows/0.27.1/geoglows-0.27.1.tar.gz

---
+++ packages/geoglows/geoglows-0.27.1.tar.gz/geoglows-0.27.1/geoglows/__init__.py
@@ -0,0 +1,10 @@
+import geoglows.bias
+import geoglows.plots
+import geoglows.streamflow
+import geoglows.analysis
+import geoglows.examples
+
+__all__ = ['bias', 'plots', 'streamflow', 'analysis']
+__version__ = '0.27.1'
+__author__ = 'Riley Hales'
+__license__ = 'BSD 3-Clause Clear License'
