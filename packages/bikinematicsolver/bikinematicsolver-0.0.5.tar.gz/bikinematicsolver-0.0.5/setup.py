7599c3ce44f34eec0b7780d30bca50bc504294d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 14:05:48 +0000

    Add package bikinematicsolver/0.0.5/bikinematicsolver-0.0.5.tar.gz

---
+++ packages/bikinematicsolver/bikinematicsolver-0.0.5.tar.gz/bikinematicsolver-0.0.5/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.5'
+DESCRIPTION = 'Bike suspension kinematic solver'
+LONG_DESCRIPTION = ''
+
+setup(
+    
+    name = "bikinematicsolver",
+    version = VERSION,
+    author = "mb",
+    author_email = "markbak7@gmail.com",
+    description = DESCRIPTION,
+    long_description = LONG_DESCRIPTION,
+    packages = find_packages(),
+    package_data = {'bikinematicsolver': ['./test_data.json']},
+    install_requires=['dijkstar','numpy','scipy'],
+
+    )
