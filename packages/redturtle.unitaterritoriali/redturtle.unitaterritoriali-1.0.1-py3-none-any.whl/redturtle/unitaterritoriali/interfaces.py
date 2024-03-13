7668e46095c42f4ccded85527af23d0ad57bacb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 03:05:00 +0000

    Add package redturtle.unitaterritoriali/1.0.1/redturtle.unitaterritoriali-1.0.1-py3-none-any.whl

---
+++ packages/redturtle.unitaterritoriali/redturtle.unitaterritoriali-1.0.1-py3-none-any.whl/redturtle/unitaterritoriali/interfaces.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""Module where all interfaces, events and exceptions live."""
+
+from zope.interface import Interface
+
+
+class IUnitaTerritorialiUtility(Interface):
+    """ """
