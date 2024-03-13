efba13161b8956fbe5492a2bfb73c86be5ded63c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 14:04:22 +0000

    Add package virtool-workflow/6.0.0/virtool_workflow-6.0.0-py3-none-any.whl

---
+++ packages/virtool-workflow/virtool_workflow-6.0.0-py3-none-any.whl/virtool_workflow/runtime/redis.py
@@ -0,0 +1,59 @@
+import asyncio
+from asyncio import CancelledError
+from typing import Callable
+
+from aioredis import Redis
+from structlog import get_logger
+
+logger = get_logger("redis")
+
+CANCELLATION_CHANNEL = "channel:cancel"
+
+
+async def get_next_job_with_timeout(
+    list_name: str, redis: Redis, timeout: int = None
+) -> str:
+    """
+    Get the next job ID from a Redis list and raise a  :class:``Timeout`` error if one
+    is not found in ``timeout`` seconds.
+
+    :param list_name: the name of the list to pop from
+    :param redis: the Redis client
+    :param timeout: seconds to wait before raising :class:``Timeout``
+    :return: the next job ID
+
+    """
+    logger.info(
+        "Waiting for a job", timeout=f"{timeout if timeout else 'infinity'} seconds"
+    )
+
+    return await asyncio.wait_for(get_next_job(list_name, redis), timeout)
+
+
+async def get_next_job(list_name: str, redis: Redis) -> str:
+    """
+    Get the next job ID from a Redis list.
+
+    :param list_name: the name of the list to pop from
+    :param redis: the Redis client
+    :return: the next job ID
+
+    """
+    result = await redis.blpop(list_name)
+
+    if result is not None:
+        job_id = str(result[1], encoding="utf-8")
+        logger.info("pulled job id from redis", id=job_id)
+        return job_id
+
+
+async def wait_for_cancellation(redis: Redis, job_id: str, func: Callable):
+    (channel,) = await redis.subscribe(CANCELLATION_CHANNEL)
+
+    try:
+        async for cancelled_job_id in channel.iter():
+            if cancelled_job_id.decode() == job_id:
+                return func()
+
+    except CancelledError:
+        ...
