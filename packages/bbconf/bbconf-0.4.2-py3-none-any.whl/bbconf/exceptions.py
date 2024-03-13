1a6c2fa44fdd35a16351e1f9a620083442e66078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 15:06:42 +0000

    Add package bbconf/0.4.2/bbconf-0.4.2-py3-none-any.whl

---
+++ packages/bbconf/bbconf-0.4.2-py3-none-any.whl/bbconf/exceptions.py
@@ -0,0 +1,41 @@
+import abc
+from .const import DOC_URL
+
+
+class BedBaseConfError(Exception):
+    """Base exception type for this package"""
+
+    __metaclass__ = abc.ABCMeta
+
+
+class BadAccessMethodError(BedBaseConfError):
+    """Access ID is not well defined"""
+
+    pass
+
+
+class MissingConfigDataError(BedBaseConfError):
+    """Exception for invalid config file."""
+
+    def __init__(self, msg):
+        spacing = " " if msg[-1] in ["?", ".", "\n"] else "; "
+        suggest = "For config format documentation please see: " + DOC_URL
+        super(MissingConfigDataError, self).__init__(msg + spacing + suggest)
+
+
+class BedBaseConnectionError(BedBaseConfError):
+    """Error type for DB connection problems"""
+
+    pass
+
+
+class MissingThumbnailError(BedBaseConfError):
+    """Error type for missing thumbnail"""
+
+    pass
+
+
+class MissingObjectError(BedBaseConfError):
+    """Error type for missing object"""
+
+    pass
