eef836432b03bd8378be79d7c8b942c4d0509148
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 16:04:32 +0000

    Add package dwave-sona-core/1.2.10/dwave_sona_core-1.2.10.tar.gz

---
+++ packages/dwave-sona-core/dwave_sona_core-1.2.10.tar.gz/dwave_sona_core-1.2.10/sona/core/storages/sources/youtube.py
@@ -0,0 +1,32 @@
+from .base import SourceBase
+
+try:
+    import yt_dlp
+except ImportError:
+    pass
+
+
+class YoutubeSource(SourceBase):
+    ydl_opts = {
+        "quiet": True,
+        "outtmpl": f"{str(SourceBase.tmp_dir)}/%(title)s",
+        "format": "bestaudio/best",
+        "postprocessors": [
+            {
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "wav",
+                "preferredquality": "lossless",
+            }
+        ],
+    }
+
+    @classmethod
+    def download(cls, file):
+        with yt_dlp.YoutubeDL(cls.ydl_opts) as ydl:
+            info = ydl.extract_info(file.path, download=True)
+            filepath = ydl.prepare_filename(info)
+            return file.mutate(path=filepath + ".wav")
+
+    @classmethod
+    def verify(cls, file):
+        return file.path and file.path.startswith("https://www.youtube.com/")
