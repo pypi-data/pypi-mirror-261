b2b084f40c8260e2b5d6c8adf5395bbaa9f8887b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 15:06:11 +0000

    Add package vincam/1.2.3/vincam-1.2.3.tar.gz

---
+++ packages/vincam/vincam-1.2.3.tar.gz/vincam-1.2.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="vincam",
+    version="1.2.3",
+    packages=find_packages(),
+    install_requires=["numpy", "comtypes"],
+    python_requires=">=3.11",
+    include_dirs=["vincam"],
+)
