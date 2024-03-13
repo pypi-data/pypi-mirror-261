1ae415f81a6cf5dc6ce31c44bcbc9d84cb17756a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 04:04:56 +0000

    Add package primod/2024.2.1/primod-2024.2.1-py3-none-any.whl

---
+++ packages/primod/primod-2024.2.1-py3-none-any.whl/primod/__init__.py
@@ -0,0 +1,7 @@
+from primod.metamod import MetaMod
+from primod.ribametamod import RibaMetaMod
+from primod.ribamod import RibaMod
+
+__all__ = ["MetaMod", "RibaMod", "RibaMetaMod"]
+
+__version__ = "2024.2.1"
