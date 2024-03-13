b7c47f6b0294969f8005defc39566b7d6b8a49ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 04:04:56 +0000

    Add package wiretap/8.0.0/wiretap-8.0.0-py3-none-any.whl

---
+++ packages/wiretap/wiretap-8.0.0-py3-none-any.whl/wiretap/filters/strip_exc_info.py
@@ -0,0 +1,17 @@
+import logging
+
+
+class StripExcInfo(logging.Filter):
+    def __init__(self):
+        super().__init__("exc_info")
+
+    def filter(self, record: logging.LogRecord) -> bool:
+        if record.exc_info:
+            exc_cls, exc, exc_tb = record.exc_info
+
+            # drop the decorator frame
+            if "telemetry.py" in exc_tb.tb_frame.__str__():
+                exc_tb = exc_tb.tb_next
+
+            record.exc_info = exc_cls, exc, exc_tb  # type: ignore
+        return True
