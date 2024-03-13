167c593442413205bf7379e26a7738d83a5cd0c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 03:04:26 +0000

    Add package castep-linter/0.2.0/castep_linter-0.2.0.tar.gz

---
+++ packages/castep-linter/castep_linter-0.2.0.tar.gz/castep_linter-0.2.0/tests/test_integer_literal_has_correct_kind.py
@@ -0,0 +1,23 @@
+# pylint: disable=W0621,C0116,C0114
+import pytest
+
+from castep_linter.scan_files import run_tests_on_code
+from castep_linter.tests import CheckFunctionDict, check_number_literal
+from tests.conftest import Parser
+
+
+@pytest.fixture
+def test_list() -> CheckFunctionDict:
+    return {"number_literal": [check_number_literal]}
+
+
+def test_integer_literal_no_dp(parse: Parser, test_list: CheckFunctionDict):
+    code = b"z = 1"
+    error_log = run_tests_on_code(parse(code), test_list, "filename")
+    assert len(error_log.errors) == 0
+
+
+def test_integer_literal_has_dp(parse: Parser, test_list: CheckFunctionDict):
+    code = b"z = 1_dp"
+    error_log = run_tests_on_code(parse(code), test_list, "filename")
+    assert len(error_log.errors) == 1
