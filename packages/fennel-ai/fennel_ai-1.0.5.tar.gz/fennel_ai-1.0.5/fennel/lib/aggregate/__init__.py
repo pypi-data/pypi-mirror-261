f0ed2136754b7929bf56e48bbcdcfbcdf9cd38b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 06:05:39 +0000

    Add package fennel-ai/1.0.5/fennel_ai-1.0.5.tar.gz

---
+++ packages/fennel-ai/fennel_ai-1.0.5.tar.gz/fennel_ai-1.0.5/fennel/lib/aggregate/__init__.py
@@ -0,0 +1,12 @@
+# Soon to be deprecated, keep just for backward compatibility
+from fennel.datasets.aggregate import (
+    Count,
+    Distinct,
+    Sum,
+    LastK,
+    Min,
+    Max,
+    Average,
+    AggregateType,
+    Stddev,
+)
