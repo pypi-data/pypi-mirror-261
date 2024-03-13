59900548cb78463a207469cc5093489c57bc8c8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 05:04:45 +0000

    Add package google-suite/0.1.2/google_suite-0.1.2.tar.gz

---
+++ packages/google-suite/google_suite-0.1.2.tar.gz/google_suite-0.1.2/google_suite/models/__init__.py
@@ -0,0 +1,15 @@
+from .gmail_models import (
+    Filter,
+    Action,
+    Criteria,
+    SizeComparison,
+    Label,
+    LabelListVisibility,
+    MessageListVisibility,
+    UserMessageList,
+    Message,
+    MessagePart,
+    MessagePartBody,
+    Header,
+)
+from .gsuite_models import Service
