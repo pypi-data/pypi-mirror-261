4baef8281445d3b145b0cbcd1fde779a201a2589
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 03:05:11 +0000

    Add package ValeraLib/1.1.4/valeralib-1.1.4.tar.gz

---
+++ packages/ValeraLib/valeralib-1.1.4.tar.gz/valeralib-1.1.4/ValeraLib/__init__.py
@@ -0,0 +1,18 @@
+# the default general-purpose package (try not to overload it)
+from .Valera import *
+from .Binance import Binance as Binance
+from .utils.DuckTypes import *
+from .utils.rust_types import (
+	Ok,
+	Err,
+	Result,
+	Option,
+	Some,
+	N,
+)
+
+chk = TimePerfCounters()
+dtf = decide_on_datetime_format
+rtd = round_time_down
+tw = time_wrapper
+sw = silent_wrapper
