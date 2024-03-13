c45475285cf4083a7297b013d780d1a618d793f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 05:04:10 +0000

    Add package ralphisencryptiongod/0.1.2/ralphisencryptiongod-0.1.2.tar.gz

---
+++ packages/ralphisencryptiongod/ralphisencryptiongod-0.1.2.tar.gz/ralphisencryptiongod-0.1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+name='ralphisencryptiongod',
+version='0.1.2',
+author='Ralph de Catelan',
+author_email='FUCKYOUMARCO@MARCOK.COM',
+description='Encrypts data and decrypts data using XOR operation',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.8.10',
+)
