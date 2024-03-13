8fcb6b665d7b5981b6b318d1eee7cd801b887596
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 14:04:36 +0000

    Add package aiogram3-form/1.0.2/aiogram3_form-1.0.2-py3-none-any.whl

---
+++ packages/aiogram3-form/aiogram3_form-1.0.2-py3-none-any.whl/aiogram3_form/__init__.py
@@ -0,0 +1,14 @@
+from .field import FormField, FormFieldData, FormFieldInfo
+from .filters import FormFilter
+from .form import Form, FormMeta
+from .state import FormState
+
+__all__ = (
+    "FormField",
+    "FormFieldInfo",
+    "FormFieldData",
+    "FormFilter",
+    "Form",
+    "FormMeta",
+    "FormState",
+)
