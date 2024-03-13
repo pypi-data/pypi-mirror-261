9125f157789608758cf56136775fe874b05ff090
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 05:04:08 +0000

    Add package zhmiscellany/1.8.5/zhmiscellany-1.8.5-py3-none-any.whl

---
+++ packages/zhmiscellany/zhmiscellany-1.8.5-py3-none-any.whl/zhmiscellany/misc.py
@@ -0,0 +1,74 @@
+import os, signal, time, importlib
+from ._misc_supportfuncs import set_activity_timeout, activity
+import zhmiscellany.math
+import zhmiscellany.string
+
+
+def die():
+    os.kill(os.getpid(), signal.SIGTERM)
+
+
+def show_progress(things, total_things, extra_data='', smart_ratelimit=False, max_prints=1000):
+    do_print = True
+
+    if smart_ratelimit:
+        if total_things > max_prints:
+            if not smart_every_nth(things, round(total_things/max_prints), total_things):
+                do_print = False
+
+    if do_print:
+        print(f'\r{zhmiscellany.math.smart_percentage(things, total_things)}% {extra_data}', end='')
+    if things == total_things:
+        print('')
+
+
+def every_nth(number, n):
+    if number % n == 0:
+        return True
+    return False
+
+
+def smart_every_nth(number, n, total):
+    if number % n == 0:
+        return True
+    if number == total:
+        return True
+    return False
+
+
+def calculate_eta(timestamps, total_timestamps):
+    if not timestamps:
+        return "Not enough data to calculate ETA."
+
+    if total_timestamps <= len(timestamps):
+        return "All timestamps recorded."
+
+    # Calculate average time per timestamp
+    total_time = timestamps[-1] - timestamps[0]
+    average_time_per_timestamp = total_time / len(timestamps)
+
+    # Calculate remaining time based on the average time per timestamp
+    remaining_timestamps = total_timestamps - len(timestamps)
+    estimated_remaining_time = remaining_timestamps * average_time_per_timestamp
+
+    # Calculate the estimated arrival time
+    current_time = time.time()
+    estimated_arrival_time = current_time + estimated_remaining_time
+
+    return zhmiscellany.string.timestamp_to_time(estimated_arrival_time)
+
+
+def decide(options, text):
+    output = input(f'{text} ({"/".join(options)})')
+    while not output in options:
+        output = input(f'{text} ({"/".join(options)})')
+    return output
+
+
+def import_module_from_path(path, module_name=None):
+    if not module_name:
+        module_name = zhmiscellany.string.get_universally_unique_string()
+    spec = importlib.util.spec_from_file_location(module_name, path)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module
