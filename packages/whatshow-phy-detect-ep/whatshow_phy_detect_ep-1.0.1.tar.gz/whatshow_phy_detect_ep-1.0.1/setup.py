f571f1e7013dc507bfd7d83fab7b25ab2089ee0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 16:04:34 +0000

    Add package whatshow-phy-detect-ep/1.0.1/whatshow_phy_detect_ep-1.0.1.tar.gz

---
+++ packages/whatshow-phy-detect-ep/whatshow_phy_detect_ep-1.0.1.tar.gz/whatshow_phy_detect_ep-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+      name="whatshow_phy_detect_ep",
+      version="1.0.1",
+      packages=find_packages(),
+      install_requires=[
+          'numpy>=1.23.5'
+      ],
+);
