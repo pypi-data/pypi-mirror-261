ff024efd42182ba3a22d3066b802777bb136ca90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 04:04:58 +0000

    Add package lcsvec/0.0.1/lcsvec-0.0.1-cp38-cp38-win_amd64.whl

---
+++ packages/lcsvec/lcsvec-0.0.1-cp38-cp38-win_amd64.whl/lcsvec/lcs_bind.pyi
@@ -0,0 +1,11 @@
+"""Python interface."""
+
+from __future__ import annotations
+
+"""def lcs(seq1: ndarray, seq2: ndarray) -> list[int]: ...
+
+
+def lcs_length(seq1: ndarray, seq2: ndarray) -> int: ...
+
+
+def lcs_table(seq1: ndarray, seq2: ndarray) -> list[list[int]]: ..."""
