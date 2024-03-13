163708366c01d7aa54d64889b6176f629611da86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 16:05:02 +0000

    Add package geney/1.0.1/geney-1.0.1-py2.py3-none-any.whl

---
+++ packages/geney/geney-1.0.1-py2.py3-none-any.whl/geney/__init__.py
@@ -0,0 +1,1 @@
+from .config_setup import config_setup
---
+++ packages/geney/geney-1.0.1-py2.py3-none-any.whl/geney/config_setup.py
@@ -0,0 +1,10 @@
+import os
+import json
+from pathlib import Path
+
+config_file = os.path.join(os.path.expanduser('~'), '.oncosplice_setup', 'config.json')
+if Path(config_file).exists():
+    config_setup = {k: Path(p) for k, p in json.loads(open(config_file).read()).items()}
+else:
+    print("Database not set up.")
+    config_setup = {}
