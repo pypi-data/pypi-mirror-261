1ac79a1438645e94b23a2973d05fe7a78cd39a10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-13 15:06:58 +0000

    Add package vbl-aquarium/0.0.2/vbl_aquarium-0.0.2-py3-none-any.whl

---
+++ packages/vbl-aquarium/vbl_aquarium-0.0.2-py3-none-any.whl/vbl_aquarium/__init__.py
@@ -0,0 +1,4 @@
+import models_logging as logging
+import models_generic as generic
+import models_unity as unity
+import models_urchin as urchin
---
+++ packages/vbl-aquarium/vbl_aquarium-0.0.2-py3-none-any.whl/vbl_aquarium/csharp/generic/IDListIntData.cs
@@ -0,0 +1,8 @@
+
+using UnityEngine;
+    
+public struct IDListIntData
+{
+    public string[] id;
+    public int value;
+}
